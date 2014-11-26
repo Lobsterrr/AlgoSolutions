4e85a553ef83b14b42634e183e17e816dbb3daff
mitcc <mitcc@qq.com>
2014-11-26 13:36:18 +0800

    :octocat:

--- leetcode/SearchA2DMatrix.java
+++ leetcode/SearchA2DMatrix.java
@@ -1,19 +1,20 @@
 /*
- * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ * Write an efficient algorithm that searches for a value in an m x n matrix. 
+ * This matrix has the following properties:
 
  * Integers in each row are sorted from left to right.
- * The first integer of each row is greater than the last integer of the previous row.
+ * The first integer of each row is greater than the last integer of the 
+ * previous row.
+ *
  * For example,
 
  * Consider the following matrix:
-
  * [
  *   [1,   3,  5,  7],
  *   [10, 11, 16, 20],
  *   [23, 30, 34, 50]
  * ]
  * Given target = 3, return true.
- * 
  */
 public class SearchA2DMatrix {
     
@@ -34,15 +35,16 @@ public class SearchA2DMatrix {
 /******************** updated 2013/12/06 *********************/
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
-        int i = 0, j = m * n;
-        while(i < j) {
+        int yLen = matrix[0].length;
+        int i = 0; 
+        int j = matrix.length * yLen;
+        while (i < j) {
             int mid = i + (j - i) / 2;
-            if(target == matrix[mid / n][mid % n])
+            if (matrix[mid / yLen][mid % yLen] == target)
                 return true;
-            else if(target > matrix[mid / n][mid % n])
+            else if (matrix[mid / yLen][mid % yLen] < target)
                 i = mid + 1;
-            else
+            else 
                 j = mid;
         }
         return false;
