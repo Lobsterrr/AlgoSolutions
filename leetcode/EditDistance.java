196b6a9476d6130bfbe780240ce4770d02be1c21
mitcc <mitcc@qq.com>
2014-11-26 14:17:12 +0800

    :octocat:

--- leetcode/EditDistance.java
+++ leetcode/EditDistance.java
@@ -10,25 +10,23 @@
  * c) Replace a character
  */
 public class EditDistance {
+
     public int minDistance(String word1, String word2) {
-        int word1Len = word1.length();
-        int word2Len = word2.length();
-        int[][] dp = new int[word1Len + 1][word2Len + 1];
-        for (int i = 1; i <= word1Len; i++) {
-            dp[i][0] = i;
-        }
-        for (int i = 1; i <= word2Len; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 1; i <= word1Len; i++) {
-            for (int j = 1; j <= word2Len; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = (i == 0 ? j : i);
+                else if (word1.charAt(i - 1) == word2.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1];
-                else 
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, 
-                            Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], 
+                            Math.min(dp[i][j - 1], dp[i - 1][j]));
             }
         }
-        return dp[word1Len][word2Len]; 
+        return dp[len1][len2]; 
     }
+
 }
