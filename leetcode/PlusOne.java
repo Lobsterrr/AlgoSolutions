d8f24fb5542d13fcf69590bd1e6e17187d1105f6
mitcc <mitcc@qq.com>
2014-12-03 15:52:50 +0800

    :octocat:

--- leetcode/PlusOne.java
+++ leetcode/PlusOne.java
@@ -1,5 +1,7 @@
 /*
  * Given a number represented as an array of digits, plus one to the number.
+ * The digits are stored such that the most significant digit is at the head 
+ * of the list.
  */
 public class PlusOne {
 
