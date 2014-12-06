4bffbd2a6b755bfead1120e9637c84f8b98ea1d8
mitcc <mitcc@qq.com>
2014-12-07 00:25:35 +0800

    :octocat:

--- leetcode/FindPeakElement.java
+++ leetcode/FindPeakElement.java
@@ -38,9 +38,7 @@ public class FindPeakElement {
             if (start + 1 == end)
                 return num[start] > num[end] ? start : end;
             int mid = start + (end - start) / 2;
-            if (num[mid] > num[mid - 1] && num[mid] > num[mid + 1])
-                return mid;
-            else if (num[mid] < num[mid - 1])
+            if (num[mid] < num[mid - 1])
                 end = mid;
             else
                 start = mid;
