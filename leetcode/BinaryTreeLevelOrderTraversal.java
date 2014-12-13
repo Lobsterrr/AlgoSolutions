1e786fb2c68013022ca8377ca784ed21703342ff
mitcc <mitcc@qq.com>
2014-12-13 20:09:10 +0800

    :octocat:

--- leetcode/BinaryTreeLevelOrderTraversal.java
+++ leetcode/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,6 @@
 /*
- * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * Given a binary tree, return the level order traversal of its 
+ * nodes' values. (ie, from left to right, level by level).
 
  * For example:
  * Given binary tree {3,9,20,#,#,15,7},
@@ -48,29 +49,30 @@ public class BinaryTreeLevelOrderTraversal {
         return result;
     }
 
-/****************************** updated 2013/12/02 ********************************/
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if(root != null) {
-            Queue<TreeNode> curLevel = new LinkedList<TreeNode>();
-            Queue<TreeNode> nextLevel = new LinkedList<TreeNode>();
-            ArrayList<Integer> saveNodeVal = new ArrayList<Integer>();
-            curLevel.offer(root);
-            while(!curLevel.isEmpty()) {
-                TreeNode cur = curLevel.poll();
-                if(cur.left != null)
-                    nextLevel.offer(cur.left);
-                if(cur.right != null)
-                    nextLevel.offer(cur.right);
-                saveNodeVal.add(cur.val);
-                if(curLevel.isEmpty()) {
-                    res.add(saveNodeVal);
-                    saveNodeVal = new ArrayList<Integer>();
-                    curLevel = nextLevel;
-                    nextLevel = new LinkedList<TreeNode>();
-                }
+/*****************************************************************************/
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Queue<TreeNode> cur = new LinkedList<TreeNode>();
+        if (root != null)
+            cur.offer(root);
+        List<Integer> list = new ArrayList<Integer>();
+        Queue<TreeNode> next = new LinkedList<TreeNode>();
+        while (!cur.isEmpty()) {
+            TreeNode node = cur.poll();
+            list.add(node.val);
+            if (node.left != null)
+                next.offer(node.left);
+            if (node.right != null)
+                next.offer(node.right);
+            if (cur.isEmpty()) {
+                result.add(list);
+                list = new ArrayList<Integer>();
+                cur = next;
+                next = new LinkedList<TreeNode>();
             }
         }
-        return res;
+        return result;
     }
+
 }
