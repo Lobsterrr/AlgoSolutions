2b8c44101f9313343b2d8b566da667c1f45512d9
mitcc <mitcc@qq.com>
2014-12-17 15:32:51 +0800

    :octocat:

--- leetcode/MaximumGap.java
+++ leetcode/MaximumGap.java
@@ -13,29 +13,27 @@ public class MaximumGap {
 
     // O(n) 
     public int maximumGap(int[] num) {
-        int len = num.length;
-        if (len < 2)
+        if (num.length < 2)
             return 0;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < num.length; i++) {
             max = Math.max(max, num[i]);
             min = Math.min(min, num[i]);
         }
-        double w = (double) (max - min) / (len + 1);
-        int[][] bucket = new int[2][len + 1];
+        double w = (double) (max - min) / (num.length + 1);
+        int[][] bucket = new int[2][num.length + 1];
         Arrays.fill(bucket[0], min);
         Arrays.fill(bucket[1], max);
-        bucket[0][len] = max;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < num.length; i++) {
             int k = (int) Math.floor((num[i] - min) / w);
-            if (k == len + 1)
-                continue;
+            if (k == num.length + 1)
+                k--;
             bucket[0][k] = Math.max(bucket[0][k], num[i]);
             bucket[1][k] = Math.min(bucket[1][k], num[i]);
         }
         int result = Integer.MIN_VALUE;
-        for (int i = 1, j = 0; i < len + 1; i++) {
+        for (int i = 1, j = 0; i < num.length + 1; i++) {
             if (bucket[0][i] != min || bucket[1][i] != max) {
                 result = Math.max(result, bucket[1][i] - bucket[0][j]);
                 j = i;
