e881b6060ada887fb43fb0d70600a4c2c9143efa
mitcc <mitcc@qq.com>
2014-12-17 14:45:51 +0800

    :octocat:

--- leetcode/MaximumGap.java
+++ leetcode/MaximumGap.java
@@ -14,7 +14,7 @@ public class MaximumGap {
     // to be continued!
     public int maximumGap(int[] num) {
         int len = num.length;
-        if (len < 2) 
+        if (len < 2)
             return 0;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
@@ -22,12 +22,15 @@ public class MaximumGap {
             max = Math.max(max, num[i]);
             min = Math.min(min, num[i]);
         }
-        double w = (max - min) / (len + 1);
+        double w = (double) (max - min) / (len + 1);
         int[][] bucket = new int[2][len + 1];
         Arrays.fill(bucket[0], min);
         Arrays.fill(bucket[1], max);
+        bucket[0][len] = max;
         for (int i = 0; i < len; i++) {
             int k = (int) Math.floor((num[i] - min) / w);
+            if (k == len + 1)
+                continue;
             bucket[0][k] = Math.max(bucket[0][k], num[i]);
             bucket[1][k] = Math.min(bucket[1][k], num[i]);
         }
