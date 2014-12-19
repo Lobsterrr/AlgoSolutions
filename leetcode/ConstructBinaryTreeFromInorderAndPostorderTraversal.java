5691ed8dab9059a9712b115f476b6abed53d14a3
mitcc <mitcc@qq.com>
2014-12-19 16:32:41 +0800

    :octocat:

--- leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -37,4 +37,24 @@ public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
         return root;
     }
 
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return buildTree(inorder, 0, inorder.length - 1, 
+                postorder, 0, postorder.length - 1);
+    }
+
+    public TreeNode buildTree(int[] inorder, int iStart, int iEnd, 
+            int[] postorder, int pStart, int pEnd) {
+        if (iEnd - iStart != pEnd - pStart || iStart > iEnd || pStart > pEnd) 
+            return null;
+        TreeNode root = new TreeNode(postorder[pEnd]);
+        int i = iStart;
+        while (i <= iEnd && inorder[i] != postorder[pEnd]) {
+            i++;
+        }
+        root.left = buildTree(inorder, iStart, i - 1, postorder, pStart, pStart - iStart + i - 1);
+        root.right = buildTree(inorder, i + 1, iEnd, postorder, pStart - iStart + i, pEnd - 1);
+        return root;
+    }
+
 }
