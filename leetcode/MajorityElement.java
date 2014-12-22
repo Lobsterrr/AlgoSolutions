26f129dd56556c0d80ea8f1237c44bcbdce06419
mitcc <mitcc@qq.com>
2014-12-22 16:07:30 +0800

    :octocat:

--- leetcode/MajorityElement.java
+++ leetcode/MajorityElement.java
@@ -5,7 +5,6 @@
  * You may assume that the array is non-empty and the majority 
  * element always exist in the array. 
  */
-
 public class MajorityElement {
 
     public int majorityElement(int[] num) {
