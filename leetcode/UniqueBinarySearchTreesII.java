b104b35f794ec55bfd5fd43646965e1566bdfa23
mitcc <mitcc@qq.com>
2014-12-29 19:52:50 +0800

    :octocat:

--- leetcode/UniqueBinarySearchTreesII.java
+++ leetcode/UniqueBinarySearchTreesII.java
@@ -1,6 +1,6 @@
 /*
- * Given n, generate all structurally unique BST's (binary search trees) that 
- * store values 1...n.
+ * Given n, generate all structurally unique BST's (binary search trees) 
+ * that store values 1...n.
 
  * For example,
  * Given n = 3, your program should return all 5 unique BST's shown below.
@@ -21,26 +21,28 @@
  * }
  */
 public class UniqueBinarySearchTreesII {
-    public ArrayList<TreeNode> generateTrees(int n) {
+
+    public List<TreeNode> generateTrees(int n) {
         return generate(1, n);
     }
-        
-    public ArrayList<TreeNode> generate(int start, int end) {
-        ArrayList<TreeNode> res = new ArrayList<TreeNode>();
-        if(start > end) 
-            res.add(null);
-        for(int i = start; i <= end; i++) {
-            ArrayList<TreeNode> leftChildren = generate(start, i - 1);
-            ArrayList<TreeNode> rightChildren = generate(i + 1, end);
-            for(TreeNode leftChild : leftChildren) {
-                for(TreeNode rightChild : rightChildren) {
+
+    public List<TreeNode> generate(int start, int end) {
+        List<TreeNode> result = new ArrayList<TreeNode>();
+        if (start > end)
+            result.add(null);
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftChildren = generate(start, i - 1);
+            List<TreeNode> rightChildren = generate(i + 1, end);
+            for (TreeNode leftChild : leftChildren) {
+                for (TreeNode rightChild : rightChildren) {
                     TreeNode root = new TreeNode(i);
                     root.left = leftChild;
                     root.right = rightChild;
-                    res.add(root);
+                    result.add(root);        
                 }
             }
         }
-        return res;
+        return result;
     }
+
 }
