4de05a8ea4a9e7cbf96ffcdba644ac1b3e7b2f65
mitcc <mitcc@qq.com>
2014-12-31 20:26:50 +0800

    :octocat:

---
+++ leetcode/BinarySearchTreeIterator.java
@@ -0,0 +1,18 @@
+public class BinarySearchTreeIterator {
+
+    public BinarySearchTreeIterator (TreeNode root) {
+
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        
+    }
+    
+    
+}
