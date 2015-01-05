3fa81f3cc98630bafdd020d3429cac80851ee2da
mitcc <mitcc@qq.com>
2015-01-05 23:11:54 +0800

    :octocat:

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -15,17 +15,16 @@
 public class TowSum {
 
     public int[] twoSum(int[] numbers, int target) {
-        int[] res = new int[2];
+        int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < numbers.length; i++) {
-            if (!map.containsKey(numbers[i])) 
-                map.put(target - numbers[i], i + 1);
-            else {
-                res[0] = map.get(numbers[i]);
-                res[1] = i + 1;
+            if (map.containsKey(numbers[i])) {
+                result[0] = map.get(numbers[i]);
+                result[1] = i + 1;
                 break;
             }
+            map.put(target - numbers[i], i + 1);
         }
-        return res;
+        return result;
     }
 }
