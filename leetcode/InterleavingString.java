c969d28f2d120e8876bdfbf609fb6dac5f8f92b4
mitcc <mitcc@qq.com>
2015-01-09 19:09:52 +0800

    :octocat:

--- leetcode/InterleavingString.java
+++ leetcode/InterleavingString.java
@@ -1,5 +1,6 @@
 /*
- * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+ * Given s1, s2, s3, find whether s3 is formed 
+ * by the interleaving of s1 and s2.
 
  * For example,
  * Given:
