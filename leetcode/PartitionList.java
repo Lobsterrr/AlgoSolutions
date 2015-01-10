4738bb62e53118a3f04938d72df43e1b6ecc0023
mitcc <mitcc@qq.com>
2015-01-10 23:44:39 +0800

    :octocat:

--- leetcode/PartitionList.java
+++ leetcode/PartitionList.java
@@ -42,24 +42,4 @@ public class PartitionList {
         return small.next;
     }
 
-/*******************************************************************/
-
-    public ListNode partition(ListNode head, int x) {
-        ListNode small = new ListNode(0);
-        ListNode large = new ListNode(0);
-        ListNode sCur = small, lCur = large;
-        while(head != null) {
-            if(head.val < x) {
-                sCur.next = head;
-                sCur = sCur.next;
-            } else {
-                lCur.next = head;
-                lCur = lCur.next;
-            }
-            head = head.next;
-        }
-        sCur.next = large.next;
-        lCur.next = null;
-        return small.next;
-    }
 }
