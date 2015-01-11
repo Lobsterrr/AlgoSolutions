d3a9cc6e86a665c116575bf6aa8aa9b09154bd9e
mitcc <mitcc@qq.com>
2015-01-11 22:18:20 +0800

    :octocat:

--- basic/Quicksort.java
+++ basic/Quicksort.java
@@ -1,22 +1,25 @@
 public class Quicksort {
+
+/*************************** updated 20150111 **********************/
+
     public void quicksort(int[] array, int low, int high) {
-        if(low >= high) 
-            return ;
-        int i = low - 1;
-        for(int j = low; j < high; j++) {
-            if(array[j] <= array[high]) {
-                i++;
-                swap(array, i, j);
+        if (low >= high)
+            return;
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= array[high]) {
+                swap(array, i++, j);
             }
         }
-        swap(array, i + 1, high);
-        quicksort(array, low, i);
-        quicksort(array, i + 1, high);
+        swap(array, i, high);
+        quicksort(array, low, i - 1);
+        quicksort(array, i, high);
     }
 
     public void swap(int[] array, int i, int j) {
-        int temp = array[i];
+        int tmp = array[i];
         array[i] = array[j];
-        array[j] = temp;
+        array[j] = tmp;
     }
+    
 }
