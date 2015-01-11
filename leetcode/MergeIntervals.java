cd59d6670fc5003207bdf73dbde354663402890e
mitcc <mitcc@qq.com>
2015-01-12 00:04:46 +0800

    :octocat:

--- leetcode/MergeIntervals.java
+++ leetcode/MergeIntervals.java
@@ -15,42 +15,6 @@
  * }
  */
 public clss MergeIntervals {
-    public void quicksort(ArrayList<Interval> intervals, int low, int high) {
-        if(low >= high)
-            return ;
-        int i = low - 1;
-        for(int j = low; j < high; j++) {
-            if(intervals.get(j).start <= intervals.get(high).start) {
-                i++;
-                swap(intervals, i, j);
-            }
-        }
-        swap(intervals, i + 1, high);
-        quicksort(intervals, low, i);
-        quicksort(intervals, i + 1, high);
-    }
-    
-    public void swap(ArrayList<Interval> intervals, int i, int j) {
-        Interval temp = intervals.get(i);
-        intervals.set(i, intervals.get(j));
-        intervals.set(j, temp);
-    }
-
-    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
-        quicksort(intervals, 0, intervals.size() - 1);
-        for(int i = 0; i < intervals.size() - 1; i++) {
-            while(i < intervals.size() - 1 && intervals.get(i).end >= intervals.get(i + 1).start) {
-                if(intervals.get(i).end >= intervals.get(i + 1).start) {
-                    intervals.get(i).start = Math.min(intervals.get(i).start, intervals.get(i + 1).start);
-                    intervals.get(i).end = Math.max(intervals.get(i).end, intervals.get(i + 1).end);
-                    intervals.remove(i + 1);
-                }
-            }
-        }
-        return intervals;
-    }    
-
-/*****************************************************************************/
 
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> result = new ArrayList<Interval>();
