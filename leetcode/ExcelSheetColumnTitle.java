6dd0db14a0dfc351c470611babb619d8d1cfd5a1
mitcc <mitcc@qq.com>
2015-01-16 02:06:23 +0800

    :octocat:

--- leetcode/ExcelSheetColumnTitle.java
+++ leetcode/ExcelSheetColumnTitle.java
@@ -29,4 +29,15 @@ public class ExcelSheetColumnTitle {
         return result;
     }
 
+/*******************************************************************/
+
+    public String convertToTitle(int n) {
+        String result = "";
+        while (n > 0) {
+            result = (char) (65 + (n - 1) % 26) + result;
+            n = (n - 1) / 26;
+        }
+        return result;
+    }
+
 }
