56e68e239333eb1a0a5182c68b1795a4bbc8a1f8
mitcc <mitcc@qq.com>
2015-01-17 23:59:50 +0800

    :octocat:

--- leetcode/CombineTwoTables.sql
+++ leetcode/CombineTwoTables.sql
@@ -1,1 +1,1 @@
-SELECT Person.FirstName, Person.LastName, Address.City, Address.State FROM Person, Address WHERE Person.PersonId = Address.PersonId;
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State FROM Person, Address WHERE (Person.PersonId = NUL OR Address.PersonId OR Person.PersonId = Address.PersonId);
