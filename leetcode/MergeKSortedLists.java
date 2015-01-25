7054729aabc6a20e1cfed40785bb08b94db6ff15
mitcc <mitcc@qq.com>
2015-01-25 15:52:32 +0800

    :octocat:

--- leetcode/MergeKSortedLists.java
+++ leetcode/MergeKSortedLists.java
@@ -14,6 +14,35 @@
  * }
  */
 public class MergeKSortLists {
+
+    public ListNode mergeKList(List<ListNode> lists) {
+        if (lists.size() == 0)
+            return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.size(), 
+                new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode a, ListNode b) {
+                return a.val - b.val;
+            }
+        });
+        for (ListNode node : lists) {
+            if (node != null)
+                pq.add(node);
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            ListNode node = (ListNode) pq.poll();
+            if (node.next != null)
+                pq.add(node.next);
+            cur.next = node;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+/*****************************************************************************/
+
     public ListNode mergeKLists(ArrayList<ListNode> lists) {
         if(lists == null || lists.size() == 0)
             return null; 
@@ -21,7 +50,8 @@ public class MergeKSortLists {
         while(last > 0) {
             int cur = 0;
             while(cur < last) {
-                lists.set(cur, mergeTwoLists(lists.get(cur++), lists.get(last--)));
+                lists.set(cur, mergeTwoLists(lists.get(cur++), 
+                            lists.get(last--)));
             }
         }
         return lists.get(0);
@@ -40,4 +70,5 @@ public class MergeKSortLists {
             return l2;
         }
     }
+
 }
