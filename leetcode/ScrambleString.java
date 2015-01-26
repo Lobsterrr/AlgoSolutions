a80bf0073059e6ffdb49b1de32384c9c0033e617
mitcc <mitcc@qq.com>
2015-01-26 19:03:43 +0800

    :octocat:

--- leetcode/ScrambleString.java
+++ leetcode/ScrambleString.java
@@ -65,8 +65,6 @@ public class ScrambleString {
 */   
 
     public boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length())
-            return false;
         char[] array1 = s1.toCharArray();
         char[] array2 = s2.toCharArray();
         Arrays.sort(array1);
