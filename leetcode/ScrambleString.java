a38d4b25f2f3f088a650aac5bc3a962faad4f128
mitcc <mitcc@qq.com>
2015-01-26 19:13:23 +0800

    :octocat:

--- leetcode/ScrambleString.java
+++ leetcode/ScrambleString.java
@@ -1,5 +1,6 @@
 /* 
- * Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+ * Given a string s1, we may represent it as a binary tree 
+ * by partitioning it to two non-empty substrings recursively.
 
  * Below is one possible representation of s1 = "great":
 
@@ -10,9 +11,11 @@
  * g   r  e   at
  *            / \
  *           a   t
- * To scramble the string, we may choose any non-leaf node and swap its two children.
+ * To scramble the string, we may choose any non-leaf node 
+ * and swap its two children.
 
- * For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+ * For example, if we choose the node "gr" and swap its 
+ * two children, it produces a scrambled string "rgeat".
 
  *     rgeat
  *    /    \
@@ -23,7 +26,8 @@
  *           a   t
  * We say that "rgeat" is a scrambled string of "great".
 
- * Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+ * Similarly, if we continue to swap the children of nodes 
+ * "eat" and "at", it produces a scrambled string "rgtae".
 
  *     rgtae
  *    /    \
@@ -34,7 +38,8 @@
  *       t   a
  * We say that "rgtae" is a scrambled string of "great".
 
- * Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+ * Given two strings s1 and s2 of the same length, 
+ * determine if s2 is a scrambled string of s1.
  */
 public class ScrambleString {
 /*  public boolean isScramble(String s1, String s2) {
