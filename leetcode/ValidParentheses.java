77c504e6424e36808119800a4ff9935f7ef30454
mitcc <mitcc@qq.com>
2015-02-02 12:38:46 +0800

    :octocat:

--- leetcode/ValidParentheses.java
+++ leetcode/ValidParentheses.java
@@ -18,6 +18,17 @@ public class ValidParentheses {
         return stack.isEmpty();
     }
 
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            else if (stack.isEmpty() || s.charAt(i) - stack.pop() > 2)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
 /*****************************************************************************/
 
     public boolean isValid(String s) {
