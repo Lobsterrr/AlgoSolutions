ac09e6eabcadf79039524b5c9240a0a9325e363d
mitcc <mitcc@qq.com>
2015-02-03 13:23:32 +0800

    :octocat:

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -29,4 +29,15 @@ public class PalindromeNumber {
         return reverse == x;
     }
 
+/*******************************************************************/
+
+    public boolean isPalindrome(int x) {
+        String s = x + "";
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i))
+                return false;
+        }
+        return true;
+    }
+
 }
