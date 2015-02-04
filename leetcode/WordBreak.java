2c3f9a471534afa4e50e959c4f26d98ed393e9d1
mitcc <mitcc@qq.com>
2015-02-04 14:38:21 +0800

    :octocat:

--- leetcode/WordBreak.java
+++ leetcode/WordBreak.java
@@ -8,7 +8,7 @@
 
  * Return true because "leetcode" can be segmented as "leet code".
  */
-public class WordBrealen {
+public class WordBreak {
 /*  public boolean wordBreak(String s, Set<String> dict) {
         boolean[][] dp = new boolean[s.length()][s.length() + 1];
         for(int len = 1; len <= s.length(); len++) {
@@ -41,17 +41,20 @@ public class WordBrealen {
         return dp[s.length()];
     }
 */
+
     public boolean wordBreak(String s, Set<String> dict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-        for(int i = 0; i < s.length(); i++) {
-            if(dp[i]) {
-                for(int j = i + 1; j <= s.length(); j++) {
-                    if(dict.contains(s.substring(i, j)))
+        for (int i = 0; i < s.length(); i++) {
+            if (dp[i]) {
+                for (int j = i + 1; j <= s.length(); j++) {
+                    if (dict.contains(s.substring(i, j))) {
                         dp[j] = true;
+                    }
                 }
             }
         }
         return dp[s.length()];
     }
+
 }
