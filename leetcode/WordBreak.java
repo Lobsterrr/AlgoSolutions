ced934c299c9ace512fae88da2000e58beee60c0
mitcc <mitcc@qq.com>
2015-02-04 14:52:25 +0800

    :octocat:

--- leetcode/WordBreak.java
+++ leetcode/WordBreak.java
@@ -22,19 +22,4 @@ public class WordBreak {
         return dp[s.length()];
     } 
 
-    public boolean wordBreak(String s, Set<String> dict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-        for (int i = 0; i < s.length(); i++) {
-            if (dp[i]) {
-                for (int j = i + 1; j <= s.length(); j++) {
-                    if (dict.contains(s.substring(i, j))) {
-                        dp[j] = true;
-                    }
-                }
-            }
-        }
-        return dp[s.length()];
-    }
-
 }
