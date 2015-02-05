5fbce6cd169b5a036ac07c7f9faa57802da92575
mitcc <mitcc@qq.com>
2015-02-05 14:53:08 +0800

    :octocat:

--- leetcode/RotateImage.java
+++ leetcode/RotateImage.java
@@ -7,16 +7,16 @@
  * Could you do this in-place?
  */
 public class RotateImage {
-    // two different rotating methods.
+
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int i = 0; i < n / 2; i++) {
-            for(int j = 0; j < (n + 1) / 2; j++) {
-                int top = matrix[i][j];
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int tmp = matrix[i][j];
                 matrix[i][j] = matrix[n - 1 - j][i];
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = top;
+                matrix[j][n - 1 - i] = tmp;
             }
         }
     }
