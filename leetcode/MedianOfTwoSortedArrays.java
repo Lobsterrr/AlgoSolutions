fd0e375fc1b2b6f5fc3acc8e81840c48db0f119f
mitcc <mitcc@qq.com>
2015-02-11 15:39:13 +0800

    :octocat:

---
+++ leetcode/MedianOfTwoSortedArrays.java
@@ -0,0 +1,28 @@
+public class MedianOfTwoSortedArrays {
+
+    public double findMediaSortedArrays(int A[], int B[]) {
+        double result = 0;
+        int[] C = new int[A.length + B.length];
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (a < A.length && b < B.length) {
+            if (A[a] < B[b]) {
+                C[c++] = A[a++];
+            } else {
+                C[c++] = B[b++];
+            }
+        }
+        while (a < A.length) {
+            C[c++] = A[a++];
+        }
+        while (b < B.length) {
+            C[c++] = B[b++];
+        }
+        if (C.length % 2 == 0)
+            return (double) (C[C.length / 2 - 1] + C[C.length / 2]) / 2;
+        else
+            return (double) C[C.length / 2];
+    }
+
+}
