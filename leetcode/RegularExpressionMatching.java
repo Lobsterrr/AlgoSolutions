fdeeac8a8a7ec704d3adeb7d160ec1acf18ef713
mitcc <mitcc@qq.com>
2015-02-12 14:42:06 +0800

    :octocat:

--- leetcode/RegularExpressionMatching.java
+++ leetcode/RegularExpressionMatching.java
@@ -19,18 +19,22 @@
  * isMatch("aab", "c*a*b") ? true
  */
 public class RegularExpressionMatching {
+
     public boolean isMatch(String s, String p) {
-        if(p.length() == 0)
+        if (p.length() == 0)
             return s.length() == 0;
-        else if(p.length() == 1)
-            return p.equals(s) || s.length() == 1 && p.equals(".");
+        else if (p.length() == 1)
+            return p.equals(s) || p.equals(".") && s.length() == 1;
         else {
-            if(p.charAt(1) != '*') 
-                return s.length() > 0 && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') 
+            if (p.charAt(1) != '*') 
+                return s.length() > 0 && 
+                    (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) 
                     && isMatch(s.substring(1), p.substring(1));
-            if(isMatch(s, p.substring(2)))
-                return true;
-            return s.length() > 0 && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p);
+            else
+                return isMatch(s, p.substring(2)) || s.length() > 0 && 
+                    (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) && 
+                    isMatch(s.substring(1), p);
         }
     }
+
 }
