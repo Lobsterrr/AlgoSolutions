d6355b98b9399abd150a856f20110e3962e2362c
mitcc <mitcc@qq.com>
2015-02-13 21:40:29 +0800

    :octocat:

--- leetcode/PalindromePartitioning.java
+++ leetcode/PalindromePartitioning.java
@@ -13,25 +13,24 @@
  *  ]
  */
 
-
 public class PalindromePartitioning {
 
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<List<String>>();
         if (isPalindrome(s)) {
-		    List<String> list = new ArrayList<String>();
+            List<String> list = new ArrayList<String>();
             list.add(s);
             result.add(list);
         }
-		for (int i = 1; i < s.length(); i++) {
-			if (isPalindrome(s.substring(0, i))) {
-				for (List<String> list : partition(s.substring(i))) {
-					list.add(0, s.substring(0, i));
-					result.add(list);
-				}
-			}
-		}
-		return result;
+        for (int i = 1; i < s.length(); i++) {
+            if (isPalindrome(s.substring(0, i))) {
+                for (List<String> list : partition(s.substring(i))) {
+                    list.add(0, s.substring(0, i));
+                    result.add(list);
+                }
+            }
+        }
+        return result;
     }
 
     public boolean isPalindrome(String s) {
