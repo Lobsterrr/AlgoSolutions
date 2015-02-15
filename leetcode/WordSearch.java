11e4208ae98f5e845e0f58dd9fbbb276f0b02ca6
mitcc <mitcc@qq.com>
2015-02-15 15:49:41 +0800

    :octocat:

--- leetcode/WordSearch.java
+++ leetcode/WordSearch.java
@@ -39,4 +39,33 @@ public class WordSearch {
         board[i][j] = s.charAt(0);
         return flag;
     }
+
+
+
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, i, j, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    } 
+
+    public boolean dfs(char[][] board, int i, int j, String word) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+            board[i][j] != word.charAt(0))
+            return false;
+        if (word.length() == 1)
+            return true;
+        String s = word.substring(1);
+        board[i][j] = '#';
+        boolean flag = dfs(board, i - 1, j, s) || dfs(i + 1, j, s) || 
+                    dfs(board, i, j - 1, s) || dfs(i, j + 1, s);
+        board[i][j] = word.charAt(0);
+        return flag;
+    }
+
 }
