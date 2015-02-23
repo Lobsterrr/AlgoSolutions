6ae4c48ce486857996bfd477801f85d55b95d8fd
mitcc <mitcc@qq.com>
2015-02-23 11:04:31 +0800

    :octocat:

--- project-euler/pe004.py
+++ project-euler/pe004.py
@@ -9,23 +9,10 @@ made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
-def find_max_palindrome(min, max):
-    max_palindrome = 0
-    i = 999
-    while i > 99:
-        j = 999
-        while j >= i:
-            product = i * j
-            if product > max_palindrome and str(product) == str(product)[::-1]:
-                max_palindrome = product
-            j -= 1
-        i -= 1
-    return max_palindrome
-
 def largest_product_palindrome(start, end):
     result = 0
-    for i in range(end, start, -1):
-        for j in range(end, i, -1):
+    for i in range(end, start - 1, -1):
+        for j in range(end, i - 1, -1):
             product = i * j
             if product > result and str(product) == str(product)[::-1]:
                 result = product
@@ -33,6 +20,5 @@ def largest_product_palindrome(start, end):
 
 if __name__ == '__main__':
     start = time.clock()
-    print(find_max_palindrome(100, 999))
     print(largest_product_palindrome(100, 999))
     print('Runtime is', time.clock() - start)
