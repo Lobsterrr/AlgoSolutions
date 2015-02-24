96ade4a4a2be285e378a3acdf2934d218526a29c
mitcc <mitcc@qq.com>
2015-02-24 13:36:28 +0800

    :octocat:

--- project-euler/pe005.py
+++ project-euler/pe005.py
@@ -1,3 +1,5 @@
+__author__ = 'SUN'
+
 '''
 2520 is the smallest number that can be divided by each of the numbers from 1
 to 10 without any remainder.
@@ -5,7 +7,6 @@ to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20?
 '''
-__author__ = 'SUN'
 
 def gcd(a, b):
     if b == 0:
