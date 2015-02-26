ab1d374d446a76af2111a4ac9023f685fb850e56
mitcc <mitcc@qq.com>
2015-02-26 19:22:39 +0800

    :octocat:

--- project-euler/pe006.py
+++ project-euler/pe006.py
@@ -1,3 +1,5 @@
+import time
+
 __author__ = 'SUN'
 
 def sum_square_diff(start, end):
@@ -8,19 +10,7 @@ def sum_square_diff(start, end):
     total *= 2
     return total
 
-def sum1(start, end):
-    total = 0
-    for i in range(start, end + 1):
-        total += i * i
-    return total
-
-def sum2(start, end):
-    return end * end * (end + 1) * (end + 1) // 4 - \
-           start * start * (start - 1) * (start - 1) // 4
-
-def sum_square_diff_1(start, end):
-    return sum2(start, end) - sum1(start, end)
-
 if __name__ == '__main__':
-    print(sum_square_diff_1(1, 100))
+    start = time.clock()
     print(sum_square_diff(1, 100))
+    print('Runtime is', time.clock() - start)
