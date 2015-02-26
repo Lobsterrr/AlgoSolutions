c6732349a30cc37f69c40ab1cc1867bbbab55cb0
mitcc <mitcc@qq.com>
2015-02-26 19:26:04 +0800

    :octocat:

--- project-euler/pe007.py
+++ project-euler/pe007.py
@@ -1,8 +1,9 @@
 '''
 10001st prime
 Problem 7
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
-the 6th prime is 13. What is the 10 001st prime number?
+By listing the first six prime numbers: 2, 3,
+5, 7, 11, and 13, we can see that the 6th prime
+is 13. What is the 10 001st prime number?
 '''
 __author__ = 'SUN'
 
@@ -15,7 +16,7 @@ if __name__ == '__main__':
             while j < 200000:
                 prime[j] = False
                 j += i;
-    cnt = 0;
+    cnt = 0
     for i in range(200000):
         if prime[i]:
             cnt += 1
