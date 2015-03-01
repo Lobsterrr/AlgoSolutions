42eaf3301981a8ca7bb26f13503273d7df79c61e
mitcc <mitcc@qq.com>
2015-03-01 10:56:19 +0800

    :octocat:

--- project-euler/pe007.py
+++ project-euler/pe007.py
@@ -5,6 +5,8 @@ By listing the first six prime numbers: 2, 3,
 5, 7, 11, and 13, we can see that the 6th prime
 is 13. What is the 10 001st prime number?
 '''
+from pe069 import prime_sieve
+
 __author__ = 'SUN'
 
 if __name__ == '__main__':
@@ -23,3 +25,7 @@ if __name__ == '__main__':
         if cnt == 10001:
             print(i)
             break
+
+    prime = prime_sieve(150000)
+    print(prime.__len__())
+    print(prime[10000])
