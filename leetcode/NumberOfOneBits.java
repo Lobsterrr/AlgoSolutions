8d819b698e67964e001dc8f23a69432ab2216dc3
mitcc <mitcc@qq.com>
2015-03-15 15:43:55 +0800

    :octocat:

--- leetcode/NumberOfOneBits.java
+++ leetcode/NumberOfOneBits.java
@@ -9,15 +9,6 @@
 public class NumberOfOneBits {
 
     public int hammingWeight(int n) {
-        int count = 0;
-        while (n != 0) {
-            count += n & 1;
-            n = (n >> 1);
-        }
-        return count;
-    }
-
-    public int hammingWeight(int n) {
         if (n == 0 || n == 1)
             return n;
         return 1 + hammingWeight(n - (int) Math.pow(2, (int) (Math.log(n) / Math.log(2))));
