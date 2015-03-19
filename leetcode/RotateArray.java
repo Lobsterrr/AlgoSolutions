211f410c3155cb2d2bf39337ec1cb7163bfc0848
mitcc <mitcc@qq.com>
2015-03-20 00:06:32 +0800

    :octocat:

--- leetcode/RotateArray.java
+++ leetcode/RotateArray.java
@@ -12,17 +12,6 @@ public class RotateArray {
 
     public void rotate(int[] nums, int k) {
         k %= nums.length;
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = nums.length - k; i < nums.length; i++) {
-            list.add(nums[i]);
-        }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            nums[i] = i >= k ? nums[i - k] : list.get(i);
-        }
-    }
-
-    public void rotate(int[] nums, int k) {
-        k %= nums.length;
         int[] tmp = new int[k];
         for (int i = 0; i < k; i++) {
             tmp[i] = nums[nums.length - k + i];
