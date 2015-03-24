f7cbdd84971f686a6b79f87ec1998784c52e1a47
mitcc <mitcc@qq.com>
2015-03-24 18:27:27 +0800

    :octocat:

--- leetcode/MinStack.java
+++ leetcode/MinStack.java
@@ -3,5 +3,29 @@
  */
 public class MinStack {
 
+    public Stack<Integer> stack = new Stack<Integer>();
+    public Stack<Integer> minStack = new Stack<Integer>();
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.size() == 0 || minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (minStack.peek().equals(stack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
 
 }
