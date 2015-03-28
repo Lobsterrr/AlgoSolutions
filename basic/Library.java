61a302eea2e83bd61f3aaa69e27b4b2e6418a3bc
mitcc <mitcc@qq.com>
2015-03-28 21:02:30 +0800

    :octocat:

--- basic/Library.java
+++ basic/Library.java
@@ -1,11 +1,11 @@
 public class Library {
 
-    public static int gcd(int a, int b) {
+    public int gcd(int a, int b) {
         return b == 0 ? a : gcd(b, a % b);
     }
 
-    public static int gcd(int a, int b) {
-        while(b != 0) {
+    public int gcd(int a, int b) {
+        while (b != 0) {
             int temp = a;
             a = b;
             b = temp % a;
@@ -13,7 +13,7 @@ public class Library {
         return a;
     }
 
-    public static int log2(int a) {
+    public int log2(int a) {
         int res = 0;
         while(a >= 2) {
             res++;
