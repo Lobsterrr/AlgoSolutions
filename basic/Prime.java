c51249326f4f832e82f94ad9fd479f77d196178e
mitcc <mitcc@qq.com>
2015-03-29 19:28:40 +0800

    :octocat:

--- basic/Prime.java
+++ basic/Prime.java
@@ -1,13 +1,14 @@
 public class Prime {
-    public static boolean isPrime(int n) {
-        if(n < 0)
+
+    public boolean isPrime(int n) {
+        if (n < 0)
             throw new IllegalArgumentException("negative number");
-        if(n == 0 || n == 1)
+        if (n == 0 || n == 1)
             return false;
-        if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+        if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
             return n == 2 || n == 3 || n == 5 || n == 7;
-        for(int i = 11; i * i <= n; i += 2) {
-            if(n % i == 0)
+        for (int i = 11; i * i <= n; i += 2) {
+            if (n % i == 0)
                 return false;
         }
         return true;
