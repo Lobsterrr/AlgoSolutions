f79f729df25249a75d7e483e61b13fabf8938e80
mitcc <mitcc@qq.com>
2015-03-29 13:44:23 +0800

    :octocat:

--- basic/Insertsort.java
+++ basic/Insertsort.java
@@ -1,11 +1,13 @@
 public class Insertsort {
-    public void insertsort(int[] A, int low, int high) {
-        for(int i = low + 1; i <= high; i++) {
-            for(int j = i - 1; j >= 0 && A[j] > A[j + 1]; j--) {
-                int temp = A[j];
+
+    public void insertSort(int[] A, int low, int high) {
+        for (int i = low + 1; i <= high; i++) {
+            for (int j = i - 1; j >= 0 && A[j] > A[j + 1]; j--) {
+                int tmp = A[j];
                 A[j] = A[j + 1];
-                A[j + 1] = temp;
+                A[j + 1] = tmp;
             }
         }
     }
+
 }
