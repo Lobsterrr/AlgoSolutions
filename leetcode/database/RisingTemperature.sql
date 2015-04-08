7af2e1e1b26277d3c61ac2bf764cf343552e66cc
mitcc <mitcc@qq.com>
2015-04-08 23:17:24 +0800

    :basketball:

--- leetcode/database/RisingTemperature.sql
+++ leetcode/database/RisingTemperature.sql
@@ -1,5 +1,7 @@
 /*
-Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+Given a Weather table, write a SQL query to find all 
+dates' Ids with higher temperature compared to its 
+previous (yesterday's) dates.
 
 +---------+------------+------------------+
 | Id(INT) | Date(DATE) | Temperature(INT) |
@@ -9,7 +11,8 @@ Given a Weather table, write a SQL query to find all dates' Ids with higher temp
 |       3 | 2015-01-03 |               20 |
 |       4 | 2015-01-04 |               30 |
 +---------+------------+------------------+
-For example, return the following Ids for the above Weather table:
+For example, return the following Ids for the above 
+Weather table:
 +----+
 | Id |
 +----+
@@ -19,4 +22,6 @@ For example, return the following Ids for the above Weather table:
 
 */
 
-SELECT W1.Id FROM Weather W1 INNER JOIN Weather W2 ON TO_DAYS(W1.Date) = TO_DAYS(W2.Date) + 1 AND W1.Temperature > W2.Temperature;
+SELECT W1.Id FROM Weather W1 INNER JOIN Weather W2 
+ON TO_DAYS(W1.Date) = TO_DAYS(W2.Date) + 1 AND 
+W1.Temperature > W2.Temperature;
