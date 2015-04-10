0477a6ed09b5f368fe3a270b3489834b3c69359c
mitcc <mitcc@qq.com>
2015-04-10 20:05:17 +0800

    :basketball:

--- leetcode/algorithms/NumberOfIslands.java
+++ leetcode/algorithms/NumberOfIslands.java
@@ -24,11 +24,24 @@ public class NumberOfIslands {
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
             }
         }
-
         return count;
+    }
+
+    public void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || 
+                y >= grid[0].length || grid[x][y] != '1')
+            return;
+        grid[x][y] = '2';
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
     }
 
 }
