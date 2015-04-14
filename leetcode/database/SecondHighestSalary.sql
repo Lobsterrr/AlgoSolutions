a7fd2b544e10bb89ab40204e69e2deda2639fcfe
mitcc <mitcc@qq.com>
2015-04-14 23:43:14 +0800

    :basketball:

--- leetcode/database/SecondHighestSalary.sql
+++ leetcode/database/SecondHighestSalary.sql
@@ -1,5 +1,6 @@
 /*
-Write a SQL query to get the second highest salary from the Employee table.
+Write a SQL query to get the second highest 
+salary from the Employee table.
 
 +----+--------+
 | Id | Salary |
@@ -8,7 +9,11 @@ Write a SQL query to get the second highest salary from the Employee table.
 | 2  | 200    |
 | 3  | 300    |
 +----+--------+
-For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
+For example, given the above Employee table, 
+the second highest salary is 200. If there 
+is no second highest salary, then the query 
+should return null.
 */
 
-SELECT MAX(Salary) FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee); 
+SELECT MAX(Salary) FROM Employee WHERE 
+Salary < (SELECT MAX(Salary) FROM Employee); 
