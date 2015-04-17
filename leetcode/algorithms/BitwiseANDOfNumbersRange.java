9d1aa43a1e344e1e0c7a05ba27bfb3cbe71236fd
mitcc <mitcc@qq.com>
2015-04-18 01:33:31 +0800

    :basketball:

--- leetcode/algorithms/BitwiseANDOfNumbersRange.java
+++ leetcode/algorithms/BitwiseANDOfNumbersRange.java
@@ -8,33 +8,9 @@ public class BitwiseANDOfNumbersRange {
 
     public int rangeBitwiseAnd(int m, int n) {
         int result = 0;
-        String sm = toBinaryString(m);
-        String sn = toBinaryString(n);
-        int p = n - m;
-        for (int i = 0; i < sm.length(); i++) {
-            if (sm.charAt(i) == '1' && sn.charAt(i) == '1' 
-                    && p <= (1 << sm.length() -  1 - i)) 
-                result += 1 << sm.length() - 1 - i;
-        }
-        return result;
-    }
-
-    public String toBinaryString(int x) {
-        String str = "";
-        for (int i = 0; i < 32; i++) {
-            str += (x & (1 << 31 - i)) != 0 ? "1" : "0";
-        }
-        return str;
-    }
-
-/*********************************************************/
-
-    public int rangeBitwiseAnd(int m, int n) {
-        int result = 0;
-        int k = n - m;
         for (int i = 0; i < 32; i++) {
             int mask = 1 << 31 - i;
-            if ((m & mask) != 0 && (n & mask) != 0 && k <= mask)
+            if ((m & mask) != 0 && (n & mask) != 0 && (n - m) <= mask)
                 result += mask;
         }
         return result;
