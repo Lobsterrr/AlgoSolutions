118f0ba728552da415ecc73aa1ec27dc78be0e5d
mitcc <mitcc@qq.com>
2015-04-19 17:01:17 +0800

    :basketball:

--- leetcode/algorithms/MultiplyStrings.java
+++ leetcode/algorithms/MultiplyStrings.java
@@ -4,6 +4,7 @@
  * Note: The numbers can be arbitrarily large and are non-negative. 
  */
 public class MultiplyStrings {
+
     public String multiply(String num1, String num2) {
         int len1 = num1.length(), len2 = num2.length();
         int[] num = new int[len1 + len2];
@@ -30,4 +31,34 @@ public class MultiplyStrings {
         }
         return result;
     }
+
+    
+    public String multiply(String num1, String num2) {
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] data = new int[len1 + len2];  
+        for (int i = 0; i < len1; i++) {
+            int carrier = 0;
+            int a = num1.charAt(len1 - 1 - i) - '0';
+            for (int j = 0; j < len2; j++) {
+                int b = num2.charAt(len2 - 1 - j) - '0';
+                int tmp = data[len1 - 1 - i + len2 - j];
+                data[len1 - 1 - i + len2 - j] = (data[len1 - 1 - i + len2 - j] + carrier + a * b) % 10;
+                carrier = (tmp + carrier + a * b) / 10;
+            }
+            data[len1 - 1 - i] = (data[len1 - 1 - i] + carrier) % 10;
+        }
+        String result = "";
+        boolean flag = false;
+        for (int i = 0; i < len1 + len2; i++) {
+            if (data[i] != 0)
+                flag = true;
+            if (flag)
+                result += data[i];
+        }
+        if (result.equals(""))
+            result = "0";
+        return result;
+    }
+
 }
