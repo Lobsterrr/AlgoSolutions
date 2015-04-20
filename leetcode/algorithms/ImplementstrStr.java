98f55e5d3559e23dddc4be115f60534e21eced0f
mitcc <mitcc@qq.com>
2015-04-20 22:27:54 +0800

    :basketball:

--- leetcode/algorithms/ImplementstrStr.java
+++ leetcode/algorithms/ImplementstrStr.java
@@ -1,34 +1,25 @@
 /* 
- * Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
+ * Returns a pointer to the first occurrence of needle in 
+ * haystack, or null if needle is not part of haystack.
+ *
+ * Implement strStr().
+
+ * Returns the index of the first occurrence of needle 
+ * in haystack, or -1 if needle is not part of haystack.
  */
 public class ImplementstrStr {
-    public String strStr(String haystack, String needle) {
-        if(needle.length() == 0 || haystack.equals(needle))
-            return haystack;
-        int i = 0, j = 0;
-        for(; i <= haystack.length() - needle.length() && j < needle.length(); i++) {
-            if(haystack.charAt(i) == needle.charAt(0)) {
-                for(; j < needle.length(); j++) {
-                    if(haystack.charAt(i + j) != needle.charAt(j)) {
-                        j = 0;
-                        break;
-                    }
-                }
-            }
-        }
-        return j == needle.length() ? haystack.substring(i - 1) : null;
-    }
-
 
     public int strStr(String haystack, String needle) {
-        if (needle.length == 0)
+        if (needle.length() == 0)
             return 0;
-        for (int i = 0; i < haystack.length() - needle.length(); i++) {
-
-            for (int j = 0; j < needle.length(); j++) {
-                
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            for (int j = 0, k = i; j < needle.length() && k < haystack.length() 
+                    && needle.charAt(j) == haystack.charAt(k); j++, k++) {
+                if (j == needle.length() - 1)
+                    return i;
             }
         }
+        return -1;
     }
 
 }
