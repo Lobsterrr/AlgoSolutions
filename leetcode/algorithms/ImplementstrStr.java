d00ceefba6b0627d734047d184a607cd50001f39
mitcc <mitcc@qq.com>
2015-04-20 22:04:40 +0800

    :basketball:

--- leetcode/algorithms/ImplementstrStr.java
+++ leetcode/algorithms/ImplementstrStr.java
@@ -23,7 +23,7 @@ public class ImplementstrStr {
     public int strStr(String haystack, String needle) {
         if (needle.length == 0)
             return 0;
-        for (int i = 0; i < haystack.length(); i++) {
+        for (int i = 0; i < haystack.length() - needle.length(); i++) {
 
             for (int j = 0; j < needle.length(); j++) {
                 
