a48c7a5c00d834791608c3c8b06fd943d4a03ea7
mitcc <mitcc@qq.com>
2015-04-21 23:13:08 +0800

    :basketball:

--- leetcode/algorithms/RecoverBinarySearchTree.java
+++ leetcode/algorithms/RecoverBinarySearchTree.java
@@ -4,36 +4,19 @@
  * Recover the tree without changing its structure.
 
  * Note:
- * A solution using O(n) space is pretty straight forward. Could you devise a 
- * constant space solution?
- * confused what "{1,#,2,3}" means? > read more on how binary tree is 
- * serialized on OJ.
-
-
- * OJ's Binary Tree Serialization:
- * The serialization of a binary tree follows a level order traversal, where 
- * '#' signifies a path terminator where no node exists below.
-
- * Here's an example:
- *    1
- *   / \
- *  2   3
- *     /
- *    4
- *     \
- *      5
- * The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
+ * A solution using O(n) space is pretty straight forward. Could you 
+ * devise a constant space solution?
  */
 
 public class RecoverBinarySearchTree {
 
-    /*
-     * Thanks to AnnieKim. This is the Java version of her code.
-     * https://github.com/AnnieKim/LeetCode/blob/master/RecoverBinarySearchTree.h
-     */
+/*
+* Thanks to AnnieKim. This is the Java version of her code.
+* https://github.com/AnnieKim/LeetCode/blob/master/RecoverBinarySearchTree.h
+*/
     
     public void recoverTree(TreeNode root) {
-        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        List<TreeNode> list = new ArrayList<TreeNode>();
         inorderTraversal(root, list);
         TreeNode first = null;
         TreeNode second = null;
@@ -44,31 +27,17 @@ public class RecoverBinarySearchTree {
                 second = list.get(i);
             }
         }
-        int temp = first.val;
+        int tmp = first.val;
         first.val = second.val;
-        second.val = temp;
+        second.val = tmp;
     }
-    
-    public void inorderTraversal(TreeNode root, ArrayList<TreeNode> list) {
+
+    public void inorderTraversal(TreeNode root, List<TreeNode> list) {
         if (root == null)
             return;
         inorderTraversal(root.left, list);
         list.add(root);
         inorderTraversal(root.right, list);
-    }
-
-
-
-
-
-    public void recoverTree(TreeNode root) {
-        List<Integer> list = inorderTraversal(root);
-
-    }
-
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
-
     }
 
 }
