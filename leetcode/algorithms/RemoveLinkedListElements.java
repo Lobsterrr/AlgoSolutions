55d285aa7db53097069ba6429084a75268b6b6fe
mitcc <mitcc@qq.com>
2015-04-23 23:14:43 +0800

    :basketball:

--- leetcode/algorithms/RemoveLinkedListElements.java
+++ leetcode/algorithms/RemoveLinkedListElements.java
@@ -20,24 +20,6 @@ public class RemoveLinkedListElements {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode pre = dummy;
-        ListNode cur = dummy.next;
-        while (cur != null) {
-            if (cur.val != val) {
-                pre.next = cur;
-                pre = cur;
-            }
-            cur = cur.next;
-        }
-        pre.next = cur;
-        return dummy.next;
-    }
-
-
-
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
         ListNode cur = dummy;
         while (cur.next != null) {
             if (cur.next.val == val) {
