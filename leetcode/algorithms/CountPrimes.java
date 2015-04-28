04512b71d9720f542de742e51bc899ab335e8aa0
mitcc <mitcc@qq.com>
2015-04-28 23:14:11 +0800

    :basketball:

--- leetcode/algorithms/CountPrimes.java
+++ leetcode/algorithms/CountPrimes.java
@@ -7,7 +7,6 @@ public class CountPrimes {
     public int countPrimes(int n) {
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
         for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
@@ -16,9 +15,8 @@ public class CountPrimes {
             }
         }
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (isPrime[i])
-                count++;
+        for (int i = 2; i < n; i++) {
+            count += isPrime[i] ? 1 : 0;
         }
         return count;
     }
