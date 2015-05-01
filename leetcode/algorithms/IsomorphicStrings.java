8a8b45b960105c7e66563122a7a24995d08aa199
mitcc <mitcc@qq.com>
2015-05-01 23:53:06 +0800

    :basketball:

--- leetcode/algorithms/IsomorphicStrings.java
+++ leetcode/algorithms/IsomorphicStrings.java
@@ -47,16 +47,17 @@ public class IsomorphicStrings {
 
 /*****************************************************************************/
 
-    //Time Limit Exceeded
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length())
-            return false;
+        Map<Character, Character> map1 = new HashMap<Character, Character>();
+        Map<Character, Character> map2 = new HashMap<Character, Character>();
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j) && t.charAt(i) != t.charAt(j) ||
-                        s.charAt(i) != s.charAt(j) && t.charAt(i) == t.charAt(j))
-                    return false;
-            }
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map1.containsKey(c1) && map1.get(c1) != c2 || 
+                    map2.containsKey(c2) && map2.get(c2) != c1)
+                return false;
+            map1.put(c1, c2);
+            map2.put(c2, c1);
         }
         return true;
     }
