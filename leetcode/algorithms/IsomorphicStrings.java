f6e594582b3a22c015dccf9e701ae46b0b94a950
mitcc <mitcc@qq.com>
2015-05-01 12:58:09 +0800

    :basketball:

--- leetcode/algorithms/IsomorphicStrings.java
+++ leetcode/algorithms/IsomorphicStrings.java
@@ -21,7 +21,14 @@
 public class IsomorphicStrings {
 
     public boolean isIsomorphic(String s, String t) {
-
+        if (s.length() != t.length())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                s = s.replace(s.charAt(i), t.charAt(i));
+            }
+        }
+        return s.equals(t);
     }
 
 }
