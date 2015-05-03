306a82eee50550fe82cb42cb1a71070b426edaf2
mitcc <mitcc@qq.com>
2015-05-03 21:32:58 +0800

    :basketball:

--- leetcode/algorithms/CompareVersionNumbers.java
+++ leetcode/algorithms/CompareVersionNumbers.java
@@ -66,4 +66,20 @@ public class CompareVersionNumbers {
         }
     }
 
+    public int compareVersion(String version1, String version2) {
+        String[] array1 = version1.split("\\.");
+        String[] array2 = version2.split("\\.");
+        for (int i = 0; i < Math.max(array1.length, array2.length); i++) {
+            if (i < array1.length && i < array2.length && 
+                    Integer.parseInt(array1[i]) > Integer.parseInt(array2[i]) || 
+                    i == array2.length && Integer.parseInt(array1[i]) != 0)
+                return 1;
+            if (i < array1.length && i < array2.length && 
+                    Integer.parseInt(array1[i]) < Integer.parseInt(array2[i]) || 
+                    i == array1.length && Integer.parseInt(array2[i]) != 0)
+                return -1;
+        }
+        return 0;
+    }
+
 }
