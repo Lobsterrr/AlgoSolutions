cfde7ef62014e41ff9fab65f0c6eb219fc5c8ed2
mitcc <mitcc@qq.com>
2015-05-04 22:18:48 +0800

    :basketball:

--- leetcode/algorithms/LargestNumber.java
+++ leetcode/algorithms/LargestNumber.java
@@ -9,7 +9,32 @@
 public class LargestNumber {
 
     public String largestNumber(int[] nums) {
+        quicksort(nums, 0, nums.length - 1);
+        String result = "";
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result += nums[i];
+        }
+        return result;
+    }
 
+    public void quicksort(int[] array, int start, int end) {
+        if (start >= end) 
+            return;
+        int i = start;
+        for (int j = start; j < end; j++) {
+            if (compare(array[j], array[end]) <= 0) {
+                swap(array, i++, j);
+            }
+        }
+        swap(array, i, end);
+        quicksort(array, start, i - 1);
+        quicksort(array, i, end);
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
     public int compare(int a, int b) {
