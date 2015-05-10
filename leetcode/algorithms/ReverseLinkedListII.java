f535c27858b7acc6e7fce98c267f87a0089c80f2
mitcc <mitcc@qq.com>
2015-05-10 16:08:54 +0800

    :basketball:

--- leetcode/algorithms/ReverseLinkedListII.java
+++ leetcode/algorithms/ReverseLinkedListII.java
@@ -1,5 +1,6 @@
 /*
- * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ * Reverse a linked list from position m to n. 
+ * Do it in-place and in one-pass.
 
  * For example:
  * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
