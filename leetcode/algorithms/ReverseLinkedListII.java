bd6b48733ce586493b3ba2610dfd3d04a379a25f
mitcc <mitcc@qq.com>
2015-05-10 16:08:23 +0800

    :basketball:

--- leetcode/algorithms/ReverseLinkedListII.java
+++ leetcode/algorithms/ReverseLinkedListII.java
@@ -59,22 +59,16 @@ public class ReverseLinkedListII {
 /*******************************************************************/
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        for(int i = 0; i <= (n - m) / 2; i++) {
+        for (int i = 0; i <= (n - m) / 2; i++) {
             int curM = m + i;
             int curN = n - i;
-            ListNode mNode = head;
-            ListNode nNode = head;
-            while(curM - 1 > 0) {
-                mNode = mNode.next;
-                curM--;
-            }
-            while(curN - 1 > 0) {
-                nNode = nNode.next;
-                curN--;
-            }
-            int temp = mNode.val;
-            mNode.val = nNode.val;
-            nNode.val = temp;
+            ListNode nodeM = head;
+            ListNode nodeN = head;
+            for (; curM > 1; nodeM = nodeM.next, curM--);
+            for (; curN > 1; nodeN = nodeN.next, curN--);
+            int tmp = nodeM.val;
+            nodeM.val = nodeN.val;
+            nodeN.val = tmp;
         }
         return head;
     }
