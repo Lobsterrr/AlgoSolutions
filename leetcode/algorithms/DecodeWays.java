6bb137cf3a4045422e84881ca0acd1f259b5f7c7
mitcc <mitcc@qq.com>
2015-05-12 23:21:56 +0800

    :basketball:

--- leetcode/algorithms/DecodeWays.java
+++ leetcode/algorithms/DecodeWays.java
@@ -1,16 +1,17 @@
 /*
- * A message containing letters from A-Z is being encoded to numbers using the 
- * following mapping:
+ * A message containing letters from A-Z is being 
+ * encoded to numbers using the following mapping:
 
  * 'A' -> 1
  * 'B' -> 2
  * ...
  * 'Z' -> 26
- * Given an encoded message containing digits, determine the total number of 
- * ways to decode it.
+ * Given an encoded message containing digits, 
+ * determine the total number of ways to decode it.
 
  * For example,
- * Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+ * Given encoded message "12", it could be decoded 
+ * as "AB" (1 2) or "L" (12).
 
  * The number of ways decoding "12" is 2.
  */
@@ -33,26 +34,22 @@ public class DecodeWays {
         return dp[s.length()];
     }
 
-/**************************** updated 2014.04.06 *****************************/
+/*******************************************************************/
 
     // O(n) time; O(1) space.
     public int numDecodings(String s) {
         if (s.length() == 0 || s.charAt(0) == '0')
             return 0;
-        int prev = 1;
+        int pre = 1;
         int cur = 1;
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 2; i <= s.length(); i++) {
             int next = 0;
-            if (s.charAt(i) == '0' && 
-                    (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2'))
-                next += prev;
-            else if (s.charAt(i) != '0') {
+            if (s.charAt(i - 1) != '0')
                 next += cur;
-                if (s.charAt(i - 1) == '1' 
-                        || s.charAt(i - 1) == '2' && s.charAt(i) <= '6')
-                    next += prev;
-            }
-            prev = cur;
+            if (s.charAt(i - 2) == '1' || s.charAt(i - 1) <= '6'
+                    && s.charAt(i - 2) == '2')
+                next += pre;
+            pre = cur;
             cur = next;
         }
         return cur;
