6f3840c64b6a17bf2292e5419d4e1b2b092ee1e0
mitcc <mitcc@qq.com>
2015-05-17 15:25:01 +0800

    :basketball:

--- leetcode/algorithms/MinimumSizeSubarraySum.java
+++ leetcode/algorithms/MinimumSizeSubarraySum.java
@@ -17,17 +17,22 @@
  */
 public class MinimumSizeSubarraySum {
 
+    //TLE
     public int minSubArrayLen(int s, int[] nums) {
         int result = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
             int thisSum = 0;
             for (int j = i; j < nums.length; j++) {
-                thisSum += nums[i];
-                if (thisSum >= s) 
+                thisSum += nums[j];
+                if (thisSum >= s) {
                     result = Math.min(result, j - i + 1);
+                    break;
+                }
             }
         }
         return result == Integer.MAX_VALUE ? 0 : result;
     }
+    
+
 
 }
