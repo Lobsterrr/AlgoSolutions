c3c7b82a3144a2f772075de6779736214552abfb
mitcc <mitcc@qq.com>
2015-05-26 08:44:46 +0800

    :basketball:

--- leetcode/algorithms/CombinationSumIII.java
+++ leetcode/algorithms/CombinationSumIII.java
@@ -25,7 +25,7 @@
 public class CombinationSumIII {
 
     public List<List<Integer>> combinationSum3(int k, int n) {
-        
+        List<List<Integer>> result = new ArrayList<List<Integer>>(); 
 
     }
 
