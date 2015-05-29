5caa414978e5da3fa6bb426e0872e75af8c6aa32
mitcc <mitcc@qq.com>
2015-05-29 22:18:05 +0800

    :basketball:

--- leetcode/algorithms/ContainsDuplicateII.java
+++ leetcode/algorithms/ContainsDuplicateII.java
@@ -1,8 +1,8 @@
 /*
- * Given an array of integers and an integer k, return true 
- * if and only if there are two distinct indices i and j in 
- * the array such that nums[i] = nums[j] and the difference 
- * between i and j is at most k.
+ * Given an array of integers and an integer k, return true if and 
+ * only if there are two distinct indices i and j in the array such 
+ * that nums[i] = nums[j] and the difference between i and j is at 
+ * most k.
  */
 public class ContainsDuplicateII {
 
