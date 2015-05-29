02f760e63ec32d7665baad7f4310c2b0e568f0a5
mitcc <mitcc@qq.com>
2015-05-29 23:17:01 +0800

    :basketball:

--- leetcode/algorithms/CombinationSumIII.java
+++ leetcode/algorithms/CombinationSumIII.java
@@ -25,9 +25,24 @@
 public class CombinationSumIII {
 
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>(); 
+        return combinationSum3(k, n, 1);
+    }
 
-
+    public List<List<Integer>> combinationSum3(int k, int n, int start) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (k == 1 && start <= n && n <= 9) {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(n);
+            result.add(list);
+            return result;
+        }
+        for (int i = start; i <= n; i++) {
+            List<List<Integer>> sub = combinationSum3(k - 1, n - i, i + 1);
+            for (List<Integer> list : sub) {
+                list.add(0, i);
+                result.add(list);
+            }
+        }
         return result;
     }
 
