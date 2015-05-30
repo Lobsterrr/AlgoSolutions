6b72173afb58e70fd08d110d0183b4ba5cbd00f5
mitcc <mitcc@qq.com>
2015-05-30 20:46:53 +0800

    :basketball:

--- leetcode/algorithms/KthLargestElementInAnArray.java
+++ leetcode/algorithms/KthLargestElementInAnArray.java
@@ -23,5 +23,12 @@ public class KthLargestElementInAnArray {
         }
         return nums[k - 1];
     }
+
+/*******************************************************************/
+
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
         
 }
