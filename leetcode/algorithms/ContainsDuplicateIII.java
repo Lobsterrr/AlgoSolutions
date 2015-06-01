fb64d22d2f10998021570a94cd2feccb6f364742
mitcc <mitcc@qq.com>
2015-06-01 21:35:26 +0800

    :basketball:

--- leetcode/algorithms/ContainsDuplicateIII.java
+++ leetcode/algorithms/ContainsDuplicateIII.java
@@ -4,7 +4,13 @@
 public class ContainsDuplicateIII {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-
+        for (int i = 0; i < nums.length - k; i++) {
+            for (int j = i + 1; j <= i + k; j++) {
+                if (Math.abs(nums[j] - nums[i]) <= t)
+                    return true;
+            }
+        }
+        return false;
     }
 
 }
