4f8628668e40302bd1f8524f74f01bd65afe31ba
mitcc <mitcc@qq.com>
2015-06-05 00:33:07 +0800

    :basketball:

--- leetcode/algorithms/MaximalSquare.java
+++ leetcode/algorithms/MaximalSquare.java
@@ -11,7 +11,30 @@
  */
 public class MaximalSquare {
 
-    
+    public int maximalSquare(char[][] matrix) {
+        int xLen = matrix.length;
+        int yLen = matrix[0].length;
+        int[] height = new int[yLen];
+        int result = 0;
+        for (int i = 0; i < xLen; i++) {
+            for (int j = 0; j < yLen; j++) {
+                height[j] = matrix[i][j] == '0' ? 0 : height[j] + 1;    
+            }
+            int k = 0;
+            Stack<Integer> stack = new Stack<Integer>();
+            while (k < yLen) {
+                if (stack.isEmpty() || height[k] >= height[stack.peek()]) {
+                    stack.push(k++);
+                } else {
+                    int x = k - 1 - stack.pop();
+                    int y = height[k];
+                    int a = Math.max(x, y);
+                    result = Math.max(result, a * a);
+                }
+            }
+        }
+        return result;
+    }
 
     
 }
