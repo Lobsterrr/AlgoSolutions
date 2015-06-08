aaf9cb9c141aa067f4ceffa6cab383f7f420350b
mitcc <mitcc@qq.com>
2015-06-08 23:33:10 +0800

    :basketball:

--- leetcode/algorithms/RectangleArea.java
+++ leetcode/algorithms/RectangleArea.java
@@ -1,18 +1,22 @@
 /*
- * Find the total area covered by two rectilinear rectangles in a 2D plane.
+ * Find the total area covered by two 
+ * rectilinear rectangles in a 2D plane.
 
- * Each rectangle is defined by its bottom left corner and top right corner as 
- * shown in the figure.
+ * Each rectangle is defined by its 
+ * bottom left corner and top right 
+ * corner as shown in the figure.
  * 
- * https://leetcode.com/static/images/problemset/rectangle_area.png
+ * https://leetcode.com/static/images
+ * /problemset/rectangle_area.png
  *
- * Assume that the total area is never beyond the maximum possible value of 
+ * Assume that the total area is never 
+ * beyond the maximum possible value of 
  * int.
  */
 public class RectangleArea {
 
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, 
-            int H) {
+    public int computeArea(int A, int B, int C, int D, 
+            int E, int F, int G, int H) {
 
     }
 
