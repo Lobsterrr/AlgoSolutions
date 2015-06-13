a75bbb01195e3c1f2fcf87d8531e36bf99cacb63
mitcc <mitcc@qq.com>
2015-06-13 21:31:55 +0800

    :basketball:

--- leetcode/algorithms/InvertBinaryTree.java
+++ leetcode/algorithms/InvertBinaryTree.java
@@ -42,13 +42,31 @@ public class InvertBinaryTree {
             if (cur != null) {
                 stack.push(cur);
                 cur = cur.left;
-            } else if (!stack.isEmpty()) {
+            } else {
                 TreeNode node = stack.pop();
                 TreeNode tmp = node.left;
                 node.left = node.right;
                 node.right = tmp;
                 cur = node.left;
             }
+        }
+        return root;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+            if (node.left != null)
+                stack.push(node.left);
+            if (node.right != null)
+                stack.push(node.right);
         }
         return root;
     }
