15651315490329312380992109d974464869b1a0
mitcc <mitcc@qq.com>
2015-06-14 14:46:18 +0800

    :basketball:

--- leetcode/algorithms/ImplementStackUsingQueues.java
+++ leetcode/algorithms/ImplementStackUsingQueues.java
@@ -20,24 +20,47 @@ public class ImplementStackUsingQueues {
 
     class MyStack {
         
+        Queue<Integer> queue1 = new LinkedList<Integer>();
+        Queue<Integer> queue2 = new LinkedList<Integer>();
+        int size = 0;
         // Push element x onto stack.
         public void push(int x) {
-            
+            queue1.add(x);
+            size++;
         }
 
         // Removes the element on top of the stack.
         public void pop() {
-        
+            int count = size;
+            while (count > 1) {
+                queue2.offer(queue1.poll());
+                count--;
+            }
+            queue1 = queue2;
+            queue2 = new LinkedList<Integer>();
+            if (size > 0)
+                size--;
         }
 
         // Get the top element.
         public int top() {
-        
+            if (size <= 0)
+                return Integer.parseInt(null);
+            int count = size;
+            while (count > 1) {
+                queue2.offer(queue1.poll());
+                count--;
+            }
+            int topValue = queue1.poll();
+            queue2.offer(topValue);
+            queue1 = queue2;
+            queue2 = new LinkedList<Integer>();
+            return topValue;
         }
 
         // Return whether the stack is empty.
         public boolean empty() {
-        
+            return size == 0;
         }
     }
 
