a01b8b53f90322eb0d20fa1e2cd98391e0a7d481
mitcc <mitcc@qq.com>
2015-06-15 21:26:47 +0800

    :basketball:

--- leetcode/algorithms/RectangleArea.java
+++ leetcode/algorithms/RectangleArea.java
@@ -21,8 +21,8 @@ public class RectangleArea {
             computeOverlapArea(A, B, C, D, E, F, G, H);
     }
 
-    public int computeOverlapArea(int A, int B, int C, int D,
-        int E, int F, int G, int H) {
+    public int computeOverlapArea(int A, int B, int C, int D, 
+            int E, int F, int G, int H) {
         int a = Math.max(A, E);
         int b = Math.min(C, G);
         int c = Math.max(B, F);
