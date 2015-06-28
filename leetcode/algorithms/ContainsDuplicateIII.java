c5235bddf938698aabff98190441b57aa2c33364
mitcc <mitcc@qq.com>
2015-06-28 19:48:55 +0800

    :basketball:

--- leetcode/algorithms/ContainsDuplicateIII.java
+++ leetcode/algorithms/ContainsDuplicateIII.java
@@ -2,13 +2,16 @@
  * Given an array of integers, find out whether there 
  * are two distinct indices i and j in the array such 
  * that the difference between nums[i] and nums[j] is 
- * at most  * t and the difference between i and j is 
+ * at most t and the difference between i and j is 
  * at most k.
  */
 public class ContainsDuplicateIII {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-       
+        BST bst = new BST();
+        for (int i = 0; i < k; i++) {
+            
+        }
     }
 
 }
@@ -61,9 +64,41 @@ class BST {
             return search(root.left, value);
     }
 
+    // deleteMin
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    public TreeNode deleteMin(TreeNode root) {
+        if (root.left == null) 
+            return root.right;
+        root.left = deleteMin(root.left);
+        return root;
+    }
+
     // delete
     public void delete(int value) {
+        
+    }
 
+    public TreeNode delete(TreeNode root, int value) {
+        if (root == null)
+            return null;
+        if (value < root.value) 
+            root.left = delete(root.left, value);
+        else if (value > root.value)
+            root.right = delete(root.right, value);
+        else {
+            if (root.right == null)
+                return root.left;
+            if (root.left == null)
+                return root.right;
+            TreeNode t = root;
+            root = getMin(t.right);
+            root.right = deleteMin(t.right);
+            root.left = t.left;
+        }
+        return root;
     }
 
 }
