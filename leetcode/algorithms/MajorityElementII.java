b494ae69379bb095c009a47be8e1d385f1e4a2e3
mitcc <mitcc@qq.com>
2015-07-01 00:54:37 +0800

    :basketball:

--- leetcode/algorithms/MajorityElementII.java
+++ leetcode/algorithms/MajorityElementII.java
@@ -6,7 +6,24 @@
 public class MajorityElementII {
 
     public List<Integer> majorityElement(int[] nums) {
-
+        List<Integer> result = new ArrayList<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int value : nums) {
+            int count = 0;
+            if (map.containsKey(value)) 
+                count = map.get(value);
+            map.put(value, count + 1);
+        }
+        int maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (maxCount < entry.getValue())
+                maxCount = entry.getValue();
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (maxCount == entry.getValue())
+                result.add(entry.getKey());
+        }
+        return result;
     }
 
 }
