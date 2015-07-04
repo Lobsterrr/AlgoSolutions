1db46297ed4affb6496b37e638d2b72c635a63c7
mitcc <mitcc@qq.com>
2015-07-05 01:35:58 +0800

    :basketball:

--- leetcode/algorithms/KthSmallestElementInABST.java
+++ leetcode/algorithms/KthSmallestElementInABST.java
@@ -1,11 +1,14 @@
 /*
- * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ * Given a binary search tree, write a function kthSmallest to find 
+ * the kth smallest element in it.
  * 
  * Note: 
  * You may assume k is always valid, 1 ¡Ü k ¡Ü BST's total elements.
  * 
  * Follow up:
- * What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+ * What if the BST is modified (insert/delete operations) often and 
+ * you need to find the kth smallest frequently? How would you 
+ * optimize the kthSmallest routine?
  */
 /**
  * Definition for a binary tree node.
@@ -19,7 +22,8 @@
 public class KthSmallestElementInABST {
 
     public int kthSmallest(TreeNode root, int k) {
-        
+        int leftSize = treeSize(root.left);
+
     }
 
     public int treeSize(TreeNode root) {
