b278b5ef9ce81ac4d51b688e5187727c7915b14f
mitcc <mitcc@qq.com>
2015-07-11 15:14:28 +0800

    :basketball:

--- leetcode/algorithms/LowestCommonAncestorOfABinarySearchTree.java
+++ leetcode/algorithms/LowestCommonAncestorOfABinarySearchTree.java
@@ -1,7 +1,11 @@
 /*
- * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) 
+ * of two given nodes in the BST.
 
- * According to the definition of LCA on Wikipedia: ¡°The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).¡±
+ * According to the definition of LCA on Wikipedia: ¡°The lowest common 
+ * ancestor is defined between two nodes v and w as the lowest node in T 
+ * that has both v and w as descendants (where we allow a node to be a 
+ * descendant of itself).¡±
  * 
  *         _______6______
  *        /              \
@@ -10,7 +14,9 @@
  *    0      _4       7       9
  *          /  \
  *          3   5
- * For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ * For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. 
+ * Another example is LCA of nodes 2 and 4 is 2, since a node can be a 
+ * descendant of itself according to the LCA definition.
  */
 /**
  * Definition for a binary tree node.
@@ -23,9 +29,11 @@
  */
 public class LowestCommonAncestorOfABinarySearchTree {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, 
+            TreeNode q) {
         TreeNode cur = root;
-        while (cur != null && (p.val > cur.val && q.val > cur.val || p.val < cur.val && q.val < cur.val)) {
+        while (cur != null && (p.val > cur.val && q.val > cur.val || 
+                    p.val < cur.val && q.val < cur.val)) {
             cur = p.val < cur.val && q.val < cur.val ? cur.left : cur.right;
         }
         return cur;
