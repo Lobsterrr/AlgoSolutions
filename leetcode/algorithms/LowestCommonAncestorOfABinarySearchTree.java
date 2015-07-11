31ce98ddccd56d3d30ac514c3bc78a81295dab1a
mitcc <mitcc@qq.com>
2015-07-11 15:23:43 +0800

    :basketball:

--- leetcode/algorithms/LowestCommonAncestorOfABinarySearchTree.java
+++ leetcode/algorithms/LowestCommonAncestorOfABinarySearchTree.java
@@ -31,8 +31,8 @@ public class LowestCommonAncestorOfABinarySearchTree {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, 
             TreeNode q) {
-        while (p.val > root.val && q.val > root.val || 
-                p.val < root.val && q.val < root.val) {
+        while (p.val < root.val && q.val < root.val || 
+                p.val > root.val && q.val > root.val) {
             root = p.val < root.val && q.val < root.val ? 
                 root.left : root.right;
         }
