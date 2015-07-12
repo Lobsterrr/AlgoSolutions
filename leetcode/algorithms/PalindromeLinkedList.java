e81d67e544f06d7be14f99f333b9fa56e1dad76d
mitcc <mitcc@qq.com>
2015-07-12 16:12:26 +0800

    :basketball:

--- leetcode/algorithms/PalindromeLinkedList.java
+++ leetcode/algorithms/PalindromeLinkedList.java
@@ -15,7 +15,17 @@
 public class PalindromeLinkedList {
 
     public boolean isPalindrome(ListNode head) {
-
+        List<Integer> list = new ArrayList<Integer>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int size = list.size();
+        for (int i = 0; i < size / 2; i++) {
+            if (list.get(i) != list.get(size - 1 - i))
+                return false;
+        }
+        return true;
     }
 
 }
