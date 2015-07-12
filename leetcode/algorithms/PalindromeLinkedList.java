07179a28006f1b111bfd04a26bc9b9a58e599bbd
mitcc <mitcc@qq.com>
2015-07-12 20:11:03 +0800

    :basketball:

--- leetcode/algorithms/PalindromeLinkedList.java
+++ leetcode/algorithms/PalindromeLinkedList.java
@@ -14,6 +14,49 @@
  */
 public class PalindromeLinkedList {
 
+    // O(n) time, O(1) space.
+    public boolean isPalindrome(ListNode head) {
+        ListNode second = reverse(secondHalf(head));
+        while (second != null) {
+            if (head.val != second.val)
+                return false;
+            head = head.next;
+            second = second.next;
+        }
+        return second == null;
+    }
+
+    public ListNode secondHalf(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur1 = dummy;
+        ListNode cur2 = dummy;
+        while (cur2 != null && cur2.next != null) {
+            cur1 = cur1.next;
+            cur2 = cur2.next.next;
+        }
+        return cur1.next;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode prev = head;
+        ListNode cur = prev.next;
+        while (cur != null) {
+            prev.next = cur.next;
+            cur.next = head;
+            head = cur;
+            cur = prev.next;
+        }
+        return head;
+    }
+
+/*******************************************************************/
+
+    // O(n) time, O(n) space.
     public boolean isPalindrome(ListNode head) {
         List<Integer> list = new ArrayList<Integer>();
         while (head != null) {
@@ -21,7 +64,8 @@ public class PalindromeLinkedList {
             head = head.next;
         }
         for (int i = 0; i < list.size() / 2; i++) {
-            if (!list.get(i).equals(list.get(list.size() - 1 - i)))
+            // Java boxing and unboxing!
+            if (!list.get(i).equals(list.get(list.size() - 1 - i))) 
                 return false;
         }
         return true;
