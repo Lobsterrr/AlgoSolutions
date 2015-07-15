8ac86d9ee0943316ba922f62e075ce5799c885cc
mitcc <mitcc@qq.com>
2015-07-15 20:12:54 +0800

    :basketball:

--- leetcode/algorithms/DeleteNodeInALinkedList.java
+++ leetcode/algorithms/DeleteNodeInALinkedList.java
@@ -6,10 +6,19 @@
  * are given the third node with value 3, the linked list 
  * should become 1 -> 2 -> 4 after calling your function.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public DeleteNodeInALinkedList {
 
     public void deleteNode(ListNode node) {
-        
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
 }
