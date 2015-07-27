cc5ff9aadaa59ee27915d1036cd1b2a79ad435a2
mitcc <mitcc@qq.com>
2015-07-27 23:26:17 +0800

    :basketball:

--- leetcode/algorithms/KthSmallestElementInABST.java
+++ leetcode/algorithms/KthSmallestElementInABST.java
@@ -3,7 +3,7 @@
  * the kth smallest element in it.
  * 
  * Note: 
- * You may assume k is always valid, 1 ¡Ü k ¡Ü BST's total elements.
+ * You may assume k is always valid, 1 <= k <= BST's total elements.
  * 
  * Follow up:
  * What if the BST is modified (insert/delete operations) often and 
