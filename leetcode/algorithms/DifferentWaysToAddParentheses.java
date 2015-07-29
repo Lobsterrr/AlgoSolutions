de58c4050c0da07eb8d7310e015d1a0a758d0f37
mitcc <mitcc@qq.com>
2015-07-29 22:26:21 +0800

    :basketball:

--- leetcode/algorithms/DifferentWaysToAddParentheses.java
+++ leetcode/algorithms/DifferentWaysToAddParentheses.java
@@ -23,12 +23,6 @@
  * (((2*3)-4)*5) = 10
  * Output: [-34, -14, -10, -10, 10]
  */
-
-f(1) = 1
-f(2) = 1
-f(3) = 2
-f(4) = f(1) * f(3) + f(2) * f(2) + f(3) * f(1) = 2 + 1 + 2 = 5
-
 public class DifferentWaysToAddParentheses {
 
     public List<Integer> diffWaysToCompute(String input) {
