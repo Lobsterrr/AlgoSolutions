ac10388513cb47eeff93ae7e57dd968f248e41e3
mitcc <mitcc@qq.com>
2015-08-01 17:05:49 +0800

    :basketball:

--- leetcode/algorithms/BasicCalculator.java
+++ leetcode/algorithms/BasicCalculator.java
@@ -13,6 +13,31 @@
  */
 public class BasicCalculator {
 
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(1);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    num = 10 * num + s.charAt(i) - '0';
+                    i++;
+                }
+                i--;
+                result += stack.pop() * num;
+            } else if (c == '(' || c == '+') {
+                stack.push(stack.peek());
+            } else if (c == '-') {
+                stack.push(-1 * stack.peek());
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+        return result;
+    }
 
 /*****************************************************************************/
 
