7b92227f359b6ef74913e3df219dc06df20c31e0
mitcc <mitcc@qq.com>
2015-08-02 15:37:53 +0800

    :basketball:

--- leetcode/algorithms/ValidAnagram.java
+++ leetcode/algorithms/ValidAnagram.java
@@ -12,7 +12,11 @@
 public class ValidAnagram {
     
     public boolean isAnagram(String s, String t) {
-        
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+        s = 
     }
 
 }
