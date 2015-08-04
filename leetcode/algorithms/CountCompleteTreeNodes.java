c3b10fd240048696780b4cd93d1f83e0ddd7771f
mitcc <mitcc@qq.com>
2015-08-04 23:29:36 +0800

    :basketball:

--- leetcode/algorithms/CountCompleteTreeNodes.java
+++ leetcode/algorithms/CountCompleteTreeNodes.java
@@ -64,7 +64,6 @@ public class CountCompleteTreeNodes {
         return height;
     }   
 
-
 /*******************************************************************/
 
     public int countNodes(TreeNode root) {
