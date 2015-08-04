e5b154ea4728186e5148261caa5aa9e523ebb1e3
mitcc <mitcc@qq.com>
2015-08-04 20:03:53 +0800

    :basketball:

--- leetcode/algorithms/CountCompleteTreeNodes.java
+++ leetcode/algorithms/CountCompleteTreeNodes.java
@@ -23,6 +23,7 @@ public class CountCompleteTreeNodes {
             return 0;
         else if (root.left == null && root.right == null)
             return 1;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
 }
