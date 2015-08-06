adea12dc001ed6a96d3a0dde936108390ee414cd
mitcc <mitcc@qq.com>
2015-08-06 23:43:54 +0800

    :basketball:

--- leetcode/algorithms/ContainsDuplicateIII.java
+++ leetcode/algorithms/ContainsDuplicateIII.java
@@ -12,28 +12,12 @@ public class ContainsDuplicateIII {
         for (int i = 0; i < nums.length; ++i) {
             int floor = nums[i] <= Integer.MIN_VALUE + t ? 
                 Integer.MIN_VALUE : nums[i] - t;
-            int ceil = nums[i] >= Integer.MAX_VALUE - t ? 
-                Integer.MAX_VALUE : nums[i] + t + 1;
-            if (t >= 0 && set.subSet(floor, ceil).size() > 0)
-                return true;
-            set.add(nums[i]);
-            if (i >= k) 
-                set.remove(nums[i - k]);
-        }
-        return false;
-    }
-
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        for (int i = 0; i < nums.length; ++i) {
-            Integer floor = nums[i] < Integer.MIN_VALUE + t ? 
-                Integer.MIN_VALUE : nums[i] - t;
-            Integer ceiling = nums[i] >= Integer.MAX_VALUE - t - 1 ? 
+            int ceiling = nums[i] >= Integer.MAX_VALUE - t ? 
                 Integer.MAX_VALUE : nums[i] + t + 1;
             if (t >= 0 && set.subSet(floor, ceiling).size() > 0)
                 return true;
             set.add(nums[i]);
-            if (i >= k)
+            if (i >= k) 
                 set.remove(nums[i - k]);
         }
         return false;
