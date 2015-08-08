41233d02518183b8104c9116d38c4beee5be275b
mitcc <mitcc@qq.com>
2015-08-08 23:57:07 +0800

    :basketball:

--- leetcode/algorithms/WordSearchII.java
+++ leetcode/algorithms/WordSearchII.java
@@ -59,6 +59,7 @@ public class WordSearchII {
 /*****************************************************************************/
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<String>();
+        Trie trie = new Trie();
 
     }
 
