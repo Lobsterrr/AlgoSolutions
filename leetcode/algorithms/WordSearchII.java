e96b93830532b74faaa55453bcc68c342184b3d0
mitcc <mitcc@qq.com>
2015-08-08 23:53:33 +0800

    :basketball:

--- leetcode/algorithms/WordSearchII.java
+++ leetcode/algorithms/WordSearchII.java
@@ -56,12 +56,38 @@ public class WordSearchII {
         }
         return false;
     }
+/*****************************************************************************/
+    public List<String> findWords(char[][] board, String[] words) {
+
+    }
 
 }
 
 class TrieNode {
 
     public TrieNode() {
+
+    }
+
+}
+
+class Trie {
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
+
+    public boolean startsWith(String prefix) {
 
     }
 
