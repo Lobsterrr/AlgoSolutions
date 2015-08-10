4264a6523d656830815a4e928016d54099c1fb2b
mitcc <mitcc@qq.com>
2015-08-10 22:14:35 +0800

    :basketball:

--- leetcode/algorithms/ImplementTrie.java
+++ leetcode/algorithms/ImplementTrie.java
@@ -4,7 +4,32 @@
  * Note:
  * You may assume that all inputs are consist of lowercase letters a-z.
  */
+class TrieNode {
+    
+    public TrieNode() {
+
+    }
+
+}
+
 public class ImplementTrie {
 
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
+
+    public boolean startsWith(String prefix) {
+
+    }
 
 }
