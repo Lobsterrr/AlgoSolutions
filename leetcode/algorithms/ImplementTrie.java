dcc806ae46c8ec4f9ad87c389cfa07ebfc727722
mitcc <mitcc@qq.com>
2015-08-10 22:00:14 +0800

    :basketball:

--- leetcode/algorithms/ImplementTrie.java
+++ leetcode/algorithms/ImplementTrie.java
@@ -1,5 +1,8 @@
 /*
- *
+ * Implement a trie with insert, search, and startsWith methods.
+
+ * Note:
+ * You may assume that all inputs are consist of lowercase letters a-z.
  */
 public class ImplementTrie {
 
