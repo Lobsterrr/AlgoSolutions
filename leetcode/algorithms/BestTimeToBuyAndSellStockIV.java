6837106cee7009fa6fbe2d24555b31c7c60d96c8
mitcc <mitcc@qq.com>
2015-08-11 22:39:14 +0800

    :basketball:

--- leetcode/algorithms/BestTimeToBuyAndSellStockIV.java
+++ leetcode/algorithms/BestTimeToBuyAndSellStockIV.java
@@ -32,4 +32,25 @@ public class BestTimeToBuyAndSellStockIV {
         return dp[k][prices.length - 1];
     }
 
+/*****************************************************************************/
+
+    public int maxProfit(int k, int[] prices) {
+        if (k >= prices.length / 2) {
+            int result = 0;
+            for (int i = 1; i < prices.length; ++i) {
+                result += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return result;
+        }   
+        int[] global = new int[k + 1];
+        int[] local = new int[k + 1];
+        for (int i = 1; i < prices.length; ++i) {
+            int delta = prices[i] - prices[i - 1];
+            for (int j = k; j >= 1; --j) {
+                local[j] = Math.max(global[j - 1] + Math.max(0, delta), local[j] + delta);
+                global[j] = Math.max(global[j], local[j]);
+            }
+        }
+        return global[k];
+    }       
 }
