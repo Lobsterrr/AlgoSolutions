518d8a4ee62abe85d24b605a21296c4532488171
mitcc <mitcc@qq.com>
2015-08-12 01:33:17 +0800

    :basketball:

--- basic/MergeSort.java
+++ basic/MergeSort.java
@@ -1,7 +1,21 @@
 public class MergeSort {
 
     public void mergeSort(int[] array, int low, int high) {
-        
+        if (low >= high - 1)
+            return;
+        int mid = low + (high - low) / 2;
+        mergeSort(array, low, mid);
+        mergeSort(array, mid + 1, high);
+        int[] firstHalf = Arrays.copyOfRange(array, low, mid + 1);
+        int i = low; 
+        int j = mid + 1;
+        int k = low;
+        while (i <= mid && j <= high) {
+            array[k++] = array[j] < firstHalf[i] ? array[j++] : firstHalf[i++];
+        }
+        while (i <= mid) {
+            array[k++] = firstHalf[i++];
+        }
     }
 
 }
