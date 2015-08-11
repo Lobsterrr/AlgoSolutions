09e497f68cdd6dbd1f1bd7272ceae2b3dd1106e9
mitcc <mitcc@qq.com>
2015-08-11 23:59:42 +0800

    :basketball:

--- basic/SelectionSort.java
+++ basic/SelectionSort.java
@@ -2,7 +2,10 @@ public SelectionSort {
 
     public void selectionSort(int[] array, int low, int high) {
         for (int i = low; i <= high; ++i) {
-            
+            int k = i;
+            for (int j = i + 1; j <= high; ++j) {
+
+            }
         }
     }
 
