330a25cbe5589772c08146534b559575f89c19f3
mitcc <mitcc@qq.com>
2015-08-16 21:02:54 +0800

    :basketball:

--- leetcode/algorithms/AddDigits.java
+++ leetcode/algorithms/AddDigits.java
@@ -4,7 +4,15 @@
 public class AddDigits {
 
     public int addDigits(int num) {
-        
+        int digitSum = 0;
+        while (num != 0 || digitSum >= 10) {
+            if (num == 0) {
+                num = digitSum;
+            }
+            digitSum += num % 10;
+            num /= 10;
+        }
+        return num;
     }
 
 }
