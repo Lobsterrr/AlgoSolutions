112e6988e97b2e29faf17df40dc5dd47b6b34cdc
mitcc <mitcc@qq.com>
2015-08-17 21:14:41 +0800

    :basketball:

--- leetcode/algorithms/SingleNumberIII.java
+++ leetcode/algorithms/SingleNumberIII.java
@@ -18,7 +18,10 @@
 public class SingleNumberIII {
 
     public int[] singleNumber(int[] nums) {
-
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; ++i) {
+            
+        }
     }
 
 }
