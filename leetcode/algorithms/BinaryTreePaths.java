cc250043fe716de5362b5f54043d1df37ce150be
mitcc <mitcc@qq.com>
2015-08-18 22:22:19 +0800

    :basketball:

--- leetcode/algorithms/BinaryTreePaths.java
+++ leetcode/algorithms/BinaryTreePaths.java
@@ -24,6 +24,14 @@
 public class BinaryTreePaths {
 
     public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        if (root == null)
+            return result;
+        if (root.left == null && root.right == null) {
+            result.add(root.val + "");
+            return result;
+        }
+        result.add(root.val + "->");
 
     }
 
