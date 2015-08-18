57e13c68bcc1ea00113fc192bd1b4c8e632d743e
mitcc <mitcc@qq.com>
2015-08-18 22:04:49 +0800

    :basketball:

---
+++ leetcode/algorithms/BinaryTreePaths.java
@@ -0,0 +1,19 @@
+/*
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+    }
+
+}
