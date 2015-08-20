a3751f8b610d1629211d0de774d96b4beec4ebc4
mitcc <mitcc@qq.com>
2015-08-20 22:06:21 +0800

    :basketball:

--- leetcode/algorithms/UglyNumberII.java
+++ leetcode/algorithms/UglyNumberII.java
@@ -9,16 +9,15 @@
 
  * Hint:
 
- * The naive approach is to call isUgly for every number 
+ * 1. The naive approach is to call isUgly for every number 
  * until you reach the nth one. Most numbers are not ugly. 
- * Try to focus your effort on generating only the ugly 
- * ones.
- * An ugly number must be multiplied by either 2, 3, or 5 
+ * Try to focus your effort on generating only the ugly ones.
+ * 2. An ugly number must be multiplied by either 2, 3, or 5 
  * from a smaller ugly number.
- * The key is how to maintain the order of the ugly numbers. 
- * Try a similar approach of merging from three sorted lists: 
- * L1, L2, and L3.
- * Assume you have Uk, the kth ugly number. Then Uk+1 must 
+ * 3. The key is how to maintain the order of the ugly 
+ * numbers. Try a similar approach of merging from three 
+ * sorted lists: L1, L2, and L3.
+ * 4. Assume you have Uk, the kth ugly number. Then Uk+1 must 
  * be Min(L1 * 2, L2 * 3, L3 * 5).
  */
 public class UglyNumberII {
