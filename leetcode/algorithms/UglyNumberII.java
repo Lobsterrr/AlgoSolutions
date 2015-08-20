3f59f2087bd6a09fa555635e25cf882e48f7ed10
mitcc <mitcc@qq.com>
2015-08-20 23:35:13 +0800

    :basketball:

--- leetcode/algorithms/UglyNumberII.java
+++ leetcode/algorithms/UglyNumberII.java
@@ -31,10 +31,24 @@ public class UglyNumberII {
         int next2 = result[0];
         int next3 = result[0];
         int next5 = result[0];
-        int next = result[0];
+        int next;
         for (int i = 1; i < n; ++i) {
-
+            next = Math.min(next2, Math.min(next3, next5));
+            result[i] = next;
+            if (next == next2) {
+                i2++;
+                next2 = result[i2] * 2;
+            }
+            if (next == next3) {
+                i3++;
+                next3 = result[i3] * 3;
+            }
+            if (next == next5) {
+                i5++;
+                next5 = result[i5] * 5;
+            }
         }
+        return next;
     }
 
 }
