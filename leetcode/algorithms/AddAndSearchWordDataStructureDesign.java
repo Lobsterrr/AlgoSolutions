7af06a63e373bb57e64f55b54db577fc78a41430
mitcc <mitcc@qq.com>
2015-08-24 00:52:39 +0800

    :basketball:

--- leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
+++ leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
@@ -1,5 +1,21 @@
-/**
- *
+/*
+ * Design a data structure that supports the following two operations:
+
+ * void addWord(word)
+ * bool search(word)
+ * search(word) can search a literal word or a regular expression 
+ * string containing only letters a-z or .. A . means it can represent 
+ * any one letter.
+ * 
+ * For example:
+ * 
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
  */
 public class AddAndSearchWordDataStructureDesign {
 
@@ -10,7 +26,7 @@ class WordDictionary {
     TrieNode root = new TrieNode();
 
     public void addWord(String word) {
-
+        
     }
 
     public boolean search(String word) {
