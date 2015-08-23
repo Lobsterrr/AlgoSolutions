8ad28af40514e02c2a36cc0c6fc153d2a00b1d8d
mitcc <mitcc@qq.com>
2015-08-23 21:19:45 +0800

    :basketball:

--- leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
+++ leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
@@ -7,6 +7,15 @@ public class AddAndSearchWordDataStructureDesign {
 
 class WordDictionary {
 
+    TrieNode root = new TrieNode();
+
+    public void addWord(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
 
 }
 
