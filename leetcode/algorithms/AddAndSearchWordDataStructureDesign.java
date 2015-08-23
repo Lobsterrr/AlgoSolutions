7974516d4e81881eeb0cbe5a38d691b5541db10c
mitcc <mitcc@qq.com>
2015-08-24 03:30:33 +0800

    :basketball:

--- leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
+++ leetcode/algorithms/AddAndSearchWordDataStructureDesign.java
@@ -29,11 +29,29 @@ class WordDictionary {
     TrieNode root = new TrieNode();
 
     public void addWord(String word) {
-        
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null)
+                cur.children[c - 'a'] = new char[26];
+            cur = cur.children[c - 'a'];
+        }
+        cur.isLeaf = true;
     }
 
     public boolean search(String word) {
 
+    }
+
+    public boolean dfs(TrieNode root, String word) {
+        if (word.length() == 1 && root.children[word.charAt(0) - 'a'] != null && root.isLeaf)
+            return true;
+        if (word.char(0) == '.') {
+            
+        } else {
+            if (root.chidren[word.charAt(0) - 'a'] == null)
+                return false;
+            dfs(root.children[word.charAt(0) - 'a'], word.substring(1));
+        }
     }
 
 }
