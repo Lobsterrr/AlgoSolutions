31b270bd29a4fad883b77405d7a15d141c068cbe
mitcc <mitcc@qq.com>
2015-08-27 00:54:10 +0800

    :basketball:

--- leetcode/algorithms/WordSearchII.java
+++ leetcode/algorithms/WordSearchII.java
@@ -101,15 +101,20 @@ class Trie {
     public boolean search(String word) {
         TrieNode cur = root;
         for (char c : word.toCharArray()) {
-            if (cur.children[c - 'a'] == null)
-                return false;
-            cur = cur.children[c - 'a'];
+            if (cur.children[c - 'a'] != null)
+                cur = cur.children[c - 'a'];
         }
         return cur.isLeaf;
     }
 
     public boolean startsWith(String prefix) {
-
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (cur.children[c - 'a'] == null)
+                return false;
+            cur = cur.children[c - 'a'];
+        }
+        return true;
     }
 
 }
