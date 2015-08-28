d50a9d42002779a8b1879e2b31dc1d699c89e7b3
mitcc <mitcc@qq.com>
2015-08-28 09:11:50 +0800

    :basketball:

--- leetcode/algorithms/CloneGraph.java
+++ leetcode/algorithms/CloneGraph.java
@@ -1,6 +1,15 @@
 /*
  *
  */
+
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
 public class CloneGraph {
 
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
