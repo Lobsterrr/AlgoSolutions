911fbc6e0a488e4a6237fef536bc8e5a1c393197
mitcc <mitcc@qq.com>
2015-08-30 17:35:49 +0800

    :basketball:

--- leetcode/algorithms/MergeSortedArray.java
+++ leetcode/algorithms/MergeSortedArray.java
@@ -26,26 +26,10 @@ public class MergeSortedArray {
     public void merge(int nums1[], int m, int nums2[], int n) {
         int i = m - 1;
         int j = n - 1;
-        for (int k = m + n - 1; k >= 0; k--) 
+        for (int k = m + n - 1; k >= 0; k--) { 
             nums1[k] = (j < 0 || i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
