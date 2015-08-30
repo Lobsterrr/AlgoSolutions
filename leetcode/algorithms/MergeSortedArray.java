12b9de0a227a67adb031cb7ff18f1ea9aa213f9a
mitcc <mitcc@qq.com>
2015-08-30 17:45:02 +0800

    :basketball:

--- leetcode/algorithms/MergeSortedArray.java
+++ leetcode/algorithms/MergeSortedArray.java
@@ -1,9 +1,13 @@
 /*
- * Given two sorted integer arrays A and B, merge B into A as one sorted array.
+ * Given two sorted integer arrays nums1 and nums2, 
+ * merge nums2 into nums1 as one sorted array.
 
  * Note:
- * You may assume that A has enough space to hold additional elements from B. 
- * The number of elements initialized in A and B are m and n respectively.
+ * You may assume that nums1 has enough space (size 
+ * that is greater or equal to m + n) to hold 
+ * additional elements from nums2. The number of 
+ * elements initialized in nums1 and nums2 are m and 
+ * n respectively.
  */
 
 public class MergeSortedArray {
@@ -27,17 +31,8 @@ public class MergeSortedArray {
         int i = m - 1;
         int j = n - 1;
         for (int k = m + n - 1; k >= 0; k--) { 
-            nums1[k] = (j < 0 || i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
-        }
-    }
-
-
-
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i1 = m - 1;
-        int i2 = n - 1;
-        for (int i = i1 + i2 - 1; i >= 0; --i) {
-            nums1[i] = (i2 < 0 || i1 >= 0 && nums1[i1] > nums2[i2]) ? nums1[i1--] : nums2[i2--];
+            nums1[k] = (j < 0 || i >= 0 && nums1[i] > nums2[j]) ? 
+                nums1[i--] : nums2[j--];
         }
     }
 
