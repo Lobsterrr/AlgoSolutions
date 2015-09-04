cd80583746814cacc1761969ca9e9da4740c9275
mitcc <mitcc@qq.com>
2015-09-04 23:05:40 +0800

    :basketball:

--- leetcode/algorithms/HIndex.java
+++ leetcode/algorithms/HIndex.java
@@ -7,9 +7,16 @@ public class HIndex {
         int[] count = new int[citations.length];
         for (int i = 0; i < citations.length; ++i) {
             for (int j = 0; j < citations.length; ++j) {
-                
+                if (citations[j] >= citations[i])
+                    count[i]++;
             }
         }
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < citations.length; ++i) {
+            if (count[i] == citations[i] && count[i] >= result)
+                result = count[i];
+        }
+        return result;
     }
 
 }
