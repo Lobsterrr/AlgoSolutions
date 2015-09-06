d0fc06b3002875acb37adc15e4a1badd62a50cdd
mitcc <mitcc@qq.com>
2015-09-06 20:30:25 +0800

    :basketball:

--- leetcode/algorithms/HIndex.java
+++ leetcode/algorithms/HIndex.java
@@ -44,7 +44,8 @@ public class HIndex {
         Arrays.sort(citations);
         int result = 0;
         for (int i = 0; i < citations.length; ++i) {
-            result = Math.max(result, Math.min(citations[i], citations.length - i));
+            result = Math.max(result, 
+                    Math.min(citations[i], citations.length - i));
         }
         return result;
     }
