8a1b334c4c4e87d3f2bc4ea9cd3101dae914f517
mitcc <mitcc@qq.com>
2015-09-08 01:14:53 +0800

    :basketball:

--- lintcode/Triangle.java
+++ lintcode/Triangle.java
@@ -9,11 +9,8 @@ public class Triangle {
         for (int i = xLen - 1; i >= 0; --i) {
             int yLen = triangle.get(i).size();
             for (int j = 0; j < yLen; ++j) {
-                if (i == xLen - 1)
-                    dp[j] = triangle.get(i).get(j);
-                else 
-                    dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
-
+                dp[j] = triangle.get(i).get(j) + 
+                    (i == xLen - 1 ? 0 : Math.min(dp[j], dp[j + 1]));
             }
         }
         return dp[0];
