26e71f0e5da63dcb5d12379ca84ad2f5608aae0a
mitcc <mitcc@qq.com>
2015-09-08 01:11:33 +0800

    :basketball:

--- lintcode/Triangle.java
+++ lintcode/Triangle.java
@@ -4,14 +4,19 @@
 public class Triangle {
 
     public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        int xSize = triangle.size();
-        for (int i = size - 2; i >= 0; --i) {
-            for (int j = 0; j <= i; ++j) {
-                triangle.get(i).get(j) += Math.min(triangle.get(i + 1).get(j), 
-                        triangle.get(i + 1).get(j + 1));
+        int xLen = triangle.size();
+        int[] dp = new int[xLen];
+        for (int i = xLen - 1; i >= 0; --i) {
+            int yLen = triangle.get(i).size();
+            for (int j = 0; j < yLen; ++j) {
+                if (i == xLen - 1)
+                    dp[j] = triangle.get(i).get(j);
+                else 
+                    dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+
             }
         }
-        return triangle.get(0).get(0);
+        return dp[0];
     }
 
 }
