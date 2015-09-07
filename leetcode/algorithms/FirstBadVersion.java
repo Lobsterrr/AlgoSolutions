3448b63c5f169562c943b1783f892750d18d6d7d
mitcc <mitcc@qq.com>
2015-09-07 23:53:53 +0800

    :basketball:

--- leetcode/algorithms/FirstBadVersion.java
+++ leetcode/algorithms/FirstBadVersion.java
@@ -19,13 +19,13 @@ public class FirstBadVersion {
         int left = 1;
         int right = n;
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (isBadVersion(mid))
                 right = mid - 1;
             else 
                 left = mid + 1;
         }
-        return (left + right) / 2;
+        return left + (right - left) / 2;
     }
 
 }
