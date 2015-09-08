479b9ae6f95f2447f938fac6889abc13c5b9961e
mitcc <mitcc@qq.com>
2015-09-09 00:59:16 +0800

    :basketball:

--- leetcode/algorithms/WordBreakII.java
+++ leetcode/algorithms/WordBreakII.java
@@ -18,6 +18,16 @@ public class WordBreakII {
         
     }
 
-    public 
+    public void dfs(List<String> result, String s, Set<String> wordDict) {
+        if (s.length() == 0)
+            return;
+        for (int i = 1; i <= s.length(); ++i) {
+            if (wordDict.contains(s.substring(0, i))) {
+                result.add(s.substring(0, i));
+                dfs(result, s.substring(i), wordDict);
+                result.remove(result.size() - 1);
+            }
+        }
+    }
 
 }
