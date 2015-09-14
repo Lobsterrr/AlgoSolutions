0f4e19ab5c77b71ba8593d23166a9733eb15fe8d
mitcc <mitcc@qq.com>
2015-09-15 00:08:06 +0800

    :basketball:

--- basic/Library.java
+++ basic/Library.java
@@ -22,4 +22,17 @@ public class Library {
         return result;
     }
 
+    // add two integer with bit manipulation
+    public int add(int a, int b) {
+        int sum;
+        int carry;
+        do {
+            sum = a ^ b;
+            carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        } while (carry != 0);
+        return sum;
+    }
+
 }
