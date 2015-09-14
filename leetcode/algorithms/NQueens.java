53f64b19b6c1518472b4624e735ed9251dd55878
mitcc <mitcc@qq.com>
2015-09-15 01:53:32 +0800

    :basketball:

--- leetcode/algorithms/NQueens.java
+++ leetcode/algorithms/NQueens.java
@@ -61,4 +61,41 @@ public class NQueens {
                 return false;
         return true;
     }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        int[] board = new int[n];
+        dfs(result, board, 0);
+        return result;
+    }
+
+    public void dfs(List<List<String>> result, int[] board, int row) {
+        if (row == board.length) {
+            String[] lines = new String[board.length];
+            for (int i = 0; i < board.length; ++i) {
+                lines[i] = "";
+                for (int j = 0; j < board.length; ++j) {
+                    lines[i] += board[i] != j ? "." : "Q";
+                }
+            }
+            result.add(Arrays.asList(lines));
+            return;
+        }
+        for (int col = 0; col < board.length; ++col) {
+            if (canPlace(board, row, col)) {
+                board[row] = col;
+                dfs(result, board, row + 1);
+                board[row] = 0;
+            }
+        }
+    }
+
+    public boolean canPlace(int[] board, int row, int col) {
+        for (int i = 0; i < row; ++i) {
+            if (board[i] == col || Math.abs(row - i) == Math.abs(board[i] - col))
+                return false;
+        }
+        return true;
+    }
+
 }
