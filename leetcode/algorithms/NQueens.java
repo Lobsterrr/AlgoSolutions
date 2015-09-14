779750b52d3cc85b5aa9183d5a4df44ed2e62639
mitcc <mitcc@qq.com>
2015-09-15 01:55:24 +0800

    :basketball:

--- leetcode/algorithms/NQueens.java
+++ leetcode/algorithms/NQueens.java
@@ -27,40 +27,6 @@
  * 
  */
 public class NQueens {
-    public ArrayList<String[]> solveNQueens(int n) {
-        ArrayList<String[]> res = new ArrayList<String[]>();
-        int[] board = new int[n];
-        placeQueenOnBoard(res, 0, board);
-        return res;
-    }
-
-    public void placeQueenOnBoard(ArrayList<String[]> res, int row, int[] board) {
-        int n = board.length;
-        if(row == n) {
-            String[] s = new String[n];
-            for(int i = 0; i < n; i++) {
-                s[i] = "";
-                for(int j = 0; j < n; j++) 
-                    s[i] += (j != board[i]) ? "." : "Q";
-            }
-            res.add(s);
-        } else {
-            for(int col = 0; col < n; col++) {
-                if(isSafePlace(row, col, board)) {
-                    board[row] = col;
-                    placeQueenOnBoard(res, row + 1, board);
-                    board[row] = 0;
-                }
-            }
-        }
-    }
-
-    public boolean isSafePlace(int row, int col, int[] board) {
-        for(int i = 0; i < row; i++)
-            if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row))
-                return false;
-        return true;
-    }
 
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<List<String>>();
@@ -92,7 +58,8 @@ public class NQueens {
 
     public boolean canPlace(int[] board, int row, int col) {
         for (int i = 0; i < row; ++i) {
-            if (board[i] == col || Math.abs(row - i) == Math.abs(board[i] - col))
+            if (board[i] == col || Math.abs(row - i) == 
+                    Math.abs(board[i] - col))
                 return false;
         }
         return true;
