abd93cb30e34bca4be222ca628976d0619d39a4f
mitcc <mitcc@qq.com>
2015-09-16 00:06:49 +0800

    :basketball:

--- leetcode/algorithms/SudokuSolver.java
+++ leetcode/algorithms/SudokuSolver.java
@@ -11,4 +11,18 @@ public class SudokuSolver {
 
     }
 
+    public void dfs(char[][] board, int row) {
+        if (row == board.length)
+            return;
+        for (int i = 0; i < board.length; ++i) {
+            if (board[row][i] == '.') {
+                
+            }
+        }
+    }
+
+    public boolean canPlace(int i) {
+
+    }
+
 }
