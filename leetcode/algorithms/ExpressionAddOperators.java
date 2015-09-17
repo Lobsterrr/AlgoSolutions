60d7cb32e4b38850a8c9b27ca3253af9f3179766
mitcc <mitcc@qq.com>
2015-09-18 01:36:46 +0800

    :basketball:

--- leetcode/algorithms/ExpressionAddOperators.java
+++ leetcode/algorithms/ExpressionAddOperators.java
@@ -13,27 +13,11 @@ public class ExpressionAddOperators {
 
     public List<String> addOperators(String num, int target) {
         List<String> result = new ArrayList<String>();
-        dfs(result, "", num, target);
-        return result;
-    }
+        //TODO
 
-    public void dfs(List<String> result, String sub, String num, int target) {
-        for (int i = 1; i <= Math.min(10, num.length()); ++i) {
-            if (Long.parseLong(num.substring(0, i)) > Integer.MAX_VALUE)
-                break;
-            int a = Integer.parseInt(num.substring(0, i));
-            if (i == num.length()) {
-                if (a == target) {
-                    result.add(sub + num.substring(0, i));
-                    return;
-                }
-            } else {
-                dfs(result, sub + a + "+", num.substring(i), target - a);
-                dfs(result, sub + a + "-", num.substring(i), a - target);
-                if (a != 0)
-                    dfs(result, sub + a + "*", num.substring(i), target / a);
-            }
-        }
+
+
+        return result;
     }
 
 }
