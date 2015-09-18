67a997e04ce62e576d6e827cc84bcd51d5436ad2
mitcc <mitcc@qq.com>
2015-09-19 00:23:38 +0800

    :basketball:

---
+++ lintcode/LongestWords.java
@@ -0,0 +1,42 @@
+/*
+ *Given a dictionary, find all of the longest words in the dictionary.
+
+Example
+Given
+
+{
+  "dog",
+  "google",
+  "facebook",
+  "internationalization",
+  "blabla"
+}
+the longest words are(is) ["internationalization"].
+
+Given
+
+{
+  "like",
+  "love",
+  "hate",
+  "yes"
+}
+the longest words are ["like", "love", "hate"].
+
+Challenge
+It's easy to solve it in two passes, can you do it in one pass?
+ */
+public class LongestWords {
+
+    /**
+     * @param dictionary: an array of strings
+     * @return: an arraylist of strings
+     */
+    ArrayList<String> longestWords(String[] dictionary) {
+        int maxLength = 0;
+        for (int i = 0; i < dictionary; ++i) {
+            
+        }
+    }
+
+}
