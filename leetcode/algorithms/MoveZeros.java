1ae75e4f7844a6c5fdf3a798b2ec1f2d862d02f9
mitcc <mitcc@qq.com>
2015-09-19 23:25:36 +0800

    :basketball:

--- leetcode/algorithms/MoveZeros.java
+++ leetcode/algorithms/MoveZeros.java
@@ -17,4 +17,16 @@ public class MoveZeros {
         }
     }
 
+    public void moveZeros(int[] nums) {
+        int count = 0; // count of non-zeros.
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        for (int i = nums.length - 1; i >= count; --i) {
+            nums[i] = 0;
+        }
+    }
+
 }
