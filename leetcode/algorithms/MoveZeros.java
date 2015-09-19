657307501811dcde13d76a1054e3d451b4d54a10
mitcc <mitcc@qq.com>
2015-09-19 23:16:55 +0800

    :basketball:

--- leetcode/algorithms/MoveZeros.java
+++ leetcode/algorithms/MoveZeros.java
@@ -7,13 +7,13 @@ public class MoveZeros {
         int count = 0;
         int k = 0;
         for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != 0) {
+            if (nums[i] == 0)
                 count++;
+            else
                 nums[k++] = nums[i];
-            }
         }
-        for (int i = 0; i < k; ++i) {
-            nums[nums.length - 1 - k] = 0;
+        for (int i = 0; i < count; ++i) {
+            nums[nums.length - 1 - count] = 0;
         }
     }
 
