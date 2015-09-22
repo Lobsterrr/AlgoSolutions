6ae2d3cb116461b60d38caddc99cd33e3b2f6aeb
mitcc <mitcc@qq.com>
2015-09-22 22:22:03 +0800

    :basketball:

---
+++ lintcode/FindTheMissingNumber.java
@@ -0,0 +1,24 @@
+/*
+ * Given an array contains N numbers of 0 .. N, find which number doesn't exist in the array.
+
+Example
+Given N = 3 and the array [0, 1, 3], return 2.
+
+Challenge
+Do it in-place with O(1) extra memory and O(n) time.
+ */
+public class FindTheMissingNumber {
+    /**    
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int findMissing(int[] nums) {
+        long sum = 0;
+        for (num : nums) {
+            sum += num;
+        }
+        int n = nums.length;
+        return n * (n - 1) / 2 - sum;
+    }
+
+}
