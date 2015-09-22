274ddb4ae5b513af98b03efaaeb72c07d2bc8732
mitcc <mitcc@qq.com>
2015-09-23 01:46:13 +0800

    :basketball:

---
+++ lintcode/CountOnesInBinary.java
@@ -0,0 +1,29 @@
+/*
+ * Count how many 1 in binary representation of a 32-bit integer.
+
+Example
+Given 32, return 1
+
+Given 5, return 2
+
+Given 1023, return 9
+
+Challenge
+If the integer is n bits with m 1 bits. Can you do it in O(m) time?
+ */
+public class CountOnesInBinary {
+
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    public int countOnes(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            num = num & (num - 1);
+        }
+        return count;
+    }
+
+}
