96072852df0a5147863d381bcf46855002855aa9
mitcc <mitcc@qq.com>
2015-09-24 22:58:55 +0800

    :basketball:

--- lintcode/ValidPalindrome.java
+++ lintcode/ValidPalindrome.java
@@ -9,6 +9,11 @@ public class ValidPalindrome {
      */
     public boolean isPalindrome(String s) {
         // Write your code here
+        int i = 0; 
+        int j = s.length() - 1;
+        while (i <= j) {
+            
+        }
     }
 
 }
