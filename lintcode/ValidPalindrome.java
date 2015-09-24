5e88ada921b22713382b025b3329d5702017c305
mitcc <mitcc@qq.com>
2015-09-24 22:57:23 +0800

    :basketball:

---
+++ lintcode/ValidPalindrome.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class ValidPalindrome {
+
+    /**
+     * @param s A string
+     * @return Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {
+        // Write your code here
+    }
+
+}
