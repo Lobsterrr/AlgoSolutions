317ad94fa34aacd6141c512b05c00c550c9a6613
mitcc <mitcc@qq.com>
2015-09-24 23:37:55 +0800

    :basketball:

--- lintcode/ValidPalindrome.java
+++ lintcode/ValidPalindrome.java
@@ -11,9 +11,24 @@ public class ValidPalindrome {
         // Write your code here
         int i = 0; 
         int j = s.length() - 1;
-        while (i <= j) {
-            
+        while (i < s.length() && i <= j && j >= 0) {
+            if (!isLetter(s.charAt(i)))
+                i++;
+            else if (!isLetter(s.charAt(j)))
+                j--;
+            else {
+                int diff = Math.abs(s.charAt(i) - s.charAt(j));
+                if (diff != 'a' - 'a' && diff != 'A' - 'a')
+                    return false;
+                i++;
+                j--;
+            }
         }
+        return true;
+    }
+
+    public boolean isLetter(char c) {
+        return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
     }
 
 }
