c1cf232da11047ca2d3e70817dcdd288ec6df7cd
mitcc <mitcc@qq.com>
2015-09-30 22:26:59 +0800

    :basketball:

--- leetcode/algorithms/RepeatedDNASequences.java
+++ leetcode/algorithms/RepeatedDNASequences.java
@@ -20,11 +20,7 @@ public class RepeatedDNASequences {
         Map<String, Integer> map = new HashMap<String, Integer>();
         for (int i = 0; i <= s.length() - 10; ++i) {
             String sub = s.substring(i, i + 10);
-            if (map.containsKey(sub))
-                map.put(sub, map.get(sub) + 1);
-            else 
-                map.put(sub, 1);
-            map.put(sub, map.containsKey(sub) ? 1 + map.get(sub) : 1;
+            map.put(sub, map.containsKey(sub) ? 1 + map.get(sub) : 1);
         }
         List<String> result = new ArrayList<String>();
         for (String key : map.keySet()) {
