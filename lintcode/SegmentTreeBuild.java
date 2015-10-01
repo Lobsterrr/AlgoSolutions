6500a76480de013814f4c401b4aa5175f32d0551
mitcc <mitcc@qq.com>
2015-10-02 01:38:31 +0800

    :basketball:

---
+++ lintcode/SegmentTreeBuild.java
@@ -0,0 +1,21 @@
+/*
+ *
+ */
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end) {
+ *         this.start = start, this.end = end;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class SegmentTreeBuild {
+
+    public SegmentTreeNode build(int start, int end) {
+
+    }
+
+}
