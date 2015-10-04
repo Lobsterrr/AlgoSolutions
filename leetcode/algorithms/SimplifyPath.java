750ed174cc7a35cf715d453e363a9841d5185c50
mitcc <mitcc@qq.com>
2015-10-04 14:21:29 +0800

    :basketball:

--- leetcode/algorithms/SimplifyPath.java
+++ leetcode/algorithms/SimplifyPath.java
@@ -33,7 +33,7 @@ public class SimplifyPath {
             }
         }
         String result = "/";
-        for (int i = list.size() - 1; i >= 0; --i) {
+        for (int i = 0; i < list.size(); --i) {
             result +=list.get(i);
         }
         return result;
