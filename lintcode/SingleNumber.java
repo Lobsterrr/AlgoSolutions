406d480dedd7a2355d3593cffabdf34fb9dfa7e8
mitcc <mitcc@qq.com>
2015-10-08 22:17:16 +0800

    :basketball:

--- lintcode/SingleNumber.java
+++ lintcode/SingleNumber.java
@@ -1,5 +1,11 @@
 /*
- *
+ * Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
+
+Example
+Given [1,2,2,1,3,4,3], return 4
+
+Challenge
+One-pass, constant extra space.
  */
 public class SingleNumber {
     /**
