cab6cdd2cead7bf15abcd8067c2efba89896d379
mitcc <mitcc@qq.com>
2015-10-08 23:50:49 +0800

    :basketball:

--- lintcode/SingleNumberII.java
+++ lintcode/SingleNumberII.java
@@ -1,5 +1,11 @@
 /*
- *
+ * Given 3*n + 1 numbers, every numbers occurs triple times except one, find it.
+
+Example
+Given [1,1,2,3,3,3,2,2,4,1] return 4
+
+Challenge
+One-pass, constant extra space.
  */
 public class SingleNumberII {
 
