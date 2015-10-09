d85b8ce1515b8b0974eb87d0757561f95072387e
mitcc <mitcc@qq.com>
2015-10-10 01:26:07 +0800

    :basketball:

--- leetcode/algorithms/SubstringWithConcatenationOfAllWords.java
+++ leetcode/algorithms/SubstringWithConcatenationOfAllWords.java
@@ -24,8 +24,8 @@ public class SubstringWithConcatenationOfAllWords {
         for (int i = 0; i < s.length(); ++i) {
             int j;
             for (j = 0; j < words.length; ++j) {
-                int k = i + j * word[0].length();
-                String sub = s.substring(k, k + word[0].length);
+                int k = i + j * words[0].length();
+                String sub = s.substring(k, k + words[0].length);
                 if (!toFind.containsKey(sub))
                     break;
                 found.put(sub, found.containsKey(sub) ? 
