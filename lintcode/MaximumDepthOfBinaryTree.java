f31e0845d5e65dd38a667c337bae53a2b44aebe9
mitcc <mitcc@qq.com>
2015-10-11 22:41:11 +0800

    :basketball:

--- lintcode/MaximumDepthOfBinaryTree.java
+++ lintcode/MaximumDepthOfBinaryTree.java
@@ -3,6 +3,10 @@
  */
 public class MaximumDepthOfBinaryTree {
 
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
     public int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
