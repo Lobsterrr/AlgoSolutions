4f6dd1c479c294b4a7d0eb3e87b50043d15abe4e
mitcc <mitcc@qq.com>
2015-10-11 23:32:01 +0800

    :basketball:

--- leetcode/algorithms/MaximumDepthOfBinaryTree.java
+++ leetcode/algorithms/MaximumDepthOfBinaryTree.java
@@ -15,15 +15,13 @@
  */
 public class MaximumDepthOfBinaryTree {
 
-/**************************** Recursive Solution ****************************/
-
     public int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
-/**************************** Iterative Solution *****************************/
+/*****************************************************************************/
 
     public int maxDepth(TreeNode root) {
         if(root == null) 
