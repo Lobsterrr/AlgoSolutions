59366d605e3eeca846bf1f78e1171e6d30f66457
mitcc <mitcc@qq.com>
2015-10-19 19:56:46 +0800

    :basketball:

---
+++ leetcode/algorithms/FindMedianFromDataStream.java
@@ -0,0 +1,25 @@
+/*
+ *
+ */
+class MediaFinder {
+
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+
+    }
+
+    // Returns the median of current data stream
+    public double findMedia() {
+
+    }
+
+}
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
+
+public class FindMediaFromDataStream {
+
+
+}
