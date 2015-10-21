7ae2f546190cdae121a348cbc2f0e41beeb55db1
mitcc <mitcc@qq.com>
2015-10-21 22:56:11 +0800

    :basketball:

--- leetcode/algorithms/ShortestPalindrome.java
+++ leetcode/algorithms/ShortestPalindrome.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Given a string S, you are allowed to convert it to 
+ * a palindrome by adding characters in front of it. 
+ * Find and return the shortest palindrome you can 
+ * find by performing this transformation.
+
+ * For example:
+
+ * Given "aacecaaa", return "aaacecaaa".
+
+ * Given "abcd", return "dcbabcd".
  */
 public class ShortestPalindrome {
 
