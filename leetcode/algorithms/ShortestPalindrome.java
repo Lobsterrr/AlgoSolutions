f3d00fd9f5ab3f6c305eae728743993973acd7bb
mitcc <mitcc@qq.com>
2015-10-21 22:58:52 +0800

    :basketball:

--- leetcode/algorithms/ShortestPalindrome.java
+++ leetcode/algorithms/ShortestPalindrome.java
@@ -16,4 +16,13 @@ public class ShortestPalindrome {
 
     }
 
+    public boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; ++i) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
