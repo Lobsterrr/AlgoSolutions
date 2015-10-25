f2002d618ba6062e34723a25e33d549ad8a1305c
mitcc <mitcc@qq.com>
2015-10-25 18:56:00 +0800

    :basketball:

--- hackerrank/Dynamic Programming/TheCoinChangeProblem.java
+++ hackerrank/Dynamic Programming/TheCoinChangeProblem.java
@@ -1,17 +1,28 @@
 /*
  *
  */
+
 public class TheCoinChangeProblem {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        int[] coins = new int[M];
+        for (int i = 0; i < M; ++i) {
+            coins[i] = in.nextInt();
+        }
+        System.out.println(getWays(coins, N));
     }
 
     public static int getWays(int[] coins, int N) {
-        int[] dp = new int[coins.length + 1];
+        int[] dp = new int[N + 1];
         for (int coin : coins) {
-
+            for (int i = coin; i <= N; ++i) {
+                dp[i] += dp[i - coin];
+            }
         }
+        return dp[N];
     }
 
 }
