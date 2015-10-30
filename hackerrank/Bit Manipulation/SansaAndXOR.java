6c07404751f5e420a493a366157447940f73e7e6
mitcc <mitcc@qq.com>
2015-10-31 00:00:02 +0800

    :basketball:

--- hackerrank/Bit Manipulation/SansaAndXOR.java
+++ hackerrank/Bit Manipulation/SansaAndXOR.java
@@ -1,14 +1,18 @@
 /*
  * Problem Statement
 
-Sansa has an array. She wants to find the value obtained by XOR-ing the contiguous subarrays, followed by XOR-ing the values thus obtained. Can you help her in this task?
+Sansa has an array. She wants to find the value obtained by XOR-ing the 
+contiguous subarrays, followed by XOR-ing the values thus obtained. Can you 
+help her in this task?
 
 Note : [5,7,5] is contiguous subarray of [4,5,7,5] while [4,7,5] is not.
 
 Input Format 
 First line contains an integer T, number of the test cases. 
-The first line of each test case contains an integer N, number of elements in the array. 
-The second line of each test case contains N integers that are elements of the array.
+The first line of each test case contains an integer N, number of elements in 
+the array. 
+The second line of each test case contains N integers that are elements of the 
+array.
 
 Output Format 
 Print the answer corresponding to each test case in a separate line.
