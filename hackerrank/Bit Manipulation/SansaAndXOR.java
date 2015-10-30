96d6fd2b9071a064f3e617c8698135bdfd902505
mitcc <mitcc@qq.com>
2015-10-31 02:52:00 +0800

    :basketball:

--- hackerrank/Bit Manipulation/SansaAndXOR.java
+++ hackerrank/Bit Manipulation/SansaAndXOR.java
@@ -45,8 +45,14 @@ public class SansaAndXOR {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
+        int T = sc.nextInt();
+        for (int i = 0; i < T; ++i) {
+            int N = sc.nextInt();
+            int xor = 0;
+            for (int j = 0; j < N; ++j) {
+                
+            }
+        }
     }
 
 }
