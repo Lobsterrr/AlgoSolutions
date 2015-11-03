934812f5981d92dbf24547e2c8b68881895803ee
mitcc <mitcc@qq.com>
2015-11-03 19:52:37 +0800

    :basketball:

--- leetcode/algorithms/LongestIncreasingSubsequence.java
+++ leetcode/algorithms/LongestIncreasingSubsequence.java
@@ -21,14 +21,17 @@ public class LongestIncreasingSubsequence {
         int[] dp = new int[nums.length];
         dp[0] = 1;
         for (int i = 1; i < nums.length; ++i) {
-            dp[i] = dp[i - 1];
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
-        return dp[nums.length - 1];
+        int result = 0;
+        for (int value : dp) {
+            result = Math.max(result, value);
+        }
+        return result;
     }
 
 }
