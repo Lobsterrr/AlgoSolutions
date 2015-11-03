87b797b165e09259c6fccafd81ac94c423bfae8b
mitcc <mitcc@qq.com>
2015-11-04 00:44:34 +0800

    :basketball:

--- hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
+++ hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
@@ -1,5 +1,42 @@
 /*
+ * Problem Statement
+
+ * An Introduction to the Longest Increasing Subsequence Problems
+
+ * The task is to find the length of the longest subsequence in a given array 
+ * of integers such that all elements of the subsequence are sorted in 
+ * ascending order. For example, the length of the LIS for { 15, 27, 14, 38, 
+ * 26, 55, 46, 65, 85 } is 6 and the longest increasing subsequence is {15, 
+ * 27, 38, 55, 65, 85}. 
  *
+ * Input Format
+
+In the first line of input, there is a single number N. 
+In the next N lines input the value of a[i].
+
+Constraints 
+1 ¡Ü N ¡Ü 106 
+1 ¡Ü a[i] ¡Ü 105
+
+Output Format
+
+In a single line, output the length of the longest increasing sub-sequence.
+
+Sample Input
+
+5
+2
+7
+4
+3
+8
+Sample Output
+
+3
+Explanation
+
+{2,7,8} is the longest increasing sub-sequence, hence the answer is 3 (the 
+length of this sub-sequence).
  */
 import java.util.Arrays;
 import java.util.Scanner;
