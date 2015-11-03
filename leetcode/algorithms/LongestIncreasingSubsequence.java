476a6baa30fa10c87cbb4d2a96413b7ff97c2a2c
mitcc <mitcc@qq.com>
2015-11-03 18:47:59 +0800

    :basketball:

--- leetcode/algorithms/LongestIncreasingSubsequence.java
+++ leetcode/algorithms/LongestIncreasingSubsequence.java
@@ -8,7 +8,7 @@
  * the length is 4. Note that there may be more than one LIS 
  * combination, it is only necessary for you to return the length.
 
- * Your algorithm should run in O(n2) complexity.
+ * Your algorithm should run in O(n^2) complexity.
 
  * Follow up: Could you improve it to O(n log n) time complexity?
  */
