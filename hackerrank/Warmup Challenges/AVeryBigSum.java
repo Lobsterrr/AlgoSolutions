2ded823cd7605edaffc99131f9942b7bc16fdb66
mitcc <mitcc@qq.com>
2015-11-05 19:57:50 +0800

    :basketball:

--- hackerrank/Warmup Challenges/AVeryBigSum.java
+++ hackerrank/Warmup Challenges/AVeryBigSum.java
@@ -4,7 +4,13 @@
 public class AVeryBigSum {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += in.nextInt();
+        }
+        System.out.println(sum);
     }
 
 }
