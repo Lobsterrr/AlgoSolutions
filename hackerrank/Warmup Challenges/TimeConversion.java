2dbead3ccd01e35072b405dc60dd3b90610246c2
mitcc <mitcc@qq.com>
2015-11-05 22:41:53 +0800

    :basketball:

--- hackerrank/Warmup Challenges/TimeConversion.java
+++ hackerrank/Warmup Challenges/TimeConversion.java
@@ -1,5 +1,23 @@
 /*
- *
+ * Problem Statement
+
+You are given time in AM/PM format. Can you convert this into a 24-hour format?
+
+Input
+
+Input consists of time in the AM/PM format i.e. hh:mm:ssAM or hh:mm:ssPM 
+where 01¡Ühh¡Ü12.
+
+Sample: 07:05:45PM
+
+Output
+
+You need to print the time in a 24-hour format i.e. hh:mm:ss 
+where 00¡Ühh¡Ü23.
+
+Sample output for the above input: 19:05:45
+
+Note: Midnight is 12:00:00AM or 00:00:00. Noon is 12:00:00PM.
  */
 public class TimeConversion {
 
