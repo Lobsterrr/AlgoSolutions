f114189c74c9fab5a511c4f69f0fd31632b25bc2
mitcc <mitcc@qq.com>
2015-11-05 22:41:33 +0800

    :basketball:

---
+++ hackerrank/Warmup Challenges/TimeConversion.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class TimeConversion {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        String start = input.substring(0, 2);
+        String end = input.substring(input.length() - 2);
+
+    }
+
+}
