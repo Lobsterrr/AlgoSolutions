ce401ccbc20442591ea79a6bc1489a57c41ec895
mitcc <mitcc@qq.com>
2015-11-05 23:27:23 +0800

    :basketball:

--- hackerrank/Warmup Challenges/TimeConversion.java
+++ hackerrank/Warmup Challenges/TimeConversion.java
@@ -26,14 +26,14 @@ public class TimeConversion {
         String input = in.next();
         String start = input.substring(0, 2);
         String end = input.substring(input.length() - 2);
-        if (end.equals("PM")) {
+        if (end.equals("PM") && !start.equals("12")) {
             start = Integer.parseInt(start) + 12 + "";
             if (start.equals("24")) {
                 start = "00";
             }
         }
         if (end.equals("AM") && start.equals("12")) {
-            start = Integer.parseInt(start) - 12 + "";
+            start = "00";
         }
         System.out.println(start + input.substring(2, input.length() - 2));
     }
