fc0eb2606ca2ddff752c53d76f9b895b60b03c67
mitcc <mitcc@qq.com>
2015-11-08 22:35:08 +0800

    :basketball:

--- hackerrank/Bit Manipulation/XoringNinja.java
+++ hackerrank/Bit Manipulation/XoringNinja.java
@@ -65,13 +65,8 @@ public class XoringNinja {
     
     public static BigInteger xoringNinja(int[] a) {
         BigInteger sum = BigInteger.valueOf(0);
+
     }
-
-
-
-
-
-
 
     public static long xoringNinja(int[] a) {
         long sum = 0;
