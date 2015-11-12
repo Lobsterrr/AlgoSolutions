808b00b29af0b3319534a7fde994a6bf8ebb26b1
mitcc <mitcc@qq.com>
2015-11-12 23:17:01 +0800

    :basketball:

--- leetcode/algorithms/RangeSumQuery2DImmutable.java
+++ leetcode/algorithms/RangeSumQuery2DImmutable.java
@@ -25,16 +25,16 @@ public class RangeSumQuery2DImmutable {
 
 class NumMatrix {
 
-    int[][] sum;
+    int[][] dp;
 
     public NumMatrix(int[][] matrix) {
         if (matrix == null || matrix.length == 0)
             return;
-        sum = new int[matrix.length + 1][matrix[0].length + 1];
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 0; i < matrix.length; ++i) {
             for (int j = 0; j < matrix[0].length; ++j) {
-                sum[i + 1][j + 1] = sum[i + 1][j] 
-                    + sum[i][j + 1] - sum[i][j] + matrix[i][j];
+                dp[i + 1][j + 1] = dp[i + 1][j] 
+                    + dp[i][j + 1] - dp[i][j] + matrix[i][j];
             }
         }
     }
