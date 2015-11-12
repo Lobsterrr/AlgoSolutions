098841feeddf0dda2814f8cc1749c1450074123f
mitcc <mitcc@qq.com>
2015-11-12 23:32:58 +0800

    :basketball:

--- leetcode/algorithms/RangeSumQuery2DImmutable.java
+++ leetcode/algorithms/RangeSumQuery2DImmutable.java
@@ -46,58 +46,6 @@ class NumMatrix {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-class NumMatrix {
-
-    int[][] sum;
-
-    public NumMatrix(int[][] matrix) {
-        if (matrix == null || matrix.length == 0)
-            return;
-        int matrix.length = matrix.length;
-        int matrix[0].length = matrix[0].length;
-        sum = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (i == 0 && j == 0) {
-                    sum[i][j] = matrix[i][j];
-                } else if (i == 0 && j > 0) {
-                    sum[i][j] = sum[i][j - 1] + matrix[i][j];
-                } else if (i > 0 && j == 0) {
-                    sum[i][j] = sum[i - 1][j] + matrix[i][j];
-                } else {
-                    sum[i][j] = sum[i][j - 1] + sum[i - 1][j] 
-                        - sum[i - 1][j - 1] + matrix[i][j];
-                }
-            }
-        }
-    }
-
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int result = sum[row2][col2];
-        if (row1 > 0) {
-            result -= sum[row1 - 1][col2];
-        }
-        if (col1 > 0) {
-            result -= sum[row2][col1 - 1];
-        }
-        if (row1 > 0 && col1 > 0) {
-            result += sum[row1 - 1][col1 - 1];
-        }
-        return result;
-    }
-
-}
-
 // Your NumMatrix object will be instantiated and called as such:
 // NumMatrix numMatrix = new NumMatrix(matrix);
 // numMatrix.sumRegion(0, 1, 2, 3);
