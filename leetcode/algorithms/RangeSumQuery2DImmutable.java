2cb86f10915772533155d4891252daed3803216b
mitcc <mitcc@qq.com>
2015-11-12 23:15:22 +0800

    :basketball:

--- leetcode/algorithms/RangeSumQuery2DImmutable.java
+++ leetcode/algorithms/RangeSumQuery2DImmutable.java
@@ -30,18 +30,18 @@ class NumMatrix {
     public NumMatrix(int[][] matrix) {
         if (matrix == null || matrix.length == 0)
             return;
-        int matrix.length = matrix.length;
-        int matrix[0].length = matrix[0].length;
         sum = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 0; i < matrix.length; ++i) {
             for (int j = 0; j < matrix[0].length; ++j) {
-                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + matrix[i][j];
+                sum[i + 1][j + 1] = sum[i + 1][j] 
+                    + sum[i][j + 1] - sum[i][j] + matrix[i][j];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] - sum[row2 + 1][col1] + sum[row1][col1];
+        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] 
+            - sum[row2 + 1][col1] + sum[row1][col1];
     }
 
 }
