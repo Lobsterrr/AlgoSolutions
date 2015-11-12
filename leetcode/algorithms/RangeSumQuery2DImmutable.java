6f66b8f29f7898d4a7497daca4143d321b38b8d5
mitcc <mitcc@qq.com>
2015-11-12 09:31:56 +0800

    :basketball:

---
+++ leetcode/algorithms/RangeSumQuery2DImmutable.java
@@ -0,0 +1,18 @@
+/*
+ *
+ */
+public class RangeSumQuery2DImmutable {
+
+}
+
+class NumMatrix {
+
+    public NumMatrix(int[][] matrix) {
+
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+    }
+
+}
