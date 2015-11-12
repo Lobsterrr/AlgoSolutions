3f4ce06b8f57423f5bdb34f15df5db69ee1e083e
mitcc <mitcc@qq.com>
2015-11-12 10:17:21 +0800

    :basketball:

--- leetcode/algorithms/RangeSumQuery2DImmutable.java
+++ leetcode/algorithms/RangeSumQuery2DImmutable.java
@@ -12,22 +12,25 @@ class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
-        int xLen = matrix.length;
-        int yLen = matrix[0].length;
-        sum = new int[xLen][yLen];
-        for (int i = 0; i < xLen; ++i) {
-            for (int j = 0; j < yLen; ++j) {
-                if (i == 0 && j == 0) {
-                    sum[i][j] = matrix[i][j];
-                } else if (i == 0 && j > 0) {
-                    sum[i][j] = sum[i][j - 1] + matrix[i][j];
-                } else if (i > 0 && j == 0) {
-                    sum[i][j] = sum[i - 1][j] + matrix[i][j];
-                } else {
-                    sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + matrix[i][j];
+        if (matrix != null && matrix.length > 0) {
+            int xLen = matrix.length;
+            int yLen = matrix[0].length;
+            sum = new int[xLen][yLen];
+            for (int i = 0; i < xLen; ++i) {
+                for (int j = 0; j < yLen; ++j) {
+                    if (i == 0 && j == 0) {
+                        sum[i][j] = matrix[i][j];
+                    } else if (i == 0 && j > 0) {
+                        sum[i][j] = sum[i][j - 1] + matrix[i][j];
+                    } else if (i > 0 && j == 0) {
+                        sum[i][j] = sum[i - 1][j] + matrix[i][j];
+                    } else {
+                        sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + matrix[i][j];
+                    }
                 }
             }
         }
+        
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
