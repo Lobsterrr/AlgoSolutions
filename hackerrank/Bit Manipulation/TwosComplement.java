5c522bc717bd8afa577c0d5a98889b38385c1d35
mitcc <mitcc@qq.com>
2015-11-15 23:26:09 +0800

    :basketball:

--- hackerrank/Bit Manipulation/TwosComplement.java
+++ hackerrank/Bit Manipulation/TwosComplement.java
@@ -33,19 +33,30 @@ For the first case, -2 contains 31 1's followed by a 0, -1 contains 32 1's and
 For the second case, the answer is 31 + 31 + 32 + 0 + 1 + 1 + 2 + 1 = 99
  */
 public class TwosComplement {
-
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int T = in.nextInt();
         for (int i = 0; i < T; ++i) {
             int A = in.nextInt();
             int B = in.nextInt();
-            int count = 0;
-            for (int j = A; j <= B; ++j) {
-                count += hammingWeight(j);
-            }
-            System.out.println(count);
+            System.out.println(solve(A, B));
         }
+
+        System.out.println(twosComplement(Integer.MAX_VALUE - 1));
+    }
+
+    public static long solve(int a, int b) {
+        if (a > b)
+            throw new IllegalArgumentException();
+        long count = 0;
+        if (a < 0 && b < 0)
+            count = twosComplement(a) - twosComplement(b + 1);
+        else if (a < 0 && b >= 0)
+            count = twosComplement(a) + twosComplement(b);
+        else if (a >= 0 && b >= 0)
+            count = twosComplement(b) - twosComplement(Math.max(0, a - 1));
+        return count;
     }
 
     public static int hammingWeight(int n) {
@@ -53,8 +64,6 @@ public class TwosComplement {
         n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
         return (((n + (n >>> 4)) & 0x0f0f0f0f) * 0x01010101) >>> 24;
     }
-
-    
 
     public static long twosComplement(int n) {
         if (n == 0)
@@ -67,8 +76,7 @@ public class TwosComplement {
             n++;
             return (-(long) n + 1) * 32 - twosComplement(-n);
         }
-    }   
-
+    }
 
 
 
