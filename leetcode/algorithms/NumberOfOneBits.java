889047da4646e4d679c9ae7453159fa0e68e96ac
mitcc <mitcc@qq.com>
2015-11-15 21:46:06 +0800

    :basketball:

--- leetcode/algorithms/NumberOfOneBits.java
+++ leetcode/algorithms/NumberOfOneBits.java
@@ -21,6 +21,14 @@ public class NumberOfOneBits {
 /*********************************************************/
 
     public int hammingWeight(int n) {
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        return (((n + (n >>> 4)) & 0x0f0f0f0f) * 0x01010101) >>> 24;
+    }
+
+/*********************************************************/
+
+    public int hammingWeight(int n) {
         int count = 0;
         while (n != 0) {
             count++;
