f4de65c3c27fd86967825b6f6ac0d824e146db09
mitcc <mitcc@qq.com>
2015-11-15 21:59:24 +0800

    :basketball:

--- hackerrank/Bit Manipulation/TwosComplement.java
+++ hackerrank/Bit Manipulation/TwosComplement.java
@@ -45,6 +45,12 @@ public class TwosComplement {
         }
     }
 
+    public int hammingWeight(int n) {
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        return (((n + (n >>> 4)) & 0x0f0f0f0f) * 0x01010101) >>> 24;
+    }
+
     public static int twosComplement(int n) {
         if (n == 0 || n == 1) 
             return n;
@@ -56,16 +62,6 @@ public class TwosComplement {
             return k * (1 << (k - 1)) + 1;
         }
         return k * (1 << k - 1) + twosComplement(n - (1 << k) + 1) + n - (1 << k) + 1;
-    }
-
-    public static int onesCount(int[] n) {
-        int count = 0;
-        for (int i = 0; i < 32; ++i) {
-            if ((1 << i) & n) != 0) {
-                count++;
-            }
-        }
-        return count;
     }
 
 }
