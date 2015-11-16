024639892a0fc5a246b4da5a374cbe36e8161cee
mitcc <mitcc@qq.com>
2015-11-16 23:47:53 +0800

    :basketball:

--- lintcode/BestTimeToBuyAndSellStockII.java
+++ lintcode/BestTimeToBuyAndSellStockII.java
@@ -6,9 +6,7 @@ public class BestTimeToBuyAndSellStockII {
     public int maxProfit(int[] prices) {
         int result = 0;
         for (int i = 1; i < prices.length; ++i) {
-            if (prices[i - 1] < prices[i]) {
-                result += prices[i] - prices[i - 1];
-            }
+            result += Math.max(0, prices[i] - prices[i - 1]);
         }
         return result;
     }    
