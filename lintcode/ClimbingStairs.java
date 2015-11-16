7c9a579f63d8559b55625489d3f76d2cfc36170a
mitcc <mitcc@qq.com>
2015-11-17 02:43:00 +0800

    :basketball:

--- lintcode/ClimbingStairs.java
+++ lintcode/ClimbingStairs.java
@@ -1,7 +1,9 @@
 /*
- * You are climbing a stair case. It takes n steps to reach to the top.
+ * You are climbing a stair case. It takes n steps to 
+ * reach to the top.
 
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Each time you can either climb 1 or 2 steps. In how 
+[<0;82;27Mmany distinct ways can you climb to the top?
 
 Example
 Given an example n=3 , 1+1+1=2+1=1+2=3
@@ -14,18 +16,10 @@ public class ClimbingStairs {
         int a = 1;
         int b = 1;
         for (int i = 1; i <= n; ++i) {
-            int tmp = a;
-            a = b;
-            b += tmp;
+            b += a;
+            a = b - a;
         }
         return a;
-    }
-
-    // TLE
-    public int climbStairs(int n) {
-        if (n == 0 || n == 1)
-            return 1;
-        return climbStairs(n - 2) + climbStairs(n - 1);
     }
 
 }
