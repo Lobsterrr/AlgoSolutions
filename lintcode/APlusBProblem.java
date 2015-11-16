efa40c0ba5c419cb6e1afc36270480f3e428ac5c
mitcc <mitcc@qq.com>
2015-11-16 21:46:40 +0800

    :basketball:

--- lintcode/APlusBProblem.java
+++ lintcode/APlusBProblem.java
@@ -1,14 +1,17 @@
 /*
- * Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
+ * Write a function that add two numbers A and B. You should not use + or any 
+ * arithmetic operators.
 
 Example
 Given a=1 and b=2 return 3
 
 Note
-There is no need to read data from standard input stream. Both parameters are given in function aplusb, you job is to calculate the sum and return it.
+There is no need to read data from standard input stream. Both parameters are 
+given in function aplusb, you job is to calculate the sum and return it.
 
 Challenge
-Of course you can just return a + b to get accepted. But Can you challenge not do it like that?
+Of course you can just return a + b to get accepted. But Can you challenge not 
+do it like that?
 
 Clarification
 Are a and b both 32-bit integers?
