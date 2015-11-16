4bb95eeb6699079a5cb3a3e9dce51a8bf19c7b46
mitcc <mitcc@qq.com>
2015-11-16 21:45:43 +0800

    :basketball:

--- lintcode/APlusBProblem.java
+++ lintcode/APlusBProblem.java
@@ -3,7 +3,13 @@
  */
 public class APlusBProblem {
 
+    /*
+     * param a: The first integer
+     * param b: The second integer
+     * return: The sum of a and b
+     */
     public int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
         return a + b;
     }
 
