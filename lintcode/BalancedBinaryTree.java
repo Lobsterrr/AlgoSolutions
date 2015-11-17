e76d7a52f04217f474dd5d0fd557f52b02173475
mitcc <mitcc@qq.com>
2015-11-17 23:38:32 +0800

    :basketball:

---
+++ lintcode/BalancedBinaryTree.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class BalancedBinaryTree {
+
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        
+    }
+
+}
