a97dd43ba266d6f22c86bbcf1be7d44553d4ffcc
mitcc <mitcc@qq.com>
2015-11-17 23:04:48 +0800

    :basketball:

--- lintcode/UniqueCharacter.java
+++ lintcode/UniqueCharacter.java
@@ -1,5 +1,6 @@
 /*
- * Implement an algorithm to determine if a string has all unique characters.
+ * Implement an algorithm to determine if a string 
+ * has all unique characters.
 
 Example
 Given "abc", return true.
@@ -12,8 +13,6 @@ What if you can not use additional data structures?
 public class UniqueCharacter {
 
     public boolean isUnique(String str) {
-        if (str.length() <= 1)
-            return true;
         boolean[] isUnique = new boolean[128];
         for (int i = 0; i < str.length(); ++i) {
             if (isUnique[str.charAt(i)]) 
