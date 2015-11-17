1deefc5e5f3009d23b95765f03aa637f917ed0c2
mitcc <mitcc@qq.com>
2015-11-17 23:47:27 +0800

    :basketball:

--- lintcode/BalancedBinaryTree.java
+++ lintcode/BalancedBinaryTree.java
@@ -1,5 +1,19 @@
 /*
- *
+ * Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a 
+binary tree in which the depth of the two subtrees of every node
+never differ by more than 1.
+
+Example
+Given binary tree A={3,9,20,#,#,15,7}, B={3,#,20,15,7}
+
+A)  3            B)    3 
+   / \                  \
+  9  20                 20
+    /  \                / \
+   15   7              15  7
+The binary tree A is a height-balanced binary tree, but B is not.
  */
 public class BalancedBinaryTree {
 
@@ -7,6 +21,13 @@ public class BalancedBinaryTree {
      * @param root: The root of binary tree.
      * @return: True if this Binary tree is Balanced, or false.
      */
+
+    public boolean isBalanced(TreeNode root) {
+
+    }
+
+/*******************************************************************/
+
     public boolean isBalanced(TreeNode root) {
         if (root == null)
             return true;
