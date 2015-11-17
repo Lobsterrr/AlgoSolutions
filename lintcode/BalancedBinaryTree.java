0f56c6df4b373d6a79240f57cd238f8319e9a8c4
mitcc <mitcc@qq.com>
2015-11-17 23:45:50 +0800

    :basketball:

--- lintcode/BalancedBinaryTree.java
+++ lintcode/BalancedBinaryTree.java
@@ -8,7 +8,16 @@ public class BalancedBinaryTree {
      * @return: True if this Binary tree is Balanced, or false.
      */
     public boolean isBalanced(TreeNode root) {
-        
+        if (root == null)
+            return true;
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 
+            && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(depth(root.left), depth(root.right));
     }
 
 }
