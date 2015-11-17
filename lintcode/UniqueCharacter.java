4e41818f42d05410cb32f9ad022928ccff7d8d0c
mitcc <mitcc@qq.com>
2015-11-17 23:01:24 +0800

    :basketball:

--- lintcode/UniqueCharacter.java
+++ lintcode/UniqueCharacter.java
@@ -12,7 +12,14 @@ What if you can not use additional data structures?
 public class UniqueCharacter {
 
     public boolean isUnique(String str) {
+        if (str.length() <= 1)
+            return true;
+        boolean[] isUnique = new boolean[str.length()];
+        for (int i = 0; i < str.length(); ++i) {
+            if (isUnique[str.charAt(i)]) {
 
+            }
+        }
     }
 
 }
