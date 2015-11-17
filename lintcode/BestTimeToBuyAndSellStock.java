01fcfb26ac0f9c431d32213f1853dee49402754a
mitcc <mitcc@qq.com>
2015-11-17 22:39:42 +0800

    :basketball:

--- lintcode/BestTimeToBuyAndSellStock.java
+++ lintcode/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Medium Best Time to Buy and Sell Stock Show result 
+
+41% Accepted
+Say you have an array for which the ith element is the 
+price of a given stock on day i.
+
+If you were only permitted to complete at most one 
+transaction (ie, buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
+
+Example
+Given an example [3,2,3,1,2], return 1
  */
 public class BestTimeToBuyAndSellStock {
 
