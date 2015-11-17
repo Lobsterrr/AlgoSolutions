34cfb2ea3cd676eba02f9972d84cf88103bcdca2
mitcc <mitcc@qq.com>
2015-11-17 22:47:47 +0800

    :basketball:

--- lintcode/UniqueCharacter.java
+++ lintcode/UniqueCharacter.java
@@ -1,5 +1,13 @@
 /*
- *
+ * Implement an algorithm to determine if a string has all unique characters.
+
+Example
+Given "abc", return true.
+
+Given "aab", return false.
+
+Challenge
+What if you can not use additional data structures?
  */
 public class UniqueCharacter {
 
