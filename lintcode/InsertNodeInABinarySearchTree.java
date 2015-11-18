5324cc21658c24801d1801640b057aae836f49b3
mitcc <mitcc@qq.com>
2015-11-18 22:42:14 +0800

    :basketball:

--- lintcode/InsertNodeInABinarySearchTree.java
+++ lintcode/InsertNodeInABinarySearchTree.java
@@ -1,5 +1,17 @@
 /*
- *
+ * Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
+
+Have you met this question in a real interview? Yes
+Example
+Given binary search tree as follow, after Insert node 6, the tree should be:
+
+  2             2
+ / \           / \
+1   4   -->   1   4
+   /             / \ 
+  3             3   6
+Challenge
+Can you do it without recursion?
  */
 public class InsertNodeInABinarySearchTree {
 
