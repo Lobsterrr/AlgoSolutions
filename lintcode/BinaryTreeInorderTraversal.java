21ae68203b55c2e4fbbdde2e01db87033ff18ab1
mitcc <mitcc@qq.com>
2015-11-20 22:56:56 +0800

    :basketball:

--- lintcode/BinaryTreeInorderTraversal.java
+++ lintcode/BinaryTreeInorderTraversal.java
@@ -1,5 +1,21 @@
 /*
- *
+ * Given a binary tree, return the inorder traversal of its nodes' 
+ * values.
+
+Example
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+ 
+
+return [1,3,2].
+
+Challenge
+Can you do it without recursion?
  */
 public class BinaryTreeInorderTraversal {
 
