23e97216eea71619c0a03ea8c724f79cde084093
mitcc <mitcc@qq.com>
2015-11-21 02:05:57 +0800

    :basketball:

--- leetcode/algorithms/AdditiveNumber.java
+++ leetcode/algorithms/AdditiveNumber.java
@@ -33,7 +33,7 @@ public class AdditiveNumber {
     }
 
     public boolean dfs(int sum, String num) {
-        if (num.length() <= 10 && Long.parseLong(num) == (long) sum)
+        if (num.length() > 0 && num.length() <= 10 && Long.parseLong(num) == (long) sum)
             return true;
         for (int i = 1; i <= 10 && i < num.length() && checkInt(num.substring(0, i)); ++i) {
             sum += Integer.parseInt(num.substring(0, i));
