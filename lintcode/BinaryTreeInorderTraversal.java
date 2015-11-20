c880dca7ee8a8ef4caa087ef6b3c374e370f55eb
mitcc <mitcc@qq.com>
2015-11-20 22:56:19 +0800

    :basketball:

--- lintcode/BinaryTreeInorderTraversal.java
+++ lintcode/BinaryTreeInorderTraversal.java
@@ -5,6 +5,15 @@ public class BinaryTreeInorderTraversal {
 
     public ArrayList<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
+
+
+
+
+        return result;
+    }
+
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
         if (root != null) {
             result.addAll(inorderTraversal(root.left));
             result.add(root.val);
