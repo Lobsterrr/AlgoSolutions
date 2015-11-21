f7f41469fbefe9f86f03d9d490ccfd62ceec2f13
mitcc <mitcc@qq.com>
2015-11-21 23:22:40 +0800

    :basketball:

--- lintcode/BinaryTreePostorderTraversal.java
+++ lintcode/BinaryTreePostorderTraversal.java
@@ -1,5 +1,21 @@
 /*
- *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+
+Have you met this question in a real interview? Yes
+Example
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+ 
+
+return [3,2,1].
+
+Challenge
+Can you do it without recursion?
  */
 public class BinaryTreePostorderTraversal {
 
