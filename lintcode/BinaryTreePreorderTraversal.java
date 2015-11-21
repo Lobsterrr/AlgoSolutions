d2a711e0d1af79589491b5fbba954019d4c0f377
mitcc <mitcc@qq.com>
2015-11-21 23:25:22 +0800

    :basketball:

--- lintcode/BinaryTreePreorderTraversal.java
+++ lintcode/BinaryTreePreorderTraversal.java
@@ -15,6 +15,17 @@ return [1,2,4,5,3].
 Challenge
 Can you do it without recursion?
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class BinaryTreePreorderTraversal {
 
     // iteration
