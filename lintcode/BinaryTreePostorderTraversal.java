372b928874ec8704d185b2b1a70eae808a1fea12
mitcc <mitcc@qq.com>
2015-11-21 23:24:26 +0800

    :basketball:

--- lintcode/BinaryTreePostorderTraversal.java
+++ lintcode/BinaryTreePostorderTraversal.java
@@ -16,6 +16,17 @@ return [3,2,1].
 Challenge
 Can you do it without recursion?
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class BinaryTreePostorderTraversal {
 
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
