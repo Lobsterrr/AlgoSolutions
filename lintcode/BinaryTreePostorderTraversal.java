96a3ec5d9ad12dc1d6fdc82cac67eb47c2439dcf
mitcc <mitcc@qq.com>
2015-11-22 23:02:59 +0800

    :basketball:

--- lintcode/BinaryTreePostorderTraversal.java
+++ lintcode/BinaryTreePostorderTraversal.java
@@ -32,7 +32,12 @@ public class BinaryTreePostorderTraversal {
 
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        
+        if (root != null) {
+            result.addAll(postorderTraversal(root.left));
+            result.addAll(postorderTraversal(root.right));
+            result.add(root.val);
+        }
+        return result;
     }
 
 }
