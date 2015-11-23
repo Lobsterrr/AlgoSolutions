c8ba1829384aa5c720c9bda86c34986a9eb02313
mitcc <mitcc@qq.com>
2015-11-24 02:17:04 +0800

    :basketball:

--- lintcode/DeleteNodeInTheMiddleOfSinglyLinkedList.java
+++ lintcode/DeleteNodeInTheMiddleOfSinglyLinkedList.java
@@ -1,5 +1,9 @@
 /*
- *
+ * Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+
+Have you met this question in a real interview? Yes
+Example
+Given 1->2->3->4, and node 3. return 1->2->4
  */
 public class DeleteNodeInTheMiddleOfSinglyLinkedList {
 
