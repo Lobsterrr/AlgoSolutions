32174ee9c5d981bfdf6ccd462b804b99a0c519b1
mitcc <mitcc@qq.com>
2015-11-25 01:30:13 +0800

    :basketball:

--- lintcode/FlipBits.java
+++ lintcode/FlipBits.java
@@ -8,7 +8,9 @@ public class FlipBits {
     }
 
     public static int hammingWeight(int n) {
-
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        return (((n + (n >>> 4)) & 0x0f0f0f0f) * 0x01010101) >>> 24;
     }
 
 }
