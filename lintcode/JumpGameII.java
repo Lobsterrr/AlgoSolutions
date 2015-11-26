1e80cd560b473602d25004de50c5ae3e33a11387
mitcc <mitcc@qq.com>
2015-11-26 23:07:04 +0800

    :basketball:

--- lintcode/JumpGameII.java
+++ lintcode/JumpGameII.java
@@ -6,7 +6,7 @@ public class JumpGameII {
     public int jump(int[] A) {
         int[] dp = new int[A.length];
         for (int i = 0; i < A.length; ++i) {
-            for (int j = i + 1; j <= i + A[i]; ++j) {
+            for (int j = i + 1; j < A.length && j <= i + A[i]; ++j) {
                 if (dp[j] == 0) {
                     dp[j] = dp[i] + 1;
                 } else {
