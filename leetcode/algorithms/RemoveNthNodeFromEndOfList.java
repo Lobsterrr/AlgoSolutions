e26fa9a1b2939bcea087a1a6af198ba93354eefa
mitcc <mitcc@qq.com>
2015-11-26 13:46:36 +0800

    :basketball:

--- leetcode/algorithms/RemoveNthNodeFromEndOfList.java
+++ leetcode/algorithms/RemoveNthNodeFromEndOfList.java
@@ -10,6 +10,14 @@
  * Given n will always be valid.
  * Try to do this in one pass.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class RemoveNthNodeFromEndOfList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
@@ -25,7 +33,34 @@ public class RemoveNthNodeFromEndOfList {
 
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        while (first.next != null) {
+            first = first.next;
+            n--;
+            if (n < 0) {
+                second = second.next;
+            }
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
 
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        while (first.next != null) {
+            first = first.next;
+            if (--n < 0) {
+                second = second.next;
+            }
+        }
+        second.next = second.next.next;
+        return dummy.next;
     }
 
 }
