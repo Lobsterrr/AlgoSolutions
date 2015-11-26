618aec0172e1a26e02af24ec55510a08df7769a1
mitcc <mitcc@qq.com>
2015-11-26 13:37:50 +0800

    :basketball:

--- lintcode/NthToLastNodeInList.java
+++ lintcode/NthToLastNodeInList.java
@@ -24,7 +24,6 @@ public class NthToLastNodeInList {
      * @param n: An integer.
      * @return: Nth to last node of a singly linked list. 
      */
-
     ListNode nthToLast(ListNode head, int n) {
         ListNode cur = head;
         while (cur != null) {
