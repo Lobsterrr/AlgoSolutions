065f3d804ebf54ff7c543426facda6b7f73ee450
mitcc <mitcc@qq.com>
2015-11-26 12:41:57 +0800

    :basketball:

--- lintcode/NthToLastNodeInList.java
+++ lintcode/NthToLastNodeInList.java
@@ -1,5 +1,11 @@
 /*
- *
+ * Find the nth to last element of a singly linked list. 
+
+The minimum number of nodes in list is n.
+
+Have you met this question in a real interview? Yes
+Example
+Given a List  3->2->1->5->null and n = 2, return node  whose value is 1.
  */
 /**
  * Definition for ListNode.
