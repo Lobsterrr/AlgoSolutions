2bd48e7b081466f013ea6582242c3d86d592991c
mitcc <mitcc@qq.com>
2015-11-27 19:06:28 +0800

    :basketball:

--- lintcode/LinkedListCycle.java
+++ lintcode/LinkedListCycle.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given a linked list, determine if it has a cycle in it.
+
+
+
+Have you met this question in a real interview? Yes
+Example
+Given -21->10->4->5, tail connects to node index 1, return true
+
+Challenge
+Follow up:
+Can you solve it without using extra space?
  */
 /**
  * Definition for ListNode.
