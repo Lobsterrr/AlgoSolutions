e06d31de7715e541ef1d4f2514438dc7066fa1e3
mitcc <mitcc@qq.com>
2015-11-27 19:05:32 +0800

    :basketball:

--- lintcode/LinkedListCycle.java
+++ lintcode/LinkedListCycle.java
@@ -18,5 +18,16 @@ public class LinkedListCycle {
      * @param head: The first node of linked list.
      * @return: True if it has a cycle, or false
      */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            if (cur == head) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
