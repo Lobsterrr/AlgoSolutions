31b7f07691542574a2e2d0ec08dea2dff1cac5fa
mitcc <mitcc@qq.com>
2015-11-27 19:22:47 +0800

    :basketball:

--- leetcode/algorithms/LinkedListCycle.java
+++ leetcode/algorithms/LinkedListCycle.java
@@ -18,18 +18,6 @@
 public class LinkedListCycle {
 
     public boolean hasCycle(ListNode head) {
-        ListNode cur1 = head;
-        ListNode cur2 = head;
-        while (cur2 != null && cur2.next != null) {
-            cur1 = cur1.next;
-            cur2 = cur2.next.next;
-            if (cur1 == cur2)
-                return true;
-        }
-        return false;
-    }
-
-    public boolean hasCycle(ListNode head) {
         for (ListNode cur = head; cur != null && cur.next != null; ) {
             cur = cur.next.next;
             head = head.next;
