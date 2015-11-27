dfa2c1b95aee5b08a1c4c612de51364df7f08cad
mitcc <mitcc@qq.com>
2015-11-27 19:13:38 +0800

    :basketball:

--- lintcode/LinkedListCycle.java
+++ lintcode/LinkedListCycle.java
@@ -30,20 +30,6 @@ public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {  
         // write your code here
         ListNode fast = head;
-        ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean hasCycle(ListNode head) {  
-        // write your code here
-        ListNode fast = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             head = head.next;
