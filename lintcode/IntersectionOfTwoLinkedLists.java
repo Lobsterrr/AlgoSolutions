dfc78cf29fcd78c596bb86dd9c6533d5817f04bf
mitcc <mitcc@qq.com>
2015-11-29 22:57:01 +0800

    :basketball:

--- lintcode/IntersectionOfTwoLinkedLists.java
+++ lintcode/IntersectionOfTwoLinkedLists.java
@@ -4,7 +4,9 @@
 public class IntersectionOfTwoLinkedLists {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
+        if (headA == null || headB == null) 
+            return null;
+        for (ListNode cur = headA; cur.next != null; 
     }
 
 }
