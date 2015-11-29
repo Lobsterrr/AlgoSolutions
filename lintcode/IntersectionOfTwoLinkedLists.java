bc660d0c76fe42728da242facfd5fb22e6f8a1ce
mitcc <mitcc@qq.com>
2015-11-29 23:18:32 +0800

    :basketball:

--- lintcode/IntersectionOfTwoLinkedLists.java
+++ lintcode/IntersectionOfTwoLinkedLists.java
@@ -1,5 +1,23 @@
 /*
- *
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+
+Have you met this question in a real interview? Yes
+Example
+The following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+Note
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Challenge
+Your code should preferably run in O(n) time and use only O(1) memory.
  */
 public class IntersectionOfTwoLinkedLists {
 
@@ -11,7 +29,7 @@ public class IntersectionOfTwoLinkedLists {
             cur = cur.next;
         }
         cur.next = headB;
-
+        
     }
 
 }
