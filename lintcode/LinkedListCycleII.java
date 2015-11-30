e5bbc5e5026fa9e26c64eaeae901a211c9d57405
mitcc <mitcc@qq.com>
2015-11-30 12:48:24 +0800

    :basketball:

---
+++ lintcode/LinkedListCycleII.java
@@ -0,0 +1,37 @@
+/*
+ * Given a linked list, return the node where the cycle begins.
+
+If there is no cycle, return null.
+
+Have you met this question in a real interview? Yes
+Example
+Given -21->10->4->5, tail connects to node index 1，return 10
+
+Challenge
+Follow up:
+
+Can you solve it without using extra space?
+ */
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class LinkedListCycleII {
+
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {
+
+    }
+
+}
