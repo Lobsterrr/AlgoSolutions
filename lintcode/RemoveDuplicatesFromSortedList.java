234342fd4163176fc08dae2bf869a15464bbc5fd
mitcc <mitcc@qq.com>
2015-12-01 01:49:35 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedList.java
+++ lintcode/RemoveDuplicatesFromSortedList.java
@@ -1,5 +1,6 @@
 /*
- * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * Given a sorted linked list, delete all duplicates 
+ * such that each element appear only once.
 
 Example
 Given 1->1->2, return 1->2.
@@ -18,8 +19,20 @@ Given 1->1->2->3->3, return 1->2->3.
  */
 public class RemoveDuplicatesFromSortedList {
 
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of linked list
+     */
     public static ListNode deleteDuplicates(ListNode head) {
-
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == cur.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
     }
 
 }
