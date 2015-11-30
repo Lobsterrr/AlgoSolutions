4a252c82f20519fdcb4bc58b4c0d8c67e492509f
mitcc <mitcc@qq.com>
2015-12-01 01:34:47 +0800

    :basketball:

--- lintcode/MaximumSubarray.java
+++ lintcode/MaximumSubarray.java
@@ -18,15 +18,10 @@ public class MaximumSubarray {
      */
     public int maxSubArray(int[] nums) {
         int maxSum = Integer.MIN_VALUE;
-        int curSum = 0;
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0, curSum = 0; i < nums.length; ++i) {
             curSum += nums[i];
-            if (maxSum < curSum) {
-                maxSum = curSum;
-            }
-            if (curSum < 0) {
-                curSum = 0;
-            }
+            maxSum = Math.max(maxSum, curSum);
+            curSum = Math.max(curSum, 0);
         }
         return maxSum;
     }
