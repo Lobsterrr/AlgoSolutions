8cd085dc0497cf59758d7efb3a18ad9cf3d70dfc
mitcc <mitcc@qq.com>
2015-12-01 23:54:08 +0800

    :basketball:

---
+++ lintcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,43 @@
+/*
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+Example
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ 
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+Challenge
+Challenge 1: Using only 1 queue to implement it.
+
+Challenge 2: Use DFS algorithm to do it.
+ */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+
+    }
+
+}
