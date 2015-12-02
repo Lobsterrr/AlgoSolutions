989b319663508b50244eb98616e214561db75d60
mitcc <mitcc@qq.com>
2015-12-03 01:35:04 +0800

    :basketball:

--- lintcode/GrayCode.java
+++ lintcode/GrayCode.java
@@ -1,7 +1,11 @@
 /*
- * The gray code is a binary numeral system where two successive values differ in only one bit.
+ * The gray code is a binary numeral system where two 
+ * successive values differ in only one bit.
 
-Given a non-negative integer n representing the total number of bits in the code, find the sequence of gray code. A gray code sequence must begin with 0 and with cover all 2n integers.
+Given a non-negative integer n representing the total 
+number of bits in the code, find the sequence of gray code.
+A gray code sequence must begin with 0 and with cover all 
+2n integers.
 
 Example
 Given n = 2, return [0,1,3,2]. Its gray code sequence is:
@@ -13,7 +17,8 @@ Given n = 2, return [0,1,3,2]. Its gray code sequence is:
 Note
 For a given n, a gray code sequence is not uniquely defined.
 
-[0,2,3,1] is also a valid gray code sequence according to the above definition.
+[0,2,3,1] is also a valid gray code sequence according to 
+the above definition.
 
 Challenge
 O(2n) time.
