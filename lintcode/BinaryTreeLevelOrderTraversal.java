05b086f6758a8bd8430fc11c4dd4f1c223a95daf
mitcc <mitcc@qq.com>
2015-12-02 21:47:15 +0800

    :basketball:

--- lintcode/BinaryTreeLevelOrderTraversal.java
+++ lintcode/BinaryTreeLevelOrderTraversal.java
@@ -44,7 +44,26 @@ public class BinaryTreeLevelOrderTraversal {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
         ArrayList<Integer> list = new ArrayList<Integer>();
-
+        int curLevel = 1;
+        int nextLevel = 0;
+        while (queue.peek() != null) {
+            TreeNode node = queue.poll();
+            curLevel--;
+            list.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+                nextLevel++;
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                nextLevel++;
+            }
+            if (curLevel == 0) {
+                result.add(list);
+                list = new ArrayList<Integer>();
+            }
+        }
+        return result;
     }
 
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
