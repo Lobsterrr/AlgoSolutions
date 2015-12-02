e200a6106a9957ef60d92f6f7be0d3da39870c83
mitcc <mitcc@qq.com>
2015-12-03 01:33:38 +0800

    :basketball:

--- lintcode/GrayCode.java
+++ lintcode/GrayCode.java
@@ -3,6 +3,10 @@
  */
 public class GrayCode {
 
+    /**
+     * @param n a number
+     * @return Gray code
+     */
     public ArrayList<Integer> grayCode(int n) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < (1 << n); ++i) {
