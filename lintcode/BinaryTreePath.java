6ade7a4d8abda637d8c174015cf500a7c6f511c8
mitcc <mitcc@qq.com>
2015-12-02 22:44:52 +0800

    :basketball:

--- lintcode/BinaryTreePath.java
+++ lintcode/BinaryTreePath.java
@@ -1,8 +1,38 @@
 /*
- *
+ * Given a binary tree, return all root-to-leaf paths.
+
+Example
+Given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+[
+  "1->2->5",
+  "1->3"
+]
+ */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
 public class BinaryTreePath {
 
+    /**
+     * @param root the root of the binary tree
+     * @return all root-to-leaf paths
+     */
     public List<String> binaryTreePath(TreeNode root) {
 
     }
