8dd0264a331c976a5b357d6e7c956c8ef0c805dd
mitcc <mitcc@qq.com>
2015-12-03 22:20:42 +0800

    :basketball:

--- lintcode/GenerateParentheses.java
+++ lintcode/GenerateParentheses.java
@@ -9,25 +9,6 @@ Given n = 3, a solution set is:
  */
 public class GenerateParentheses {
 
-    public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> result = new ArrayList<String>();
-        if (n == 0) {
-            result.add("");
-        } else if (n == 1) {
-            result.add("()");
-        } else if (n > 1) {
-            for (int i = 0; i < n; ++i) {
-                for (String inner : generateParenthesis(i)) {
-                    for (String outer : generateParenthesis(n - i - 1)) {
-                        result.add("(" + inner + ")" + outer);
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-/*****************************************************************************/
 
     public ArrayList<String> generateParenthesis(int n) {
         ArrayList<String> result = new ArrayList<String>();
@@ -44,6 +25,26 @@ public class GenerateParentheses {
         }
         dfs(result, s + "(", left - 1, right);
         dfs(result, s + ")", left, right - 1);
+    }
+
+/*****************************************************************************/
+
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> result = new ArrayList<String>();
+        if (n == 0) {
+            result.add("");
+        } else if (n == 1) {
+            result.add("()");
+        } else if (n > 1) {
+            for (int i = 0; i < n; ++i) {
+                for (String inner : generateParenthesis(i)) {
+                    for (String outer : generateParenthesis(n - i - 1)) {
+                        result.add("(" + inner + ")" + outer);
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 }
