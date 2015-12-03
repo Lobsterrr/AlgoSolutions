16ea0ccc28a727522a29135c18a8b5782ffcea6c
mitcc <mitcc@qq.com>
2015-12-03 23:53:23 +0800

    :basketball:

--- lintcode/PalindromeLinkedList.java
+++ lintcode/PalindromeLinkedList.java
@@ -18,6 +18,11 @@ public class PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
         int len = 0;
         for (ListNode cur = head; cur != null; cur = cur.next, len++);
+        
+        ListNode cur = head;
+        for (int i = 0; i < (len - 1) / 2; ++i, cur = cur.next);
+        ListNode second = cur.next;
+        
 
     }
 
