db906201c2ca3f0293e57dab4bc565b030bde9dd
mitcc <mitcc@qq.com>
2015-12-03 22:29:28 +0800

    :basketball:

---
+++ lintcode/SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+/*
+ * Given a linked list, swap every two adjacent nodes and return its head.
+
+Example
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Challenge
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.`
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class SwapNodesInPairs {
+
+    /**
+     * @param head a ListNode
+     * @return a ListNode
+     */
+    public ListNode swapPairs(ListNode head) {
+
+    }
+
+}
