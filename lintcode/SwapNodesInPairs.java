841ff2ca4816938a3605711f15ad4f9c090f72c6
mitcc <mitcc@qq.com>
2015-12-03 22:39:01 +0800

    :basketball:

--- lintcode/SwapNodesInPairs.java
+++ lintcode/SwapNodesInPairs.java
@@ -22,7 +22,18 @@ public class SwapNodesInPairs {
      * @return a ListNode
      */
     public ListNode swapPairs(ListNode head) {
-
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            prev.next = cur.next;
+            cur.next = cur.next.next;
+            prev.next.next = cur;
+            prev = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
     }
 
 }
