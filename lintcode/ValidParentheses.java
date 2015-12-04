92a42fa1ec2a8aa4232fb71b22d5af97e4ce785a
mitcc <mitcc@qq.com>
2015-12-04 09:16:05 +0800

    :basketball:

--- lintcode/ValidParentheses.java
+++ lintcode/ValidParentheses.java
@@ -1,8 +1,10 @@
 /*
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * Given a string containing just the characters '(', ')', '{', 
+ * '}', '[' and ']', determine if the input string is valid.
 
 Example
-The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+The brackets must close in the correct order, "()" and "()[]{}" 
+are all valid but "(]" and "([)]" are not.
  */
 public class ValidParentheses {
 
