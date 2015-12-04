fc74a122a238597b58d0f04bd3457075ae34c86a
mitcc <mitcc@qq.com>
2015-12-04 23:53:40 +0800

    :basketball:

--- lintcode/Pow.java
+++ lintcode/Pow.java
@@ -35,4 +35,23 @@ public class Pow {
         return result;
     }
 
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double result = 1.0;
+        double base = 1.0;
+        for (int i = 0; i < 32; ++i) {
+            if (n > 0) {
+                base *= x;
+            } else {
+                base /= x;
+            }
+            if (((1 << i) & n) != 0) {
+                result *= base;
+            }
+        }
+        return result;
+    }
+
 }
