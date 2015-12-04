c16f604d6adf9d88f55e6b202e132879720b1d93
mitcc <mitcc@qq.com>
2015-12-04 13:03:04 +0800

    :basketball:

--- lintcode/Pow.java
+++ lintcode/Pow.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Implement pow(x, n).
+
+Have you met this question in a real interview? Yes
+Example
+Pow(2.1, 3) = 9.261
+Pow(0, 1) = 0
+Pow(1, 0) = 1
+Note
+You don't need to care about the precision of your answer, it's acceptable if the expected answer and your answer 's difference is smaller than 1e-3.
+
+Challenge
+O(logn) time
  */
 public class Pow {
 
