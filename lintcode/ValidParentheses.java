667fdc142a58715f246d919043e6693536145943
mitcc <mitcc@qq.com>
2015-12-04 09:00:21 +0800

    :basketball:

--- lintcode/ValidParentheses.java
+++ lintcode/ValidParentheses.java
@@ -3,6 +3,10 @@
  */
 public class ValidParentheses {
 
+    /**
+     * @param s A string
+     * @return whether the string is a valid parentheses
+     */
     public boolean isValidParentheses(String s) {
 
     }
