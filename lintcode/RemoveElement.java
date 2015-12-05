30bb742404b6fe639202008836930735eca5f3f2
mitcc <mitcc@qq.com>
2015-12-05 18:35:56 +0800

    :basketball:

--- lintcode/RemoveElement.java
+++ lintcode/RemoveElement.java
@@ -1,20 +1,16 @@
 /*
- *
+ * Given an array and a value, remove all occurrences of 
+ * that value in place and return the new length.
+
+The order of elements can be changed, and the elements 
+after the new length don't matter.
+
+Example
+Given an array [0,4,4,0,0,2,4,4], value=4
+
+return 4 and front four elements of the array is [0,0,0,2]
  */
 public class RemoveElement {
-
-    public int removeElement(int[] A, int elem) {
-        int i = 0;
-        int j = 0;
-        while (i < A.length) {
-            if (A[i] != elem) {
-                A[j] = A[i];
-                j++;
-            }
-            i++;
-        }
-        return j;
-    }
 
     public int removeElement(int[] A, int elem) {
         int index = 0;
@@ -25,4 +21,5 @@ public class RemoveElement {
         }
         return index;
     }
+
 }
