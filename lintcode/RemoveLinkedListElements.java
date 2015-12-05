fd4dad1c21b404bc63624b9cbf56d318f35b5caa
mitcc <mitcc@qq.com>
2015-12-05 23:55:10 +0800

    :basketball:

--- lintcode/RemoveLinkedListElements.java
+++ lintcode/RemoveLinkedListElements.java
@@ -1,10 +1,13 @@
 /*
- *
+ * Remove all elements from a linked list of integers that have value val.
+
+Example
+Given 1->2->3->3->4->5->3, val = 3, you should return the list as 1->2->4->5
  */
 public class RemoveLinkedListElements {
 
 	public ListNode removeElements(ListNode head, int val) {
-
+		
 	}
 
 }
