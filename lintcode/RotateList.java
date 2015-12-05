31c9f1fe2078a9043b7be0ae403caae506ee7c7b
mitcc <mitcc@qq.com>
2015-12-05 21:07:03 +0800

    :basketball:

--- lintcode/RotateList.java
+++ lintcode/RotateList.java
@@ -1,8 +1,27 @@
 /*
- *
+ * Given a list, rotate the list to the right by k places, where k is non-negative.
+
+Example
+Given 1->2->3->4->5 and k = 2, return 4->5->1->2->3.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
  */
 public class RotateList {
 
+    /**
+     * @param head: the List
+     * @param k: rotate to the right k places
+     * @return: the list after rotation
+     */
     public ListNode rotateRight(ListNode head, int k) {
 
     }
