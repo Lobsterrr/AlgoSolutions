7589915a6f00f7efaeba05e0148bdea924d3b5c9
mitcc <mitcc@qq.com>
2015-12-06 00:41:54 +0800

    :basketball:

--- lintcode/RemoveLinkedListElements.java
+++ lintcode/RemoveLinkedListElements.java
@@ -33,20 +33,4 @@ public class RemoveLinkedListElements {
         return dummy.next;
     }
 
-
-
-	public ListNode removeElements(ListNode head, int val) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode cur = dummy;
-        while (cur.next != null) {
-            if (cur.next.val == val) {
-                cur.next = cur.next.next;
-            } else {
-                cur = cur.next;
-            }
-        }
-        return dummy.next;
-    }
-		
 }
