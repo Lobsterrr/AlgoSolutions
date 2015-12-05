98e5d364483c30aaab0e5239620b8d82de9b76c3
mitcc <mitcc@qq.com>
2015-12-05 23:55:28 +0800

    :basketball:

--- lintcode/RemoveLinkedListElements.java
+++ lintcode/RemoveLinkedListElements.java
@@ -4,6 +4,14 @@
 Example
 Given 1->2->3->3->4->5->3, val = 3, you should return the list as 1->2->4->5
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class RemoveLinkedListElements {
 
 	public ListNode removeElements(ListNode head, int val) {
