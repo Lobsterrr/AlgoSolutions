4b930adbf972339fa91a26a2a95fff2aba4beb1c
mitcc <mitcc@qq.com>
2015-12-06 19:56:54 +0800

    :basketball:

--- lintcode/MergeTwoSortedLists.java
+++ lintcode/MergeTwoSortedLists.java
@@ -1,6 +1,17 @@
 /*
  *
  */
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
 public class MergeTwoSortedLists {
 
     /**
