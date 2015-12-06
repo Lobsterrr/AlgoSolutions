0cc3ce642fb303ecaca89b9677c89d07b9da4716
mitcc <mitcc@qq.com>
2015-12-06 21:52:15 +0800

    :basketball:

--- lintcode/UniqueBinarySearchTree.java
+++ lintcode/UniqueBinarySearchTree.java
@@ -1,8 +1,21 @@
 /*
- *
+ * Given n, how many structurally unique BSTs (binary search trees) that store values 1...n?
+
+Example
+Given n = 3, there are a total of 5 unique BST's.
+
+1           3    3       2      1
+ \         /    /       / \      \
+  3      2     1       1   3      2
+ /      /       \                  \
+2     1          2                  3
  */
 public class UniqueBinarySearchTree {
 
+    /**
+     * @paramn n: An integer
+     * @return: An integer
+     */
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
