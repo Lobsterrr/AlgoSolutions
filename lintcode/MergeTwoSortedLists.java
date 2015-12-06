422ed49c3f2f52893910ce527bdfdc5e82ddba59
mitcc <mitcc@qq.com>
2015-12-06 21:15:40 +0800

    :basketball:

--- lintcode/MergeTwoSortedLists.java
+++ lintcode/MergeTwoSortedLists.java
@@ -38,9 +38,6 @@ public class MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null) {
-            return l2 == null ? l1 : l2;
-        }
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
         while (l1 != null || l2 != null) {
