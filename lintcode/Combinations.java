fd053860248b10c68b5a3a37978073bfcf41d3d4
mitcc <mitcc@qq.com>
2015-12-06 23:55:28 +0800

    :basketball:

--- lintcode/Combinations.java
+++ lintcode/Combinations.java
@@ -3,6 +3,11 @@
  */
 public class Combinations {
 
+    /**
+     * @param n: Given the range of numbers
+     * @param k: Given the numbers of combinations
+     * @return: All the combinations of k numbers out of 1..n
+     */
     public List<List<Integer>> combine(int n, int k) {
 
     }
