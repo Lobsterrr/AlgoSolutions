c019fe8b58fe1efc36b3c43fcb6174d09bd1dfac
mitcc <mitcc@qq.com>
2015-12-08 02:01:08 +0800

    :basketball:

--- lintcode/PartitionArrayByOddAndEven.java
+++ lintcode/PartitionArrayByOddAndEven.java
@@ -1,5 +1,6 @@
 /*
- * Partition an integers array into odd number first and even number second.
+ * Partition an integers array into odd 
+ * number first and even number second.
 
 Example
 Given [1, 2, 3, 4], return [1, 3, 2, 4]
