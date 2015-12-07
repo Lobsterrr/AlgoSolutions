6ba96d715819f62e938383f5761d2c320b88d6b1
mitcc <mitcc@qq.com>
2015-12-08 00:02:25 +0800

    :basketball:

--- lintcode/ImplementQueueByTwoStacks.java
+++ lintcode/ImplementQueueByTwoStacks.java
@@ -9,6 +9,7 @@ class Queue {
     
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
+    int topValue;
 
     public Queue() {
         stack1 = new Stack<Integer>();
@@ -16,17 +17,23 @@ class Queue {
     }
 
     public void push(int element) {
+        if (stack1.isEmpty()) {
+            topValue = element;
+        }
         stack1.push(element);
     }
 
     public int pop() {
         while (!stack.isEmpty()) {
-            
+            stack2.push(stack1.pop());
         }
+        int popValue = stack2.pop();
+        topValue = stack2.peek();
+        return popValue;
     }
 
     public int top() {
-
+        return topValue; 
     }
 
 }
