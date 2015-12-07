05920a660e643ccdd773041614bfcb0bb5ea8b20
mitcc <mitcc@qq.com>
2015-12-08 02:27:14 +0800

    :basketball:

--- lintcode/UniquePaths.java
+++ lintcode/UniquePaths.java
@@ -1,7 +1,10 @@
 /*
- * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * A robot is located at the top-left corner of a m x n 
+ * grid (marked 'Start' in the diagram below).
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+The robot can only move either down or right at any point 
+in time. The robot is trying to reach the bottom-right 
+corner of the grid (marked 'Finish' in the diagram below).
 
 How many possible unique paths are there?
 
@@ -18,12 +21,14 @@ public class UniquePaths {
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                dp[i][j] = (i == 0 || j == 0) ? 1 : dp[i][j - 1] + dp[i - 1][j];
+                dp[i][j] = (i == 0 || j == 0) ? 1 
+                    : dp[i][j - 1] + dp[i - 1][j];
             }
         }
         return dp[m - 1][n - 1];
     }
 
+/*********************************************************/
 
     public int uniquePaths(int m, int n) {
         int[] dp = new int[m];
