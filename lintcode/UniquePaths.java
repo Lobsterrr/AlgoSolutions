597ae1f6bb44090c86727b96c6ebdf7f2f985177
mitcc <mitcc@qq.com>
2015-12-08 02:25:48 +0800

    :basketball:

--- lintcode/UniquePaths.java
+++ lintcode/UniquePaths.java
@@ -24,4 +24,16 @@ public class UniquePaths {
         return dp[m - 1][n - 1];
     }
 
+
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[m];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+
 }
