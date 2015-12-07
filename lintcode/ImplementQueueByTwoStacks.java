245b789db0a3a3ccb41032b7aa15f04c9565a238
mitcc <mitcc@qq.com>
2015-12-08 00:11:33 +0800

    :basketball:

--- lintcode/ImplementQueueByTwoStacks.java
+++ lintcode/ImplementQueueByTwoStacks.java
@@ -1,9 +1,13 @@
 /*
- * As the title described, you should only use two stacks to implement a queue's actions.
+ * As the title described, you should only use 
+ * two stacks to implement a queue's actions.
 
-The queue should support push(element), pop() and top() where pop is pop the first(a.k.a front) element in the queue.
+The queue should support push(element), pop() 
+and top() where pop is pop the first(a.k.a front) 
+element in the queue.
 
-Both pop and top methods should return the value of first element.
+Both pop and top methods should return the 
+value of first element.
 
 Example
 push(1)
@@ -13,7 +17,9 @@ push(3)
 top()     // return 2
 pop()     // return 2
 Challenge
-implement it by two stacks, do not use any other data structure and push, pop and top should be O(1) by AVERAGE.`
+implement it by two stacks, do not use any other 
+data structure and push, pop and top should be
+O(1) by AVERAGE.`
  */
 public class ImplementQueueByTwoStacks {
 
