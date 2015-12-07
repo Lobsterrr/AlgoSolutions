75856e06fb824d1c0e5f75ad7204ef1e912265cb
mitcc <mitcc@qq.com>
2015-12-07 23:45:40 +0800

    :basketball:

--- lintcode/Combinations.java
+++ lintcode/Combinations.java
@@ -16,6 +16,28 @@ public class Combinations {
      */
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        dfs(result, list, 1, n, k);
+        return result;
+    }
+
+    public void dfs(List<List<Integer>> result, List<Integer> list, 
+            int position, int n, int k) {
+        if (k == 0) { 
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = position; i <= n - k + 1; ++i) {
+            list.add(i);
+            dfs(result, list, i + 1, n, k - 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+/*******************************************************************/
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (k == 0) {
             result.add(new ArrayList<Integer>());
             return result;
@@ -27,26 +49,6 @@ public class Combinations {
             }
         }
         return result;
-    }
-
-    
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> list = new ArrayList<Integer>();
-        dfs(result, list, 1, n, k);
-        return result;
-    }
-
-    public void dfs(List<List<Integer>> result, List<Integer> list, int position, int n, int k) {
-        if (k == 0) { 
-            result.add(new ArrayList<Integer>(list));
-            return;
-        }
-        for (int i = position; i <= n - k + 1; ++i) {
-            list.add(i);
-            dfs(result, list, i + 1, n, k - 1);
-            list.remove(list.size() - 1);
-        }
     }
 
 }
