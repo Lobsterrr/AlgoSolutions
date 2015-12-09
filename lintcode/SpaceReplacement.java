e6862bd3470eb4aae79e4fd917ab2253b1e2c02a
mitcc <mitcc@qq.com>
2015-12-10 02:01:14 +0800

    :basketball:

---
+++ lintcode/SpaceReplacement.java
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+public class SpaceReplacement {
+
+    public int replaceBlank(char[] string, int length) {
+        int blankCount = 0;
+        for (int i = 0; i < string; ++i) {
+            if (string[i] == ' ') {
+                blankCount++;
+            }
+        }
+
+    }
+
+}
