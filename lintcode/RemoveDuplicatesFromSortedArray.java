8cb7390742a90f984b125f3e33e31408e24088d7
mitcc <mitcc@qq.com>
2015-12-09 23:46:58 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedArray.java
+++ lintcode/RemoveDuplicatesFromSortedArray.java
@@ -34,7 +34,7 @@ public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
         int index = 0;
         for (int i = 0; i < nums.length; ++i) {
-            if (index == 0 || nums[i] != nums[index - 1]) {
+            if (index == 0 || nums[index - 1] != nums[i]) {
                 nums[index++] = nums[i];
             }
         }
