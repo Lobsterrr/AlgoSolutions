f854c7434f19642e274f8015b080367de2058a95
mitcc <mitcc@qq.com>
2015-12-09 23:54:56 +0800

    :basketball:

--- lintcode/RomanToInteger.java
+++ lintcode/RomanToInteger.java
@@ -26,6 +26,14 @@ public class RomanToInteger {
      * @return an integer
      */
     public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
     }
 
