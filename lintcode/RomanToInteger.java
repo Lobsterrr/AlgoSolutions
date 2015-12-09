db216d4e2539506977cb1dc90446db1621c88537
mitcc <mitcc@qq.com>
2015-12-10 01:17:14 +0800

    :basketball:

--- lintcode/RomanToInteger.java
+++ lintcode/RomanToInteger.java
@@ -36,7 +36,8 @@ public class RomanToInteger {
         map.put('M', 1000);
         int result = 0;
         for (int i = 0; i < s.length(); ++i) {
-            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+            if (i < s.length() - 1 && 
+                    map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
                  result += map.get(s.charAt(i + 1)) - map.get(s.charAt(i++));
             } else {
                 result += map.get(s.charAt(i));
