4d391e5abcc0c959d6c4f7c7863af1668a5d7b47
mitcc <mitcc@qq.com>
2015-12-11 00:20:44 +0800

    :basketball:

--- lintcode/ConvertSortedArrayToBinarySearchTreeWithMinimalHeight.java
+++ lintcode/ConvertSortedArrayToBinarySearchTreeWithMinimalHeight.java
@@ -35,6 +35,15 @@ public class ConvertSortedArrayToBinarySearchTreeWithMinimalHeight {
     }
 
     public TreeNode helper(int[] A, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        TreeNode root = new TreeNode(A[(low + high) / 2]);
+        root.left = helper(A, low, (low + high) / 2 - 1);
+        root.right = helper(A, (low + high) / 2 + 1, high);
+        return root;
+
+
         if (low < 0 || low > high) {
             return null;
         }
