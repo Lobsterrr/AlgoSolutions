fe47ce0c0a7a13e30724c2beced833a9867d6dce
mitcc <mitcc@qq.com>
2015-12-10 23:14:54 +0800

    :basketball:

--- lintcode/MinimumPathSum.java
+++ lintcode/MinimumPathSum.java
@@ -1,5 +1,6 @@
 /*
- * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * Given a m x n grid filled with non-negative numbers, find a path from top 
+ * left to bottom right which minimizes the sum of all numbers along its path.
 
 Example
 Note
@@ -14,13 +15,11 @@ public class MinimumPathSum {
     public int minPathSum(int[][] grid) {
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[0].length; ++j) {
-                if (i == 0 && j == 0) {
-            
-                } else if (i == 0 && j > 0) {
+                if (i == 0 && j > 0) {
                     grid[i][j] += grid[i][j - 1];
                 } else if (i > 0 && j == 0) {
                     grid[i][j] += grid[i - 1][j];
-                } else {
+                } else if (i > 0 && j > 0) {
                     grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
                 }
             }
