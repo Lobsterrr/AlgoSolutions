c51c49812b44fc032146efe9941d843ce3198fbe
mitcc <mitcc@qq.com>
2015-12-10 23:45:00 +0800

    :basketball:

--- lintcode/ConvertSortedArrayToBinarySearchTreeWithMinimalHeight.java
+++ lintcode/ConvertSortedArrayToBinarySearchTreeWithMinimalHeight.java
@@ -1,8 +1,25 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */ 
 public class ConvertSortedArrayToBinarySearchTreeWithMinimalHeight {
 
+    /**
+     * @param A: an integer array
+     * @return: a tree node
+     */
+    public TreeNode sortedArrayToBST(int[] A) {
 
+    }
 
 }
