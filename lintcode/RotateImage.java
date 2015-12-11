a36f7ef2f1b7af915a0ebf09cde95537f28115b9
mitcc <mitcc@qq.com>
2015-12-11 21:24:52 +0800

    :basketball:

--- lintcode/RotateImage.java
+++ lintcode/RotateImage.java
@@ -1,5 +1,23 @@
 /*
- *
+ * You are given an n x n 2D matrix representing an image.
+Rotate the image by 90 degrees (clockwise).
+
+Have you met this question in a real interview? Yes
+Example
+Given a matrix
+
+[
+    [1,2],
+    [3,4]
+]
+rotate it by 90 degrees (clockwise), return
+
+[
+    [3,1],
+    [4,2]
+]
+Challenge
+Do it in-place.
  */
 public class RotateImage {
 
