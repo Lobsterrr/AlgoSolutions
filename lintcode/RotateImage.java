dd20603cfb5491753401cf13592f6e7491f78a65
mitcc <mitcc@qq.com>
2015-12-13 02:59:15 +0800

    :basketball:

--- lintcode/RotateImage.java
+++ lintcode/RotateImage.java
@@ -23,7 +23,7 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         int xLen = matrix.length;
         int yLen = matrix[0].length;
-        for (int i = 0; i < xLen / 2; ++i) {
+        for (int i = 0; i < (xLen + 1) / 2; ++i) {
             for (int j = 0; j < yLen / 2; ++j) {
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[xLen - 1 - j][i];
