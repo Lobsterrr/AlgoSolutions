1c18a3f2d7cae024ec9aadb3dbcc6dbb53645fa1
mitcc <mitcc@qq.com>
2015-12-13 23:00:20 +0800

    :basketball:

--- lintcode/Candy.java
+++ lintcode/Candy.java
@@ -8,7 +8,7 @@ public class Candy {
             return ratings.length;
         }
         int[] candy = new int[ratings.length];
-        candy[0] = candy[candy.length - 1] = 1;
+        Arrays.fill(candy, 1);
         for (int i = 1; i < ratings.length; ++i) {
             if (ratings[i] > ratings[i - 1]) {
                 candy[i] = candy[i - 1] + 1;
