8b70f05249ab144207c71f5f3d4b6969d7686d87
mitcc <mitcc@qq.com>
2015-12-14 00:04:48 +0800

    :basketball:

--- lintcode/TrailingZeros.java
+++ lintcode/TrailingZeros.java
@@ -1,5 +1,6 @@
 /*
- * Write an algorithm which computes the number of trailing zeros in n factorial.
+ * Write an algorithm which computes the 
+ * number of trailing zeros in n factorial.
 
 Example
 11! = 39916800, so the out should be 2
