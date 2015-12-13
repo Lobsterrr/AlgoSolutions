aaf9a65402b0f0d3edf0e28f156418ad67bd6f69
mitcc <mitcc@qq.com>
2015-12-14 00:03:54 +0800

    :basketball:

--- lintcode/TrailingZeros.java
+++ lintcode/TrailingZeros.java
@@ -16,7 +16,8 @@ public class TrailingZeros {
     public long trailingZeros(long n) {
         long result = 0;
         while (n > 0) {
-            result += n / 5;
+            n /= 5;
+            result += n;
         }
         return result;
     }
