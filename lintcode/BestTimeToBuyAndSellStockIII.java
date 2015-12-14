65de05ec4780b63ac0eb82fe309e5c4d3680e456
mitcc <mitcc@qq.com>
2015-12-14 23:06:58 +0800

    :basketball:

---
+++ lintcode/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,22 @@
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Example
+Given an example [4,4,6,1,1,4,2,5], return 6.
+
+Note
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ */
+public class BestTimeToBuyAndSellStockIII {
+
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    public int maxProfit(int[] prices) {
+
+    }
+
+}
