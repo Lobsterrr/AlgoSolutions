37663b7dd98f779bc530ed1efea5aca4408178af
mitcc <mitcc@qq.com>
2015-12-15 23:02:29 +0800

    :basketball:

--- lintcode/FindMinimumInRotatedSortedArray.java
+++ lintcode/FindMinimumInRotatedSortedArray.java
@@ -1,8 +1,22 @@
 /*
- *
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+Example
+Given [4, 5, 6, 7, 0, 1, 2] return 0
+
+Note
+You may assume no duplicate exists in the array.
  */
 public class FindMinimumInRotatedSortedArray {
 
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
     public int findMin(int[] num) {
 
     }
