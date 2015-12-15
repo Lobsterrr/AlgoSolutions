69ee0f7d3bb28f5319c664d63a4097dc0197faab
mitcc <mitcc@qq.com>
2015-12-16 01:08:23 +0800

    :basketball:

--- lintcode/LongestCommonSubsequence.java
+++ lintcode/LongestCommonSubsequence.java
@@ -25,16 +25,12 @@ public class LongestCommonSubsequence {
             return 0;
         }
         int[][] dp = new int[A.length()][B.length()];
-        for (int i = 0; i < A.length(); ++i) {
-            for (int j = 0; j < B.length(); ++j) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
+        for (int i = 1; i < A.length(); ++i) {
+            for (int j = 1; j < B.length(); ++j) {
+                if (A.charAt(i) == B.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    if (A.charAt(i) == B.charAt(j)) {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                    }
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
