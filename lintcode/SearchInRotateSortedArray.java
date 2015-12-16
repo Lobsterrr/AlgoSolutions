d38a682c4afb7ce2c0c00c9a1a36c75cc3645ef4
mitcc <mitcc@qq.com>
2015-12-16 09:23:48 +0800

    :basketball:

--- lintcode/SearchInRotateSortedArray.java
+++ lintcode/SearchInRotateSortedArray.java
@@ -4,7 +4,17 @@
 public class SearchInRotateSortedArray {
 
     public int search(int[] A, int target) {
-
+        int low = 0;
+        int high = A.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (A[mid] < A[low]) {
+                high = mid;
+            } else if (A[mid] > A[low]) {
+                low = mid + 1;
+            }
+        }
+        return A[low];
     }
 
 }
