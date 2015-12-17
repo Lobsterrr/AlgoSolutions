52eef46d78a27591cf620fd6c4531264a0e7cf1f
mitcc <mitcc@qq.com>
2015-12-17 23:32:23 +0800

    :basketball:

--- lintcode/IntegerToRoman.java
+++ lintcode/IntegerToRoman.java
@@ -28,6 +28,18 @@ public class IntegerToRoman {
      * @return Roman representation
      */
     public String intToRoman(int n) {
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thousands = {"", "M", "MM", "MMM", "", "", "", "", "", ""};
+        String result = thousands[n / 1000 % 10];
+        result += hundreds[n / 100 % 10];
+        result += tens[n / 10 % 10];
+        result += ones[n % 10];
+        return result;
+    }
+
+    public String intToRoman(int n) {
         String[] ones = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         String[] tens = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] hundreds = {"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
