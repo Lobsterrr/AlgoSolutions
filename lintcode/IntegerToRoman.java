78d99604345500d815d7b65b5f90610a1656a98a
mitcc <mitcc@qq.com>
2015-12-17 22:32:09 +0800

    :basketball:

---
+++ lintcode/IntegerToRoman.java
@@ -0,0 +1,34 @@
+/*
+ * Given an integer, convert it to a roman numeral.
+
+The number is guaranteed to be within the range from 1 to 3999.
+
+Example
+4 -> IV
+
+12 -> XII
+
+21 -> XXI
+
+99 -> XCIX
+
+more examples at: http://literacy.kent.edu/Minigrants/Cinci/romanchart.htm
+
+Clarification
+What is Roman Numeral?
+
+https://en.wikipedia.org/wiki/Roman_numerals
+https://zh.wikipedia.org/wiki/%E7%BD%97%E9%A9%AC%E6%95%B0%E5%AD%97
+http://baike.baidu.com/view/42061.htm
+ */
+public class IntegerToRoman {
+
+    /**
+     * @param n The integer
+     * @return Roman representation
+     */
+    public String intToRoman(int n) {
+
+    }
+
+}
