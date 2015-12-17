eafb8baa338cef2acc949bec303612c3792c1d6a
mitcc <mitcc@qq.com>
2015-12-18 02:35:04 +0800

    :basketball:

--- lintcode/PlusOne.java
+++ lintcode/PlusOne.java
@@ -1,7 +1,9 @@
 /*
- * Given a non-negative number represented as an array of digits, plus one to the number.
+ * Given a non-negative number represented as an array of 
+ * digits, plus one to the number.
 
-The digits are stored such that the most significant digit is at the head of the list.
+The digits are stored such that the most significant digit 
+is at the head of the list.
 
 Example
 Given [1,2,3] which represents 123, return [1,2,4].
