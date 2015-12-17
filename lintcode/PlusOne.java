653c9b37bfa6838762c3ecfd6b6c33ccf67b91a3
mitcc <mitcc@qq.com>
2015-12-17 23:51:24 +0800

    :basketball:

--- lintcode/PlusOne.java
+++ lintcode/PlusOne.java
@@ -3,6 +3,10 @@
  */
 public class PlusOne {
 
+    /**
+     * @param digits a number represented as an array of digits
+     * @return the result
+     */
     public int[] plusOne(int[] digits) {
 
     }
