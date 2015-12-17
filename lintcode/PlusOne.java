4b564f80bd4bb14c357d1b43f00fe2840edcc6ed
mitcc <mitcc@qq.com>
2015-12-18 02:38:29 +0800

    :basketball:

--- lintcode/PlusOne.java
+++ lintcode/PlusOne.java
@@ -43,4 +43,22 @@ public class PlusOne {
         return digits;
     }
 
+
+
+    public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        for (; i >= 0; --i) {
+            if (digits[i] != 9) {
+                digits[i] += 1;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if (i < 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+        return digits;
+    }
 }
