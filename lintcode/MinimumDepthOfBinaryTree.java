48bf4c761c73cb0cfef31a475a9c44678a6646e5
mitcc <mitcc@qq.com>
2015-12-19 01:47:31 +0800

    :basketball:

--- lintcode/MinimumDepthOfBinaryTree.java
+++ lintcode/MinimumDepthOfBinaryTree.java
@@ -1,8 +1,35 @@
 /*
- *
+ * Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Example
+Given a binary tree as follow:
+
+  1
+ / \ 
+2   3
+   / \
+  4   5
+The minimum depth is 2.
+ */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
 public class MinimumDepthOfBinaryTree {
 
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
     public int minDepth(TreeNode root) {
 
     }
