e26a2f62d69ba07b6e7b1603b0626c21ecac1a71
mitcc <mitcc@qq.com>
2015-12-19 21:13:03 +0800

    :basketball:

--- lintcode/SortColorsII.java
+++ lintcode/SortColorsII.java
@@ -4,6 +4,39 @@
 public class SortColorsII {
 
     public void sortColors2(int[] colors, int k) {
+        
+    }
+
+/*******************************************************************/
+
+    public void sortColors2(int[] colors, int k) {
+        quicksort(colors);    
+    }
+
+    public void quicksort(int[] A, int low, int high) {
+        if (low >= high) {
+            return;
+        } 
+        int index = low;
+        for (int i = low; i < high; ++i) {
+            if (A[i] < A[high]) {
+                swap(A, index++, i);
+            }
+        }
+        swap(A, index, high);
+        quicksort(A, low, index - 1);
+        quicksort(A, index + 1, high);
+    }
+
+    public void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+
+/*******************************************************************/
+
+    public void sortColors2(int[] colors, int k) {
         int[] count = new int[k];
         for (int color : colors) {
             count[color - 1]++;
