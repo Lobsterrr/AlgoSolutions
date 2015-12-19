42b28db44bb86e6884eb1462fd8932f0211205ec
mitcc <mitcc@qq.com>
2015-12-19 15:11:29 +0800

    :basketball:

--- lintcode/MinimumDepthOfBinaryTree.java
+++ lintcode/MinimumDepthOfBinaryTree.java
@@ -35,12 +35,8 @@ public class MinimumDepthOfBinaryTree {
     public int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
-        } else if (root.left == null && root.right == null) {
-            return 1;
-        } else if (root.left == null) {
-            return 1 + minDepth(root.right);
-        } else if (root.right == null) {
-            return 1 + minDepth(root.left);
+        } else if (root.left == null || root.right == null) {
+            return 1 + Math.max(minDepth(root.left), minDepth(root.right));
         }
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
