97cee604388093ddce8f5ea45f642683e18209ed
mitcc <mitcc@qq.com>
2015-12-20 14:02:55 +0800

    :basketball:

--- lintcode/MaximumProductSubarray.java
+++ lintcode/MaximumProductSubarray.java
@@ -1,8 +1,15 @@
 /*
- *
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example
+For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has the largest product = 6.
  */
 public class MaximumProductSubarray {
 
+    /**
+     * @param nums: an array of integers
+     * @return: an integer
+     */
     public int maxProduct(int[] nums) {
 
     }
