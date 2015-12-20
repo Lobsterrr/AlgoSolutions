c966b61d44b330207a48e3e6f69b7cbba9c406d5
mitcc <mitcc@qq.com>
2015-12-20 18:12:08 +0800

    :basketball:

--- lintcode/MaximumProductSubarray.java
+++ lintcode/MaximumProductSubarray.java
@@ -10,6 +10,20 @@ public class MaximumProductSubarray {
      * @param nums: an array of integers
      * @return: an integer
      */
+    public tin maxProduct(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int curMax = nums[0];
+        int curMin = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            int tmp = curMax;
+            curMax = Math.max(nums[i], Math.max(curMax * nums[i], curMin * nums[i]));
+            curMin = Math.min(nums[i], Math.min(tmp * nums[i], curMin * nums[i]));
+            result = Math.max(result, curMax);
+        }
+        return result;
+    }
+
+
     public int maxProduct(int[] nums) {
         int result = Integer.MIN_VALUE;
         int curMax = Integer.MIN_VALUE;
