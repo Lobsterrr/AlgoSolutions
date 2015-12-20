b47e50d9b6e66d1feaea5f7fe6de6fa5296f3312
mitcc <mitcc@qq.com>
2015-12-21 00:19:07 +0800

    :basketball:

--- lintcode/LongestConsecutiveSequence.java
+++ lintcode/LongestConsecutiveSequence.java
@@ -1,8 +1,21 @@
 /*
- *
+ * Given an unsorted array of integers, find the length 
+ * of the longest consecutive elements sequence.
+
+Example
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. 
+Return its length: 4.
+
+Clarification
+Your algorithm should run in O(n) complexity.
  */
 public class LongestConsecutiveSequence {
 
+    /**
+     * @param nums: A list of integers
+     * @return an integer
+     */
     public int longestConsecutive(int[] nums) {
 
     }
