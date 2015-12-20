1ff795e6c5879f888326ac13b57da141b55a3521
mitcc <mitcc@qq.com>
2015-12-21 02:47:37 +0800

    :basketball:

---
+++ lintcode/SpiralMatrixII.java
@@ -0,0 +1,26 @@
+/*
+ * Given an integer n, generate a square matrix filled 
+ * with elements from 1 to n^2 in spiral order.
+
+Example
+Given n = 3,
+
+You should return the following matrix:
+
+[
+  [ 1, 2, 3 ],
+  [ 8, 9, 4 ],
+  [ 7, 6, 5 ]
+]
+ */
+public class SpiralMatrixII {
+
+    /**
+     * @param n an integer
+     * @return a square matrix
+     */
+    public int[][] generateMatrix(int n) {
+
+    }
+
+}
