ad89004f8606f7df26cd65fc0e53fe34b5bb4cb9
mitcc <mitcc@qq.com>
2015-12-22 02:01:41 +0800

    :basketball:

--- lintcode/NextPermutation.java
+++ lintcode/NextPermutation.java
@@ -1,8 +1,22 @@
 /*
- *
+ * Given a list of integers, which denote a permutation.
+
+Find the next permutation in ascending order.
+
+Example
+For [1,3,2,3], the next permutation is [1,3,3,2]
+
+For [4,3,2,1], the next permutation is [1,2,3,4]
+
+Note
+The list may contains duplicate integers.
  */
 public class NextPermutation {
 
+    /**
+     * @param nums: an array of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
     public int[] nextPermutation(int[] nums) {
 
     }
