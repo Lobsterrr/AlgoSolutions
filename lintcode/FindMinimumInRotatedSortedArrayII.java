a1dc80cd5b8ec6dd4cea4bf03de563c3261af1d1
mitcc <mitcc@qq.com>
2015-12-21 23:42:31 +0800

    :basketball:

--- lintcode/FindMinimumInRotatedSortedArrayII.java
+++ lintcode/FindMinimumInRotatedSortedArrayII.java
@@ -18,7 +18,16 @@ public class FindMinimumInRotatedSortedArrayII {
      * @return: the minimum number in the array
      */
     public int findMin(int[] num) {
-
+        int low = 0;
+        int high = num.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (num[mid] > num[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
     }
 
 }
