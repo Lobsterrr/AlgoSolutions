ab3a0278ed8c1ad3f3b619ed423f568df555f10b
mitcc <mitcc@qq.com>
2015-12-23 20:15:53 +0800

    :basketball:

--- lintcode/SearchInRotateSortedArrayII.java
+++ lintcode/SearchInRotateSortedArrayII.java
@@ -23,52 +23,6 @@ public class SearchInRotatedSortedArrayII {
             } else if (A[low] == A[high]) {
                 high--;
             } else {
-                if (A[mid] > A[high] && (target > A[mid] || target < A[mid] && target < A[low]) || A[mid] < target && target < A[high]) {
-                    low = mid + 1;
-                }
-                if (A[high] < A[mid] && A[mid] < target || target < A[mid] && A[high] < A[mid] || A[mid] < target && target < A[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-
-
-                if (A[mid] > A[high]) {
-                    if (target > A[mid]) {
-                        low = mid + 1;
-                    } else if (target < A[mid]) {
-                        if (target < A[low]) {
-                            low = mid + 1;
-                        } else if (target > A[low]) {
-                            high = mid - 1;
-                        }
-                    }
-                } else if (A[mid] < A[high]) {
-                    if (target < A[mid]) {
-                        high = mid - 1;
-                    } else if (target > A[mid]) {
-                        if (target > A[high]) {
-                            high = mid - 1;
-                        } else if (target < A[high]) {
-                            low = mid + 1;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean search(int[] A, int target) {
-        int low = 0;
-        int high = A.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (A[mid] == target) {
-                return true;
-            } else if (A[low] == A[high]) {
-                high--;
-            } else {
                 if (A[mid] > A[high]) {
                     if (A[low] < target && target < A[mid]) {
                         high = mid - 1;
