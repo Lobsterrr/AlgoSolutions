3cc4e1f79b5a5f6f6e0572a1e40ff910ee62eeff
mitcc <mitcc@qq.com>
2015-12-24 22:55:53 +0800

    :basketball:

--- lintcode/LongestIncreasingSubsequence.java
+++ lintcode/LongestIncreasingSubsequence.java
@@ -1,8 +1,10 @@
 /*
- * Given a sequence of integers, find the longest increasing subsequence (LIS).
+ * Given a sequence of integers, find the longest increasing 
+ * subsequence (LIS).
 
 You code should return the length of the LIS.
 
+Have you met this question in a real interview? Yes
 Example
 For [5, 4, 1, 2, 3], the LIS  is [1, 2, 3], return 3
 
@@ -14,7 +16,11 @@ Time complexity O(n^2) or O(nlogn)
 Clarification
 What's the definition of longest increasing subsequence?
 
-    * The longest increasing subsequence problem is to find a subsequence of a given sequence in which the subsequence's elements are in sorted order, lowest to highest, and in which the subsequence is as long as possible. This subsequence is not necessarily contiguous, or unique.  
+    * The longest increasing subsequence problem is to find a 
+    * subsequence of a given sequence in which the subsequence's 
+    * elements are in sorted order, lowest to highest, and in which 
+    * the subsequence is as long as possible. This subsequence is 
+    * not necessarily contiguous, or unique.  
 
     * https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
  */
