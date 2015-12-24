3702129210caa4c59440db537ae2d700728729fd
mitcc <mitcc@qq.com>
2015-12-24 22:42:52 +0800

    :basketball:

--- lintcode/LongestIncreasingContinuousSubsequence.java
+++ lintcode/LongestIncreasingContinuousSubsequence.java
@@ -20,11 +20,11 @@ public class LongestIncreasingContinuousSubsequence {
     public int longestIncreasingContinuousSubsequence(int[] A) {
         int result = 0;
         for (int i = 1, count = 1; i < A.length; ++i) {
-            count = 1 + (A[i] > A[i - 1]) ? count : 1;
+            count = 1 + (A[i] > A[i - 1] ? count : 1);
             result =  Math.max(result, count);
         }
         for (int i = A.length - 1, count = 1; i >= 1; --i) {
-            count = 1 + (A[i] < A[i - 1]) ? count : 1;
+            count = 1 + (A[i] < A[i - 1] ? count : 1);
             result =  Math.max(result, count);
         }
         return result;
