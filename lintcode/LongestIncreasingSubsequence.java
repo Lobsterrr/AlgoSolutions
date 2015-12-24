664875c5d1f883576720dabb6a748f3ae31b70f2
mitcc <mitcc@qq.com>
2015-12-25 01:48:00 +0800

    :basketball:

--- lintcode/LongestIncreasingSubsequence.java
+++ lintcode/LongestIncreasingSubsequence.java
@@ -38,7 +38,7 @@ public class LongestIncreasingSubsequence {
         Arrays.fill(dp, 1);
         for (int i = 1; i < nums.length; ++i) {
             for (int j = i + 1; j < nums.length; ++j) {
-                if (nums[j] > nums[i]) {
+                if (nums[j] >= nums[i]) {
                     dp[j] = Math.max(dp[j], dp[i] + 1);
                 }
             }
