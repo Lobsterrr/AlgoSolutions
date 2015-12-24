9572e79f743130d377da72acd2022c59b446a213
mitcc <mitcc@qq.com>
2015-12-24 22:52:49 +0800

    :basketball:

--- lintcode/LongestIncreasingSubsequence.java
+++ lintcode/LongestIncreasingSubsequence.java
@@ -3,6 +3,10 @@
  */
 public class LongestIncreasingSubsequence {
 
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
     public int longestIncreasingSubsequence(int[] nums) {
 
     }
