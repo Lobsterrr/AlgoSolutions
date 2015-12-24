d074bc683b4f03daae8793bc51ba5f7b0ac87f43
mitcc <mitcc@qq.com>
2015-12-24 22:06:25 +0800

    :basketball:

--- lintcode/SegmentTreeQuery.java
+++ lintcode/SegmentTreeQuery.java
@@ -41,12 +41,9 @@ public class SegmentTreeQuery {
      *@return: The maximum number in the interval [start, end]
      */
     public int query(SegmentTreeNode root, int start, int end) {
-        if (start <= root.start&& end >= root.right) {
+        if (start <= root.start && root.end <= end) {
             return root.max;
-        } else if (start <= root.left && end <= root.right) {
-
-        } else if (start >= 
-
+        }
     }
 
 }
