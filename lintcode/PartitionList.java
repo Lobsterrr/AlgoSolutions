2ad412e2161471f5bda5c2c56d30da82d9818e9b
mitcc <mitcc@qq.com>
2015-12-25 13:14:33 +0800

    :basketball:

--- lintcode/PartitionList.java
+++ lintcode/PartitionList.java
@@ -29,6 +29,25 @@ public class PartitionList {
      * @return: a ListNode 
      */
     public ListNode partition(ListNode head, int x) {
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = new ListNode(0);
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        while (head != null) {
+            if (head.val < x) {
+                cur1.next = head;
+                cur1 = cur1.next;
+            } else {
+                cur2.next = head;
+                cur2 = cur2.next;
+            }
+            head = head.next;
+        }
+        cur1.next = l2;
+        return l1;
+    }
+
+    public ListNode partition(ListNode head, int x) {
         List<Integer> small = new ArrayList<Integer>();
         List<Integer> large = new ArrayList<Integer>();
         for (ListNode cur = head; cur != null; cur = cur.next) {
