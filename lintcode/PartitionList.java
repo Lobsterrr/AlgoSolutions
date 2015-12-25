a57e109bf6d116acb1880b9c168dcc1ad7a29f25
mitcc <mitcc@qq.com>
2015-12-25 09:24:52 +0800

    :basketball:

--- lintcode/PartitionList.java
+++ lintcode/PartitionList.java
@@ -3,6 +3,11 @@
  */
 public class PartitionList {
 
+    /**
+     * @param head: The first node of linked list.
+     * @param x: an integer
+     * @return: a ListNode 
+     */
     public ListNode partition(ListNode head, int x) {
 
     }
