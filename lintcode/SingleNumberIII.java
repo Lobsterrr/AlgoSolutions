be20a9f1669b308c95104fc5cfc976eeead6095e
mitcc <mitcc@qq.com>
2015-12-26 09:37:21 +0800

    :basketball:

--- lintcode/SingleNumberIII.java
+++ lintcode/SingleNumberIII.java
@@ -3,6 +3,10 @@
  */
 public class SingleNumberIII {
 
+    /**
+     * @param A : An integer array
+     * @return : Two integers
+     */
     public List<Integer> singleNumberIII(int[] A) {
 
     }
