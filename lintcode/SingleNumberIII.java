b6460e61574ac965e31da99654ffad9ef36af08c
mitcc <mitcc@qq.com>
2015-12-26 19:29:17 +0800

    :basketball:

--- lintcode/SingleNumberIII.java
+++ lintcode/SingleNumberIII.java
@@ -1,5 +1,6 @@
 /*
- * Given 2*n + 2 numbers, every numbers occurs twice except two, find them.
+ * Given 2*n + 2 numbers, every numbers occurs twice except 
+ * two, find them.
 
 Example
 Given [1,2,2,3,4,4,5,3] return 1 and 5
