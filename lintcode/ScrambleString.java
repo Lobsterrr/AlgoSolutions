85572d839a6dd2ec08c123c97405a247cccf9e35
mitcc <mitcc@qq.com>
2015-12-26 22:37:16 +0800

    :basketball:

--- lintcode/ScrambleString.java
+++ lintcode/ScrambleString.java
@@ -3,6 +3,11 @@
  */
 public class ScrambleString {
 
+    /**
+     * @param s1 A string
+     * @param s2 Another string
+     * @return whether s2 is a scrambled string of s1
+     */
     public boolean isScamble(String s1, String s2) {
 
     }
