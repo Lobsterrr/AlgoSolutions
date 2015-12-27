2fb1171488898e10fbd16709274bf44717b58d57
mitcc <mitcc@qq.com>
2015-12-27 21:54:16 +0800

    :basketball:

--- lintcode/SegmentTreeBuildII.java
+++ lintcode/SegmentTreeBuildII.java
@@ -1,5 +1,6 @@
 /*
- * The structure of Segment Tree is a binary tree which each node has two attributes start and end denote an segment / interval.
+ * The structure of Segment Tree is a binary tree which each node has two 
+ * attributes start and end denote an segment / interval.
 
 start and end are both integers, they should be assigned in following rules:
 
@@ -7,7 +8,9 @@ The root's start and end is given by build method.
 The left child of node A has start=A.left, end=(A.left + A.right) / 2.
 The right child of node A has start=(A.left + A.right) / 2 + 1, end=A.right.
 if start equals to end, there will be no children for this node.
-Implement a build method with a given array, so that we can create a corresponding segment tree with every node value represent the corresponding interval max value in the array, return the root of this segment tree.
+Implement a build method with a given array, so that we can create a 
+corresponding segment tree with every node value represent the corresponding 
+interval max value in the array, return the root of this segment tree.
 
 Example
 Given [3,2,1,4]. The segment tree will be:
@@ -18,7 +21,8 @@ Given [3,2,1,4]. The segment tree will be:
         /        \               /             \
 [0, 0](max = 3)  [1, 1](max = 2)[2, 2](max = 1) [3, 3] (max = 4)
 Clarification
-Segment Tree (a.k.a Interval Tree) is an advanced data structure which can support queries like:
+Segment Tree (a.k.a Interval Tree) is an advanced data structure which can 
+support queries like:
 
 which of these intervals contain a given point
 which of these points are in a given interval
