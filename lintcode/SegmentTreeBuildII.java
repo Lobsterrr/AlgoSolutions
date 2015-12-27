899e8ffe084356ab2ff8489957cc3bf7d8010db1
mitcc <mitcc@qq.com>
2015-12-27 21:52:25 +0800

    :basketball:

--- lintcode/SegmentTreeBuildII.java
+++ lintcode/SegmentTreeBuildII.java
@@ -9,7 +9,6 @@ The right child of node A has start=(A.left + A.right) / 2 + 1, end=A.right.
 if start equals to end, there will be no children for this node.
 Implement a build method with a given array, so that we can create a corresponding segment tree with every node value represent the corresponding interval max value in the array, return the root of this segment tree.
 
-Have you met this question in a real interview? Yes
 Example
 Given [3,2,1,4]. The segment tree will be:
 
