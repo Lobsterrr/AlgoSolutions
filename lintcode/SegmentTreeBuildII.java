c5ae0e54323ca3c57205079d4cc8ef4f628a5b7a
mitcc <mitcc@qq.com>
2015-12-27 21:50:55 +0800

    :basketball:

--- lintcode/SegmentTreeBuildII.java
+++ lintcode/SegmentTreeBuildII.java
@@ -3,6 +3,10 @@
  */
 public class SegmentTreeBuildII {
 
+    /**
+     *@param A: a list of integer
+     *@return: The root of Segment Tree
+     */
     public SegmentTreeNode build(int[] A) {
 
     }
