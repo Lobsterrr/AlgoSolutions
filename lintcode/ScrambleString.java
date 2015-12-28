689927443cd6319793c316cb415fb34e28459368
mitcc <mitcc@qq.com>
2015-12-28 14:57:13 +0800

    :basketball:

--- lintcode/ScrambleString.java
+++ lintcode/ScrambleString.java
@@ -53,20 +53,7 @@ public class ScrambleString {
      * @return whether s2 is a scrambled string of s1
      */
     public boolean isScramble(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
-        for (int i = 1; i < s1.length(); ++i) {
-            if (isScramble(s1.substring(0, i), s2.substring(0, i)) 
-                    && isScramble(s1.substring(i), s2.substring(i)) 
-                    || isScramble(s1.substring(0, i), 
-                        s2.substring(s2.length() - i)) 
-                    && isScramble(s1.substring(i), 
-                        s2.substring(0, s2.length() - i))) {
-                return true;
-            }
-        }
-        return false;
+
     }
 
 /*******************************************************************/
