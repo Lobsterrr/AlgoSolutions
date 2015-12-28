8fe94e9caea0a89f5b62ec8aa36c9cc1f11eb212
mitcc <mitcc@qq.com>
2015-12-28 21:52:08 +0800

    :basketball:

--- lintcode/FirstPositionOfTarget.java
+++ lintcode/FirstPositionOfTarget.java
@@ -3,6 +3,11 @@
  */
 public class FirstPositionOfTarget {
 
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
     public int binarySearch(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
