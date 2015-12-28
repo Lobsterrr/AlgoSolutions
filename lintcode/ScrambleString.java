95804227503c4c678ac167d35b69589ccc824d1f
mitcc <mitcc@qq.com>
2015-12-28 14:56:52 +0800

    :basketball:

--- lintcode/ScrambleString.java
+++ lintcode/ScrambleString.java
@@ -69,6 +69,8 @@ public class ScrambleString {
         return false;
     }
 
+/*******************************************************************/
+
     public boolean isScramble(String s1, String s2) {
         if (s1.equals(s2)) {
             return true;
