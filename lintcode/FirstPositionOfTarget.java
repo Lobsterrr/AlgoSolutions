3ab2fa3c5615cc0739d1a8cd2905a044a97832d5
mitcc <mitcc@qq.com>
2015-12-28 21:42:07 +0800

    :basketball:

--- lintcode/FirstPositionOfTarget.java
+++ lintcode/FirstPositionOfTarget.java
@@ -4,7 +4,22 @@
 public class FirstPositionOfTarget {
 
     public int binarySearch(int[] nums, int target) {
-        
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (start == mid && nums[start] == target) {
+                break;
+            }
+            if (nums[mid] == target) {
+                high = mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+        }
+        return start;
     }
 
 }
