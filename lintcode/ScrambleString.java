f3255e9385ccc40d77f831800b700d60f8f8354a
mitcc <mitcc@qq.com>
2015-12-28 14:56:13 +0800

    :basketball:

--- lintcode/ScrambleString.java
+++ lintcode/ScrambleString.java
@@ -69,4 +69,21 @@ public class ScrambleString {
         return false;
     }
 
+    public boolean isScramble(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+        for (int i = 1; i < s1.length(); ++i) {
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) 
+                    && isScramble(s1.substring(i), s2.substring(i)) 
+                    || isScramble(s1.substring(0, i), 
+                        s2.substring(s2.length() - i)) 
+                    && isScramble(s1.substring(i), 
+                        s2.substring(0, s2.length() - i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
