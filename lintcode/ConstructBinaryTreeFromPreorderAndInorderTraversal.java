a64ce560f60e94e2b5538b54e279c9348993f42d
mitcc <mitcc@qq.com>
2015-12-30 02:08:59 +0800

    :basketball:

--- lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -29,7 +29,8 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
      *@return : Root of a tree
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+        return buildTree(preorder, 0, preorder.length - 1, 
+                inorder, 0, inorder.length - 1);
     }
 
     public TreeNode buildTree(int[] preorder, int pLow, int pHigh, 
@@ -40,8 +41,10 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         TreeNode root = new TreeNode(preorder[pLow]);
         int i = iLow;
         for (; i < iHigh && inorder[i] != preorder[pLow]; ++i);
-        root.left = buildTree(preorder, pLow + 1, i - iLow + pLow, inorder, iLow, i - 1);
-        root.right = buildTree(preorder, i - iLow + 1 + pLow, pHigh, inorder, i + 1, iHigh);
+        root.left = buildTree(preorder, pLow + 1, i - iLow + pLow, 
+                inorder, iLow, i - 1);
+        root.right = buildTree(preorder, i - iLow + 1 + pLow, pHigh, 
+                inorder, i + 1, iHigh);
         return root;
     }
 
