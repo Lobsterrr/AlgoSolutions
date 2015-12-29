f92c440e5ea646eb456310267b9467a06f40a07d
mitcc <mitcc@qq.com>
2015-12-29 23:57:19 +0800

    :basketball:

--- lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -3,6 +3,11 @@
  */
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
+    /**
+     *@param preorder : A list of integers that preorder traversal of a tree
+     *@param inorder : A list of integers that inorder traversal of a tree
+     *@return : Root of a tree
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
 
     }
