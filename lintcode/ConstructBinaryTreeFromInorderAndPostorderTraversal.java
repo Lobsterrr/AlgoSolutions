c44178867863d2c40b1d107f38148df928a11919
mitcc <mitcc@qq.com>
2015-12-29 09:02:52 +0800

    :basketball:

--- lintcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ lintcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Have you met this question in a real interview? Yes
+Example
+Given inorder [1,2,3] and postorder [1,3,2], return a tree:
+
+  2
+ / \
+1   3
+Note
+You may assume that duplicates do not exist in the tree.
  */
 /**
  * Definition of TreeNode:
