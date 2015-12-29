ea66369ddd6fb168994d2e714278db2d166dddac
mitcc <mitcc@qq.com>
2015-12-29 23:57:56 +0800

    :basketball:

--- lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ lintcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Example
+Given in-order [1,2,3] and pre-order [2,1,3], return a tree:
+
+  2
+ / \
+1   3
+Note
+You may assume that duplicates do not exist in the tree.
  */
 /**
  * Definition of TreeNode:
