c750d05a8cc37a10580fb29f25f6de7369e4c88b
mitcc <mitcc@qq.com>
2015-12-30 23:13:39 +0800

    :basketball:

--- lintcode/DistinctSubsequences.java
+++ lintcode/DistinctSubsequences.java
@@ -3,6 +3,10 @@
  */
 public class DistinctSubsequences {
 
+    /**
+     * @param S, T: Two string.
+     * @return: Count the number of distinct subsequences
+     */
     public int numDistinct(String S, String T) {
 
     }
