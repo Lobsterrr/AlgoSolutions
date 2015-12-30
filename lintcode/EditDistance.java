140fe4e45c4d11f6472099e44c83a04642aab632
mitcc <mitcc@qq.com>
2015-12-30 22:09:27 +0800

    :basketball:

--- lintcode/EditDistance.java
+++ lintcode/EditDistance.java
@@ -25,9 +25,9 @@ public class EditDistance {
         for (int j = 0; j < word2.length(); ++j) {
             dp[0][j + 1] = j + 1;
         }
-        for (int i = 0; i < word1.length(); ++i) {
-            for (int j = 0; j < word2.length(); ++j) {
-                if (word1.charAt(i) == word2.charAt(j)) {
+        for (int j = 0; j < word2.length(); ++j) {
+            for (int i = 0; i < word1.length(); ++i) {
+                if (word2.charAt(j) == word1.charAt(i)) {
                     dp[i + 1][j + 1] = dp[i][j];
                 } else {
                     dp[i + 1][j + 1] = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
