5a5d05f9eaef6f27b429f1dc7f3f1fb4b701ec49
mitcc <mitcc@qq.com>
2015-12-30 22:02:00 +0800

    :basketball:

--- lintcode/EditDistance.java
+++ lintcode/EditDistance.java
@@ -20,6 +20,12 @@ public class EditDistance {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 0; i < word1.length(); ++i) {
+            dp[i + 1][0] = i + 1;
+        }
+        for (int j = 0; j < word2.length(); ++j) {
+            dp[0][j + 1] = j + 1;
+        }
+        for (int i = 0; i < word1.length(); ++i) {
             for (int j = 0; j < word2.length(); ++j) {
                 if (word1.charAt(i) == word2.charAt(j)) {
                     dp[i + 1][j + 1] = dp[i][j];
