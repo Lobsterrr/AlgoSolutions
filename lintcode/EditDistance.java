9c97921bbfaa196ff95d3cfb89dbc6d1060aad59
mitcc <mitcc@qq.com>
2015-12-30 22:46:35 +0800

    :basketball:

--- lintcode/EditDistance.java
+++ lintcode/EditDistance.java
@@ -42,32 +42,11 @@ public class EditDistance {
                 } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 
+                            Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                 }
             }
         }    
-        return dp[word1.length()][word2.length()];
-    }
-
-
-
-    public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        for (int i = 0; i < word1.length(); ++i) {
-            dp[i + 1][0] = i + 1;
-        }
-        for (int j = 0; j < word2.length(); ++j) {
-            dp[0][j + 1] = j + 1;
-        }
-        for (int j = 0; j < word2.length(); ++j) {
-            for (int i = 0; i < word1.length(); ++i) {
-                if (word2.charAt(j) == word1.charAt(i)) {
-                    dp[i + 1][j + 1] = dp[i][j];
-                } else {
-                    dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i][j + 1], dp[i + 1][j])) + 1;
-                }
-            }
-        }
         return dp[word1.length()][word2.length()];
     }
 
