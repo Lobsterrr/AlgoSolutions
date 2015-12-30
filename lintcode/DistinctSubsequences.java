8556fdc1e220d61edb612b693f167717d863c79e
mitcc <mitcc@qq.com>
2015-12-30 23:14:40 +0800

    :basketball:

--- lintcode/DistinctSubsequences.java
+++ lintcode/DistinctSubsequences.java
@@ -1,7 +1,12 @@
 /*
- * Given a string S and a string T, count the number of distinct subsequences of T in S.
+ * Given a string S and a string T, count the number of 
+ * distinct subsequences of T in S.
 
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed 
+from the original string by deleting some (can be none) of 
+the characters without disturbing the relative positions 
+of the remaining characters. (ie, "ACE" is a subsequence of 
+"ABCDE" while "AEC" is not).
 
 Example
 Given S = "rabbbit", T = "rabbit", return 3.
@@ -9,7 +14,8 @@ Given S = "rabbbit", T = "rabbit", return 3.
 Challenge
 Do it in O(n^2) time and O(n) memory.
 
-O(n2) memory is also acceptable if you do not know how to optimize memory.
+O(n2) memory is also acceptable if you do not know how to 
+optimize memory.
  */
 public class DistinctSubsequences {
 
