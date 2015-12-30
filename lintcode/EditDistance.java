6a35de525e7cc98488d4a7eb7f0fd137553a5a33
mitcc <mitcc@qq.com>
2015-12-30 21:35:25 +0800

    :basketball:

--- lintcode/EditDistance.java
+++ lintcode/EditDistance.java
@@ -3,6 +3,10 @@
  */
 public class EditDistance {
 
+    /**
+     * @param word1 & word2: Two string.
+     * @return: The minimum number of steps.
+     */
     public int minDistance(String word1, String word2) {
         
     }
