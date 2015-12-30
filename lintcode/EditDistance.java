f9c20b49b91903d506d1f74cbfea3be24aed3afa
mitcc <mitcc@qq.com>
2015-12-30 22:31:13 +0800

    :basketball:

--- lintcode/EditDistance.java
+++ lintcode/EditDistance.java
@@ -18,6 +18,23 @@ public class EditDistance {
      * @return: The minimum number of steps.
      */
     public int minDistance(String word1, String word2) {
+        int[] dp = new int[word2.length() + 1];
+        dp[0] = 1;
+        for (int i = 0; i < word2.length(); ++i) {
+            for (int j = 0; j < word1.length(); ++j) {
+                if (word1.charAt(j) == word2.charAt(i)) {
+                    dp[i + 1] = Math.min(dp[i + 1], dp[i]);
+                } else {
+                    dp[i + 1] = Math.min(dp[i + 1], dp[i]);
+                }
+            }
+        }
+        return dp[word2.length()];
+    }
+
+
+
+    public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 0; i < word1.length(); ++i) {
             dp[i + 1][0] = i + 1;
@@ -30,7 +47,7 @@ public class EditDistance {
                 if (word2.charAt(j) == word1.charAt(i)) {
                     dp[i + 1][j + 1] = dp[i][j];
                 } else {
-                    dp[i + 1][j + 1] = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
+                    dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i][j + 1], dp[i + 1][j])) + 1;
                 }
             }
         }
