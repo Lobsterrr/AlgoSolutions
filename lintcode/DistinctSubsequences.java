a2d9b17976ab8b1eb8672a7346dd2e2b4e7ee614
mitcc <mitcc@qq.com>
2016-01-01 01:04:56 +0800

    :basketball:

--- lintcode/DistinctSubsequences.java
+++ lintcode/DistinctSubsequences.java
@@ -25,10 +25,9 @@ public class DistinctSubsequences {
      */
     public int numDistinct(String S, String T) {
         int[][] dp = new int[S.length() + 1][T.length() + 1];
-        dp[0][0] = 1;
-        for (int i = 0; i <= S.length(); ++i) {
-            dp[i + 1][0] = dp[i][0];
-            for (int j = 0; j <= T.length(); ++j) {
+        for (int i = 0; i < S.length(); ++i) {
+            dp[i + 1][0] = 1;
+            for (int j = 0; j < T.length(); ++j) {
                 dp[i + 1][j + 1] = dp[i][j + 1];
                 if (S.charAt(i) == T.charAt(j)) {
                     dp[i + 1][j + 1] += dp[i][j];
