1b39bbf6e6d9e3eb73811bd050c244c02e91e131
mitcc <mitcc@qq.com>
2015-12-31 19:31:11 +0800

    :basketball:

--- lintcode/DistinctSubsequences.java
+++ lintcode/DistinctSubsequences.java
@@ -25,6 +25,11 @@ public class DistinctSubsequences {
      */
     public int numDistinct(String S, String T) {
         int[][] dp = new int[S.length() + 1][T.length() + 1];
+        for (int i = 0; i < S.length(); ++i) {
+            for (int j = 0; j < T.length(); ++j) {
+
+            }
+        }
     }
 
 }
