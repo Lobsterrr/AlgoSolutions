4fa08c246de46e5a37727f16993c9a0934ef02c9
mitcc <mitcc@qq.com>
2016-01-02 19:35:30 +0800

    :basketball:

--- lintcode/NQueensII.java
+++ lintcode/NQueensII.java
@@ -3,6 +3,11 @@
  */
 public class NQueensII {
 
+    /**
+     * Calculate the total number of distinct N-Queen solutions.
+     * @param n: The number of queens.
+     * @return: The total number of distinct solutions.
+     */
     public int totalNQueens(int n) {
 
     }
