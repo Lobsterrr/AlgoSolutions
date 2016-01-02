ca59dd088891ec90f685e904b05afa3b848e6cdd
mitcc <mitcc@qq.com>
2016-01-03 00:16:05 +0800

    :basketball:

--- lintcode/NQueensII.java
+++ lintcode/NQueensII.java
@@ -1,7 +1,8 @@
 /*
  * Follow up for N-Queens problem.
 
-Now, instead outputting board configurations, return the total number of distinct solutions.
+Now, instead outputting board configurations, 
+return the total number of distinct solutions.
 
 Example
 For n=4, there are 2 distinct solutions.
