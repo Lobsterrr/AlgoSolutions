cc8159c511407bf41685e9146eb5172785ab2e55
mitcc <mitcc@qq.com>
2016-01-05 23:59:42 +0800

    :basketball:

--- lintcode/NQueensII.java
+++ lintcode/NQueensII.java
@@ -17,30 +17,28 @@ public class NQueensII {
     int result = 0;
 
     public int totalNQueens(int n) {
-        int[][] board = new int[n][n];
+        int[] board = new int[n];
         dfs(board, 0);
         return result;
     }
 
-    public void dfs(int[][] board, int row) {
+    public void dfs(int[] board, int row) {
         if (row == board.length) {
             result++;
             return;
         }
         for (int col = 0; col < board.length; ++col) {
             if (canPlace(board, row, col)) {
-                board[row][col] = 1;
+                board[row] = col;
                 dfs(board, row + 1);
-                board[row][col] = 0;
+                board[row];
             }
         }
     }
     
-    public boolean canPlace(int[][] board, int row, int col) {
-        for (int i = 0; i < board.length; ++i) {
-            if (board[row][i] == 1 || board[i][col] == 1 || 
-                    board[i][Math.abs(i - row + col) % board.length] == 1 || 
-                    board[i][Math.abs(row + col - i) % board.length] == 1) {
+    public boolean canPlace(int[] board, int row, int col) {
+        for (int i = 0; i < row; ++i) {
+            if (board[i] == col || Math.abs(row - i) == Math.abs(col - board[i])) {
                 return false;
             }
         }
