018c77c8ca69af3f319f18d6aa930572acc03885
mitcc <mitcc@qq.com>
2016-01-06 23:11:59 +0800

    :basketball:

--- lintcode/ThreeSum.java
+++ lintcode/ThreeSum.java
@@ -7,6 +7,37 @@ public class ThreeSum {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         Arrays.sort(numbers);
         for (int i = 0; i < numbers.length - 2; ++i) {
+            if (i > 0 && numbers[i] == numbers[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = numbers.length - 1;
+            int sum = numbers[i] + numbers[low] + numbers[high];
+            if (sum == 0) {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(numbers[i]);
+                list.add(numbers[low]);
+                list.add(numbers[high]);
+                result.add(list);
+                do {
+                    low++;
+                } while (low > i && low < numbers.length && numbers[low] == numbers[low - 1]);
+                do {
+                    high--;
+                } while (high >= 0 && high < numbers.length - 1 && numbers[high] == numbers[high + 1]);
+            } else if (sum < 0) {
+                low++;
+            } else {
+                high++;
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<ArrayList<Integer>> threeSum(int[] numbers) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 2; ++i) {
             Set<Integer> set = new HashSet<Integer>();
             for (int j = i + 1; j < numbers.length; ++j) {
                 if (set.contains(-numbers[i] - numbers[j])) {
