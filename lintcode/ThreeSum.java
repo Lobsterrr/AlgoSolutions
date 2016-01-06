6c39e81dfb89820fb04b392f89dfb231e56b5b10
mitcc <mitcc@qq.com>
2016-01-06 22:41:17 +0800

    :basketball:

--- lintcode/ThreeSum.java
+++ lintcode/ThreeSum.java
@@ -4,7 +4,22 @@
 public class ThreeSum {
 
     public ArrayList<ArrayList<Integer>> threeSum(int[] numbers) {
-
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 2; ++i) {
+            Set<Integer> set = new HashSet<Integer>();
+            for (int j = i + 1; j < numbers.length; ++j) {
+                if (set.contains(-numbers[i] - numbers[j])) {
+                    ArrayList<Integer> list = new ArrayList<Integer>();
+                    list.add(numbers[i]);
+                    list.add(numbers[j]);
+                    list.add(-numbers[i] - numbers[j]);
+                    result.add(list);
+                }
+                set.add(numbers[j]);
+            }
+        }
+        return result;
     }
 
 }
