db457ef4d1c836f14cc0ffda8ee1585000201610
mitcc <mitcc@qq.com>
2016-01-06 23:53:17 +0800

    :basketball:

---
+++ lintcode/ThreeSumClosest.java
@@ -0,0 +1,17 @@
+/*
+ *
+ */
+public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] numbers, int target) {
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 2; ++i) {
+            int low = i + 1;
+            int high = numbers.length - 1;
+            while (low < high) {
+
+            }
+        }
+    }
+
+}
