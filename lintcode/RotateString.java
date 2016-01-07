6bb3e47d2e4eb56dbe76711c7d3425a78e01756d
mitcc <mitcc@qq.com>
2016-01-07 23:17:35 +0800

    :basketball:

--- lintcode/RotateString.java
+++ lintcode/RotateString.java
@@ -5,7 +5,7 @@ public class RotateString {
 
     public void rotateString(char[] str, int offset) {
         for (int i = 0; i < offset; ++i) {
-            int tmp = str[str.length - 1];
+            char tmp = str[str.length - 1];
             for (int j = str.length - 1; j > 0; --j) {
                 str[j] = str[j - 1];
             }
