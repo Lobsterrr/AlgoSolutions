7a4236f8f87376d8349a433f6138e8501128f416
mitcc <mitcc@qq.com>
2016-01-07 09:26:29 +0800

    :basketball:

--- lintcode/Subsets.java
+++ lintcode/Subsets.java
@@ -3,17 +3,17 @@
  */
 public class Subsets {
 
-    public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         result.add(new ArrayList<Integer>());
         for (int num : nums) {
+            ArrayList<ArrayList<Integer>> newResult = new ArrayList<ArrayList<Integer>>();
             for (ArrayList<Integer> list : result) {
                 ArrayList<Integer> newList = new ArrayList<Integer>(list);
                 newList.add(num);
-                result.add(newList);
+                newResult.add(newList);
             }
+            result.addAll(newResult);
         }
         return result;
-    }
 
 }
