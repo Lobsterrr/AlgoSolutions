a23d49e5ba46801dbd18d90afd85abf8670ba603
mitcc <mitcc@qq.com>
2016-01-07 23:53:16 +0800

    :basketball:

--- lintcode/RotateString.java
+++ lintcode/RotateString.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Given a string and an offset, rotate string by offset. (rotate from left to right)
+
+Have you met this question in a real interview? Yes
+Example
+Given "abcdefg".
+
+offset=0 => "abcdefg"
+offset=1 => "gabcdef"
+offset=2 => "fgabcde"
+offset=3 => "efgabcd"
+Challenge
+Rotate in-place with O(1) extra memory.
  */
 public class RotateString {
 
