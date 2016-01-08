3aa0ea09c83d3bb5e134772c67a2a1e9c50a299d
mitcc <mitcc@qq.com>
2016-01-08 09:24:00 +0800

    :basketball:

--- lintcode/TwoSum.java
+++ lintcode/TwoSum.java
@@ -3,6 +3,11 @@
  */
 public class TwoSum {
 
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+     */
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < numbers.length; ++i) {
