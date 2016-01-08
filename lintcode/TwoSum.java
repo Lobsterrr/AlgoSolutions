a9cd8625f1802f0bb724022fb766c886b3977126
mitcc <mitcc@qq.com>
2016-01-08 09:21:22 +0800

    :basketball:

--- lintcode/TwoSum.java
+++ lintcode/TwoSum.java
@@ -4,7 +4,14 @@
 public class TwoSum {
 
     public int[] twoSum(int[] numbers, int target) {
-
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; ++i) {
+            if (map.containsKey(numbers[i])) {
+                return new int[] {map.get(numbers[i]), i + 1};
+            }
+            map.put(target - numbers[i], i + 1);
+        }
+        return new int[2];
     }
 
 }
