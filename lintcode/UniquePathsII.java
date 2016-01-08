57a67535f4c7693a4474c58447f7e5161ddba7bc
mitcc <mitcc@qq.com>
2016-01-08 10:18:16 +0800

    :basketball:

--- lintcode/UniquePathsII.java
+++ lintcode/UniquePathsII.java
@@ -7,8 +7,12 @@ public class UniquePathsII {
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
         for (int i = 0; i < obstacleGrid.length; ++i) {
             for (int j = 0; j < obstacleGrid[0].length; ++j) {
-                if (i == 0 || j == 0) {
+                if (i == 0 && j == 0) {
                     dp[i][j] = 1;
+                } else if (i == 0 && j > 0) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (i > 0 && j == 0) {
+                    dp[i][j] = dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
