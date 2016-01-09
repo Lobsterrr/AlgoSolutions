7e3e47220af7b6d7249e5a595103be31b155c4e4
mitcc <mitcc@qq.com>
2016-01-09 23:41:29 +0800

    :basketball:

--- lintcode/ReverseLinkedListII.java
+++ lintcode/ReverseLinkedListII.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Reverse a linked list from position m to n.
+
+Example
+Given 1->2->3->4->5->NULL, m = 2 and n = 4, return 1->4->3->2->5->NULL.
+
+Note
+Given m, n satisfy the following condition: 1 ≤ m ≤ n ≤ length of list.
+
+Challenge
+Reverse it in-place and in one-pass
  */
 /**
  * Definition for ListNode
