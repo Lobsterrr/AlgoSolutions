32f0cae64225cb84cc6021684eb4f1958c980fd0
mitcc <mitcc@qq.com>
2016-01-09 23:39:24 +0800

    :basketball:

--- lintcode/ReverseLinkedListII.java
+++ lintcode/ReverseLinkedListII.java
@@ -3,6 +3,11 @@
  */
 public class ReverseLinkedListII {
 
+    /**
+     * @param ListNode head is the head of the linked list 
+     * @oaram m and n
+     * @return: The head of the reversed ListNode
+     */
     public ListNode reverseBetween(ListNode head, int m, int n) {
 
     }
