63bfc5327693b75aac9b6cde42cf480672a6be8c
mitcc <mitcc@qq.com>
2016-01-10 18:49:15 +0800

    :basketball:

--- lintcode/SortList.java
+++ lintcode/SortList.java
@@ -3,6 +3,11 @@
  */
 public class SortList {
 
+    /**
+     * @param head: The head of linked list.
+     * @return: You should return the head of the sorted linked list,
+                    using constant space complexity.
+     */
     public ListNode sortList(ListNode head) {
 
     }
