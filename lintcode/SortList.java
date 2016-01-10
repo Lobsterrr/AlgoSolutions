5df7652969d033b22a17dce3e1c1304b8a9cd04c
mitcc <mitcc@qq.com>
2016-01-10 22:07:43 +0800

    :basketball:

--- lintcode/SortList.java
+++ lintcode/SortList.java
@@ -40,7 +40,7 @@ public class SortList {
     public ListNode mergeTwoList(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
-        while (l1 != null && l2 != null) {
+        for (; l1 = null && l2 != null; cur = cur.next) {
             if (l1.val < l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
@@ -48,7 +48,6 @@ public class SortList {
                 cur.next = l2;
                 l2 = l2.next;
             }
-            cur = cur.next;
         }
         cur.next = l1 != null ? l1 : l2;
         return dummy.next;
