96047f6c7ed2a7a890554471feb7020f00ab8c41
mitcc <mitcc@qq.com>
2016-01-11 00:13:17 +0800

    :basketball:

--- lintcode/ReorderList.java
+++ lintcode/ReorderList.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You must do this in-place without altering the nodes' values.
+
+
+
+Have you met this question in a real interview? Yes
+Example
+For example,
+Given 1->2->3->4->null, reorder it to 1->4->2->3->null.
  */
 /**
  * Definition for ListNode.
