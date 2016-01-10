cdfebb6d31978dff1b6dc1448ffad47a406fd0e0
mitcc <mitcc@qq.com>
2016-01-10 21:27:35 +0800

    :basketball:

--- lintcode/SortList.java
+++ lintcode/SortList.java
@@ -26,6 +26,37 @@ public class SortList {
         if (head == null || head.next == null) {
             return head;
         }
+        ListNode cur1 = head;
+        ListNode cur2 = head.next;
+        while (cur2 != null && cur2.next != null) {
+            cur2 = cur2.next.next;
+            cur1 = cur1.next;
+        }
+    }
+
+    public ListNode mergeList(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
         ListNode node = head;
         head = sortList(head.next);
         node.next = null;
