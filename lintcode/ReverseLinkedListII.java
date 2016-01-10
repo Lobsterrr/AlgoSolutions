d58901b273d91ab4b60d414a13bc70431aa53ea5
mitcc <mitcc@qq.com>
2016-01-10 18:34:03 +0800

    :basketball:

--- lintcode/ReverseLinkedListII.java
+++ lintcode/ReverseLinkedListII.java
@@ -53,23 +53,4 @@ public class ReverseLinkedListII {
         return dummy.next;
     }
 
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        for (int i = 1; i < m; ++i) {
-            prev = prev.next;
-        }
-        ListNode head2 = prev;
-        prev = prev.next;
-        ListNode cur = prev.next;
-        for (int i = m; i < n; ++i) {
-            prev.next = cur.next;
-            cur.next = head2.next;
-            head2.next = cur;
-            cur = prev.next;
-        }
-        return dummy.next;
-    }
-
 }
