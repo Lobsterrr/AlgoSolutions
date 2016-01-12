ca4f64b4db14748d5728fecbec829b149a110efb
mitcc <mitcc@qq.com>
2016-01-12 21:02:23 +0800

    :basketball:

--- leetcode/algorithms/PowerOfThree.java
+++ leetcode/algorithms/PowerOfThree.java
@@ -7,6 +7,10 @@
 public class PowerOfThree {
 
     public boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+
+    public boolean isPowerOfThree(int n) {
         long power = 1;
         while (power < n) {
             power *= 3;
