3953e9f1560712005991863711b46bbafeeb1a7e
mitcc <mitcc@qq.com>
2016-01-13 00:35:10 +0800

    :basketball:

--- lintcode/Anagrams.java
+++ lintcode/Anagrams.java
@@ -4,7 +4,10 @@
 public class Anagrams {
 
     public List<String> anagrams(String[] strs) {
-
+        List<String> result = new ArrayList<String>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+        }
     }
 
 }
