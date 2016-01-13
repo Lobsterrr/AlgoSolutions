5a44049ce849391a84cfff525277e7dbe1e82cea
mitcc <mitcc@qq.com>
2016-01-13 09:42:03 +0800

    :basketball:

--- lintcode/Anagrams.java
+++ lintcode/Anagrams.java
@@ -1,5 +1,13 @@
 /*
- * 
+ * Given an array of strings, return all groups of strings that are anagrams.
+
+Example
+Given ["lint", "intl", "inlt", "code"], return ["lint", "inlt", "intl"].
+
+Given ["ab", "ba", "cd", "dc", "e"], return ["ab", "ba", "cd", "dc"].
+
+Note
+All inputs will be in lower-case
  */
 public class Anagrams {
 
