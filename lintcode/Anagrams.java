cbd18fdfeaf98f23d0c897ad1721f9f022c28f7c
mitcc <mitcc@qq.com>
2016-01-13 09:32:44 +0800

    :basketball:

--- lintcode/Anagrams.java
+++ lintcode/Anagrams.java
@@ -5,9 +5,21 @@ public class Anagrams {
 
     public List<String> anagrams(String[] strs) {
         List<String> result = new ArrayList<String>();
+        Map<String, String> map = new HashMap<String, String>();
         for (String str : strs) {
             char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String s = new String(array);
+            if (!map.containsKey(s)) {
+                map.put(s, str);
+            } else {
+                if (!result.containsKey(map.get(s))) {
+                    result.add(map.get(s));
+                }
+                result.add(str);
+            }
         }
+        return result; 
     }
 
 }
