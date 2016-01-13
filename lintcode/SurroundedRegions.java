1ddf9320e95b5838ed92691d12836e78d0e61e2b
mitcc <mitcc@qq.com>
2016-01-14 00:46:59 +0800

    :basketball:

--- lintcode/SurroundedRegions.java
+++ lintcode/SurroundedRegions.java
@@ -1,7 +1,9 @@
 /*
- * Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+ * Given a 2D board containing 'X' and 'O', capture all regions 
+ * surrounded by 'X'.
 
-A region is captured by flipping all 'O''s into 'X''s in that surrounded region.
+A region is captured by flipping all 'O''s into 'X''s in that 
+surrounded region.
 
 Example
 X X X X
