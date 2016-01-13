d3df33f1484d20c0a7b61e746e7c80333b136e4a
mitcc <mitcc@qq.com>
2016-01-14 03:20:41 +0800

    :basketball:

--- lintcode/SurroundedRegions.java
+++ lintcode/SurroundedRegions.java
@@ -100,7 +100,8 @@ public class SurroundedRegions {
     Queue<Integer> queue = new LinkedList<Integer>();
 
     public void fill(char[][] board, int i, int j) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+        if (i < 0 || i >= board.length || j < 0 || 
+                j >= board[0].length || board[i][j] != 'O') {
             return;
         }    
         queue.offer(i * board.length + j);
