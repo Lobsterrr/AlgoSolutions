54ac004a0f99ecf1828a9369293d11030ad7e94d
mitcc <mitcc@qq.com>
2016-01-14 00:41:52 +0800

    :basketball:

--- lintcode/SurroundedRegions.java
+++ lintcode/SurroundedRegions.java
@@ -14,8 +14,10 @@ public class SurroundedRegions {
         }
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
-                if (board[i][j] == '#') {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
                 }
             }
         }
