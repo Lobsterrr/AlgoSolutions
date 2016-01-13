7c25cd4358403e9472907ed29e3f5a59d957c53e
mitcc <mitcc@qq.com>
2016-01-14 00:30:18 +0800

    :basketball:

--- lintcode/SurroundedRegions.java
+++ lintcode/SurroundedRegions.java
@@ -7,4 +7,16 @@ public class SurroundedRegions {
 
     }
 
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || 
+                j >= board[i].length || board[i][j] == 'X') {
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+
 }
