ab2a14b9b89ac81f1f718ffad826eac8413e36f2
mitcc <mitcc@qq.com>
2016-01-14 09:14:28 +0800

    :basketball:

--- lintcode/SpiralMatrix.java
+++ lintcode/SpiralMatrix.java
@@ -1,8 +1,23 @@
 /*
- *
+ * Given a matrix of m x n elements (m rows, n columns), 
+ * return all elements of the matrix in spiral order.
+
+Example
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
  */
 public class SpiralMatrix {
 
+    /**
+     * @param matrix a matrix of m x n elements
+     * @return an integer list
+     */
     public List<Integer> spiralOrder(int[][] matrix) {
 
     }
