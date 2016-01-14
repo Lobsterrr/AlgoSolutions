2d263d222cfc2f613d62b0044c1e89f0b4760f41
mitcc <mitcc@qq.com>
2016-01-15 01:59:42 +0800

    :basketball:

--- lintcode/GasStation.java
+++ lintcode/GasStation.java
@@ -27,25 +27,18 @@ public class GasStation {
      * @return: an integer
      */
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int gasSum = 0;
-        int costSum = 0;
-        for (int i = 0; i < gas.length; ++i) {
-            gasSum += gas[i];
-            costSum += cost[i];
-        }
-        if (gasSum < costSum) {
-            return -1;
-        }
         int result = 0;
+        int margin = 0;
         int remainder = 0;
         for (int i = 0; i < gas.length; ++i) {
+            margin += gas[i] - cost[i];
             remainder += gas[i] - cost[i];
             if (remainder < 0) {
                 remainder = 0;
                 result = i + 1;
             }
         }
-        return result;
+        return margin < 0 ? -1 : result;
     }
 
 }
