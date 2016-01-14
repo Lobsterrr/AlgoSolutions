64d7aa57a1157dad1f765ab9e7414c8e5e88c91f
mitcc <mitcc@qq.com>
2016-01-15 01:17:14 +0800

    :basketball:

--- lintcode/GasStation.java
+++ lintcode/GasStation.java
@@ -14,8 +14,15 @@ public class GasStation {
         if (gasSum < costSum) {
             return result;
         }
-        
-
+        int remainder = 0;
+        for (int i = 0; i < gas.length; ++i) {
+            remainder += gas[i] - cost[i];
+            if (remainder < 0) {
+                remainder = 0;
+                result = i + 1;
+            }
+        }
+        return result;
     }
 
 }
