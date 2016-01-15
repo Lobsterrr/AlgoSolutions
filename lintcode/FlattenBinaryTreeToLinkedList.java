95e5b4ad8420a6fcb0b8aaff48a37448e1e8c44a
mitcc <mitcc@qq.com>
2016-01-15 23:59:21 +0800

    :basketball:

--- lintcode/FlattenBinaryTreeToLinkedList.java
+++ lintcode/FlattenBinaryTreeToLinkedList.java
@@ -3,6 +3,10 @@
  */
 public class FlattenBinaryTreeToLinkedList {
 
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
     public void flatten(TreeNode root) {
 
     }
