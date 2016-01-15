47ec206aa519ac73467445d5245a30486ba9da16
mitcc <mitcc@qq.com>
2016-01-15 23:59:56 +0800

    :basketball:

--- lintcode/FlattenBinaryTreeToLinkedList.java
+++ lintcode/FlattenBinaryTreeToLinkedList.java
@@ -1,5 +1,37 @@
 /*
- *
+ * Flatten a binary tree to a fake "linked list" in pre-order traversal.
+
+Here we use the right pointer in TreeNode as the next pointer in ListNode.
+
+Have you met this question in a real interview? Yes
+Example
+              1
+               \
+     1          2
+    / \          \
+   2   5    =>    3
+  / \   \          \
+ 3   4   6          4
+                     \
+                      5
+                       \
+                        6
+Note
+Don't forget to mark the left child of each node to null. Or you will get Time Limit Exceeded or Memory Limit Exceeded.
+
+Challenge
+Do it in-place without any extra memory.
+ */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
 public class FlattenBinaryTreeToLinkedList {
 
