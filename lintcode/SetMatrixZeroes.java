2aaffaa10f5dc4d5fc6a4e9349468eb819c5054a
mitcc <mitcc@qq.com>
2016-01-16 00:06:58 +0800

    :basketball:

--- lintcode/SetMatrixZeroes.java
+++ lintcode/SetMatrixZeroes.java
@@ -55,18 +55,14 @@ public class SetMatrixZeroes {
                 }
             }
         }
-        for (int i = 0; i < matrix.length; ++i) {
-            if (matrix[i][0] == 0) {
-                for (int j = 0; j < matrix[0].length; ++j) {
-                    matrix[i][j] = 0;
-                }
+        for (int i = 1; i < matrix.length; ++i) {
+            for (int j = 0; matrix[i][0] == 0 && j < matrix[0].length; ++j) {
+                matrix[i][j] = 0;
             }
         }
-        for (int j = 0; j < matrix[0].length; ++j) {
-            if (matrix[0][j] == 0) {
-                for (int i = 0; i < matrix.length; ++i) {
-                    matrix[i][j] = 0;
-                }
+        for (int j = 1; j < matrix[0].length; ++j) {
+            for (int i = 0; matrix[0][j] == 0 && i < matrix.length; ++i) {
+                matrix[i][j] = 0;
             }
         }
         for (int j = 0; isRowZeros && j < matrix[0].length; ++j) {
