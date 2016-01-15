10716b898cb5905cc20a71c332fd39ffd3a91db9
mitcc <mitcc@qq.com>
2016-01-15 23:56:50 +0800

    :basketball:

--- lintcode/SetMatrixZeroes.java
+++ lintcode/SetMatrixZeroes.java
@@ -47,8 +47,8 @@ public class SetMatrixZeroes {
                 break;
             }
         }
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
+        for (int i = 1; i < matrix.length; ++i) {
+            for (int j = 1; j < matrix[0].length; ++j) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
