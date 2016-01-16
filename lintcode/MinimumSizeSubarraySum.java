8bcd448661c3c067b551aaa4b7e52fc69a37e348
mitcc <mitcc@qq.com>
2016-01-17 00:11:21 +0800

    :basketball:

--- lintcode/MinimumSizeSubarraySum.java
+++ lintcode/MinimumSizeSubarraySum.java
@@ -1,11 +1,15 @@
 /*
- * Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return -1 instead.
+ * Given an array of n positive integers and a positive integer s, 
+ * find the minimal length of a subarray of which the sum ≥ s. If 
+ * there isn't one, return -1 instead.
 
 Example
-Given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal length under the problem constraint.
+Given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the 
+minimal length under the problem constraint.
 
 Challenge
-If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+If you have figured out the O(n) solution, try coding another 
+solution of which the time complexity is O(n log n).
  */
 public class MinimumSizeSubarraySum {
 
