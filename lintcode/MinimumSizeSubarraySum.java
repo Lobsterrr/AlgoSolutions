21e3b7f4570a6830686e0fb8c92e8a59736ac5ef
mitcc <mitcc@qq.com>
2016-01-16 23:47:53 +0800

    :basketball:

--- lintcode/MinimumSizeSubarraySum.java
+++ lintcode/MinimumSizeSubarraySum.java
@@ -3,6 +3,13 @@
  */
 public class MinimumSizeSubarraySum {
 
+    /**
+     * @param nums: an array of integers
+     * @param s: an integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
 
+    }
 
 }
