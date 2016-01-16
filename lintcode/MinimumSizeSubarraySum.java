23fbf449d51074e16afa7efd2aa9ed9b54a7750f
mitcc <mitcc@qq.com>
2016-01-17 00:08:07 +0800

    :basketball:

--- lintcode/MinimumSizeSubarraySum.java
+++ lintcode/MinimumSizeSubarraySum.java
@@ -23,7 +23,6 @@ public class MinimumSizeSubarraySum {
             return -1;
         }
         int result = Integer.MAX_VALUE;
-        sum = 0;
         for (int i = 0; i < nums.length; ++i) {
             sum = 0;
             for (int j = i; sum < s && j < nums.length; ++j) {
