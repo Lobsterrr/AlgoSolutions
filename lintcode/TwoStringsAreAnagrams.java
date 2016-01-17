9e918a6b859564065d635d30776d1599d935c902
mitcc <mitcc@qq.com>
2016-01-18 00:01:08 +0800

    :basketball:

--- lintcode/TwoStringsAreAnagrams.java
+++ lintcode/TwoStringsAreAnagrams.java
@@ -1,5 +1,6 @@
 /*
- * Write a method anagram(s,t) to decide if two strings are anagrams or not.
+ * Write a method anagram(s,t) to decide if two 
+ * strings are anagrams or not.
 
 Example
 Given s="abcd", t="dcab", return true.
