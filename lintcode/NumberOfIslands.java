c9a53a23a4c3411ef30d2409d5a0d64af3d0bfee
mitcc <mitcc@qq.com>
2016-01-17 22:13:11 +0800

    :basketball:

--- lintcode/NumberOfIslands.java
+++ lintcode/NumberOfIslands.java
@@ -14,7 +14,9 @@ Given graph:
 return 3.
 
 Note
-0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
+0 is represented as the sea, 1 is represented as the island. 
+If two 1 is adjacent, we consider them in the same island. 
+We only consider up/down/left/right adjacent.
  */
 public class NumberOfIslands {
 
