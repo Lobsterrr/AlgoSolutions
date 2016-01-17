8c8b69f45bf23260a3ae2b5c571d720510ba7656
mitcc <mitcc@qq.com>
2016-01-18 00:14:30 +0800

    :basketball:

--- lintcode/TwoStringsAreAnagrams.java
+++ lintcode/TwoStringsAreAnagrams.java
@@ -29,20 +29,4 @@ public class TwoStringsAreAnagrams {
         return true;
     }
 
-    public boolean anagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        char[] array1 = s.toCharArray();
-        char[] array2 = t.toCharArray();
-        Arrays.sort(array1);
-        Arrays.sort(array2);
-        for (int i = 0; i < array1.length; ++i) {
-            if (array1[i] != array2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
