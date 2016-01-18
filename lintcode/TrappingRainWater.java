9e30a80c8d953eb9beca7c331c3aac71fb6baffe
mitcc <mitcc@qq.com>
2016-01-18 22:45:00 +0800

    :basketball:

--- lintcode/TrappingRainWater.java
+++ lintcode/TrappingRainWater.java
@@ -3,6 +3,10 @@
  */
 public class TrappingRainWater {
 
+    /**
+     * @param heights: an array of integers
+     * @return: a integer
+     */
     public int trapRainWater(int[] heights) {
 
     }
