eec7bf4f103e54e19ab67b170246cba0dfde743d
mitcc <mitcc@qq.com>
2016-01-18 23:22:53 +0800

    :basketball:

--- lintcode/TrappingRainWater.java
+++ lintcode/TrappingRainWater.java
@@ -24,7 +24,19 @@ public class TrappingRainWater {
         if (heights.length == 0) {
             return 0;
         }
-        
+        int[] leftMax = new int[heights.length];
+        int[] rightMax = new int[heights.length];
+        leftMax[0] = heights[0];
+        rightMax[heights.length - 1] = heights[heights.length - 1];
+        for (int i = 1; i < heights.length; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], heights[i]);
+            rightMax[heights.length - 1 - i] = Math.max(rightMax[heights.length - i], heights[heights.length - 1 - i]);
+        }
+        int result = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            result += Math.min(leftMax[i], rightMax[i]) - heights[i];
+        }
+        return result;
     }
 
 }
