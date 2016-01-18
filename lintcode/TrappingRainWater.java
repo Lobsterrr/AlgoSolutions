58bc25fc4398ccc651c6e4c27e32c4308dc8de90
mitcc <mitcc@qq.com>
2016-01-18 22:45:47 +0800

    :basketball:

--- lintcode/TrappingRainWater.java
+++ lintcode/TrappingRainWater.java
@@ -1,5 +1,8 @@
 /*
- * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ * Given n non-negative integers representing an 
+ * elevation map where the width of each bar is 
+ * 1, compute how much water it is able to trap 
+ * after raining.
 
 Trapping Rain Water
 
