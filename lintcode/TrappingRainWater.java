9c255facdbc5a3723e838f250ab0c1f90c1ede09
mitcc <mitcc@qq.com>
2016-01-18 22:45:14 +0800

    :basketball:

--- lintcode/TrappingRainWater.java
+++ lintcode/TrappingRainWater.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+Trapping Rain Water
+
+Have you met this question in a real interview? Yes
+Example
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+
+Challenge
+O(n) time and O(1) memory
+
+O(n) time and O(n) memory is also acceptable.
  */
 public class TrappingRainWater {
 
