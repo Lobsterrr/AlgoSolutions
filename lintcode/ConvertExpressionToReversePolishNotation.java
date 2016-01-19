5db22f86bead3c5a4458a4f46327008ce734ae07
mitcc <mitcc@qq.com>
2016-01-19 23:51:45 +0800

    :basketball:

--- lintcode/ConvertExpressionToReversePolishNotation.java
+++ lintcode/ConvertExpressionToReversePolishNotation.java
@@ -3,6 +3,10 @@
  */
 public class ConvertExpressionToReversePolishNotation {
 
+    /**
+     * @param expression: A string array
+     * @return: The Reverse Polish notation of this expression
+     */
     public ArrayList<String> convertToRPN(String[] expression) {
 
     }
