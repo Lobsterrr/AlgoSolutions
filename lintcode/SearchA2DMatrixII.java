d2988548d4e7c1bbb21afeb23e2b328515488dc2
mitcc <mitcc@qq.com>
2016-01-21 01:18:45 +0800

    :basketball:

--- lintcode/SearchA2DMatrixII.java
+++ lintcode/SearchA2DMatrixII.java
@@ -30,7 +30,7 @@ public class SearchA2DMatrixII {
     public int searchMatrix(int[][] matrix, int target) {
         int count = 0;
         for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j  <matrix[0].length; ++j) {
+            for (int j = 0; j < matrix[0].length; ++j) {
                 if (matrix[i][j] == target) {
                     count++;
                 }
