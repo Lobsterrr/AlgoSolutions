16c7a4cde7cb3a8cbfa704da74fdbb8f1fb584bb
mitcc <mitcc@qq.com>
2016-01-20 23:57:57 +0800

    :basketball:

--- lintcode/SearchA2DMatrixII.java
+++ lintcode/SearchA2DMatrixII.java
@@ -3,6 +3,11 @@
  */
 public class SearchA2DMatrixII {
 
+    /**
+     * @param matrix: A list of lists of integers
+     * @param: A number you want to search in the matrix
+     * @return: An integer indicate the occurrence of target in the given matrix
+     */
     public int searchMatrix(int[][] matrix, int target) {
 
     }
