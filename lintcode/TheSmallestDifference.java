68b7b08771fa5e6a71ac6863af3efc47362e25b2
mitcc <mitcc@qq.com>
2016-01-21 22:42:38 +0800

    :basketball:

---
+++ lintcode/TheSmallestDifference.java
@@ -0,0 +1,25 @@
+/*
+ *
+ */
+public class TheSmallestDifference {
+
+    public int smallestDifference(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int result = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        while (i < A.length && j < B.length) {
+            result = Math.min(result, Math.abs(A[i] - B[j]));
+            if (A[i] == B[j]) {
+                return 0;
+            } else if (A[i] < B[j]) {
+                i++;
+            } else {
+                j++;
+            } 
+        }
+        return result;
+    }
+    // [3, 4, 6, 7], [2, 3, 8, 9]
+}
