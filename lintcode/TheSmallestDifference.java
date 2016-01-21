c4b849c418b927b70dee85fd275d23c9dc10f66a
mitcc <mitcc@qq.com>
2016-01-21 22:48:24 +0800

    :basketball:

--- lintcode/TheSmallestDifference.java
+++ lintcode/TheSmallestDifference.java
@@ -1,5 +1,9 @@
 /*
- * Given two array of integers(the first array is array A, the second array is array B), now we are going to find a element in array A which is A[i], and another element in array B which is B[j], so that the difference between A[i] and B[j] (|A[i] - B[j]|) is as small as possible, return their smallest difference.
+ * Given two array of integers(the first array is array A, the second 
+ * array is array B), now we are going to find a element in array A 
+ * which is A[i], and another element in array B which is B[j], so 
+ * that the difference between A[i] and B[j] (|A[i] - B[j]|) is as 
+ * small as possible, return their smallest difference.
 
 Example
 For example, given array A = [3,6,7,4], B = [2,8,9,3], return 0
@@ -29,5 +33,5 @@ public class TheSmallestDifference {
         }
         return result;
     }
-    // [3, 4, 6, 7], [2, 3, 8, 9]
+
 }
