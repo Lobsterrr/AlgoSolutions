7a98e6e0077566efdb7c5873ad768f39af193db0
mitcc <mitcc@qq.com>
2016-01-22 01:45:20 +0800

    :basketball:

--- lintcode/WordBreak.java
+++ lintcode/WordBreak.java
@@ -15,21 +15,6 @@ public class WordBreak {
     public boolean wordBreak(String s, Set<String> dict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-        for (int i = 0; i < s.length(); ++i) {
-            if (i == 0 || dp[i]) {
-                for (int j = i + 1; j <= s.length(); ++j) {
-                    dp[j] = dp[j] || dict.contains(s.substring(i, j));
-                }
-            }
-        }
-        return dp[s.length()];
-    }
-
-/*****************************************************************************/
-
-    public boolean wordBreak(String s, Set<String> dict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
         int maxLen = getMaxLength(dict);
         for (int i = 0; i < s.length(); ++i) {
             if (i == 0 || dp[i]) {
