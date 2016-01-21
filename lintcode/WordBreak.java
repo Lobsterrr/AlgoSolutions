155fe4673a7bd9ed61bb6d4a9c27f3ddf185235a
mitcc <mitcc@qq.com>
2016-01-22 01:04:47 +0800

    :basketball:

--- lintcode/WordBreak.java
+++ lintcode/WordBreak.java
@@ -3,6 +3,10 @@
  */
 public class WordBreak {
 
+    /**
+     * @param s: A string s
+     * @param dict: A dictionary of words dict
+     */
     public boolean wordBreak(String s, Set<String> dict) {
 
     }
