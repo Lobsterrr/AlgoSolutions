17e1647019643245a41ec93bdb624bf15481f41f
mitcc <mitcc@qq.com>
2016-01-23 02:36:10 +0800

    :basketball:

--- lintcode/SearchInsertPosition.java
+++ lintcode/SearchInsertPosition.java
@@ -23,7 +23,20 @@ public class SearchInsertPosition {
      * return : an integer
      */
     public int searchInsert(int[] A, int target) {
-
+        int low = 0;
+        int high = A.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (A[mid] == target) {
+                return mid;
+            }
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
     }
 
 }
