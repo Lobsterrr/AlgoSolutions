df562a656de0017c069278508d4236a275e5c6eb
mitcc <mitcc@qq.com>
2016-01-23 03:34:29 +0800

    :basketball:

--- lintcode/SearchA2DMatrix.java
+++ lintcode/SearchA2DMatrix.java
@@ -4,7 +4,8 @@
 This matrix has the following properties:
 
 Integers in each row are sorted from left to right.
-The first integer of each row is greater than the last integer of the previous row.
+The first integer of each row is greater than the last integer of the previous 
+row.
 Example
 Consider the following matrix:
 
