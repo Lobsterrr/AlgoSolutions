6b8124412ef4e38078922c3f131ad9409b208eee
mitcc <mitcc@qq.com>
2016-01-23 01:48:28 +0800

    :basketball:

--- lintcode/SearchInsertPosition.java
+++ lintcode/SearchInsertPosition.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume NO duplicates in the array.
+
+Have you met this question in a real interview? Yes
+Example
+[1,3,5,6], 5 ¡ú 2
+
+[1,3,5,6], 2 ¡ú 1
+
+[1,3,5,6], 7 ¡ú 4
+
+[1,3,5,6], 0 ¡ú 0
+
+Challenge
+O(log(n)) time
  */
 public class SearchInsertPosition {
 
