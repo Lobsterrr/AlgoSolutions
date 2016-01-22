3845f20e0db1fe437f61cc7f2cc4f7e799861cc5
mitcc <mitcc@qq.com>
2016-01-23 01:48:08 +0800

    :basketball:

--- lintcode/SearchInsertPosition.java
+++ lintcode/SearchInsertPosition.java
@@ -3,6 +3,11 @@
  */
 public class SearchInsertPosition {
 
+    /** 
+     * param A : an integer sorted array
+     * param target :  an integer to be inserted
+     * return : an integer
+     */
     public int searchInsert(int[] A, int target) {
 
     }
