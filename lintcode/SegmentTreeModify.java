feba097dbe977abc6a9cf3f66211b00da3758c5c
mitcc <mitcc@qq.com>
2016-01-22 21:56:11 +0800

    :basketball:

--- lintcode/SegmentTreeModify.java
+++ lintcode/SegmentTreeModify.java
@@ -1,6 +1,19 @@
 /*
  *
  */
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, max;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int max) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.max = max
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class SegmentTreeModify {
 
     /**
