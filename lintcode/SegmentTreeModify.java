9d9ec83f921a316d3e21ec9415e437c2b0a90291
mitcc <mitcc@qq.com>
2016-01-22 21:55:58 +0800

    :basketball:

--- lintcode/SegmentTreeModify.java
+++ lintcode/SegmentTreeModify.java
@@ -3,6 +3,11 @@
  */
 public class SegmentTreeModify {
 
+    /**
+     *@param root, index, value: The root of segment tree and 
+     *@ change the node's value with [index, index] to the new given value
+     *@return: void
+     */
     public void modify(SegmentTreeNode root, int index, int value) {
 
     }
