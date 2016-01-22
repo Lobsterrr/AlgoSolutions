f95da337e8f5913f10a2d73ef84ffa439a323686
mitcc <mitcc@qq.com>
2016-01-23 03:03:48 +0800

    :basketball:

--- lintcode/SearchA2DMatrix.java
+++ lintcode/SearchA2DMatrix.java
@@ -26,7 +26,27 @@ public class SearchA2DMatrix {
      * @return a boolean, indicate whether matrix contains target
      */
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        if (matrix.length == 0) {
+            return false;
+        }
+        int xLen = matrix.length;
+        int yLen = matrix[0].length;
+        int low = 0;
+        int high = xLen * yLen - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int x = mid / yLen;
+            int y = mid % yLen;
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
     }
 
 }
