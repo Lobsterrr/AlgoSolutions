3643226c03f470e1cd8a0c96683ef40a06c312e3
mitcc <mitcc@qq.com>
2016-01-23 21:28:21 +0800

    :basketball:

--- lintcode/ProductOfArrayExcludeItself.java
+++ lintcode/ProductOfArrayExcludeItself.java
@@ -1,5 +1,10 @@
 /*
- *
+ * Given an integers array A.
+
+Define B[i] = A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1], calculate B WITHOUT divide operation.
+
+Example
+For A = [1, 2, 3], return [6, 3, 2].
  */
 public class ProductOfArrayExcludeItself {
 
