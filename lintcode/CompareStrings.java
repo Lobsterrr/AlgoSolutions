01786a6f53868fbcafd6267f87b8e1cdb5e46b75
mitcc <mitcc@qq.com>
2016-01-24 00:28:20 +0800

    :basketball:

--- lintcode/CompareStrings.java
+++ lintcode/CompareStrings.java
@@ -4,7 +4,22 @@
 public class CompareStrings {
 
     public boolean compareStrings(String A, String B) {
-
+        if (A.length() < B.length()) {
+            return false;
+        }
+        int[] chars = new int[256];
+        for (int i = 0; i < A.length(); ++i) {
+            chars[A.charAt(i)]++;
+            if (i < B.length()) {
+                chars[B.charAt(i)]--;
+            }
+        }
+        for (int i = 0; i < chars.length; ++i) {
+            if (chars[i] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
