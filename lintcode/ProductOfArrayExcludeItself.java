c45bad549b43a93707e3b37fbc6b29cbcf1b9576
mitcc <mitcc@qq.com>
2016-01-23 21:27:45 +0800

    :basketball:

--- lintcode/ProductOfArrayExcludeItself.java
+++ lintcode/ProductOfArrayExcludeItself.java
@@ -4,7 +4,21 @@
 public class ProductOfArrayExcludeItself {
 
     public ArrayList<Long> productExcludeItself(ArrayList<Integer> A) {
-
+        ArrayList<Long> result = new ArrayList<Long>();
+        long[] products = new long[A.size()];
+        Arrays.fill(products, 1);
+        for (int i = 0; i < products.length; ++i) {
+            for (int j = 0; j < products.length; ++j) {
+                if (i == j) {
+                    continue;
+                }
+                products[i] *= A.get(j);
+            }
+        }
+        for (long product : products) {
+            result.add(product);
+        }
+        return result;
     }
 
 }
