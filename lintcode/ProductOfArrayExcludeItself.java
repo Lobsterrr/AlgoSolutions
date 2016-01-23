da079e2e596fe95816a3fe57ea7a2fada3ba7e5d
mitcc <mitcc@qq.com>
2016-01-23 21:29:01 +0800

    :basketball:

--- lintcode/ProductOfArrayExcludeItself.java
+++ lintcode/ProductOfArrayExcludeItself.java
@@ -1,7 +1,8 @@
 /*
  * Given an integers array A.
 
-Define B[i] = A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1], calculate B WITHOUT divide operation.
+Define B[i] = A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1], 
+calculate B WITHOUT divide operation.
 
 Example
 For A = [1, 2, 3], return [6, 3, 2].
