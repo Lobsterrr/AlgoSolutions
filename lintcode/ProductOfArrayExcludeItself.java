aee11c1d44d7911abd5f1c734ef10944c8e9497b
mitcc <mitcc@qq.com>
2016-01-23 23:55:55 +0800

    :basketball:

--- lintcode/ProductOfArrayExcludeItself.java
+++ lintcode/ProductOfArrayExcludeItself.java
@@ -14,6 +14,19 @@ public class ProductOfArrayExcludeItself {
      * @return: A Long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
      */
     public ArrayList<Long> productExcludeItself(ArrayList<Integer> A) {
+        ArrayList<Long> result = new ArrayList<Long>(A.size());
+        for (int i = 0; i < A.size(); ++i) {
+            result.set(i, i == 0 ? 1L : result.get(i - 1) * A.get(i - 1));
+        }
+        long product = 1L;
+        for (int i = A.size() - 1; i >= 0; --i) {
+            product *= i == A.size() - 1 ? 1 : A.get(i + 1);
+            result.set(i, result.get(i) * product);
+        }
+        return result;
+    }
+
+    public ArrayList<Long> productExcludeItself(ArrayList<Integer> A) {
         ArrayList<Long> result = new ArrayList<Long>();
         long product = 1L;
         for (int i = 0; i < A.size(); ++i) {
