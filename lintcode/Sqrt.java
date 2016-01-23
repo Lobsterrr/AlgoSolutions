e1e2d112b9a7fc819dcc46022793f52edb820fd4
mitcc <mitcc@qq.com>
2016-01-23 20:39:47 +0800

    :basketball:

--- lintcode/Sqrt.java
+++ lintcode/Sqrt.java
@@ -11,4 +11,18 @@ public class Sqrt {
         return (int) i;
     }
 
+    public int sqrt(int x) {
+        int low = 1;
+        int high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid < x / mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + (high - low) / 2;
+    }
+
 }
