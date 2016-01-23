e017faa02d52ed85b77b8e371d5d28e825785a0a
mitcc <mitcc@qq.com>
2016-01-23 21:10:07 +0800

    :basketball:

--- lintcode/Sqrt.java
+++ lintcode/Sqrt.java
@@ -1,8 +1,26 @@
 /*
- *
+ * Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+Example
+sqrt(3) = 1
+
+sqrt(4) = 2
+
+sqrt(5) = 2
+
+sqrt(10) = 3
+
+Challenge
+O(log(x))
  */
 public class Sqrt {
 
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
     public int sqrt(int x) {
         double i = 1.0;
         while ((int) i != (int) (x / i)) {
