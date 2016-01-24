4f032fcf5793b5f07848a7a0632bd4e23c208934
mitcc <mitcc@qq.com>
2016-01-24 21:34:48 +0800

    :basketball:

--- lintcode/ReverseWordsInAString.java
+++ lintcode/ReverseWordsInAString.java
@@ -1,8 +1,25 @@
 /*
- *
+ * Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Example
+Clarification
+What constitutes a word?
+A sequence of non-space characters constitutes a word.
+Could the input string contain leading or trailing spaces?
+Yes. However, your reversed string should not contain leading or trailing spaces.
+How about multiple spaces between two words?
+Reduce them to a single space in the reversed string.
  */
 public class ReverseWordsInAString {
 
+    /**
+     * @param s : A string
+     * @return : A string
+     */
     public String reverseWords(String s) {
 
     }
