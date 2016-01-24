6feb40b384b79c370da7e50ec330bc4d83a6d524
mitcc <mitcc@qq.com>
2016-01-25 00:33:21 +0800

    :basketball:

---
+++ lintcode/MaximumSubarrayII.java
@@ -0,0 +1,25 @@
+/*
+ * Given an array of integers, find two non-overlapping subarrays which have the largest sum.
+The number in each subarray should be contiguous.
+Return the largest sum.
+
+Example
+For given [1, 3, -1, 2, -1, 2], the two subarrays are [1, 3] and [2, -1, 2] or [1, 3, -1, 2] and [2], they both have the largest sum 7.
+
+Note
+The subarray should contain at least one number
+
+Challenge
+Can you do it in time complexity O(n) ?
+ */
+public class MaximumSubarrayII {
+
+    /**
+     * @param nums: A list of integers
+     * @return: An integer denotes the sum of max two non-overlapping subarrays
+     */
+    public int maxTwoSubArrays(ArrayList<Integer> nums) {
+
+    }
+
+}
