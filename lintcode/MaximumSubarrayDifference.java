f416642ac9d61059957d17b4e5823f989f0d4a52
mitcc <mitcc@qq.com>
2016-01-24 22:45:39 +0800

    :basketball:

--- lintcode/MaximumSubarrayDifference.java
+++ lintcode/MaximumSubarrayDifference.java
@@ -4,12 +4,16 @@
 public class MaximumSubarrayDifference {
 
     public int maxDiffSubArrays(int[] nums) {
-        return maxSubArray(nums) - minSubArray(nums);
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; ++i) {
+            result = Math.max(result, Math.abs(maxSubArray(nums, 0, i) - minSubArray(nums, i + 1, nums.length - 1)));
+        }
+        return result;
     }
 
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray(int[] nums, int low, int high) {
         int maxSum = Integer.MIN_VALUE;
-        for (int i = 0, curSum = 0; i < nums.length; ++i) {
+        for (int i = low, curSum = 0; i <= high; ++i) {
             curSum += nums[i];
             maxSum = Math.max(maxSum, curSum);
             curSum = Math.max(curSum, 0);
@@ -17,9 +21,9 @@ public class MaximumSubarrayDifference {
         return maxSum;
     }
 
-    public int minSubArray(int[] nums) {
+    public int minSubArray(int[] nums, int low, int high) {
         int minSum = Integer.MAX_VALUE;
-        for (int i = 0, curSum = 0; i < nums.length; ++i) {
+        for (int i = low, curSum = 0; i <= high; ++i) {
             curSum += nums[i];
             minSum = Math.min(minSum, curSum);
             curSum = Math.min(curSum, 0);
