7be86d8b1d24d2e6c5969449b49251173b53a8fa
mitcc <mitcc@qq.com>
2016-01-25 21:39:03 +0800

    :basketball:

--- lintcode/MajorityNumberII.java
+++ lintcode/MajorityNumberII.java
@@ -3,6 +3,10 @@
  */
 public class MajorityNumberII {
 
+    /**
+     * @param nums: A list of integers
+     * @return: The majority number that occurs more than 1/3
+     */
     public int majorityNumber(ArrayList<Integer> nums) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums) {
@@ -11,7 +15,7 @@ public class MajorityNumberII {
         int result = 0;
         int count = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue > count) {
+            if (entry.getValue() > count) {
                 result = entry.getKey();
                 count = entry.getValue();
             }
