b7644b8310e9812524b7afe819acd47cd54464f4
mitcc <mitcc@qq.com>
2016-01-25 21:59:27 +0800

    :basketball:

--- lintcode/MajorityNumberII.java
+++ lintcode/MajorityNumberII.java
@@ -31,8 +31,10 @@ public class MajorityNumberII {
                 count2++;
             } else if (count1 == 0) {
                 candidate1 = num;
+                count1 = 1;
             } else if (count2 == 0) {
                 candidate2 = num;
+                count2 = 1;
             } else {
                 count1--;
                 count2--;
