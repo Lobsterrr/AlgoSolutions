756f069aa065adb72e7d1b3cd5deb42931882559
mitcc <mitcc@qq.com>
2016-01-26 22:57:42 +0800

    :basketball:

--- lintcode/Median.java
+++ lintcode/Median.java
@@ -3,6 +3,10 @@
  */
 public class Median {
 
+    /**
+     * @param nums: A list of integers.
+     * @return: An integer denotes the middle number of the array.
+     */
     public int median(int[] nums) {
 
     }
