29538d7ce8d0a2042a68cb231e831fd35ba89830
mitcc <mitcc@qq.com>
2016-01-26 09:06:37 +0800

    :basketball:

--- lintcode/RecoverRotatedSortedArray.java
+++ lintcode/RecoverRotatedSortedArray.java
@@ -3,6 +3,10 @@
  */
 public class RecoverRotatedSortedArray {
 
+    /**
+     * @param nums: The rotated sorted array
+     * @return: void
+     */
     public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
 
     }
