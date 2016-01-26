c5a91fb08b6c5f11e63b1ea04ab77e837ee0a02c
mitcc <mitcc@qq.com>
2016-01-26 09:09:48 +0800

    :basketball:

--- lintcode/RecoverRotatedSortedArray.java
+++ lintcode/RecoverRotatedSortedArray.java
@@ -10,7 +10,8 @@ In-place, O(1) extra space and O(n) time.
 Clarification
 What is rotated array?
 
-For example, the orginal array is [1,2,3,4], The rotated array of it can be [1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]
+For example, the orginal array is [1,2,3,4], The rotated array of it 
+can be [1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]
  */
 public class RecoverRotatedSortedArray {
 
