92c24bb2f58d7d39da5a8e3afdb3a094c9d4fe7d
mitcc <mitcc@qq.com>
2016-01-27 23:28:44 +0800

    :basketball:

--- lintcode/InsertInterval.java
+++ lintcode/InsertInterval.java
@@ -3,6 +3,12 @@
  */
 public class InsertInterval {
 
+    /**
+     * Insert newInterval into intervals.
+     * @param intervals: Sorted interval list.
+     * @param newInterval: A new interval.
+     * @return: A new sorted interval list.
+     */
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
 
     }
