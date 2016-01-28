a987355339ed214e54ed595e749c7a93848190b1
mitcc <mitcc@qq.com>
2016-01-28 23:02:41 +0800

    :basketball:

--- lintcode/MaximumSubarrayIII.java
+++ lintcode/MaximumSubarrayIII.java
@@ -3,6 +3,11 @@
  */
 public class MaximumSubarrayIII {
 
+    /**
+     * @param nums: A list of integers
+     * @param k: An integer denote to find k non-overlapping subarrays
+     * @return: An integer denote the sum of max k non-overlapping subarrays
+     */
     public int maxSubArray(int[] nums, int k) {
 
     }
