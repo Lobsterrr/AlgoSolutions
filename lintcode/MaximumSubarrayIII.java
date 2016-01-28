32115d18c7d0f3d136627f37bc1ac7cedf8d0fca
mitcc <mitcc@qq.com>
2016-01-28 23:02:57 +0800

    :basketball:

--- lintcode/MaximumSubarrayIII.java
+++ lintcode/MaximumSubarrayIII.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given an array of integers and a number k, find k non-overlapping subarrays which have the largest sum.
+
+The number in each subarray should be contiguous.
+
+Return the largest sum.
+
+Example
+Given [-1,4,-2,3,-2,3], k=2, return 8
+
+Note
+The subarray should contain at least one number
  */
 public class MaximumSubarrayIII {
 
