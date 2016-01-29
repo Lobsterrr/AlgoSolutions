2cf87be4f81a7a4076febf623220e2f2ef43358c
mitcc <mitcc@qq.com>
2016-01-29 22:45:04 +0800

    :basketball:

--- lintcode/SearchForARange.java
+++ lintcode/SearchForARange.java
@@ -3,6 +3,11 @@
  */
 public class SearchForARange {
 
+    /** 
+     *@param A : an integer sorted array
+     *@param target :  an integer to be inserted
+     *return : a list of length 2, [index1, index2]
+     */
     public int[] searchRange(int[] A, int target) {
 
     }
