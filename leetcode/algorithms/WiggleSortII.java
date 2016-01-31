6381085dd1092aa9087d2bbd7b2ea7d649661898
mitcc <mitcc@qq.com>
2016-01-31 21:42:49 +0800

    :basketball:

--- leetcode/algorithms/WiggleSortII.java
+++ leetcode/algorithms/WiggleSortII.java
@@ -54,7 +54,7 @@ public class WiggleSortII {
     }
 
     public void wiggleSort(int[] nums) {
-        int[] tmp = Arrays.sort(nums, nums.length);
+        int[] tmp = Arrays.copyOf(nums, nums.length);
         Arrays.sort(tmp);
         for (int i = 0, j = (nums.length - 1) / 2, k = nums.length - 1; ++i) {
             nums[i] = tmp[i % 2 == 0 ? nums[j--] : nums[k--]];
