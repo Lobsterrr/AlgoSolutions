6ff4ed7ef16ebec898a55d9eba97399b2dd0cd55
mitcc <mitcc@qq.com>
2016-01-31 23:21:36 +0800

    :basketball:

--- lintcode/CombinationSum.java
+++ lintcode/CombinationSum.java
@@ -1,5 +1,6 @@
 /*
- * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+ * Given a set of candidate numbers (C) and a target number (T), find all 
+ * unique combinations in C where the candidate numbers sums to T.
 
 The same repeated number may be chosen from C unlimited number of times.
 
@@ -18,7 +19,8 @@ A solution set is:
 
 Note
 All numbers (including target) will be positive integers.
-Elements in a combination (a1, a2, ¡­ , ak) must be in non-descending order. (ie, a1 ¡Ü a2 ¡Ü ¡­ ¡Ü ak).
+Elements in a combination (a1, a2, ¡­ , ak) must be in non-descending order. 
+(ie, a1 ¡Ü a2 ¡Ü ¡­ ¡Ü ak).
 The solution set must not contain duplicate combinations.
  */
 public class CombinationSum {
