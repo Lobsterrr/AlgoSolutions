aefadd76addcfea1a874acf75fd39e6b732f555d
mitcc <mitcc@qq.com>
2016-01-31 22:43:55 +0800

    :basketball:

--- lintcode/CombinationSum.java
+++ lintcode/CombinationSum.java
@@ -3,8 +3,13 @@
  */
 public class CombinationSum {
 
+    /**
+     * @param candidates: A list of integers
+     * @param target:An integer
+     * @return: A list of lists of integers
+     */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
+        
     }
 
 }
