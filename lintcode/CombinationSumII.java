841c8f88c5c7072f58b7b41b6ebba0bb74cd9c36
mitcc <mitcc@qq.com>
2016-02-01 09:06:28 +0800

    :basketball:

--- lintcode/CombinationSumII.java
+++ lintcode/CombinationSumII.java
@@ -27,7 +27,25 @@ public class CombinationSumII {
      * @return: All the combinations that sum to target
      */
     public List<List<Integer>> combinationSum2(int[] num, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(num);
+        dfs(result, new ArrayList<Integer>(), num, 0, target);
+        return result;
+    }
 
+    public void dfs(List<List<Integer>> result, List<Integer> list, int[] num, int position, int target) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = position; i < num.length && target > 0; ++i) {
+            if (i > position && num[i] == num[i - 1]) {
+                continue;
+            }
+            list.add(num[i]);
+            dfs(result, list, num, i + 1, target);
+            list.remove(list.size() - 1);
+        }
     }
 
 }
