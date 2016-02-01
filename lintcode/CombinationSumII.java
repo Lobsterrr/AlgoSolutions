59c4da330eeaa5e8bc5a5440f554be2e7677a41c
mitcc <mitcc@qq.com>
2016-02-01 09:00:49 +0800

    :basketball:

--- lintcode/CombinationSumII.java
+++ lintcode/CombinationSumII.java
@@ -3,6 +3,11 @@
  */
 public class CombinationSumII {
 
+    /**
+     * @param num: Given the candidate numbers
+     * @param target: Given the target number
+     * @return: All the combinations that sum to target
+     */
     public List<List<Integer>> combinationSum2(int[] num, int target) {
 
     }
