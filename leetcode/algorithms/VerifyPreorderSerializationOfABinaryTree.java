2684cf8f009fea148e6caf28480f61b960b58edd
mitcc <mitcc@qq.com>
2016-02-02 23:57:53 +0800

    :basketball:

--- leetcode/algorithms/VerifyPreorderSerializationOfABinaryTree.java
+++ leetcode/algorithms/VerifyPreorderSerializationOfABinaryTree.java
@@ -42,6 +42,7 @@
 public class VerifyPreorderSerializationOfABinaryTree {
 
     public boolean isValidSerialization(String preorder) {
+
     }
 
     public boolean isValidSerialization(String preorder) {
