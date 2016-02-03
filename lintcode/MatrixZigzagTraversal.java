bdced201a671de51562416a6f4ae283db19553cb
mitcc <mitcc@qq.com>
2016-02-03 19:47:41 +0800

    :basketball:

--- lintcode/MatrixZigzagTraversal.java
+++ lintcode/MatrixZigzagTraversal.java
@@ -3,6 +3,10 @@
  */
 public class MatrixZigzagTraversal {
 
+    /**
+     * @param matrix: a matrix of integers
+     * @return: an array of integers
+     */ 
     public int[] printZMatrix(int[][] matrix) {
 
     }
