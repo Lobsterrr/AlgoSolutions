4dd239fba69d5aa3a999a52d19d45816b59c062a
mitcc <mitcc@qq.com>
2016-02-07 17:41:43 +0800

    :basketball:

--- project-euler/pe003.py
+++ project-euler/pe003.py
@@ -15,5 +15,15 @@ def largest_prime_factor(n):
             i += 1
     return n
 
+def largest_prime_factor_1(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            n //= i
+        else:
+            i += 1
+    return n
+
 if __name__ == '__main__':
-    print(largest_prime_factor(600851475143))
+    print(largest_prime_factor(600851475143)
+    print(largest_prime_factor_1(600851475143)
