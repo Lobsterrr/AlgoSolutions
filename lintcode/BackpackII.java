d83a6c5f859628fad5f8d600e950ed9262590f33
mitcc <mitcc@qq.com>
2016-02-17 23:46:48 +0800

    :basketball:

--- lintcode/BackpackII.java
+++ lintcode/BackpackII.java
@@ -1,14 +1,19 @@
 /*
- * Given n items with size Ai and value Vi, and a backpack with size m. What's the maximum value can you put into the backpack?
+ * Given n items with size Ai and value Vi, and a backpack 
+ * with size m. What's the maximum value can you put into 
+ * the backpack?
 
 Example
-Given 4 items with size [2, 3, 5, 7] and value [1, 5, 2, 4], and a backpack with size 10. The maximum value is 9.
+Given 4 items with size [2, 3, 5, 7] and value [1, 5, 2, 
+4], and a backpack with size 10. The maximum value is 9.
 
 Note
-You cannot divide item into small pieces and the total size of items you choose should smaller or equal to m.
+You cannot divide item into small pieces and the total size 
+of items you choose should smaller or equal to m.
 
 Challenge
-O(n x m) memory is acceptable, can you do it in O(m) memory?
+O(n x m) memory is acceptable, can you do it in O(m) 
+memory?
  */
 public class BackpackII {
 
@@ -24,7 +29,8 @@ public class BackpackII {
                 if (j < A[i - 1]) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i- 1][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
+                    dp[i][j] = Math.max(dp[i- 1][j], 
+                            dp[i - 1][j - A[i - 1]] + V[i - 1]);
                 }
             }
         }
