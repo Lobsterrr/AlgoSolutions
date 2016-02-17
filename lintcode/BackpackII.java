7943c652917d47b9fdd891172d9df55428ac80bf
mitcc <mitcc@qq.com>
2016-02-17 23:35:05 +0800

    :basketball:

--- lintcode/BackpackII.java
+++ lintcode/BackpackII.java
@@ -3,6 +3,11 @@
  */
 public class BackpackII {
 
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A & V: Given n items with size A[i] and value V[i]
+     * @return: The maximum value
+     */
     public int backPackII(int m, int[] A, int V[]) {
 
     }
