04e070410fed903f497657342eee847858b79032
mitcc <mitcc@qq.com>
2016-02-19 00:04:01 +0800

    :basketball:

--- lintcode/StrStr.java
+++ lintcode/StrStr.java
@@ -1,5 +1,6 @@
 /*
- * For a given source string and a target string, you should output the first index(from 0) of target string in source string.
+ * For a given source string and a target string, you should output the first 
+ * index(from 0) of target string in source string.
 
 If target does not exist in source, just return -1.
 
@@ -14,7 +15,9 @@ O(n2) is acceptable. Can you implement an O(n) algorithm? (hint: KMP)
 Clarification
 Do I need to implement KMP Algorithm in a real interview?
 
-Not necessary. When you meet this problem in a real interview, the interviewer may just want to test your basic implementation ability. But make sure your confirm with the interviewer first.
+Not necessary. When you meet this problem in a real interview, the interviewer 
+may just want to test your basic implementation ability. But make sure your 
+confirm with the interviewer first.
  */
 public class StrStr {
 
