ed3fb7557845212fd21e76ab24219152f1c4fa12
mitcc <mitcc@qq.com>
2016-02-18 23:37:29 +0800

    :basketball:

--- lintcode/StrStr.java
+++ lintcode/StrStr.java
@@ -3,6 +3,12 @@
  */
 public class StrStr {
 
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
     public int strStr(String source, String target) {
 
     }
