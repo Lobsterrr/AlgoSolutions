ef6d29babf5da15aef9e26f1de1cfa0d77aa6f23
mitcc <mitcc@qq.com>
2016-02-22 23:14:10 +0800

    :basketball:

--- lintcode/LowestCommonAncestor.java
+++ lintcode/LowestCommonAncestor.java
@@ -3,6 +3,11 @@
  */
 public class LowestCommonAncestor {
 
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
 
     }
