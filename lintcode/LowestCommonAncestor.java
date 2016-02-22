92a5503d0ff4e4d448ad806fd513291674e04f11
mitcc <mitcc@qq.com>
2016-02-22 23:14:23 +0800

    :basketball:

--- lintcode/LowestCommonAncestor.java
+++ lintcode/LowestCommonAncestor.java
@@ -1,6 +1,17 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class LowestCommonAncestor {
 
     /**
