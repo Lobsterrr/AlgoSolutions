0905e8940c90a10113bfac7cc97223abcab7fb3d
mitcc <mitcc@qq.com>
2016-02-24 00:04:11 +0800

    :basketball:

--- lintcode/CheckPowerOf2.java
+++ lintcode/CheckPowerOf2.java
@@ -1,5 +1,13 @@
 /*
- *
+ * Using O(1) time to check whether an integer n is a power of 2.
+
+Example
+For n=4, return true;
+
+For n=5, return false;
+
+Challenge
+O(1) time
  */
 public class CheckPowerOf2 {
 
