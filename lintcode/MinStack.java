0dc87d64b0a5f76d8c7377ab83d74ef98f91d67e
mitcc <mitcc@qq.com>
2016-02-24 00:56:05 +0800

    :basketball:

---
+++ lintcode/MinStack.java
@@ -0,0 +1,22 @@
+/*
+ *
+ */
+public class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int number) {
+
+    }
+
+    public int pop() {
+
+    }
+    
+    public int min() {
+
+    }
+
+}
