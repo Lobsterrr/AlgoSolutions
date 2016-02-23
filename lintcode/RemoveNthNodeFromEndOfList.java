0912fae989f1403c68b1b2c5ef7656cc204ab5a0
mitcc <mitcc@qq.com>
2016-02-24 00:00:21 +0800

    :basketball:

--- lintcode/RemoveNthNodeFromEndOfList.java
+++ lintcode/RemoveNthNodeFromEndOfList.java
@@ -1,10 +1,12 @@
 /*
- * Given a linked list, remove the nth node from the end of list and return its head.
+ * Given a linked list, remove the nth node from the end of 
+ * list and return its head.
 
 Example
 Given linked list: 1->2->3->4->5->null, and n = 2.
 
-After removing the second node from the end, the linked list becomes 1->2->3->5->null.
+After removing the second node from the end, the linked 
+list becomes 1->2->3->5->null.
 Note
 The minimum number of nodes in list is n.
 
