20946db3f3793b6dfb2ca12aeca475b54e496c1b
mitcc <mitcc@qq.com>
2016-02-23 10:06:18 +0800

    :basketball:

--- lintcode/KSumII.java
+++ lintcode/KSumII.java
@@ -30,13 +30,11 @@ public class KSumII {
     public void dfs(ArrayList<ArrayList<Integer>> result, 
             ArrayList<Integer> list, int[] A, int position, int k,
             int target) {
-        if (list.size() == k) {
-            if (target == 0) {
-                result.add(new ArrayList<Integer>(list));
-            }
+        if (list.size() == k && target == 0) {
+            result.add(new ArrayList<Integer>(list));
             return;
         }
-        for (int i = position; i < A.length; ++i) {
+        for (int i = position; list.size() < k && i < A.length; ++i) {
             list.add(A[i]);
             dfs(result, list, A, i + 1, k, target - A[i]);
             list.remove(list.size() - 1);
