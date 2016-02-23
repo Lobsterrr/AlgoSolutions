c9b232f75d36e5949e482b0d9fed9888e46c8ecc
mitcc <mitcc@qq.com>
2016-02-23 09:19:33 +0800

    :basketball:

--- lintcode/KSumII.java
+++ lintcode/KSumII.java
@@ -3,6 +3,12 @@
  */
 public class KSumII {
 
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return a list of lists of integer 
+     */ 
     public ArrayList<ArrayList<Integer>> kSumII(int[] A, int k, int target) {
 
     }
