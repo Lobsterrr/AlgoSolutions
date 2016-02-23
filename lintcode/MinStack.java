f381996a551f5526e327361589699a016afdebda
mitcc <mitcc@qq.com>
2016-02-24 00:56:29 +0800

    :basketball:

--- lintcode/MinStack.java
+++ lintcode/MinStack.java
@@ -1,5 +1,18 @@
 /*
- *
+ * Implement a stack with min() function, which will return the smallest number in the stack.
+
+It should support push, pop and min operation all in O(1) cost.
+
+Example
+push(1)
+pop()   // return 1
+push(2)
+push(3)
+min()   // return 2
+push(1)
+min()   // return 1
+Note
+min operation will never be called if there is no number in the stack.
  */
 public class MinStack {
 
