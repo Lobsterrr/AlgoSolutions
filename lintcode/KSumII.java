531615b95b3bccb475ffdf8e1d6f0bfeeb44435e
mitcc <mitcc@qq.com>
2016-02-23 09:43:36 +0800

    :basketball:

--- lintcode/KSumII.java
+++ lintcode/KSumII.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given n unique integers, number k (1<=k<=n) and target.
+
+Find all possible k integers where their sum is target.
+
+Example
+Given [1,2,3,4], k = 2, target = 5. Return:
+
+[
+  [1,4],
+  [2,3]
+]
  */
 public class KSumII {
 
