20bf3c43ee73f024438f4d7e8ca8617908decb95
mitcc <mitcc@qq.com>
2016-02-23 23:44:32 +0800

    :basketball:

--- lintcode/RemoveNthNodeFromEndOfList.java
+++ lintcode/RemoveNthNodeFromEndOfList.java
@@ -3,6 +3,11 @@
  */
 public class RemoveNthNodeFromEndOfList {
 
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: The head of linked list.
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
     }
