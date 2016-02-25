1b2843a674acc3519d35ae276dce7c3ffd4b0af6
mitcc <mitcc@qq.com>
2016-02-25 23:19:25 +0800

    :basketball:

--- lintcode/Permutations.java
+++ lintcode/Permutations.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given a list of numbers, return all possible permutations.
+
+Have you met this question in a real interview? Yes
+Example
+For nums = [1,2,3], the permutations are:
+
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+Challenge
+Do it without recursion.
  */
 public class Permutations {
 
