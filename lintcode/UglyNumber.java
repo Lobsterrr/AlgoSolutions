ec0c0613c3e67189594f43cbde1bc4c43fda7e52
mitcc <mitcc@qq.com>
2016-02-25 22:06:56 +0800

    :basketball:

--- lintcode/UglyNumber.java
+++ lintcode/UglyNumber.java
@@ -3,6 +3,10 @@
  */
 public class UglyNumber {
 
+    /**
+     * @param k: The number k.
+     * @return: The kth prime number as description.
+     */
     public long kthPrimeNumber(int k) {
 
     }
