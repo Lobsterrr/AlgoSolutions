37879a03f91ac2befbed2f497b69be117808aa89
mitcc <mitcc@qq.com>
2016-02-25 23:19:10 +0800

    :basketball:

--- lintcode/Permutations.java
+++ lintcode/Permutations.java
@@ -3,6 +3,10 @@
  */
 public class Permutations {
 
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
     public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> nums) {
 
     }
