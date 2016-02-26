c504964896999f6b0f6fd60dbe8fb1f266aff85a
mitcc <mitcc@qq.com>
2016-02-26 23:34:39 +0800

    :basketball:

--- lintcode/SegmentTreeQueryII.java
+++ lintcode/SegmentTreeQueryII.java
@@ -3,6 +3,11 @@
  */
 public class SegmentTreeQueryII {
 
+    /**
+     *@param root, start, end: The root of segment tree and 
+     *                         an segment / interval
+     *@return: The count number in the interval [start, end]
+     */
     public int query(SegmentTreeNode root, int start, int end) {
 
     }
