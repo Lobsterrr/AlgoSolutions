a203889d7869f2540969f5dcf54240a0dfe5e3a4
mitcc <mitcc@qq.com>
2016-02-26 23:34:56 +0800

    :basketball:

--- lintcode/SegmentTreeQueryII.java
+++ lintcode/SegmentTreeQueryII.java
@@ -1,6 +1,19 @@
 /*
  *
  */
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, count;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int count) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.count = count;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class SegmentTreeQueryII {
 
     /**
