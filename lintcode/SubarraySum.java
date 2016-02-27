c1cb020288236c8afceb985116004c3ba1aea123
mitcc <mitcc@qq.com>
2016-02-28 01:48:35 +0800

    :basketball:

--- lintcode/SubarraySum.java
+++ lintcode/SubarraySum.java
@@ -18,10 +18,16 @@ public class SubarraySum {
     public ArrayList<Integer> subarraySum(int[] nums) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < nums.length; ++i) {
+            int sum = 0;
             for (int j = i; j < nums.length; ++j) {
-
+                sum += nums[j];
+                if (sum == 0) {
+                    result.add(i, nums[j]);
+                    return result;
+                }
             }
         }
+        return result;
     }
 
 }
