d8ec57a89070866d29e24a2fba11df777ac2a057
mitcc <mitcc@qq.com>
2016-02-28 01:36:50 +0800

    :basketball:

--- lintcode/SubarraySum.java
+++ lintcode/SubarraySum.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Given an integer array, find a subarray where the sum of numbers is zero. Your code should return the index of the first number and the index of the last number.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+There is at least one subarray that it's sum equals to zero.
+
+Example
+Tags
+Related Problems
+ Notes
+Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
  */
 public class SubarraySum {
 
