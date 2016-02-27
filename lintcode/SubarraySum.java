dd6cdf402d5843098fd65e05ca3a5d1ebe60b865
mitcc <mitcc@qq.com>
2016-02-28 01:36:35 +0800

    :basketball:

--- lintcode/SubarraySum.java
+++ lintcode/SubarraySum.java
@@ -3,6 +3,11 @@
  */
 public class SubarraySum {
 
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
     public ArrayList<Integer> subarraySum(int[] nums) {
 
     }
