9edc89984d22b16606e7c898e99ac3d1ad48b45e
mitcc <mitcc@qq.com>
2016-02-28 15:09:36 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedListII.java
+++ lintcode/RemoveDuplicatesFromSortedListII.java
@@ -1,6 +1,17 @@
 /*
  *
  */
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class RemoveDuplicatesFromSortedListII {
 
     /**
