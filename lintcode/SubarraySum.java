72bdb7cdcb25b25c5446e0bf11b30a86a50a8203
mitcc <mitcc@qq.com>
2016-02-28 14:14:35 +0800

    :basketball:

--- lintcode/SubarraySum.java
+++ lintcode/SubarraySum.java
@@ -21,8 +21,7 @@ public class SubarraySum {
         ArrayList<Integer> result = new ArrayList<Integer>();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.put(0, -1);
-        int sum = 0;
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0, sum = 0; i < nums.length; ++i) {
             sum += nums[i];
             if (map.containsKey(sum)) {
                 result.add(map.get(sum) + 1);
