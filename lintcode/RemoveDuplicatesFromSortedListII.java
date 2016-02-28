dca28700188352b35c7831811582691074be213a
mitcc <mitcc@qq.com>
2016-02-28 15:38:26 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedListII.java
+++ lintcode/RemoveDuplicatesFromSortedListII.java
@@ -28,6 +28,23 @@ public class RemoveDuplicatesFromSortedListII {
         ListNode prev = dummy;
         ListNode cur = prev.next;
         while (cur != null) {
+            if (cur != null && prev.next.val == cur.val) {
+                cur = cur.next;
+            } else if (prev.next.next == cur) {
+                prev = prev.next;
+            } else {
+                prev.next = cur;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = prev.next;
+        while (cur != null) {
             while (cur != null && prev.next.val == cur.val) {
                 cur = cur.next;
             }
