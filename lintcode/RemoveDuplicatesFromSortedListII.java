143960a01f5b5c69211b95fee529df187ab830f7
mitcc <mitcc@qq.com>
2016-02-28 15:09:56 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedListII.java
+++ lintcode/RemoveDuplicatesFromSortedListII.java
@@ -1,7 +1,6 @@
 /*
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
 
-Have you met this question in a real interview? Yes
 Example
 Tags
 Related Problems
