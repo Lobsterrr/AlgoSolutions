ac64c8a658ddda82d5ab584f959315ab0a22e5b8
mitcc <mitcc@qq.com>
2016-02-28 15:09:24 +0800

    :basketball:

--- lintcode/RemoveDuplicatesFromSortedListII.java
+++ lintcode/RemoveDuplicatesFromSortedListII.java
@@ -3,6 +3,10 @@
  */
 public class RemoveDuplicatesFromSortedListII {
 
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of the linked list
+     */
     public static ListNode deleteDuplicates(ListNode head) {
 
     }
