ab7545df65d74a1ac4f8af63ea496e54b7a500c6
mitcc <mitcc@qq.com>
2016-02-29 17:56:49 +0800

    :basketball:

--- lintcode/LongestSubstringWithoutRepeatingCharacters.java
+++ lintcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given a string, find the length of the longest substring without repeating characters.
+
+Have you met this question in a real interview? Yes
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+
+For "bbbbb" the longest substring is "b", with the length of 1.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
 
