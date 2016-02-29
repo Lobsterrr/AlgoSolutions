1988c003ceaadb39e560f29676494c515b445bc1
mitcc <mitcc@qq.com>
2016-02-29 17:55:56 +0800

    :basketball:

--- lintcode/LongestSubstringWithoutRepeatingCharacters.java
+++ lintcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,6 +3,10 @@
  */
 public class LongestSubstringWithoutRepeatingCharacters {
 
+    /**
+     * @param s: a string
+     * @return: an integer 
+     */
     public int lengthOfLongestSubstring(String s) {
 
     }
