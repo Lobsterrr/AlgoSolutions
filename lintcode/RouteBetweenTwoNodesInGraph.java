4eac7ac3cd29140ce7d53a5ae725661f4ea8292d
mitcc <mitcc@qq.com>
2016-03-01 23:58:03 +0800

    :basketball:

--- lintcode/RouteBetweenTwoNodesInGraph.java
+++ lintcode/RouteBetweenTwoNodesInGraph.java
@@ -3,6 +3,12 @@
  */
 public class RouteBetweenTwoNodesInGraph {
 
+    /**
+     * @param graph: A list of Directed graph node
+     * @param s: the starting Directed graph node
+     * @param t: the terminal Directed graph node
+     * @return: a boolean value
+     */
     public boolean hasRoute(ArrayList<DirectedGraphNode> graph, DirectedGraphNode s, DirectedGraphNode t) {
 
     }
