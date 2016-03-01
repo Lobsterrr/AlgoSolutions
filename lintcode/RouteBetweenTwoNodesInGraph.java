53d37e49005782de4443276d1452c5e7b56e0041
mitcc <mitcc@qq.com>
2016-03-02 00:00:48 +0800

    :basketball:

--- lintcode/RouteBetweenTwoNodesInGraph.java
+++ lintcode/RouteBetweenTwoNodesInGraph.java
@@ -1,5 +1,22 @@
 /*
- *
+ * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+
+Have you met this question in a real interview? Yes
+Example
+Tags
+Related Problems
+ Notes
+Given graph:
+
+A----->B----->C
+ \     |
+  \    |
+   \   |
+    \  v
+     ->D----->E
+for s = B and t = E, return true
+
+for s = D and t = C, return false
  */
 /**
  * Definition for Directed graph.
