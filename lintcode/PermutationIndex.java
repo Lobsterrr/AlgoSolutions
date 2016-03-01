e0cc8a3bd2b2cc48b36d00eb28d4121544367967
mitcc <mitcc@qq.com>
2016-03-02 01:18:47 +0800

    :basketball:

--- lintcode/PermutationIndex.java
+++ lintcode/PermutationIndex.java
@@ -1,5 +1,11 @@
 /*
- *
+ * Given a permutation which contains no repeated number, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
+
+Have you met this question in a real interview? Yes
+Example
+Related Problems
+ Notes
+Given [1,2,4], return 1.
  */
 public class PermutationIndex {
 
