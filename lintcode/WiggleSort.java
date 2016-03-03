d88425821e7f7a17877318061e7915cf8371bf3f
mitcc <mitcc@qq.com>
2016-03-03 23:54:09 +0800

    :basketball:

--- lintcode/WiggleSort.java
+++ lintcode/WiggleSort.java
@@ -2,7 +2,6 @@
  * Given an unsorted array nums, reorder it in-place such that
 
 nums[0] <= nums[1] >= nums[2] <= nums[3]....
-Have you met this question in a real interview? Yes
  Notice
 
 Please complete the problem in-place.
