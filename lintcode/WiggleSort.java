af9e4b91c1dc467658a18ccee515a152445201d1
mitcc <mitcc@qq.com>
2016-03-04 22:24:58 +0800

    :basketball:

--- lintcode/WiggleSort.java
+++ lintcode/WiggleSort.java
@@ -25,7 +25,14 @@ public class WiggleSort {
     }
 
     public void wiggleSort(int[] nums) {
-        
+        for (int i = 1; i < nums.length; ++i) {
+            if (i % 2 == 0 && nums[i] > nums[i - 1] 
+                    || i % 2 == 1 && nums[i] < nums[i - 1]) {
+                int tmp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = tmp;
+            }
+        }
     }
 
 }
