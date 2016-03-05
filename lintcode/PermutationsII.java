3358cfb645306e879c9a2dd3c11e5b3bcfc924b3
mitcc <mitcc@qq.com>
2016-03-05 23:49:07 +0800

    :basketball:

--- lintcode/PermutationsII.java
+++ lintcode/PermutationsII.java
@@ -1,5 +1,19 @@
 /*
- *
+ * Given a list of numbers with duplicate number in it. Find all unique permutations.
+
+Have you met this question in a real interview? Yes
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+For numbers [1,2,2] the unique permutations are:
+
+[
+  [1,2,2],
+  [2,1,2],
+  [2,2,1]
+]
  */
 public class PermutationsII {
 
