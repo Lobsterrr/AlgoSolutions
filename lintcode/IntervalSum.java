3de17cf510a3eb191d2e2bdaf9b1f413ef172014
mitcc <mitcc@qq.com>
2016-03-05 23:06:09 +0800

    :basketball:

--- lintcode/IntervalSum.java
+++ lintcode/IntervalSum.java
@@ -3,6 +3,10 @@
  */
 public class IntervalSum {
 
+    /**
+     *@param A, queries: Given an integer array and an query list
+     *@return: The result list
+     */
     public ArrayList<Long> intervalSum(int[] A, ArrayList<Interval> queries) {
 
     }
