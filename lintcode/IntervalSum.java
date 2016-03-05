ccb0c63b62939a456fcbb6d05b1974c80b2bd7dd
mitcc <mitcc@qq.com>
2016-03-05 23:06:54 +0800

    :basketball:

--- lintcode/IntervalSum.java
+++ lintcode/IntervalSum.java
@@ -1,5 +1,17 @@
 /*
- *
+ * Given an integer array (index from 0 to n-1, where n is the size of this array), and an query list. Each query has two integers [start, end]. For each query, calculate the sum number between index start and end in the given array, return the result list.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+We suggest you finish problem Segment Tree Build, Segment Tree Query and Segment Tree Modify first.
+
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+For array [1,2,7,8,5], and queries [(0,4),(1,2),(2,4)], return [23,9,20]
  */
 /**
  * Definition of Interval:
