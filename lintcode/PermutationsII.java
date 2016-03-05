162c52b53c4bb1f8dfb20570a4cb70f96fd50415
mitcc <mitcc@qq.com>
2016-03-05 23:48:51 +0800

    :basketball:

--- lintcode/PermutationsII.java
+++ lintcode/PermutationsII.java
@@ -3,6 +3,10 @@
  */
 public class PermutationsII {
 
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of unique permutations.
+     */
     public ArrayList<ArrayList<Integer>> permuteUnique(ArrayList<Integer> nums) {
 
     }
