928b44189e0186b75791aaf3dbc2a85628bfe61b
mitcc <mitcc@qq.com>
2016-03-05 23:45:19 +0800

    :basketball:

--- lintcode/PermutationSequence.java
+++ lintcode/PermutationSequence.java
@@ -1,5 +1,25 @@
 /*
- *
+ * Given n and k, return the k-th permutation sequence.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+n will be between 1 and 9 inclusive.
+
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+For n = 3, all permutations are listed as follows:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+If k = 4, the fourth permutation is "231"
  */
 public class PermutationSequence {
 
