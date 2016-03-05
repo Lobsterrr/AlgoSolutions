5dc44b589760f3c8654c5566aa8dcb966d35b9ef
mitcc <mitcc@qq.com>
2016-03-05 23:45:05 +0800

    :basketball:

--- lintcode/PermutationSequence.java
+++ lintcode/PermutationSequence.java
@@ -3,6 +3,11 @@
  */
 public class PermutationSequence {
 
+    /**
+      * @param n: n
+      * @param k: the kth permutation
+      * @return: return the k-th permutation
+      */
     public String getPermutation(int n, int k) {
 
     }
