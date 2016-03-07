96b56d33aeb89ba32cb32bb9f3dc096f8d6ddb94
mitcc <mitcc@qq.com>
2016-03-07 23:52:37 +0800

    :basketball:

--- lintcode/SubsetsII.java
+++ lintcode/SubsetsII.java
@@ -33,7 +33,8 @@ public class SubsetsII {
         Collections.sort(S);
     } 
 
-    public void dfs(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, int position, ArrayList<Integer> S) {
+    public void dfs(ArrayList<ArrayList<Integer>> result, 
+            ArrayList<Integer> list, int position, ArrayList<Integer> S) {
 
     }
 
