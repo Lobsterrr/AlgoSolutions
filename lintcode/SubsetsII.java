02f09b7d0377cd0df953ce3366e6b3e9abeac668
mitcc <mitcc@qq.com>
2016-03-07 23:44:48 +0800

    :basketball:

--- lintcode/SubsetsII.java
+++ lintcode/SubsetsII.java
@@ -1,5 +1,27 @@
 /*
- *
+ * Given a list of numbers that may has duplicate numbers, return all possible subsets
+
+Have you met this question in a real interview? Yes
+ Notice
+
+Each element in a subset must be in non-descending order.
+The ordering between two subsets is free.
+The solution set must not contain duplicate subsets.
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
  */
 public class SubsetsII {
 
