f0285aa0e606acd84f9e25722abb57a74ec6cb36
mitcc <mitcc@qq.com>
2016-03-07 23:44:32 +0800

    :basketball:

--- lintcode/SubsetsII.java
+++ lintcode/SubsetsII.java
@@ -3,6 +3,10 @@
  */
 public class SubsetsII {
 
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
     public ArrayList<ArrayList<Integer>> subsetsWithDup(ArrayList<Integer> S) {
 
     } 
