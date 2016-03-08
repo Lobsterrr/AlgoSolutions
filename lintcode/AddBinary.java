313574e52c8b4c12804caa4d22550951bf273707
mitcc <mitcc@qq.com>
2016-03-08 22:48:56 +0800

    :basketball:

--- lintcode/AddBinary.java
+++ lintcode/AddBinary.java
@@ -3,6 +3,11 @@
  */
 public class AddBinary {
 
+    /**
+     * @param a a number
+     * @param b a number
+     * @return the result
+     */
     public String addBinary(String a, String b) {
 
     }
