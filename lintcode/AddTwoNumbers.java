125bbb4efe7a38d8b24f4aea9b7be2cf2cc3849d
mitcc <mitcc@qq.com>
2016-03-09 01:29:16 +0800

    :basketball:

--- lintcode/AddTwoNumbers.java
+++ lintcode/AddTwoNumbers.java
@@ -3,6 +3,11 @@
  */
 public class AddTwoNumbers {
 
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
     public ListNode addList(ListNode l1, ListNode l2) {
 
     }
