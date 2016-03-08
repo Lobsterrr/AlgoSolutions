5c969ff6a3438a60ae547748f57ba542443ca5b4
mitcc <mitcc@qq.com>
2016-03-08 22:49:08 +0800

    :basketball:

--- lintcode/AddBinary.java
+++ lintcode/AddBinary.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Given two binary strings, return their sum (also a binary string).
+
+Have you met this question in a real interview? Yes
+Example
+Tags
+Related Problems
+ Notes
+a = 11
+
+b = 1
+
+Return 100
  */
 public class AddBinary {
 
