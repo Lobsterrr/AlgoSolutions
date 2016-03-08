159ec3f273b260b81605dc9a353fd3858e786735
mitcc <mitcc@qq.com>
2016-03-09 01:22:12 +0800

    :basketball:

--- lintcode/AddBinary.java
+++ lintcode/AddBinary.java
@@ -20,6 +20,22 @@ public class AddBinary {
         int carrier = 0;
         int len = Math.max(a.length(), b.length());
         for (int i = 0; i < len; ++i) {
+            int va = i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            int vb = i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            result = ((va + vb + carrier) % 2) + result;
+            carrier = (va + vb + carrier) / 2;
+        }
+        if (carrier > 0) {
+            result = carrier + result;
+        }
+        return result;
+    }
+
+    public String addBinary(String a, String b) {
+        String result = "";
+        int carrier = 0;
+        int len = Math.max(a.length(), b.length());
+        for (int i = 0; i < len; ++i) {
             int va = 0;
             int vb = 0;
             if (i < a.length()) {
