2b8d490ae074e32d63df6f4ecdb75b931faf0492
mitcc <mitcc@qq.com>
2016-03-09 23:20:52 +0800

    :basketball:

--- lintcode/MergeIntervals.java
+++ lintcode/MergeIntervals.java
@@ -3,6 +3,10 @@
  */
 public class MergeIntervals {
 
+    /**
+     * @param intervals: Sorted interval list.
+     * @return: A new sorted interval list.
+     */
     public List<Interval> merge(List<Interval> intervals) {
 
     }
