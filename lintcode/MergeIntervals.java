e3fda2fac46fb4e22d7253520079cfe1b20ca167
mitcc <mitcc@qq.com>
2016-03-09 23:21:24 +0800

    :basketball:

--- lintcode/MergeIntervals.java
+++ lintcode/MergeIntervals.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given a collection of intervals, merge all overlapping intervals.
+
+Have you met this question in a real interview? Yes
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+Given intervals => merged intervals:
+
+[                     [
+  [1, 3],               [1, 6],
+  [2, 6],      =>       [8, 10],
+  [8, 10],              [15, 18]
+  [15, 18]            ]
+]
  */
 /**
  * Definition of Interval:
