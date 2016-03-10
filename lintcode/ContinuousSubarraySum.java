69f138fe8bdc6f47aa69709033026c89cd3fa913
mitcc <mitcc@qq.com>
2016-03-10 23:32:26 +0800

    :basketball:

--- lintcode/ContinuousSubarraySum.java
+++ lintcode/ContinuousSubarraySum.java
@@ -1,5 +1,8 @@
 /*
- * Given an integer array, find a continuous subarray where the sum of numbers is the biggest. Your code should return the index of the first number and the index of the last number. (If their are duplicate answer, return anyone)
+ * Given an integer array, find a continuous subarray where the sum 
+ * of numbers is the biggest. Your code should return the index of 
+ * the first number and the index of the last number. (If their are 
+ * duplicate answer, return anyone)
 
 Example
 Give [-3, 1, 3, -3, 4], return [1,4].
@@ -8,7 +11,8 @@ public class ContinuousSubarraySum {
 
     /**
      * @param A an integer array
-     * @return  A list of integers includes the index of the first number and the index of the last number
+     * @return  A list of integers includes the index of the 
+     * first number and the index of the last number
      */
     public ArrayList<Integer> continuousSubarraySum(int[] A) {
 
