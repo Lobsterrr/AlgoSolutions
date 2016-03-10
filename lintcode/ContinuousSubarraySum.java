4d4d55b1dcc91a842db456fcef8ec1abecefa968
mitcc <mitcc@qq.com>
2016-03-10 23:29:53 +0800

    :basketball:

--- lintcode/ContinuousSubarraySum.java
+++ lintcode/ContinuousSubarraySum.java
@@ -3,6 +3,10 @@
  */
 public class ContinuousSubarraySum {
 
+    /**
+     * @param A an integer array
+     * @return  A list of integers includes the index of the first number and the index of the last number
+     */
     public ArrayList<Integer> continuousSubarraySum(int[] A) {
 
     }
