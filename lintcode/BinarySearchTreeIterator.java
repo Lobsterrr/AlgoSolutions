3499632313e9d69be756384243b605770cf03079
mitcc <mitcc@qq.com>
2016-03-11 09:07:22 +0800

    :basketball:

--- lintcode/BinarySearchTreeIterator.java
+++ lintcode/BinarySearchTreeIterator.java
@@ -1,5 +1,21 @@
 /*
- *
+ * Design an iterator over a binary search tree with the following rules:
+
+Elements are visited in ascending order (i.e. an in-order traversal)
+next() and hasNext() queries run in O(1) time in average.
+Have you met this question in a real interview? Yes
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+For the following binary search tree, in-order traversal by using iterator is [1, 6, 10, 11, 12]
+
+   10
+ /    \
+1      11
+ \       \
+  6       12
  */
 /**
  * Definition of TreeNode:
