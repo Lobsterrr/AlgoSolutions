16c6bcaf3d4514eb8183f75b2ad305fd7279a5d5
mitcc <mitcc@qq.com>
2016-03-11 09:07:05 +0800

    :basketball:

--- lintcode/BinarySearchTreeIterator.java
+++ lintcode/BinarySearchTreeIterator.java
@@ -1,6 +1,23 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
 public class BinarySearchTreeIterator {
 
 }
