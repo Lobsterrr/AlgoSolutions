2c91feee90dd4ee91528e5faa70aca8cc9a00bf3
mitcc <mitcc@qq.com>
2016-03-13 23:16:02 +0800

    :basketball:

--- lintcode/BinarySearchTreeIterator.java
+++ lintcode/BinarySearchTreeIterator.java
@@ -43,6 +43,39 @@ class BSTIterator {
 
     Stack<TreeNode> stack;
     TreeNode cur;
+    List<TreeNode> list;
+
+    //@param root: The root of binary tree.
+    public BSTIterator(TreeNode root) {
+        cur = root;
+        list = new ArrayList<TreeNode>();
+        iterator(root, list);
+    }
+
+    public void iterator(TreeNode root, List<TreeNode> list) {
+        while (cur != null || !stack.isEmpty()) {
+
+        }
+    }
+
+    //@return: True if there has next node, or false
+    public boolean hasNext() {
+    }
+
+    //@return: return next node
+    public TreeNode next() {
+
+    }
+
+}
+
+
+
+
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+    TreeNode cur;
 
     //@param root: The root of binary tree.
     public BSTIterator(TreeNode root) {
