b559ef3c4e51841f734c8aa7e9f703baa2c02578
mitcc <mitcc@qq.com>
2016-03-14 23:13:35 +0800

    :basketball:

--- lintcode/DecodeWays.java
+++ lintcode/DecodeWays.java
@@ -8,8 +8,6 @@
 Given an encoded message containing digits, determine the total number of ways to decode it.
 
 Example
-Tags
- Notes
 Given encoded message 12, it could be decoded as AB (1 2) or L (12).
 The number of ways decoding 12 is 2.
  */
