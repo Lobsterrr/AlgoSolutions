154ba05fb3beba1bf95c02f17078f4a8c735f63a
mitcc <mitcc@qq.com>
2016-03-14 23:12:47 +0800

    :basketball:

--- lintcode/DecodeWays.java
+++ lintcode/DecodeWays.java
@@ -3,6 +3,10 @@
  */
 public class DecodeWays {
 
+    /**
+     * @param s a string,  encoded message
+     * @return an integer, the number of ways decoding
+     */
     public int numDecodings(String s) {
 
     }
