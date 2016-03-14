5329c4b69596050ffebff45152d5b0b9723e4288
mitcc <mitcc@qq.com>
2016-03-15 01:58:50 +0800

    :basketball:

--- lintcode/DecodeWays.java
+++ lintcode/DecodeWays.java
@@ -37,24 +37,4 @@ public class DecodeWays {
         return cur;
     }
 
-
-    public int numDecodings(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) > '0') {
-                dp[i + 1] += dp[i];
-            }
-            if (i > 0 && (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') && 
-                    10 <= Integer.parseInt(s.substring(i - 1, i + 1)) && 
-                    Integer.parseInt(s.substring(i - 1, i + 1)) <= 26) {
-                dp[i + 1] += dp[i - 1];
-            }
-        }
-        return dp[s.length()];
-    }
-
 }
