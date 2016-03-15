bafa55cfbcfb446b23f840e07c2f38ee4c6028a2
mitcc <mitcc@qq.com>
2016-03-15 23:37:28 +0800

    :basketball:

--- lintcode/BurstBalloons.java
+++ lintcode/BurstBalloons.java
@@ -3,6 +3,10 @@
  */
 public class BurstBalloons {
 
+    /**
+     * @param nums a list of integer
+     * @return an integer, maximum coins
+     */
     public int maxCoins(int[] nums) {
 
     }
