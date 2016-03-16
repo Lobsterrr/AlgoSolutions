d08ae15a218f6bcee4bb8e47045ac90d5fae5544
mitcc <mitcc@qq.com>
2016-03-16 23:02:09 +0800

    :basketball:

--- lintcode/SwapTwoNodesInLinkedList.java
+++ lintcode/SwapTwoNodesInLinkedList.java
@@ -1,6 +1,14 @@
 /*
  *
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class SwapTwoNodesInLinkedList {
 
     /**
