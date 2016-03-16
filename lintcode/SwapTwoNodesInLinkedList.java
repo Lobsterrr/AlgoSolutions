8a129ee663b202e1bbeb09726ab2e94234f61438
mitcc <mitcc@qq.com>
2016-03-16 23:01:57 +0800

    :basketball:

--- lintcode/SwapTwoNodesInLinkedList.java
+++ lintcode/SwapTwoNodesInLinkedList.java
@@ -3,6 +3,12 @@
  */
 public class SwapTwoNodesInLinkedList {
 
+    /**
+     * @param head a ListNode
+     * @oaram v1 an integer
+     * @param v2 an integer
+     * @return a new head of singly-linked list
+     */
     public ListNode swapNodes(ListNode head, int v1, int v2) {
 
     }
