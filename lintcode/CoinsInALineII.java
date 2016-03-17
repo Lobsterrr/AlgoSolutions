539c093af718be6b0067f60cb53ae254354cfe70
mitcc <mitcc@qq.com>
2016-03-17 23:46:50 +0800

    :basketball:

--- lintcode/CoinsInALineII.java
+++ lintcode/CoinsInALineII.java
@@ -3,6 +3,10 @@
  */
 public class CoinsInALineII {
 
+    /**
+     * @param values: an array of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
     public boolean firstWillWin(int[] values) {
 
     }
