edd831947f5cd0764295b2360a41e7eb7e89634d
mitcc <mitcc@qq.com>
2016-03-17 23:23:52 +0800

    :basketball:

--- lintcode/Singleton.java
+++ lintcode/Singleton.java
@@ -1,5 +1,9 @@
 /*
- * Singleton is a most widely used design pattern. If a class has and only has one instance at every moment, we call this design as singleton. For example, for class Mouse (not a animal mouse), we should design it in singleton.
+ * Singleton is a most widely used design pattern. If a 
+ * class has and only has one instance at every moment, we 
+ * call this design as singleton. For example, for class 
+ * Mouse (not a animal mouse), we should design it in 
+ * singleton.
 
 You job is to implement a getInstance method for given class, return the same instance of this class every time you call this method.
 
@@ -11,7 +15,8 @@ A b = A.getInstance();
 a should equal to b.
 
 Challenge
-If we call getInstance concurrently, can you make sure your code could run correctly?
+If we call getInstance concurrently, can you make sure your 
+code could run correctly?
  */
 public class Singleton {
     
