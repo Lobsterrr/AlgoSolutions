d611320749a9928f77641edbf9b9600b11830b59
mitcc <mitcc@qq.com>
2016-03-17 22:31:04 +0800

    :basketball:

--- lintcode/Singleton.java
+++ lintcode/Singleton.java
@@ -2,9 +2,14 @@
  *
  */
 public class Singleton {
+    
+    public Singleton singleton = null;
 
     public static Singleton getInstance() {
-
+        if (singleton == null) {
+            singleton = new singleton();
+        }
+        return singleton;
     }
 
 }
