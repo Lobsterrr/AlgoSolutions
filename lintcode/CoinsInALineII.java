fb07bf17d7492dd6798dd83a3f8949bf92936c05
mitcc <mitcc@qq.com>
2016-03-17 23:47:06 +0800

    :basketball:

--- lintcode/CoinsInALineII.java
+++ lintcode/CoinsInALineII.java
@@ -1,5 +1,16 @@
 /*
- *
+ * There are n coins with different value in a line. Two players take turns to take one or two coins from left side until there are no more coins left. The player who take the coins with the most value wins.
+
+Could you please decide the first player will win or lose?
+
+Have you met this question in a real interview? Yes
+Example
+Tags
+Related Problems
+ Notes
+Given values array A = [1,2,2], return true.
+
+Given A = [1,2,4], return false.
  */
 public class CoinsInALineII {
 
