620ac27cb368f817b51a42bb4391f0079eefad21
mitcc <mitcc@qq.com>
2016-03-17 22:34:01 +0800

    :basketball:

--- lintcode/Singleton.java
+++ lintcode/Singleton.java
@@ -1,5 +1,19 @@
 /*
- *
+ * Singleton is a most widely used design pattern. If a class has and only has one instance at every moment, we call this design as singleton. For example, for class Mouse (not a animal mouse), we should design it in singleton.
+
+You job is to implement a getInstance method for given class, return the same instance of this class every time you call this method.
+
+Have you met this question in a real interview? Yes
+Example
+Challenge
+Tags
+Related Problems
+ Notes
+In Java:
+
+A a = A.getInstance();
+A b = A.getInstance();
+a should equal to b.
  */
 public class Singleton {
     
