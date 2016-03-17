e7cae373604ae91e70f3e605e501ab3946214c05
mitcc <mitcc@qq.com>
2016-03-17 22:54:07 +0800

    :basketball:

--- lintcode/Singleton.java
+++ lintcode/Singleton.java
@@ -29,4 +29,9 @@ public class Singleton {
 
 /*******************************************************************/
 
+    public static Singleton singleton = new Singleton();
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
 }
