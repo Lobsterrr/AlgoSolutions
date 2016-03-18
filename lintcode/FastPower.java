a9c429e65b41de7480cfb4240e1407f6b16e43c7
mitcc <mitcc@qq.com>
2016-03-19 02:37:21 +0800

    :basketball:

--- lintcode/FastPower.java
+++ lintcode/FastPower.java
@@ -16,11 +16,14 @@ public class FastPower {
      * @return: An integer
      */
     public int fastPower(int a, int b, int n) {
-        if (n == 0 || n == 1) {
-            return (int) Math.pow(a, n) % b;
+        if (n == 0) {
+            return 1 % b;
         }
-        long half = fastPower(a, b, n / 2);
-        long result = half * half % b;
+        if (n == 1) {
+            return a % b;
+        }
+        long result = fastPower(a, b, n / 2);
+        result = result * result % b;
         if (n % 2 == 1) {
             result = result * a % b;
         }
