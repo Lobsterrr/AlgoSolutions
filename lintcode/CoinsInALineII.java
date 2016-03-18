5ffa03be740e233b040afd392bb239fdbf9c3457
mitcc <mitcc@qq.com>
2016-03-18 22:43:08 +0800

    :basketball:

--- lintcode/CoinsInALineII.java
+++ lintcode/CoinsInALineII.java
@@ -17,20 +17,6 @@ public class CoinsInALineII {
      * @return: a boolean which equals to true if the first player will win
      */
     public boolean firstWillWin(int[] values) {
-        int[] dp = new int[values.length];
-        int sum = 0;
-        for (int i = values.length - 1; i >= 0; --i) {
-            int a = i + 1 < values.length ? values[i + 1] : 0;
-            int b = i + 2 < values.length ? dp[i + 2] : 0;
-            int c = i + 3 < values.length ? dp[i + 3] : 0;
-            int d = i + 4 < values.length ? dp[i + 4] : 0;
-            dp[i] = values[i] + Math.max(Math.min(b, c), a + Math.min(c, d));
-            sum += values[i];
-        }
-        return dp[0] > sum - dp[0];
-    }
-
-    public boolean firstWillWin(int[] values) {
         int[] dp = new int[5];
         int sum = 0;
         for (int i = values.length - 1; i >= 0; --i) {
@@ -39,6 +25,20 @@ public class CoinsInALineII {
             int c = i + 3 < values.length ? dp[(i + 3) % 5] : 0;
             int d = i + 4 < values.length ? dp[(i + 4) % 5] : 0;
             dp[i % 5] = values[i] + Math.max(Math.min(b, c), a + Math.min(c, d));
+            sum += values[i];
+        }
+        return dp[0] > sum - dp[0];
+    }
+
+    public boolean firstWillWin(int[] values) {
+        int[] dp = new int[values.length];
+        int sum = 0;
+        for (int i = values.length - 1; i >= 0; --i) {
+            int a = i + 1 < values.length ? values[i + 1] : 0;
+            int b = i + 2 < values.length ? dp[i + 2] : 0;
+            int c = i + 3 < values.length ? dp[i + 3] : 0;
+            int d = i + 4 < values.length ? dp[i + 4] : 0;
+            dp[i] = values[i] + Math.max(Math.min(b, c), a + Math.min(c, d));
             sum += values[i];
         }
         return dp[0] > sum - dp[0];
