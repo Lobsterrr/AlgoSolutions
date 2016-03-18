54d5598d5c7d0594fe08460954f699a1174cf98a
mitcc <mitcc@qq.com>
2016-03-18 22:37:59 +0800

    :basketball:

--- lintcode/CoinsInALineII.java
+++ lintcode/CoinsInALineII.java
@@ -28,25 +28,4 @@ public class CoinsInALineII {
         return dp[0] > sum - dp[0];
     }
 
-    public boolean firstWillWin(int[] values) {
-        if (values.length <= 2) {
-            return true;
-        }
-        int[] dp = new int[values.length + 1];
-        dp[values.length] = 0;
-        dp[values.length - 1] = values[values.length - 1];
-        dp[values.length - 2] = values[values.length - 1] + values[values.length - 2];
-        dp[values.length - 3] = values[values.length - 3] + values[values.length - 2];
-        for (int i = values.length - 4; i >= 0; --i) {
-            int value1 = values[i] + Math.min(dp[i + 2], dp[i + 3]);
-            int value2 = values[i] + values[i + 1] + Math.min(dp[i + 3], dp[i + 4]);
-            dp[i] = Math.max(value1, value2);
-        }
-        int sum = 0;
-        for (int value : values) {
-            sum += value;
-        }
-        return dp[0] > sum - dp[0];
-    }
-
 }
