7f6c38900d6db14f12780e54c1bee7971c8d9312
mitcc <mitcc@qq.com>
2016-03-19 23:44:41 +0800

    :basketball:

--- lintcode/MergeKSortedLists.java
+++ lintcode/MergeKSortedLists.java
@@ -3,6 +3,10 @@
  */
 public class MergeKSortedLists {
 
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
     public ListNode mergeKLists(List<ListNode> lists) {
 
     }
