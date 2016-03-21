b12eba20e9d2b694a565f1992b3af34a14223c15
mitcc <mitcc@qq.com>
2016-03-21 22:17:51 +0800

    :basketball:

--- lintcode/PerfectSquares.java
+++ lintcode/PerfectSquares.java
@@ -16,8 +16,13 @@ public class PerfectSquares {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
-            
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j] + 1);
+            }
+            dp[i] = min;
         }
+        return dp[n];
     }
 
     public int numSquares(int n) {
