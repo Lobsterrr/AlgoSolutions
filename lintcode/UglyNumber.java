9e8276892450c2dd065f20a2ef71ece84b7de163
mitcc <mitcc@qq.com>
2016-03-22 01:27:31 +0800

    :basketball:

--- lintcode/UglyNumber.java
+++ lintcode/UglyNumber.java
@@ -1,5 +1,19 @@
 /*
- *
+ * Write a program to check whether a given number is an ugly number`.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+Note that 1 is typically treated as an ugly number.
+
+Example
+Tags
+Related Problems
+ Notes
+Given num = 8 return true
+Given num = 14 return false
  */
 public class UglyNumber {
 
