c7f6268ab3ea75b2834016d5aa0fa7d94abd94e0
mitcc <mitcc@qq.com>
2016-03-21 23:57:13 +0800

    :basketball:

--- leetcode/algorithms/PerfectSquares.java
+++ leetcode/algorithms/PerfectSquares.java
@@ -32,7 +32,9 @@ public class PerfectSquares {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
-
+            int minNum = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+            }
         }
     }
 
