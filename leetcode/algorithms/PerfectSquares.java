573213ea527fa46395b73341e066608b02033772
mitcc <mitcc@qq.com>
2016-03-22 00:45:43 +0800

    :basketball:

--- leetcode/algorithms/PerfectSquares.java
+++ leetcode/algorithms/PerfectSquares.java
@@ -33,12 +33,14 @@ public class PerfectSquares {
         while (n % 4 == 0) {
             n /= 4;
         }
-        if (n % 8 == 7)
+        if (n % 8 == 7) {
             return 4;
+        }
         for (int i = 1; i * i <= n; ++i) {
             int j = (int) Math.sqrt(n - i * i);
-            if (i * i + j * j == n)
+            if (i * i + j * j == n) {
                 return 2;
+            }
         }
         return 3;
     }
