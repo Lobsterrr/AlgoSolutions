7dbf3a22c39d588c64f46b03863660c4bb43135b
mitcc <mitcc@qq.com>
2016-03-22 01:27:17 +0800

    :basketball:

--- lintcode/UglyNumber.java
+++ lintcode/UglyNumber.java
@@ -3,6 +3,10 @@
  */
 public class UglyNumber {
 
+    /**
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
     public boolean isUgly(int num) {
 
     }
