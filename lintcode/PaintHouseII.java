03264f0b0c6946cd0a1a8c59a3b72ea1b69b5423
mitcc <mitcc@qq.com>
2016-03-22 21:54:32 +0800

    :basketball:

--- lintcode/PaintHouseII.java
+++ lintcode/PaintHouseII.java
@@ -3,6 +3,10 @@
  */
 public class PaintHouseII {
 
+    /**
+     * @param costs n x k cost matrix
+     * @return an integer, the minimum cost to paint all houses
+     */
     public int minCostII(int[][] costs) {
 
     }
