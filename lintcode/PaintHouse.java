d1b60b7720bf850cbdec4c38254771409d7117c7
mitcc <mitcc@qq.com>
2016-03-22 21:39:51 +0800

    :basketball:

--- lintcode/PaintHouse.java
+++ lintcode/PaintHouse.java
@@ -28,7 +28,7 @@ public class PaintHouse {
             costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
         }
         return Math.min(Math.min(costs[costs.length - 1][0], 
-                    costs[costs.length - 1][2]), costs[costs.length - 1][2]);
+                    costs[costs.length - 1][1]), costs[costs.length - 1][2]);
     }
 
 }
