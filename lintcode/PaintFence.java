c13f81870f3a8028e57fd4af3573c707a172ef3b
mitcc <mitcc@qq.com>
2016-03-23 23:47:42 +0800

    :basketball:

--- lintcode/PaintFence.java
+++ lintcode/PaintFence.java
@@ -35,7 +35,7 @@ public class PaintFence {
         int[] dp = new int[n + 1];
         dp[0] = k;
         dp[1] = (k - 1) * dp[0] + k;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 2; i < n; ++i) {
             dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2]);
         }
         return dp[n];
