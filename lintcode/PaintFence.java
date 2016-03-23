41c14eaec343de157190b58c55a4394360e458ee
mitcc <mitcc@qq.com>
2016-03-23 23:54:16 +0800

    :basketball:

--- lintcode/PaintFence.java
+++ lintcode/PaintFence.java
@@ -33,8 +33,9 @@ public class PaintFence {
         dp[0] = k;
         dp[1] = k * k;
         for (int i = 2; i < n; ++i) {
-            dp[i % 3] = (k - 1
+            dp[i % 3] = (k - 1) * (dp[(i - 1) % 3] + dp[(i - 2) % 3]);
         }
+        return dp[(n - 1) % 3];
     }
 
     public int numWays(int n, int k) {
