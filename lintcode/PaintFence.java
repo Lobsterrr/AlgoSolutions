9e90760a087fdea2ff03c21e998dcc25da642ad8
mitcc <mitcc@qq.com>
2016-03-23 23:31:33 +0800

    :basketball:

--- lintcode/PaintFence.java
+++ lintcode/PaintFence.java
@@ -1,5 +1,26 @@
 /*
- *
+ * There is a fence with n posts, each post can be painted with one of the k colors.
+You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+Return the total number of ways you can paint the fence.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+n and k are non-negative integers.
+
+Example
+Tags
+Related Problems
+ Notes
+Given n=3, k=2 return 6
+
+      post 1,   post 2, post 3
+way1    0         0       1 
+way2    0         1       0
+way3    0         1       1
+way4    1         0       0
+way5    1         0       1
+way6    1         1       0
  */
 public class PaintFence {
 
