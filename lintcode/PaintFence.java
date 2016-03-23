7a85f1411fb1e6ca3eda6a836fd7ebef20015e33
mitcc <mitcc@qq.com>
2016-03-23 23:31:12 +0800

    :basketball:

--- lintcode/PaintFence.java
+++ lintcode/PaintFence.java
@@ -3,6 +3,11 @@
  */
 public class PaintFence {
 
+    /**
+     * @param n non-negative integer, n posts
+     * @param k non-negative integer, k colors
+     * @return an integer, the total number of ways
+     */
     public int numWays(int n, int k) {
 
     }
