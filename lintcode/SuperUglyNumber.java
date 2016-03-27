0c9c6d42d806594e291df93fa889ea02b7932aa1
mitcc <mitcc@qq.com>
2016-03-28 02:30:39 +0800

    :basketball:

--- lintcode/SuperUglyNumber.java
+++ lintcode/SuperUglyNumber.java
@@ -3,6 +3,11 @@
  */
 public class SuperUglyNumber {
 
+    /**
+     * @param n a positive integer
+     * @param primes the given prime list
+     * @return the nth super ugly number
+     */
     public int nthSuperUglyNumber(int n, int[] primes) {
 
     }
