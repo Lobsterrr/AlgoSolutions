768b6909eca4a1a95f7bce6cfecdc8cb1d2d5f4b
mitcc <mitcc@qq.com>
2016-03-27 18:08:20 +0800

    :basketball:

--- leetcode/algorithms/CountingBits.java
+++ leetcode/algorithms/CountingBits.java
@@ -25,6 +25,18 @@
 public class CountingBits {
 
     public int[] countBits(int num) {
+        int[] bits = new int[num + 1];
+        for (int i = 0; i <= num; ++i) {
+            if (i % 2 == 0) {
+                bits[i] = bits[i / 2];
+            } else {
+                bits[i] = bits[i / 2] + 1;
+            }
+        }
+        return bits;
+    }
+
+    public int[] countBits(int num) {
         int[] result = new int[num + 1];
         for (int i = 0; i <= num; ++i) {
             result[i] = hammingWeight(i);
