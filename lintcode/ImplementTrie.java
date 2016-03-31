fdf07fccfb2fed78a10ac19cc6154ab2e6c6759c
mitcc <mitcc@qq.com>
2016-04-01 00:28:05 +0800

    :basketball:

--- lintcode/ImplementTrie.java
+++ lintcode/ImplementTrie.java
@@ -1,5 +1,19 @@
 /*
- *
+ * Implement a trie with insert, search, and startsWith methods.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+You may assume that all inputs are consist of lowercase letters a-z.
+
+Example
+insert("lintcode")
+search("code") // return false
+startsWith("lint") // return true
+startsWith("linterror") // return false
+insert("linterror")
+search("lintcode) // return true
+startsWith("linterror") // return true
  */
 public class ImplementTrie {
 
