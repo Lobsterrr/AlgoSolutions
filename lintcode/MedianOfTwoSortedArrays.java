e00a72d51b42b5e6f02bcdd64505b7562fb024a3
mitcc <mitcc@qq.com>
2016-03-31 23:22:29 +0800

    :basketball:

--- lintcode/MedianOfTwoSortedArrays.java
+++ lintcode/MedianOfTwoSortedArrays.java
@@ -1,7 +1,6 @@
 /*
  * There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays.
 
-Have you met this question in a real interview? Yes
 Example
 Given A=[1,2,3,4,5,6] and B=[2,3,4,5], the median is 3.5.
 
