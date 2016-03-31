bf10c77b5311b6f659da87041a835c618e2a8cd7
mitcc <mitcc@qq.com>
2016-03-31 23:36:37 +0800

    :basketball:

--- lintcode/ImplementTrie.java
+++ lintcode/ImplementTrie.java
@@ -3,6 +3,18 @@
  */
 public class ImplementTrie {
 
+    private TrieNode root;
+
+    public ImplementTrie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+
+    }
+
+
 
 }
 
