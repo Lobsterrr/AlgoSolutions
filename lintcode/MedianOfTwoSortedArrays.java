5342829dde31400b632294a68879ce53e392eda6
mitcc <mitcc@qq.com>
2016-03-31 23:21:57 +0800

    :basketball:

--- lintcode/MedianOfTwoSortedArrays.java
+++ lintcode/MedianOfTwoSortedArrays.java
@@ -3,6 +3,11 @@
  */
 public class MedianOfTwoSortedArrays{
 
+    /**
+     * @param A: An integer array.
+     * @param B: An integer array.
+     * @return: a double whose format is *.5 or *.0
+     */
     public double findMdediaSortedArrays(int[] A, int[] B) {
 
     }
