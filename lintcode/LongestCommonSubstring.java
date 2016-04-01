92e5eae404151a2504c41071d7739d935d6676fe
mitcc <mitcc@qq.com>
2016-04-01 12:49:06 +0800

    :basketball:

--- lintcode/LongestCommonSubstring.java
+++ lintcode/LongestCommonSubstring.java
@@ -3,6 +3,10 @@
  */
 public class LongestCommonSubstring {
 
+    /**
+     * @param A, B: Two string.
+     * @return: the length of the longest common substring.
+     */
     public int longestCommonSubstring(String A, String B) {
 
     }
