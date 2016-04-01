52515c4e8f11ec80336458fc02d2b46c9567458b
mitcc <mitcc@qq.com>
2016-04-01 12:49:22 +0800

    :basketball:

--- lintcode/LongestCommonSubstring.java
+++ lintcode/LongestCommonSubstring.java
@@ -1,5 +1,18 @@
 /*
- *
+ * Given two strings, find the longest common substring.
+
+Return the length of it.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+The characters in substring should occur continuously in original string. This is different with subsequence.
+
+Example
+Given A = "ABCD", B = "CBCE", return 2.
+
+Challenge
+O(n x m) time and memory.
  */
 public class LongestCommonSubstring {
 
