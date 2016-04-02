76348e6c4c438a612ec5a716297d34b9666d81b7
mitcc <mitcc@qq.com>
2016-04-02 23:03:44 +0800

    :basketball:

--- lintcode/LongestCommonSubstring.java
+++ lintcode/LongestCommonSubstring.java
@@ -24,7 +24,8 @@ public class LongestCommonSubstring {
         int result = 0;
         for (int i = 0; i < A.length(); ++i) {
             for (int j = 0; j < B.length(); ++j) {
-                for (int k = 0; k + i < A.length() && k + j < B.length() && A.charAt(k + i) == B.charAt(k + j); ++k) {
+                for (int k = 0; k + i < A.length() && k + j < B.length() 
+                        && A.charAt(k + i) == B.charAt(k + j); ++k) {
                     result = Math.max(result, k + 1);
                 }
             }
