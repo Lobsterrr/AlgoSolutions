ebac795dad3e0c788491f5a577c3806480429c44
mitcc <mitcc@qq.com>
2016-04-05 23:43:33 +0800

    :basketball:

--- lintcode/LongestPalindromicSubstring.java
+++ lintcode/LongestPalindromicSubstring.java
@@ -3,6 +3,10 @@
  */
 public class LongestPalindromicSubstring {
 
+    /**
+     * @param s input string
+     * @return the longest palindromic substring
+     */
     public String longestPalindrome(String s) {
 
     }
