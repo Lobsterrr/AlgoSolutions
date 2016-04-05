14f3d6a8ba5cdc10122984f15252fcd76d0c99e1
mitcc <mitcc@qq.com>
2016-04-05 23:58:13 +0800

    :basketball:

--- lintcode/LongestPalindromicSubstring.java
+++ lintcode/LongestPalindromicSubstring.java
@@ -1,5 +1,7 @@
 /*
- * Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ * Given a string S, find the longest palindromic substring in S. You may 
+ * assume that the maximum length of S is 1000, and there exists one unique 
+ * longest palindromic substring.
 
 Example
 Given the string = "abcdzdcab", return "cdzdc".
