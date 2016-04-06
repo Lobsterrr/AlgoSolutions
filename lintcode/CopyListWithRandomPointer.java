25b5c7d325032777ac46a3fbe9b4825ef9506781
mitcc <mitcc@qq.com>
2016-04-06 22:44:06 +0800

    :basketball:

--- lintcode/CopyListWithRandomPointer.java
+++ lintcode/CopyListWithRandomPointer.java
@@ -3,6 +3,10 @@
  */
 public class CopyListWithRandomPointer {
 
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
     public RandomListNode copyRandomList(RandomListNode head) {
 
     }
