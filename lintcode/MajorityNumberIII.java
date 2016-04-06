2da5853bf4c12684bce3f061a9a6cf4eefaf7634
mitcc <mitcc@qq.com>
2016-04-06 23:56:50 +0800

    :basketball:

--- lintcode/MajorityNumberIII.java
+++ lintcode/MajorityNumberIII.java
@@ -3,6 +3,11 @@
  */
 public class MajorityNumberIII {
 
+    /**
+     * @param nums: A list of integers
+     * @param k: As described
+     * @return: The majority number
+     */
     public int majorityNumber(ArrayList<Integer> nums, int k) {
 
     }
