2fd031627bd1289d79e86e9d8217bffa1302628b
mitcc <mitcc@qq.com>
2016-04-06 22:44:17 +0800

    :basketball:

--- lintcode/CopyListWithRandomPointer.java
+++ lintcode/CopyListWithRandomPointer.java
@@ -1,6 +1,14 @@
 /*
  *
  */
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
 public class CopyListWithRandomPointer {
 
     /**
