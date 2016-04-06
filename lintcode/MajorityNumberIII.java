a451466159f253ad0389c74f8fd2f587b9af79b7
mitcc <mitcc@qq.com>
2016-04-06 23:57:08 +0800

    :basketball:

--- lintcode/MajorityNumberIII.java
+++ lintcode/MajorityNumberIII.java
@@ -1,5 +1,18 @@
 /*
- *
+ * Given an array of integers and a number k, the majority number is the number that occurs more than 1/k of the size of the array.
+
+Find it.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+There is only one majority number in the array.
+
+Example
+Given [3,1,2,3,2,3,3,4,4,4] and k=3, return 3.
+
+Challenge
+O(n) time and O(k) extra space
  */
 public class MajorityNumberIII {
 
