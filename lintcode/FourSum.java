f200656769449f706ae1972e7332620af52c6911
mitcc <mitcc@qq.com>
2016-04-08 00:01:29 +0800

    :basketball:

--- lintcode/FourSum.java
+++ lintcode/FourSum.java
@@ -3,6 +3,12 @@
  */
 public class FourSum {
 
+    /**
+     * @param numbers : Give an array numbersbers of n integer
+     * @param target : you need to find four elements that's sum of target
+     * @return : Find all unique quadruplets in the array which gives the sum of
+     *           zero.
+     */
     public ArrayList<ArrayList<Integer>> fourSum(int[] numbers, int target) {
 
     }
