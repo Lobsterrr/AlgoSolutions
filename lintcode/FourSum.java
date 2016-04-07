6f64968e1295fa474498afb2dbf4d98cdddce335
mitcc <mitcc@qq.com>
2016-04-08 00:01:42 +0800

    :basketball:

--- lintcode/FourSum.java
+++ lintcode/FourSum.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+
+Find all unique quadruplets in the array which gives the sum of target.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ¡Ü b ¡Ü c ¡Ü d)
+The solution set must not contain duplicate quadruplets.
+
+Example
+Given array S = {1 0 -1 0 -2 2}, and target = 0. A solution set is:
+
+(-1, 0, 0, 1)
+(-2, -1, 1, 2)
+(-2, 0, 0, 2)
  */
 public class FourSum {
 
