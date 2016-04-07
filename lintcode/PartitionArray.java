8ae59310ce489419680f9916fdb3fc5c468e0ff4
mitcc <mitcc@qq.com>
2016-04-08 01:57:20 +0800

    :basketball:

--- lintcode/PartitionArray.java
+++ lintcode/PartitionArray.java
@@ -3,6 +3,11 @@
  */
 public class PartitionArray {
 
+    /** 
+     *@param nums: The integer array you should partition
+     *@param k: As description
+     *return: The index after partition
+     */
     public int partitionArray(int[] nums, int k) {
 
     }
