c477682ce35b9a262f227bbf6853d1cf1c5e8786
mitcc <mitcc@qq.com>
2016-04-08 23:37:10 +0800

    :basketball:

--- lintcode/KthLargestElement.java
+++ lintcode/KthLargestElement.java
@@ -3,6 +3,11 @@
  */
 public class KthLargestElement {
 
+    /*
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
     public int kthLargestElement(int k, int[] nums) {
 
     }
