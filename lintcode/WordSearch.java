a0c78b8b5c42ae81ef5129b6cc9eb3d02d99a787
mitcc <mitcc@qq.com>
2016-04-09 00:32:47 +0800

    :basketball:

--- lintcode/WordSearch.java
+++ lintcode/WordSearch.java
@@ -3,5 +3,12 @@
  */
 public class WordSearch {
 
+    public boolean exist(char[][] board, String word) {
+
+    }
+
+    public boolean dfs(char[][] board, String word, int i, int j) {
+
+    }
 
 }
