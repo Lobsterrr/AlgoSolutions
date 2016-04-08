71f2eb1a8d4e4fd10578ff40851aca580fb050b7
mitcc <mitcc@qq.com>
2016-04-08 23:37:25 +0800

    :basketball:

--- lintcode/KthLargestElement.java
+++ lintcode/KthLargestElement.java
@@ -1,5 +1,18 @@
 /*
- *
+ * Find K-th largest element in an array.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+You can swap elements in the array
+
+Example
+In array [9,3,2,4,8], the 3rd largest element is 4.
+
+In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4, 3rd largest element is 3 and etc.
+
+Challenge
+O(n) time, O(1) extra memory.
  */
 public class KthLargestElement {
 
