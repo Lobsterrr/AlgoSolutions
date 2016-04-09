9956d186b3a697e8d2c653c7a5d2fb1f5eeadd5a
mitcc <mitcc@qq.com>
2016-04-09 18:10:23 +0800

    :basketball:

--- lintcode/WordSearch.java
+++ lintcode/WordSearch.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Have you met this question in a real interview? Yes
+Example
+Given board =
+
+[
+  "ABCE",
+  "SFCS",
+  "ADEE"
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
  */
 public class WordSearch {
 
