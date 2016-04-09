87a94e2f65fcf585e04c710964d382e1c8957055
mitcc <mitcc@qq.com>
2016-04-09 17:46:03 +0800

    :basketball:

--- lintcode/WordSearch.java
+++ lintcode/WordSearch.java
@@ -15,16 +15,16 @@ public class WordSearch {
     }
 
     public boolean dfs(char[][] board, String word, int i, int j) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(0) || board[i][j] == '#') {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+                board[i][j] != word.charAt(0) || board[i][j] == '#') {
             return false;
         }
         if (word.length() == 1) {
             return true;
         }
-        char tmp = board[i][j];
         board[i][j] = '#';
         boolean result = dfs(board, word.substring(1), i - 1, j) || dfs(board, word.substring(1), i + 1, j) || dfs(board, word.substring(1), i, j - 1) || dfs(board, word.substring(1), i, j + 1);
-        board[i][j] = tmp;
+        board[i][j] = word.charAt(0);
         return result;
     }
 
