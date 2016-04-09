e5ad18ae2197c9c165d6c109dd00763ebbfbeae0
mitcc <mitcc@qq.com>
2016-04-09 18:10:02 +0800

    :basketball:

--- lintcode/WordSearch.java
+++ lintcode/WordSearch.java
@@ -3,6 +3,11 @@
  */
 public class WordSearch {
 
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
