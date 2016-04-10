f95293451cb3af275685472b65e30204b4701017
mitcc <mitcc@qq.com>
2016-04-10 11:56:31 +0800

    :basketball:

--- lintcode/BinaryTreeSerialization.java
+++ lintcode/BinaryTreeSerialization.java
@@ -40,6 +40,13 @@ public class BinaryTreeSerialization {
      * to serialize a binary tree which denote by a root node to a string which
      * can be easily deserialized by your own "deserialize" method later.
      */
+    public String serialize(TreeNode root) {
+
+    }
+
+    public TreeNode designed(String data) {
+
+    }
 
 /*****************************************************************************/
 
