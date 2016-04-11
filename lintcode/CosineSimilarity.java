6c54e1f00c073b364e22810294f20902b3395115
mitcc <mitcc@qq.com>
2016-04-11 23:12:48 +0800

    :basketball:

--- lintcode/CosineSimilarity.java
+++ lintcode/CosineSimilarity.java
@@ -3,6 +3,11 @@
  */
 public class CosineSimilarity {
 
+    /**
+     * @param A: An integer array.
+     * @param B: An integer array.
+     * @return: Cosine similarity.
+     */
     public double cosineSimilarity(int[] A, int[] B) {
 
     }
