c3b7a08616041e1e1923c1f0e660b3f9c529ace1
mitcc <mitcc@qq.com>
2016-04-11 23:13:05 +0800

    :basketball:

--- lintcode/CosineSimilarity.java
+++ lintcode/CosineSimilarity.java
@@ -1,5 +1,25 @@
 /*
- *
+ * Cosine similarity is a measure of similarity between two vectors of an inner product space that measures the cosine of the angle between them. The cosine of 0бу is 1, and it is less than 1 for any other angle.
+
+See wiki: Cosine Similarity
+
+Here is the formula:
+
+/media/problem/cosine-similarity.png
+
+Given two vectors A and B with the same size, calculate the cosine similarity.
+
+Return 2.0000 if cosine similarity is invalid (for example A = [0] and B = [0]).
+
+Have you met this question in a real interview? Yes
+Example
+Given A = [1, 2, 3], B = [2, 3 ,4].
+
+Return 0.9926.
+
+Given A = [0], B = [0].
+
+Return 2.0000
  */
 public class CosineSimilarity {
 
