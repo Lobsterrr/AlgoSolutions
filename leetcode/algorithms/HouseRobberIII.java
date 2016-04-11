8d212333fe19472bf194ca5088cdefcca02e96c9
mitcc <mitcc@qq.com>
2016-04-12 00:05:05 +0800

    :basketball:

--- leetcode/algorithms/HouseRobberIII.java
+++ leetcode/algorithms/HouseRobberIII.java
@@ -1,6 +1,15 @@
 /*
  *
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class HouseRobberIII {
 
     public int rob(TreeNode root) {
