2afbf27c7d6a8732c4bc869eadfea58440b902c1
mitcc <mitcc@qq.com>
2016-04-13 01:56:55 +0800

    :basketball:

--- lintcode/InterleavingString.java
+++ lintcode/InterleavingString.java
@@ -3,6 +3,13 @@
  */
 public class InterleavingString {
 
+    /**
+     * Determine whether s3 is formed by interleaving of s1 and s2.
+     * @param s1, s2, s3: As description.
+     * @return: true or false.
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
 
+    }
 
 }
