1d094cffc94afed9ef9b45487f15bcfb40ae2516
mitcc <mitcc@qq.com>
2016-04-13 01:59:42 +0800

    :basketball:

--- lintcode/InterleavingString.java
+++ lintcode/InterleavingString.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Given three strings: s1, s2, s3, determine whether s3 is formed by the interleaving of s1 and s2.
+
+Have you met this question in a real interview? Yes
+Example
+For s1 = "aabcc", s2 = "dbbca"
+
+When s3 = "aadbbcbcac", return true.
+When s3 = "aadbbbaccc", return false.
+Challenge
+O(n2) time or better
  */
 public class InterleavingString {
 
