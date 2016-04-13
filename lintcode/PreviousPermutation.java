abf10035f0f738bec7a4d83a212e7c6b39ffeca3
mitcc <mitcc@qq.com>
2016-04-13 23:26:25 +0800

    :basketball:

--- lintcode/PreviousPermutation.java
+++ lintcode/PreviousPermutation.java
@@ -3,6 +3,10 @@
  */
 public class PreviousPermutation {
 
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
     public ArrayList<Integer> previousPermutation(ArrayList<Integer> nums) {
             
     }
