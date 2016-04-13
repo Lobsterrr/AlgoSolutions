022bb01d8fea02736bd59c87b7e57ae93c787668
mitcc <mitcc@qq.com>
2016-04-13 23:26:37 +0800

    :basketball:

--- lintcode/PreviousPermutation.java
+++ lintcode/PreviousPermutation.java
@@ -1,5 +1,17 @@
 /*
- *
+ * Given a list of integers, which denote a permutation.
+
+Find the previous permutation in ascending order.
+
+Have you met this question in a real interview? Yes
+ Notice
+
+The list may contains duplicate integers.
+
+Example
+For [1,3,2,3], the previous permutation is [1,2,3,3]
+
+For [1,2,3,4], the previous permutation is [4,3,2,1]
  */
 public class PreviousPermutation {
 
