10520b9331abbccb012c759b7680d5bca13624e6
mitcc <mitcc@qq.com>
2016-04-15 02:07:33 +0800

    :basketball:

--- lintcode/InterleavingString.java
+++ lintcode/InterleavingString.java
@@ -23,25 +23,6 @@ public class InterleavingString {
         }
         boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
         dp[0][0] = true;
-        for (int i = 0; i <= s1.length(); ++i) {
-            for (int j = 0; j <= s2.length(); ++j) {
-                if (i > 0) {
-                    dp[i][j] = dp[i][j] || s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j];
-                }
-                if (j > 0) {
-                    dp[i][j] = dp[i][j] || s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1];
-                }
-            }
-        }
-        return dp[s1.length()][s2.length()];
-    }
-
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        dp[0][0] = true;
         for (int i = 0; i < s1.length(); ++i) {
             dp[i + 1][0] = s1.charAt(i) == s3.charAt(i) && dp[i][0];
         }
