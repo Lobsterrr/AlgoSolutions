37087b324121b47249b04e7fc3f0cfe110bea0d9
mitcc <mitcc@qq.com>
2016-04-14 23:55:58 +0800

    :basketball:

--- lintcode/PreviousPermutation.java
+++ lintcode/PreviousPermutation.java
@@ -45,35 +45,4 @@ public class PreviousPermutation {
         }
     }
 
-
-    public ArrayList<Integer> previousPermutation(ArrayList<Integer> nums) {
-        int i = nums.size() - 2;
-        while (i >= 0 && nums.get(i) <= nums.get(i + 1)) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = i + 1;
-            for (int k = i + 2; k < nums.size(); ++k) {
-                if (nums.get(k) < nums.get(i) && nums.get(k) > nums.get(j)) {
-                    j = k;
-                }
-            }
-            swap(nums, i, j);
-        }
-        for (int m = i + 1; m < nums.size(); ++m) {
-            for (int n = nums.size() - 2; n >= m; --n) {
-                if (nums.get(n) < nums.get(n + 1)) {
-                    swap(nums, n, n + 1);
-                }
-            }
-        }
-        return nums;
-    }
-    
-    public void swap(ArrayList<Integer> nums, int i, int j) {
-        int tmp = nums.get(i);
-        nums.set(i, nums.get(j));
-        nums.set(j, tmp);
-    }
-
 }
