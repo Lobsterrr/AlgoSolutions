cdfc3b6652616dc7b5c7f10b977bb00f89cfbe5a
mitcc <mitcc@qq.com>
2016-04-15 02:19:47 +0800

    :basketball:

--- lintcode/FirstMissingPosition.java
+++ lintcode/FirstMissingPosition.java
@@ -3,6 +3,10 @@
  */
 public class FirstMissingPositive {
 
+    /**    
+     * @param A: an array of integers
+     * @return: an integer
+     */
     public int firstMissingPositive(int[] A) {
 
     }
