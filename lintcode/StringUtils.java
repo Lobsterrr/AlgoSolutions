956b56c4be976c2e47490601e62db39009824c5a
mitcc <mitcc@qq.com>
2016-04-16 14:51:40 +0800

    :basketball:

--- lintcode/StringUtils.java
+++ lintcode/StringUtils.java
@@ -3,6 +3,12 @@
  */
 public class StringUtils {
 
+    static public String leftPad(String originalStr, int size) {
 
+    }
+
+    static public String leftPad(String originalStr, int size, char padChar) {
+
+    }
 
 }
