789aba4e1da237fa427372fed4ceeac618c5a345
mitcc <mitcc@qq.com>
2016-04-16 14:52:28 +0800

    :basketball:

--- lintcode/StringUtils.java
+++ lintcode/StringUtils.java
@@ -1,5 +1,22 @@
 /*
- *
+ * You know what, left pad is javascript package and referenced by React: 
+Github link
+
+One day his author unpublished it, then a lot of javascript projects in the world broken.
+
+You can see from github it's only 11 lines.
+
+You job is to implement the left pad function. If you do not know what left pad does, see examples below and guess.
+
+Example
+leftpad("foo", 5)
+>> "  foo"
+
+leftpad("foobar", 6)
+>> "foobar"
+
+leftpad("1", 2, "0")
+>> "01"
  */
 public class StringUtils {
 
