5be630af55a068de5c5ae13a8ac3da6fd8201cda
mitcc <mitcc@qq.com>
2016-04-18 01:03:41 +0800

    :basketball:

--- lintcode/ToyFactory.java
+++ lintcode/ToyFactory.java
@@ -1,5 +1,16 @@
 /*
- *
+ * Factory is a design pattern in common usage. Please implement a 
+ * ToyFactory which can generate proper toy based on the given type.
+
+Example
+ToyFactory tf = ToyFactory();
+Toy toy = tf.getToy('Dog');
+toy.talk(); 
+>> Wow
+
+toy = tf.getToy('Cat');
+toy.talk();
+>> Meow
  */
 /**
  * Your object will be instantiated and called as such:
