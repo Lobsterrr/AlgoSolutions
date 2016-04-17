0c65414ef45ed5e66e84cc548bdaddce8bfcd028
mitcc <mitcc@qq.com>
2016-04-18 01:02:28 +0800

    :basketball:

---
+++ lintcode/ToyFactory.java
@@ -0,0 +1,24 @@
+/*
+ *
+ */
+/**
+ * Your object will be instantiated and called as such:
+ * ToyFactory tf = new ToyFactory();
+ * Toy toy = tf.getToy(type);
+ * toy.talk();
+ */
+interface Toy {
+    void talk();
+}
+
+class Dog implements Toy {
+    // Write your code here
+}
+
+class Cat implements Toy {
+    // Write your code here
+}
+public class ToyFactory {
+
+
+}
