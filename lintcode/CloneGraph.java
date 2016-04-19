fe11eca8ad49d6689363299f861ff4fe0b9604b1
mitcc <mitcc@qq.com>
2016-04-20 00:01:31 +0800

    :basketball:

--- lintcode/CloneGraph.java
+++ lintcode/CloneGraph.java
@@ -1,18 +1,22 @@
 /*
- * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+ * Clone an undirected graph. Each node in the graph contains a label and a 
+ * list of its neighbors.
 
 How we serialize an undirected graph:
 
 Nodes are labeled uniquely.
 
-We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+We use # as a separator for each node, and , as a separator for node label and 
+each neighbor of the node.
 As an example, consider the serialized graph {0,1,2#1,2#2,2}.
 
-The graph has a total of three nodes, and therefore contains three parts as separated by #.
+The graph has a total of three nodes, and therefore contains three parts as 
+separated by #.
 
 First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
 Second node is labeled as 1. Connect node 1 to node 2.
-Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a 
+self-cycle.
 Visually, the graph looks like the following:
 
    1
@@ -29,7 +33,8 @@ return a deep copied graph.
  * class UndirectedGraphNode {
  *     int label;
  *     ArrayList<UndirectedGraphNode> neighbors;
- *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ *     UndirectedGraphNode(int x) { label = x; 
+ *     neighbors = new ArrayList<UndirectedGraphNode>(); }
  * };
  */
 public class CloneGraph {
@@ -45,14 +50,16 @@ public class CloneGraph {
         UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
         Stack<UndirectedGraphNode> stack = new Stack<UndirectedGraphNode>();
         stack.push(node);
-        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = 
+            new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
         map.put(node, copy);
         while (!stack.isEmpty()) {
             UndirectedGraphNode cur = stack.pop();
             for (UndirectedGraphNode neighbor : cur.neighbors) {
                 if (!map.containsKey(neighbor)) {
                     stack.push(neighbor);
-                    UndirectedGraphNode copyNode = new UndirectedGraphNode(neighbor.label);
+                    UndirectedGraphNode copyNode = 
+                        new UndirectedGraphNode(neighbor.label);
                     map.put(neighbor, copyNode);
                 }
                 map.get(cur).neighbors.add(map.get(neighbor));
