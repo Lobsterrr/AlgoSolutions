bb54b8dd94a53b578fc4bd99fe7d6490b13dd871
mitcc <mitcc@qq.com>
2016-04-20 01:45:25 +0800

    :basketball:

--- lintcode/SlidingWindowMaximum.java
+++ lintcode/SlidingWindowMaximum.java
@@ -1,5 +1,8 @@
 /*
- * Given an array of n integer with duplicate number, and a moving window(size k), move the window at each iteration from the start of the array, find the maximum number inside the window at each moving.
+ * Given an array of n integer with duplicate number, and a moving 
+ * window(size k), move the window at each iteration from the start 
+ * of the array, find the maximum number inside the window at each 
+ * moving.
 
 Example
 For array [1, 2, 7, 7, 8], moving window size k = 3. return [7, 7, 8]
