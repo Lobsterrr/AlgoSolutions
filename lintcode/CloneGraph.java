78622039e21e209d2fbbb3f4ded373fb8f3ff25d
mitcc <mitcc@qq.com>
2016-04-19 23:22:27 +0800

    :basketball:

---
+++ lintcode/CloneGraph.java
@@ -0,0 +1,22 @@
+/*
+ *
+ */
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     ArrayList<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class CloneGraph {
+
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+
+    }
+
+}
