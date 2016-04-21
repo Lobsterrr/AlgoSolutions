8d72911559c1eec71a7a2f745831e2da5c437aa6
mitcc <mitcc@qq.com>
2016-04-21 23:50:17 +0800

    :basketball:

--- lintcode/PermutationIndexII.java
+++ lintcode/PermutationIndexII.java
@@ -1,5 +1,9 @@
 /*
- *
+ * Given a permutation which may contain repeated numbers, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
+
+Have you met this question in a real interview? Yes
+Example
+Given the permutation [1, 4, 2, 2], return 3.
  */
 public class PermutationIndexII {
 
