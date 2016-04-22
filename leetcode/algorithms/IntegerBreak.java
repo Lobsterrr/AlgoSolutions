8e5d46f768311ee2009edded6ef2a9610f8e5612
mitcc <mitcc@qq.com>
2016-04-23 00:49:47 +0800

    :basketball:

--- leetcode/algorithms/IntegerBreak.java
+++ leetcode/algorithms/IntegerBreak.java
@@ -32,15 +32,12 @@ public class IntegerBreak {
 
     public int integerBreak(int n) {
         if (n <= 3) {
-            return n / 2 * (n - n / 2);
+            return n - 1;
         }
-        int result = 1;
-        if (n % 3 == 0) {
-            result = (int) Math.pow(3, n / 3);
-        } else if (n % 3 == 1) {
-            result = (int) Math.pow(3, n / 3 - 1) * 4;
-        } else {
-            result = (int) Math.pow(3, n / 3);
+        int result = (int) Math.pow(3, n / 3);
+        if (n % 3 == 1) {
+            result = result / 3 * 4;
+        } else if (n % 3 == 2) {
             result *= 2;
         }
         return result;
