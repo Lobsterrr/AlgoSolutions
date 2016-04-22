def7eb55bca5c5a2da8bbb51829d645f7eb45136
mitcc <mitcc@qq.com>
2016-04-23 01:34:39 +0800

    :basketball:

--- leetcode/algorithms/ReverseString.java
+++ leetcode/algorithms/ReverseString.java
@@ -1,7 +1,21 @@
 /*
- *
+ * Write a function that takes a string as 
+ * input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
  */
 public class ReverseString {
+
+    public String reverseString(String s) {
+        char[] array = s.toCharArray();
+        for (int i = 0; i < array.length / 2; ++i) {
+            char tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+        return new String(array);
+    }
 
     public String reverseString(String s) {
         return new StringBuilder(s).reverse().toString();
