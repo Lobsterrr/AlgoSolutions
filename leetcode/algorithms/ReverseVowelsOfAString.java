6b62b09cfd54ef3dea352a939348ba917b8044fd
mitcc <mitcc@qq.com>
2016-04-24 16:38:40 +0800

    :basketball:

---
+++ leetcode/algorithms/ReverseVowelsOfAString.java
@@ -0,0 +1,31 @@
+/*
+ *
+ */
+public class ReverseVowelsOfAString {
+
+    public String reverseVowels(String s) {
+        char[] array = s.toCharArray();
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j) {
+            if (!isVowelsChar(array[i])) {
+                i++;
+            } else if (!isVowelsChar(array[j])) {
+                j--;
+            } else {
+
+            }
+        }
+    }
+
+    public boolean isVowelsChar(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+}
