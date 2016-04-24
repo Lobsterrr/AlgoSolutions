0d2b72ea40c85112bbb3b8f6321b998aff03f8b0
mitcc <mitcc@qq.com>
2016-04-24 22:39:07 +0800

    :basketball:

--- lintcode/PermutationIndexII.java
+++ lintcode/PermutationIndexII.java
@@ -12,26 +12,6 @@ public class PermutationIndexII {
      */
     public long permutationIndexII(int[] A) {
         long result = 1;
-        long factorial = 1;
-        for (int i = A.length - 2; i >= 0; i--) {
-            factorial *= A.length - 1 - i;
-            int reverseCount = 0;
-            int dupilcateCount = 1;
-            for (int j = i + 1; j < A.length; ++j) {
-                if (A[i] >= A[j]) {
-                    reverseCount++;
-                }
-                if (A[i] == A[j]) {
-                    dupilcateCount++;
-                }
-            }
-            result += reverseCount * factorial / dupilcateCount;
-        }
-        return result;
-    }
-
-    public long permutationIndexII(int[] A) {
-        long result = 1;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = A.length - 2; i >= 0; --i) {
             map.put(A[i + 1], 1 + (map.containsKey(A[i + 1]) ? map.get(A[i + 1]) : 0));
