1fde9392a14acc5fa66b5d5bfd9e375e1ff77a77
mitcc <mitcc@qq.com>
2016-04-26 01:51:05 +0800

    :basketball:

--- lintcode/UpdateBits.java
+++ lintcode/UpdateBits.java
@@ -3,6 +3,11 @@
  */
 public class UpdateBits {
 
+    /**
+     *@param n, m: Two integer
+     *@param i, j: Two bit positions
+     *return: An integer
+     */
     public int updateBits(int n, int m, int i, int j) {
 
     }
