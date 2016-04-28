6c533072fb489023145f85a1a778e896a7ffc968
mitcc <mitcc@qq.com>
2016-04-29 00:34:35 +0800

    :basketball:

--- lintcode/HouseRobberIII.java
+++ lintcode/HouseRobberIII.java
@@ -1,7 +1,22 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
 public class HouseRobberIII {
 
+    /**
+     * @param root: The root of binary tree.
+     * @return: The maximum amount of money you can rob tonight
+     */
+    public int houseRobber3(TreeNode root) {
+
+    }
 
 }
