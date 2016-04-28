c51f0417667f877775d7c68125ce7074e08f9910
mitcc <mitcc@qq.com>
2016-04-28 23:55:59 +0800

    :basketball:

--- lintcode/HouseRobberII.java
+++ lintcode/HouseRobberII.java
@@ -3,6 +3,12 @@
  */
 public class HouseRobberII {
 
+    /**
+     * @param nums: An array of non-negative integers.
+     * return: The maximum amount of money you can rob tonight
+     */
+    public int houseRobber2(int[] nums) {
 
+    }
 
 }
