1c2a30307e1b67083119e0e6d4df2817ce3a7957
mitcc <mitcc@qq.com>
2016-05-01 15:18:42 +0800

    :basketball:

--- leetcode/algorithms/FindTheDuplicateNumber.java
+++ leetcode/algorithms/FindTheDuplicateNumber.java
@@ -18,7 +18,7 @@ public class FindTheDuplicateNumber {
             fast = nums[nums[fast]];
             slow = nums[slow];
         }
-        fast = 0;
+        slow = 0;
         while (fast != slow) {
             fast = nums[fast];
             slow = nums[slow];
