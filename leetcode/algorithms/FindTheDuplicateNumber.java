f586270456be384c11595da2fb1ef10cf9fdab2a
mitcc <mitcc@qq.com>
2016-05-01 15:27:15 +0800

    :basketball:

--- leetcode/algorithms/FindTheDuplicateNumber.java
+++ leetcode/algorithms/FindTheDuplicateNumber.java
@@ -27,6 +27,8 @@ public class FindTheDuplicateNumber {
         return fast;
     }
 
+/*******************************************************************/
+
     // O(n) time, O(1) spacew with array modified.
     public int findDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; ++i) {
