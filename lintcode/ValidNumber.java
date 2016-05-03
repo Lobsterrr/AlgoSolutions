6471e6e12cdfcd2f9ab77d4d953ed46638798a3f
mitcc <mitcc@qq.com>
2016-05-03 23:43:20 +0800

    :basketball:

---
+++ lintcode/ValidNumber.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class ValidNumber {
+
+    /**
+     * @param s the string that represents a number
+     * @return whether the string is a valid number
+     */
+    public boolean isNumber(String s) {
+
+    }
+
+}
