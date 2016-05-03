62060bad512722749d985e7100a31ed74f4ce29f
mitcc <mitcc@qq.com>
2016-05-03 23:53:10 +0800

    :basketball:

--- lintcode/ValidNumber.java
+++ lintcode/ValidNumber.java
@@ -22,11 +22,18 @@ public class ValidNumber {
         if (s == null || s.length() == 0) {
             return false;
         }
+        int letterCount = 0;
         for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
-            if (Character.isLetter(c) && c != 'e' && c != 'E') {
-                return false;
+            if (Character.isLetter(c)) {
+                letterCount++;
+                if (c != 'e' && c != 'E') {
+                    return false;
+                }
             }
+        }
+        if (letterCount > 1) {
+            return false;
         }
     }
 
