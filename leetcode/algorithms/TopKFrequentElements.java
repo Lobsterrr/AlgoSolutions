d691c82b001901b4467831e4d8cf14f67f315d41
mitcc <mitcc@qq.com>
2016-05-04 22:27:46 +0800

    :basketball:

--- leetcode/algorithms/TopKFrequentElements.java
+++ leetcode/algorithms/TopKFrequentElements.java
@@ -5,8 +5,9 @@
  * Given [1,1,1,2,2,3] and k = 2, return [1,2].
 
  * Note: 
- * You may assume k is always valid, 1 ¡Ü k ¡Ü number of unique elements.
- * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * You may assume k is always valid, 1 <= k <= number of unique elements.
+ * Your algorithm's time complexity must be better than O(n log n), where n is 
+ * the array's size.
  */
 public class TopKFrequentElements {
 
