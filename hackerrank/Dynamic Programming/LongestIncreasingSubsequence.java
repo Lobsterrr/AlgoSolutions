58d5b3301bc882179b9717902708ddb04255bb4e
mitcc <mitcc@qq.com>
2016-05-06 01:22:42 +0800

    :basketball:

--- hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
+++ hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
@@ -43,8 +43,6 @@ import java.util.Scanner;
 
 public class LongestIncreasingSubsequence {
     
-    //TODO  TLE
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -82,26 +80,6 @@ public class LongestIncreasingSubsequence {
             }
         }
         return low;
-    }
-
-    public static int lengthOfLIS(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-        }
-        int result = 0;
-        for (int value : dp) {
-            result = Math.max(result, value);
-        }
-        return result;
     }
 
 }
