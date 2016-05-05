5b58446a48574c4d28376e2293f325fdd8e95c7e
mitcc <mitcc@qq.com>
2016-05-06 00:52:27 +0800

    :basketball:

--- hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
+++ hackerrank/Dynamic Programming/LongestIncreasingSubsequence.java
@@ -56,6 +56,35 @@ public class LongestIncreasingSubsequence {
     }
 
     public static int lengthOfLIS(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : nums) {
+            if (list.size() == 0 || list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                int index = findPosition(list, num);
+                list.set(index, num);
+            }
+        }
+        return list.size();
+    }
+
+    public static int findPosition(List<Integer> list, int target) {
+        int low = 0;
+        int high = list.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == list.get(mid)) {
+                return mid;
+            } else if (target < list.get(mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static int lengthOfLIS(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
