c2f30d9a14bb0b203472801ca3ec5f4d86c55230
mitcc <mitcc@qq.com>
2016-05-07 00:36:24 +0800

    :basketball:

--- lintcode/ValidSudoku.java
+++ lintcode/ValidSudoku.java
@@ -1,10 +1,45 @@
 /*
- *
+ * Determine whether a Sudoku is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character ..
+
+ Notice
+
+A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+
+Clarification
+What is Sudoku?
+
+http://sudoku.com.au/TheRules.aspx
+https://zh.wikipedia.org/wiki/%E6%95%B8%E7%8D%A8
+https://en.wikipedia.org/wiki/Sudoku
+http://baike.baidu.com/subview/961/10842669.htm
  */
 public class ValidSudoku {
 
+    /**
+      * @param board: the board
+        @return: wether the Sudoku is valid
+      */
     public boolean isValidSudoku(char[][] board) {
-
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char c = board[i][j];
+                if (!Character.isDigit(c)) {
+                    continue;
+                }
+                if (row[i][c] || col[c][j] || block[i / 3 * 3 + j / 3][c]) {
+                    return false;
+                }
+                row[i][c] = true;
+                col[c][j] = true;
+                block[i / 3 * 3 + j / 3][c] = true;
+            }
+        }
+        return true;
     }
 
 }
