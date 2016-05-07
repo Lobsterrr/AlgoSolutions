ee1814e2123175f45854776bd547ede5fa96d484
mitcc <mitcc@qq.com>
2016-05-07 20:26:16 +0800

    :basketball:

---
+++ lintcode/SubarraySumClosest.java
@@ -0,0 +1,21 @@
+/*
+ * Given an integer array, find a subarray with sum closest to zero. Return the indexes of the first number and last number.
+
+Example
+Given [-3, 1, 1, -3, 5], return [0, 2], [1, 3], [1, 1], [2, 2] or [0, 4].
+
+Challenge 
+O(nlogn) time
+ */
+public class SubarraySumClosest {
+
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
+    public int[] subarraySumClosest(int[] nums) {
+
+    }
+
+}
