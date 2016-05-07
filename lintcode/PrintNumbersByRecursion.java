21e01a92a621d0a61782334a4d7d0457701f7a62
mitcc <mitcc@qq.com>
2016-05-07 10:14:27 +0800

    :basketball:

---
+++ lintcode/PrintNumbersByRecursion.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class PrintNumbersByRecursion {
+
+    /**
+     * @param n: An integer.
+     * return : An array storing 1 to the largest number with n digits.
+     */
+    public List<Integer> numbersByRecursion(int n) {
+
+    }
+
+}
