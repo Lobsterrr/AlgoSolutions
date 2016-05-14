4cafcd0bcaf1cbfd680ea1e53d839e8c6c72398f
mitcc <mitcc@qq.com>
2016-05-14 13:26:02 +0800

    :basketball:

--- lintcode/WildcardMatching.java
+++ lintcode/WildcardMatching.java
@@ -3,6 +3,11 @@
  */
 public class WildcardMatching {
 
+    /**
+     * @param s: A string 
+     * @param p: A string includes "?" and "*"
+     * @return: A boolean
+     */
     public boolean isMatch(String, String p) {
 
     }
