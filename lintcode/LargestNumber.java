c30b713e5ac97b6f991e37143d3673d464d662c1
mitcc <mitcc@qq.com>
2016-05-16 01:45:24 +0800

    :basketball:

--- lintcode/LargestNumber.java
+++ lintcode/LargestNumber.java
@@ -55,6 +55,15 @@ public class LargestNumber {
 
     public boolean compare(int x, int y) {
         return Long.parseLong("" + x + y) < Long.parseLong("" + y + x);
+
+        String s1 = "" + x;
+        String s2 = "" + y;
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); ++i) {
+            if (s1.charAt(i) > s2.charAt(k)) {
+                return false;
+            }
+        }
+
     }
 
 }
