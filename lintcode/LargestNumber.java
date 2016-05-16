3af3ea567409bfa310c075480a41d0fd247d955c
mitcc <mitcc@qq.com>
2016-05-16 12:33:20 +0800

    :basketball:

--- lintcode/LargestNumber.java
+++ lintcode/LargestNumber.java
@@ -64,16 +64,16 @@ public class LargestNumber {
         }
         if (i < s1.length()) {
             if (s1.charAt(i) > s1.charAt(0)) {
-                return true;
-            } else {
                 return false;
+            } else {
+                return true;
             }
         }
         if (i < s2.length()) {
             if (s2.charAt(i) > s2.charAt(0)) {
-                return false;
-            } else {
                 return true;
+            } else {
+                return false;
             }
         }
         return true;
