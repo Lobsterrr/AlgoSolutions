9934175a150db27d3faebab8dc9d40645f038049
mitcc <mitcc@qq.com>
2016-05-17 23:19:43 +0800

    :basketball:

--- lintcode/MinimumWindowSubstring.java
+++ lintcode/MinimumWindowSubstring.java
@@ -1,5 +1,20 @@
 /*
- *
+ * Given a string source and a string target, find the minimum window in source which will contain all the characters in target.
+
+ Notice
+
+If there is no such window in source that covers all characters in target, return the emtpy string "".
+
+If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in source.
+
+Clarification
+Should the characters in minimum window has the same order in target?
+
+Not necessary.
+Example
+For source = "ADOBECODEBANC", target = "ABC", the minimum window is "BANC"
+
+Challenge 
  */
 public class MinimumWindowSubstring {
 
