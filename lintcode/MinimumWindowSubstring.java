8f1f71c210045bc4ea35ba01943e095527386f9e
mitcc <mitcc@qq.com>
2016-05-17 23:19:02 +0800

    :basketball:

--- lintcode/MinimumWindowSubstring.java
+++ lintcode/MinimumWindowSubstring.java
@@ -3,6 +3,12 @@
  */
 public class MinimumWindowSubstring {
 
+    /**
+     * @param source: A string
+     * @param target: A string
+     * @return: A string denote the minimum window
+     *          Return "" if there is no such a string
+     */
     public String minWindow(String source, String target) {
 
     }
