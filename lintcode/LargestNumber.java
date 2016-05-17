f49a961e9f2144747fdc5bd30fb52936d8e8090f
mitcc <mitcc@qq.com>
2016-05-17 09:54:59 +0800

    :basketball:

--- lintcode/LargestNumber.java
+++ lintcode/LargestNumber.java
@@ -1,12 +1,15 @@
 /*
- * Given a list of non negative integers, arrange them such that they form the largest number.
+ * Given a list of non negative integers, arrange them such 
+ * that they form the largest number.
 
  Notice
 
-The result may be very large, so you need to return a string instead of an integer.
+The result may be very large, so you need to return a 
+string instead of an integer.
 
 Example
-Given [1, 20, 23, 4, 8], the largest formed number is 8423201.
+Given [1, 20, 23, 4, 8], the largest formed number is 
+8423201.
 
 Challenge 
 Do it in O(nlogn) time complexity.
