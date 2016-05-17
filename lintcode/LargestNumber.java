c9ee2b06b545c6f0c5263f0e330ce153f5a303f7
mitcc <mitcc@qq.com>
2016-05-17 09:52:10 +0800

    :basketball:

--- lintcode/LargestNumber.java
+++ lintcode/LargestNumber.java
@@ -25,7 +25,7 @@ public class LargestNumber {
         Arrays.sort(str, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                return s2.compareTo(s1);
+                return (s2 + s1).compareTo(s1 + s2);
             }
         });
         StringBuilder sb = new StringBuilder();
