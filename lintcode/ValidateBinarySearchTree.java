b0cc776d48f625c4ea4462fac3d294d7764c7760
mitcc <mitcc@qq.com>
2016-05-18 22:58:05 +0800

    :basketball:

--- lintcode/ValidateBinarySearchTree.java
+++ lintcode/ValidateBinarySearchTree.java
@@ -1,8 +1,23 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class ValidateBinarySearchTree {
 
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
     public boolean isValidBST(TreeNode root) {
         
     }
