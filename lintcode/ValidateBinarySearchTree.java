9e46b13805f8e1028404e2e26f7f75c98f4d1b04
mitcc <mitcc@qq.com>
2016-05-20 23:49:53 +0800

    :basketball:

--- lintcode/ValidateBinarySearchTree.java
+++ lintcode/ValidateBinarySearchTree.java
@@ -58,27 +58,6 @@ public class ValidateBinarySearchTree {
         return true;
     }
 
-    public boolean isValidBST(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode prev = null;
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            if (stack.isEmpty()) {
-                return false;
-            }
-            root = stack.pop();
-            if (prev != null && root.val <= prev.val) {
-                return false;
-            }
-            prev = root;
-            root = root.right;
-        }
-        return true;
-    }
-
 /*****************************************************************************/
 
     public boolean isValidBST(TreeNode root) {
