73a70ea1577e4ece2cd63ef224b3e62325530bc7
mitcc <mitcc@qq.com>
2016-05-21 00:46:54 +0800

    :basketball:

--- lintcode/KSum.java
+++ lintcode/KSum.java
@@ -3,6 +3,12 @@
  */
 public class KSum {
 
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return an integer
+     */
     public int kSum(int A[], int k, int target) {
 
     }
