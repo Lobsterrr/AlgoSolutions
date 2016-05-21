a708568b836a9e402f27bf6a131ebdfbe29f7472
mitcc <mitcc@qq.com>
2016-05-22 01:41:16 +0800

    :basketball:

--- lintcode/KSum.java
+++ lintcode/KSum.java
@@ -20,9 +20,10 @@ public class KSum {
      */
     public int kSum(int A[], int k, int target) {
         int[][][] dp = new int[A.length + 1][k + 1][target + 1];
-        for (int i = 0; i < A.length; ++i) {
-            for (int j = 0; j < k; ++j) {
-                for (int t = 0; t < target; ++t) {
+        for (int i = 0; i <= A.length; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                for (int t = 0; t <= target; ++t) {
+
                     dp[i + 1][j + 1][t + 1] += dp[i][j + 1][t];
                     if (t - A[i] > 0) {
                         dp[i + 1][j + 1][t + 1] += dp[i][j][t - A[i]];
