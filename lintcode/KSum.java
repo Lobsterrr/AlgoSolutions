d0993884ce2c162310ac8ae03a1945814dce8725
mitcc <mitcc@qq.com>
2016-05-21 22:13:34 +0800

    :basketball:

--- lintcode/KSum.java
+++ lintcode/KSum.java
@@ -20,6 +20,26 @@ public class KSum {
      */
     public int kSum(int A[], int k, int target) {
         Arrays.sort(A);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        dfs(A, k, target, 0, 
+        return 
+    }
+
+    public void dfs(int[] A, int k, int target, int sum, List<Integer> list, List<List<Integer>> result, int position) {
+        if (sum == target) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        if (list.size() > k) {
+            return;
+        }
+        list.add(A[position]);
+        sum += A[position];
+        for (int i = position + 1; i < A.length; ++i) {
+            dfs(A, k, target, sum, list, result, position + 1);
+        }
+        sum -= A[position];
+        list.remove(list.size() - 1);
     }
 
 }
