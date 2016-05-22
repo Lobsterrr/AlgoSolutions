ac4df031ad9ffac728e12b914ecf97c0dfe22b15
mitcc <mitcc@qq.com>
2016-05-22 23:52:28 +0800

    :basketball:

--- leetcode/algorithms/CoinChange.java
+++ leetcode/algorithms/CoinChange.java
@@ -16,7 +16,6 @@ public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        dp[0] = 1;
         for (int i = 1; i <= amount; ++i) {
             dp[i] = Integer.MAX_VALUE;
             for (int j = 0; j < coins.length; ++j) {
