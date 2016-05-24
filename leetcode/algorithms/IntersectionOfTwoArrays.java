25e9239dad37c3c2e67b479ce4072f77fae6ae79
mitcc <mitcc@qq.com>
2016-05-24 23:35:55 +0800

    :basketball:

--- leetcode/algorithms/IntersectionOfTwoArrays.java
+++ leetcode/algorithms/IntersectionOfTwoArrays.java
@@ -14,7 +14,7 @@ public class IntersectionOfTwoArrays {
         List<Integer> list = new ArrayList<Integer>();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums1) {
-            map.put(num, 1 + map.containsKey(num) ? map.get(num) : 0);
+            map.put(num, 1 + (map.containsKey(num) ? map.get(num) : 0));
         }
         for (int num : nums2) {
             if (map.cointainsKey(num) && map.get(num) > 0) {
