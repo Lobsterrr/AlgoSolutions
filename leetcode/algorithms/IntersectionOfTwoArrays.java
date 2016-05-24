7d2852c0bae7224aad11e3f92eb0df98764a8e83
mitcc <mitcc@qq.com>
2016-05-24 23:15:11 +0800

    :basketball:

--- leetcode/algorithms/IntersectionOfTwoArrays.java
+++ leetcode/algorithms/IntersectionOfTwoArrays.java
@@ -1,13 +1,18 @@
 /*
- *
+ * Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
  */
 public class IntersectionOfTwoArrays {
 
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<Integer>();
-        for (int num : nums1) {
-            
-        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 
     }
 
 }
