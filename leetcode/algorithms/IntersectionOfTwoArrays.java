39da424f6ec56d610826a63a5930424fe113a842
mitcc <mitcc@qq.com>
2016-05-24 23:45:55 +0800

    :basketball:

--- leetcode/algorithms/IntersectionOfTwoArrays.java
+++ leetcode/algorithms/IntersectionOfTwoArrays.java
@@ -11,6 +11,26 @@ The result can be in any order.
 public class IntersectionOfTwoArrays {
 
     public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums1) {
+            set.add(num);
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                list.add(num);
+                set.remove(num);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; ++i) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+
+    public int[] intersection(int[] nums1, int[] nums2) {
         List<Integer> list = new ArrayList<Integer>();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums1) {
