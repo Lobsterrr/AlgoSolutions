7c80051b8c459fed004620ed3ef17a52a79dd07c
mitcc <mitcc@qq.com>
2016-05-26 02:04:51 +0800

    :basketball:

--- lintcode/DeleteDigits.java
+++ lintcode/DeleteDigits.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Given string A representative a positive integer which has N digits, remove any k digits of the number, the remaining digits are arranged according to the original order to become a new positive integer.
+
+Find the smallest integer after remove k digits.
+
+N <= 240 and k <= N,
+
+Example
+Given an integer A = "178542", k = 4
+
+return a string "12"
  */
 public class DeleteDigits {
 
