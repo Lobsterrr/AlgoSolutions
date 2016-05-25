ea63520afb1a6a0346a866357a62dbc0135fe5ba
mitcc <mitcc@qq.com>
2016-05-26 02:04:34 +0800

    :basketball:

--- lintcode/DeleteDigits.java
+++ lintcode/DeleteDigits.java
@@ -3,6 +3,11 @@
  */
 public class DeleteDigits {
 
+    /**
+     *@param A: A positive integer which has N digits, A is a string.
+     *@param k: Remove k digits.
+     *@return: A string
+     */
     public String DeleteDigits(String A, int k) {
 
     }
