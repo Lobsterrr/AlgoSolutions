16eb897ba89b6c21239831096208f57c6f544beb
mitcc <mitcc@qq.com>
2016-05-27 01:07:13 +0800

    :basketball:

---
+++ lintcode/InterleavingPositiveAndNegativeNumbers.java
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+public class InterleavingPositiveAndNegativeNumbers {
+
+    public void rerange(int[] A) {
+        
+    }
+
+    public void swap(int[] array, int x, int y) {
+        int tmp = array[x];
+        array[x] = array[y];
+        array[y] = tmp;
+    }
+
+}
