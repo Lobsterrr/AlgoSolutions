4d6ef20d614c1dd1675ab5461a4e66a74ca90928
mitcc <mitcc@qq.com>
2016-05-30 17:46:03 +0800

    :basketball:

--- lintcode/ReversePairs.java
+++ lintcode/ReversePairs.java
@@ -1,5 +1,10 @@
 /*
- *
+ * For an array A, if i < j, and A [i] > A [j], called (A [i], A [j]) is a reverse pair.
+return total of reverse pairs in A.
+
+Have you met this question in a real interview? Yes
+Example
+Given A = [2, 4, 1, 3, 5] , (2, 1), (4, 1), (4, 3) are reverse pairs. return 3
  */
 public class ReversePairs {
 
