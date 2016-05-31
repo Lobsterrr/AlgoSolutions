8d2e7dfbc639b6746de43d62a22bd461483ce872
mitcc <mitcc@qq.com>
2016-06-01 00:49:32 +0800

    :basketball:

--- lintcode/Rehashing.java
+++ lintcode/Rehashing.java
@@ -3,6 +3,10 @@
  */
 public class Rehashing {
 
+    /**
+     * @param hashTable: A list of The first node of linked list
+     * @return: A list of The first node of linked list which have twice size
+     */  
     public ListNode[] rehashing(ListNode[] hashTable) {
 
     }
