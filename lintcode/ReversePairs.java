3354b9ffddcd362424dc5efed5e66998336486db
mitcc <mitcc@qq.com>
2016-05-31 23:53:10 +0800

    :basketball:

--- lintcode/ReversePairs.java
+++ lintcode/ReversePairs.java
@@ -14,6 +14,7 @@ public class ReversePairs {
      * @param A an array
      * @return total of reverse pairs
      */
+    // O(nlg(n) time, O(n) space.
     public long reversePairs(int[] A) {
         long result = 0;
         List<Integer> list = new LinkedList<Integer>();
