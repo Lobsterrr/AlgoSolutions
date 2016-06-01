b448b8bae9de33df5f442276976d3a67b190f182
mitcc <mitcc@qq.com>
2016-06-02 00:41:33 +0800

    :basketball:

--- lintcode/CountOfSmallerNumberBeforeItself.java
+++ lintcode/CountOfSmallerNumberBeforeItself.java
@@ -3,6 +3,11 @@
  */
 public class CountOfSmallerNumberBeforeItself {
 
+    /**
+     * @param A: An integer array
+     * @return: Count the number of element before this element 'ai' is 
+     *          smaller than it and return count number array
+     */ 
     public ArrayList<Integer> countOfSmallerNumberII(int[] A) {
 
     }
