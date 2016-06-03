2f20927779081b164693b89bf5374cc56ba44af3
mitcc <mitcc@qq.com>
2016-06-04 00:43:23 +0800

    :basketball:

--- lintcode/MaximalRectangle.java
+++ lintcode/MaximalRectangle.java
@@ -3,6 +3,10 @@
  */
 public class MaximalRectangle {
 
+    /**
+     * @param matrix a boolean 2D matrix
+     * @return an integer
+     */
     public int maximalRectangle(boolean[][] matrix) {
 
     }
