3810685ee3ff4ab16909b723e3df9d227766ca03
mitcc <mitcc@qq.com>
2016-06-04 00:43:35 +0800

    :basketball:

--- lintcode/MaximalRectangle.java
+++ lintcode/MaximalRectangle.java
@@ -1,5 +1,18 @@
 /*
- *
+ * Given a 2D boolean matrix filled with False and True, find the largest rectangle containing all True and return its area.
+
+Have you met this question in a real interview? Yes
+Example
+Given a matrix:
+
+[
+  [1, 1, 0, 0, 1],
+  [0, 1, 0, 0, 1],
+  [0, 0, 1, 1, 1],
+  [0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 1]
+]
+return 6.
  */
 public class MaximalRectangle {
 
