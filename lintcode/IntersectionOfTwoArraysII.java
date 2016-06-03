aa04503cdd71d68fe90e124a11d0ab6f5da07a58
mitcc <mitcc@qq.com>
2016-06-03 23:29:54 +0800

    :basketball:

--- lintcode/IntersectionOfTwoArraysII.java
+++ lintcode/IntersectionOfTwoArraysII.java
@@ -3,6 +3,11 @@
  */
 public class IntersectionOfTwoArraysII {
 
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
     public int[] intersection(int[] nums1, int[] nums2) {
 
     }
