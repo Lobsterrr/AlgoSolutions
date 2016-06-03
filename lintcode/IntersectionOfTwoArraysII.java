11538fdf35228cbe491ba5d0c709ddce5ad86bf0
mitcc <mitcc@qq.com>
2016-06-03 23:33:31 +0800

    :basketball:

--- lintcode/IntersectionOfTwoArraysII.java
+++ lintcode/IntersectionOfTwoArraysII.java
@@ -21,7 +21,17 @@ public class IntersectionOfTwoArraysII {
      * @return an integer array
      */
     public int[] intersection(int[] nums1, int[] nums2) {
-
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            map.put(num, 1 + (map.containsKey(num) ? map.get(num) : 0));
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
     }
 
 }
