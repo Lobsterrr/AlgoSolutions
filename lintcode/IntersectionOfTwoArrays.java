baeb9180d68ad030e03cdeb278b5799c14f3d22d
mitcc <mitcc@qq.com>
2016-06-03 23:23:02 +0800

    :basketball:

--- lintcode/IntersectionOfTwoArrays.java
+++ lintcode/IntersectionOfTwoArrays.java
@@ -1,5 +1,15 @@
 /*
- *
+ * Given two arrays, write a function to compute their intersection.
+
+ Notice
+
+Each element in the result must be unique.
+The result can be in any order.
+Example
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Challenge 
+Can you implement it in three different algorithms?
  */
 public class IntersectionOfTwoArrays {
 
