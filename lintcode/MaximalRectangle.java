72d0c1921c3bfb1f213383fd8624e0150d3bd505
mitcc <mitcc@qq.com>
2016-06-05 00:41:48 +0800

    :basketball:

--- lintcode/MaximalRectangle.java
+++ lintcode/MaximalRectangle.java
@@ -24,7 +24,13 @@ public class MaximalRectangle {
         int result = 0;
         int[] height = new int[matrix[0].length];
         for (int i = 0; i < matrix.length; ++i) {
-            
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j]) {
+                    height[j] += 1;
+                } else {
+                    height[j] = 0;
+                }
+            }
         }
     }
 
