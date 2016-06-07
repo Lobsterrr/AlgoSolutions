d0e94b791927e8f5e93d492c9394c824f7fc2402
mitcc <mitcc@qq.com>
2016-06-07 22:44:55 +0800

    :basketball:

--- ctci/Chapter8_5.java
+++ ctci/Chapter8_5.java
@@ -5,7 +5,25 @@
  * input: 3 (e.g., 3 pairs of parentheses)
  * output: ()()(), ()(()), (())(), ((()))
  */
-public class Solution {
+public class Chapter8_5 {
+
+    public void printParentheses(int n) {
+
+    }
+
+    public void dfs(String s, int left, int right) {
+        if (left == 0 && right == 0) {
+            System.out.println(s);
+        }
+        if (left > 0) {
+            dfs(s + "(", left - 1, right);
+        }
+        if (left < right) {
+            dfs(s + ")", left, right - 1);
+        }
+    }
+
+
     public void printParentheses(int n) {
         helper("", n, n);
     }
