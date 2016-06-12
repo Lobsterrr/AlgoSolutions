5de6d8ae0876030632855011bf7e19aa57f337f8
mitcc <mitcc@qq.com>
2016-06-12 08:53:27 +0800

    :basketball:

--- lintcode/MaximalSquare.java
+++ lintcode/MaximalSquare.java
@@ -1,5 +1,14 @@
 /*
- *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.
+
+Example
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 4.
  */
 public class MaximalSquare {
 
