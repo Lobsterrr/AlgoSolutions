9e33dc322b6bfd116398fcba1d1a74c51ff1bd97
mitcc <mitcc@qq.com>
2016-06-12 08:53:08 +0800

    :basketball:

--- lintcode/MaximalSquare.java
+++ lintcode/MaximalSquare.java
@@ -3,7 +3,12 @@
  */
 public class MaximalSquare {
 
+    /**
+     * @param matrix: a matrix of 0 and 1
+     * @return: an integer
+     */
     public int maxSquare(int[][] matrix) {
+        
 
     }
 
