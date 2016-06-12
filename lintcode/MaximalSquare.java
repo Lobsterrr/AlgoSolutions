d3b0e253ebd20a5c1fc83fdcd591ab0ff63d1233
mitcc <mitcc@qq.com>
2016-06-13 01:25:44 +0800

    :basketball:

--- lintcode/MaximalSquare.java
+++ lintcode/MaximalSquare.java
@@ -67,10 +67,12 @@ public class MaximalSquare {
                 if (i == 0 || j == 0) {
                     dp[i][j] = matrix[i][j];
                 } else {
-                    dp[i][j] = 
+                    dp[i][j] = matrix[i][j] == 0 ? 0 : Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    maxSize = Math.max(maxSize, dp[i][j]);
                 }
             }
         }
+        return maxSize * maxSize;
     }
 
 }
