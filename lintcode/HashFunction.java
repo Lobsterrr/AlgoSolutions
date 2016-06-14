fd6f30f6773eae37a0dc26f3a57e37e6b1be3be7
mitcc <mitcc@qq.com>
2016-06-15 00:53:23 +0800

    :basketball:

--- lintcode/HashFunction.java
+++ lintcode/HashFunction.java
@@ -3,6 +3,11 @@
  */
 public class HashFunction {
  
+    /**
+     * @param key: A String you should hash
+     * @param HASH_SIZE: An integer
+     * @return an integer
+     */
     public int hashCode(char[] key, int HASH_SIZE) {
 
     }
