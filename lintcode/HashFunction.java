1db5490b6338004218d56b9604eb958d8f2a1c35
mitcc <mitcc@qq.com>
2016-06-16 23:39:07 +0800

    :basketball:

--- lintcode/HashFunction.java
+++ lintcode/HashFunction.java
@@ -61,7 +61,7 @@ public class HashFunction {
         }
         long half = powMod(base, exp / 2, mod);
         if (exp % 2 == 0) {
-            return (half % mod) * (half % mod) % mod;
+            return half * half % mod;
         } else {
             return (half * half % mod) * (base % mod) % mod;
         }
