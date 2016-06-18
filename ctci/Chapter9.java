17bfbfc214f4bbeb38defa7b90abb21f3107f047
mitcc <mitcc@qq.com>
2016-06-18 15:01:18 +0800

    :basketball:

--- ctci/Chapter9.java
+++ ctci/Chapter9.java
@@ -14,4 +14,18 @@ public class Chapter9 {
         return dp[x - 1][y - 1];
     }
 
+    public int countWays(int x, int y) {
+        int[] dp = new int[y];
+        for (int i = 0; i < x; ++i) {
+            for (int j = 0; j < y; ++j) {
+                if (i == 0 || j == 0) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[y - 1];
+    }
+
 }
