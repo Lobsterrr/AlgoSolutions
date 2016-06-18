a22496922b12438a73bb2d9fc5116aa5cb7bc499
mitcc <mitcc@qq.com>
2016-06-18 14:12:36 +0800

    :basketball:

---
+++ ctci/Chapter17.java
@@ -0,0 +1,11 @@
+public class Chapter17 {
+
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+        AB[0] ^= AB[1];
+        AB[1] ^= AB[0];
+        AB[0] ^= AB[1];
+        return AB;
+    }
+
+}
