96e816022ca716f6440c00062e8def08591aa89c
mitcc <mitcc@qq.com>
2016-06-18 21:22:55 +0800

    :basketball:

--- ctci/Chapter9.java
+++ ctci/Chapter9.java
@@ -6,7 +6,7 @@ public class Chapter9 {
         int[] dp = new int[4];
         dp[0] = 1;
         for (int i = 1; i <= n; ++i) {
-            dp[i % 3] = (dp[(i + 2) % 3] + dp[(i + 1) % 3] + dp[i % 3]) % mod;
+            dp[i % 3] = ((dp[(i + 2) % 3] + dp[(i + 1) % 3]) % mod + dp[i % 3]) % mod;
         }
         return dp[n % 3];
     }
