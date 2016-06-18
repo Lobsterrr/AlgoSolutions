f745ad1ed3b42f699119c11446c24c8bf21deefe
mitcc <mitcc@qq.com>
2016-06-18 14:17:38 +0800

    :basketball:

--- ctci/Chapter17.java
+++ ctci/Chapter17.java
@@ -8,4 +8,14 @@ public class Chapter17 {
         return AB;
     }
 
+    // 17.9
+    public int getFrequency(String[] article, int n, String word) {
+        // write code here
+        int count = 0;
+        for (String s : article) {
+            count += s.equals(word) ? 1 : 0;
+        }
+        return count;
+    }
+
 }
