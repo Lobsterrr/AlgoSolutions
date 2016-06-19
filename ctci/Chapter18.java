f3c11539ff222f313781351ddeb8435e08dd201c
mitcc <mitcc@qq.com>
2016-06-19 23:58:56 +0800

    :basketball:

--- ctci/Chapter18.java
+++ ctci/Chapter18.java
@@ -4,7 +4,7 @@ public class Chapter18 {
     public int addAB(int A, int B) {
         while (B != 0) {
             int tmp = A ^ B;
-            B = A & B << 1;
+            B = (A & B) << 1;
             A = tmp;
         }
         return A;
