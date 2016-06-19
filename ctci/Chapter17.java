5099f0e898ea94b9bc05cce36fbbd1bd571ee037
mitcc <mitcc@qq.com>
2016-06-20 01:08:15 +0800

    :basketball:

--- ctci/Chapter17.java
+++ ctci/Chapter17.java
@@ -19,6 +19,14 @@ public class Chapter17 {
         return result;
     }
 
+    public int getFactorSuffixZero(int n) {
+        int result = 0;
+        for (; n > 0; n /= 5) {
+            result += n / 5;
+        }
+        return result;
+    }
+
     // 17.4
     public int getMax(int a, int b) {
         int sign = (a - b) >> 31;
