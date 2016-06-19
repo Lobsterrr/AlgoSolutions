79b696fff46dde4e0bbfd72a843c22b3435a22a6
mitcc <mitcc@qq.com>
2016-06-19 23:16:35 +0800

    :basketball:

---
+++ ctci/Chapter2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Chapter2 {
+
+    // 2.3
+    public boolean removeNode(ListNode pNode) {
+
+    }
+
+}
