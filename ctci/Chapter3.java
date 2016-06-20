7cad2feba62140f0a7d93f20750f20d644f21dad
mitcc <mitcc@qq.com>
2016-06-20 23:12:27 +0800

    :basketball:

---
+++ ctci/Chapter3.java
@@ -0,0 +1,14 @@
+public class Chapter3 {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+
+    }
+
+    public int pop() {
+
+    }
+
+}
