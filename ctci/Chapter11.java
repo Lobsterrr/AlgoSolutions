d66d565d23d47f21ee42cea2490ba54ad7b567a6
mitcc <mitcc@qq.com>
2016-06-20 23:30:55 +0800

    :basketball:

--- ctci/Chapter11.java
+++ ctci/Chapter11.java
@@ -3,7 +3,7 @@ public class Chapter11 {
     public int[] findElement(int[][] mat, int n, int m, int x) {
         int i = 0;
         int j = m - 1;
-        while (true) {
+        while (0 <= i && i < n && 0 <= j && j < m) {
             if (mat[i][j] == x) {
                 return new int[2] {i, j};
             } else if (mat[i][j] < x) {
