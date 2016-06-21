14fe35a4db84045ce8f92e0f29befc7d4bb722b5
mitcc <mitcc@qq.com>
2016-06-22 00:00:38 +0800

    :basketball:

--- lintcode/SortIntegers.java
+++ lintcode/SortIntegers.java
@@ -1,5 +1,7 @@
 /*
- * Given an integer array, sort it in ascending order. Use selection sort, bubble sort, insertion sort or any O(n2) algorithm.
+ * Given an integer array, sort it in ascending order. Use 
+ * selection sort, bubble sort, insertion sort or any O(n2) 
+ * algorithm.
 
 Example
 Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
@@ -47,6 +49,7 @@ public class SortIntegers {
         }
     }
 
+    // quick sort
     public void quicksort(int[] A, int low, int high) {
         if (low >= high) {
             return;
