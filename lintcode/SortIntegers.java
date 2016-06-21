08f4f4f5eb5a5f7de179555c76af808ea3910290
mitcc <mitcc@qq.com>
2016-06-21 23:53:55 +0800

    :basketball:

--- lintcode/SortIntegers.java
+++ lintcode/SortIntegers.java
@@ -27,7 +27,15 @@ public class SortIntegers {
 
     // selection sort
     public void selectSort(int[] A) {
-
+        for (int i = 0; i < A.length; ++i) {
+            int minIndex = i;
+            for (int j = i + 1; j < A.length; ++j) {
+                if (A[j] < A[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(A, i, minIndex);
+        }
     }
 
     // insertion sort
