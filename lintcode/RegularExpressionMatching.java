4334e75ae4b08f0ebaa048ff27f91a4c2d6f2ab7
mitcc <mitcc@qq.com>
2016-06-22 09:25:44 +0800

    :basketball:

--- lintcode/RegularExpressionMatching.java
+++ lintcode/RegularExpressionMatching.java
@@ -1,5 +1,21 @@
 /*
- *
+ * Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+Example
+isMatch("aa","a") ¡ú false
+isMatch("aa","aa") ¡ú true
+isMatch("aaa","aa") ¡ú false
+isMatch("aa", "a*") ¡ú true
+isMatch("aa", ".*") ¡ú true
+isMatch("ab", ".*") ¡ú true
+isMatch("aab", "c*a*b") ¡ú true
  */
 public class RegularExpressionMatching {
 
