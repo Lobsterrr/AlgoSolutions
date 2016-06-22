a49d15cfbc6698de113a58d93be6f546c706dc4c
mitcc <mitcc@qq.com>
2016-06-22 09:25:25 +0800

    :basketball:

---
+++ lintcode/RegularExpressionMatching.java
@@ -0,0 +1,15 @@
+/*
+ *
+ */
+public class RegularExpressionMatching {
+
+    /**
+     * @param s: A string 
+     * @param p: A string includes "." and "*"
+     * @return: A boolean
+     */
+    public boolean isMatch(String s, String p) {
+
+    }
+
+}
