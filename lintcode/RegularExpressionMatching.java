4f78db51ed205ec01a5409370ec2ba35177c078b
mitcc <mitcc@qq.com>
2016-06-23 21:40:47 +0800

    :basketball:

--- lintcode/RegularExpressionMatching.java
+++ lintcode/RegularExpressionMatching.java
@@ -30,6 +30,9 @@ public class RegularExpressionMatching {
         }
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
+        for (int j = 0; j < p.length(); ++j) {
+            dp[0][j + 1] = p.charAt(j) == '*' && dp[0][j - 1];
+        }
         for (int i = 0; i < s.length(); ++i) {
             for (int j = 0; j < p.length(); ++j) {
                 if (p.charAt(j) != '.' && p.charAt(j) != '*') {
@@ -38,8 +41,7 @@ public class RegularExpressionMatching {
                     dp[i + 1][j + 1] = dp[i][j];
                 } else if (p.charAt(j) == '*') {
                     if (j >= 1) {
-                        dp[i + 1][j + 1] = dp[i][j - 1] || dp[i + 1][j - 1] || (i > 0) && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) && dp[i][j + 1];
-                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i + 1][j - 1] || (i > 0) && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i)) && dp[i][j + 1];
+                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i + 1][j - 1] || (i > 0) && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) && dp[i][j + 1];
                     }
                 }
             }
