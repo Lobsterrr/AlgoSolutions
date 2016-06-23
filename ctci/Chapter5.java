cbfe413e0679ea75fb302542320029178d03a364
mitcc <mitcc@qq.com>
2016-06-23 21:58:23 +0800

    :basketball:

--- ctci/Chapter5.java
+++ ctci/Chapter5.java
@@ -1,5 +1,12 @@
 public class Chapter5 {
 
+    // 5.1
+    public int binInsert(int n, int m, int j, int i) {
+        int left = n >> i;
+        int right = n << (32 - j) >> (32 - j);
+        return left | (m << j) | right;
+    }
+
     // 5.5
     public int calcCost(int A, int B) {
         // write code here
