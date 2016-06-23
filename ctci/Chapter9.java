8c789bc59ae2f5aeb0eef9273faf95af4546d9e1
mitcc <mitcc@qq.com>
2016-06-23 22:59:46 +0800

    :basketball:

--- ctci/Chapter9.java
+++ ctci/Chapter9.java
@@ -33,13 +33,14 @@ public class Chapter9 {
         if (map == null) {
             return 0;
         }
+        int mod = 1000000007;
         int[][] dp = new int[x][y];
         for (int i = 0; i < x; ++i) {
             for (int j = 0; j < y; ++j) {
                 if (i == 0 || j == 0) {
                     dp[i][j] = map[i][j] != 1 ? 0 : 1;
                 } else {
-                    dp[i][j] = map[i][j] != 1 ? 0 : dp[i][j - 1] + dp[i - 1][j];
+                    dp[i][j] = map[i][j] != 1 ? 0 : (dp[i][j - 1] + dp[i - 1][j]) % mod;
                 }
             }
         }
