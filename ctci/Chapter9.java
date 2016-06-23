054ed0079e5d154806ab2aa5b910ecb898e7df7d
mitcc <mitcc@qq.com>
2016-06-23 23:10:02 +0800

    :basketball:

--- ctci/Chapter9.java
+++ ctci/Chapter9.java
@@ -37,10 +37,18 @@ public class Chapter9 {
         int[][] dp = new int[x][y];
         for (int i = 0; i < x; ++i) {
             for (int j = 0; j < y; ++j) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = map[i][j] != 1 ? 0 : 1;
+                if (map[i][j] != 1) {
+                    dp[i][j] = 0;
                 } else {
-                    dp[i][j] = map[i][j] != 1 ? 0 : (dp[i][j - 1] + dp[i - 1][j]) % mod;
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                    } else if (i == 0 && j > 0) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else if (i > 0 && j == 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else if (i > 0 && j > 0) {
+                        dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                    }
                 }
             }
         }
