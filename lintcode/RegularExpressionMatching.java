f0579b9840ac49697a8c36d2fb7d45083479e744
mitcc <mitcc@qq.com>
2016-06-23 08:44:45 +0800

    :basketball:

--- lintcode/RegularExpressionMatching.java
+++ lintcode/RegularExpressionMatching.java
@@ -25,6 +25,9 @@ public class RegularExpressionMatching {
      * @return: A boolean
      */
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
         for (int i = 0; i < s.length(); ++i) {
