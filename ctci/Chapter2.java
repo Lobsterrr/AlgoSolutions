d5287dbdf685ab8305e7afcd62e9e2eb9a3c4c82
mitcc <mitcc@qq.com>
2016-06-24 08:47:24 +0800

    :basketball:

--- ctci/Chapter2.java
+++ ctci/Chapter2.java
@@ -11,6 +11,23 @@ public class ListNode {
 }*/
 public class Chapter2 {
 
+    // 2.2
+    public ListNode FindKthToTail(ListNode head, int k) {
+        int len = 0;
+        for (ListNode cur = head; cur != null; cur = cur.next, len++);
+        ListNode cur1 = head;
+        ListNode cur2 = head;
+        int step = 0;
+        while (cur1 != null) {
+            cur1 = cur1.next;
+            step++;
+            if (step > len - k) {
+                cur2 = cur2.next;
+            }
+        }
+        return cur2;
+    }
+
     // 2.3
     public boolean removeNode(ListNode pNode) {
         if (pNode == null || pNode.next == null) {
