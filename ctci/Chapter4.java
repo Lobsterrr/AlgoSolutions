0103486a94e1bbb836dc08e6d5088a93d34102b7
mitcc <mitcc@qq.com>
2016-06-26 23:44:54 +0800

    :basketball:

--- ctci/Chapter4.java
+++ ctci/Chapter4.java
@@ -1,3 +1,16 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
 public class Chapter4 {
 
     // 4.9
