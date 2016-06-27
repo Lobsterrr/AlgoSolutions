12856cf93bb7afed4a5496162b6f2c80ba69599d
mitcc <mitcc@qq.com>
2016-06-27 23:36:20 +0800

    :basketball:

--- ctci/Finder.java
+++ ctci/Finder.java
@@ -1,7 +1,14 @@
 public class Finder {
 
     public int findElement(int[] A, int n, int x) {
-
+        int low = 0;
+        int high = A.lenght - 1;
+        while (low <= high) {
+            int mid = low + (high - low) >> 1;
+            if (A[mid] == x) {
+                return mid;
+            }
+        }
     }
 
 }
