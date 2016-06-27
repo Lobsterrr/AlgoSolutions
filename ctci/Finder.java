ba357035551d588ab7e7ae6d1a72b3f980139c6f
mitcc <mitcc@qq.com>
2016-06-27 23:47:54 +0800

    :basketball:

--- ctci/Finder.java
+++ ctci/Finder.java
@@ -8,7 +8,21 @@ public class Finder {
             if (A[mid] == x) {
                 return mid;
             }
+            if (A[mid] <= A[high]) {
+                if (A[mid] < x && x <= A[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (A[low] <= x && x < A[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
         }
+        return low;
     }
 
 }
