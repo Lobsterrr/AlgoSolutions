0a21631413829a7b4e40da4fa4c4f1802191887f
mitcc <mitcc@qq.com>
2016-06-30 00:43:25 +0800

    :basketball:

--- ctci/Chapter7.java
+++ ctci/Chapter7.java
@@ -7,7 +7,38 @@ public class Chapter7 {
 
     // 7.4
     public int calc(int a, int b, int type) {
+        
+    }
 
+    public int negative(int x) {
+        int base = x > 0 ? -1 : 1;
+        int result = 0;
+        for (; x != 0; x += base, result += base);
+        return result;
+    }
+
+    public int multiple(int a, int b) {
+        boolean flag = a >= 0 && b >= 0 || a < 0 && b < 0;
+        if (a < 0) {
+            a = negative(a);
+        }
+        if (b < 0) {
+            b = negative(b);
+        }
+        if (a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        int result = 0;
+        for (int i = 0; i < b; ++i) {
+            result += a;
+        }
+        return flag ? result : negative(result);
+    }
+
+    public int minus(int a, int b) {
+        return a + negative(b);
     }
 
 }
