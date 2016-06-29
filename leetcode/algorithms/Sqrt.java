e46ba909354c2d02975e9b50e27dcacee5a96382
mitcc <mitcc@qq.com>
2016-06-30 01:21:01 +0800

    :basketball:

--- leetcode/algorithms/Sqrt.java
+++ leetcode/algorithms/Sqrt.java
@@ -29,21 +29,6 @@ public class Sqrt {
         return low - 1;
     }
 
-    public int sqrt(int x) {
-        int start = 1;
-        int end = x;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid == x / mid)
-                return mid;
-            else if (mid < x / mid)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-        return (start + end) / 2;
-    }
-
 /*  public int sqrt(int x) {
         if(x < 100) {
             int i = 0; 
