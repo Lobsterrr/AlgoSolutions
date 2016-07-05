520bcbc1e5ad40cdb2c6475d4b25f6cf8042b216
mitcc <mitcc@qq.com>
2016-07-05 11:36:51 +0800

    :basketball:

--- leetcode/algorithms/LargestDivisibleSubset.java
+++ leetcode/algorithms/LargestDivisibleSubset.java
@@ -28,7 +28,6 @@ public class LargestDivisibleSubset {
         int[] preIndex = new int[nums.length];
         dp[0] = 1;
         int maxIndex = 0;
-        int maxLength = 1;
         for (int i = 1; i < nums.length; ++i) {
             for (int j = i - 1; j >= 0; --j) {
                 if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
@@ -36,8 +35,7 @@ public class LargestDivisibleSubset {
                     preIndex[i] = j;
                 }
             }
-            if (maxLength < dp[i]) {
-                maxLength = dp[i];
+            if (dp[i] > dp[maxIndex]) {
                 maxIndex = i;
             }
         }
