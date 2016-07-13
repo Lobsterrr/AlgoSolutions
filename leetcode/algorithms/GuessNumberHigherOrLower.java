e9ccec333ee2dd5142c8d309c427cd5ffc8f061b
mitcc <mitcc@qq.com>
2016-07-13 16:35:46 +0800

    :basketball:

--- leetcode/algorithms/GuessNumberHigherOrLower.java
+++ leetcode/algorithms/GuessNumberHigherOrLower.java
@@ -11,6 +11,24 @@ public class GuessNumberHigherOrLower {
         if (low <= 0 || low > high) {
             throw new IllegalArgumentException("illegal input");
         }
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int guess = guess(mid);
+            if (guess == 0) {
+                return mid;
+            } else if (guess = -1) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public int guessNumber(int low, int high) {
+        if (low <= 0 || low > high) {
+            throw new IllegalArgumentException("illegal input");
+        }
         int mid = low + (high - low) / 2;
         if (guess(mid) == 0) {
             return mid;
