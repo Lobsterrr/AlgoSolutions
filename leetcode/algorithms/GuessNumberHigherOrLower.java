385f9b342589c15532a2f90e8cb2c43406fbc1df
mitcc <mitcc@qq.com>
2016-07-13 16:29:47 +0800

    :basketball:

--- leetcode/algorithms/GuessNumberHigherOrLower.java
+++ leetcode/algorithms/GuessNumberHigherOrLower.java
@@ -11,7 +11,7 @@ public class GuessNumberHigherOrLower {
         int mid = low + (high - low) / 2;
         if (guess(mid) == 0) {
             return mid;
-        } else if (if (guess(mid) == -1)) {
+        } else if (guess(mid) == -1) {
             return guessNumber(low, mid - 1);
         } else {
             return guessNumber(mid + 1, high);
