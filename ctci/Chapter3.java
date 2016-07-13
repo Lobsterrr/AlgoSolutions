7bad2878d2a079c3bfd73aea9864f39a60f09fc7
mitcc <mitcc@qq.com>
2016-07-13 22:46:30 +0800

    :basketball:

--- ctci/Chapter3.java
+++ ctci/Chapter3.java
@@ -1,5 +1,6 @@
 public class Chapter3 {
 
+    // 3.5
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
 
@@ -14,6 +15,13 @@ public class Chapter3 {
             }
         }
         return stack2.pop();
+    }
+
+    // 3.6
+    public ArrayList<Integer> twoStacksSort(int[] numbers) {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
     }
 
 }
