45f01402f4e4aefc56f263d9c2d414e6c4e51ccf
mitcc <mitcc@qq.com>
2016-07-14 09:14:28 +0800

    :basketball:

--- leetcode/algorithms/LongestIncreasingSubsequence.java
+++ leetcode/algorithms/LongestIncreasingSubsequence.java
@@ -52,21 +52,19 @@ public class LongestIncreasingSubsequence {
 
     // O(n ^ 2) time, O(n) space.
     public int lengthOfLIS(int[] nums) {
-        if (nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
+        int result = 0;
         for (int i = 1; i < nums.length; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-        }
-        int result = 0;
-        for (int value : dp) {
-            result = Math.max(result, value);
+            result = Math.max(result, dp[i]);
         }
         return result;
     }
