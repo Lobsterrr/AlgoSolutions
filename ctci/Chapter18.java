e263fdbc8aa2732ef90da511485ae5af64fb242c
mitcc <mitcc@qq.com>
2016-07-15 23:31:36 +0800

    :basketball:

--- ctci/Chapter18.java
+++ ctci/Chapter18.java
@@ -36,40 +36,4 @@ public class Chapter18 {
         return result;
     }
 
-    public int getDistance(String[] article, int n, String x, String y) {
-        if (article == null || article.length == 0) {
-            return -1;
-        }
-        List<Integer> xList = new ArrayList<Integer>();
-        List<Integer> yList = new ArrayList<Integer>();
-        for (int i = 0; i < article.length; ++i) {
-            if (article[i].equals(x)) {
-                xList.add(i);
-            }
-            if (article[i].equals(y)) {
-                yList.add(i);
-            }
-        }
-        if (xList.get(0) >= yList.get(yList.size() - 1)) {
-            return xList.get(0) - yList.get(yList.size() - 1);
-        }
-        if (xList.get(xList.size() - 1) <= yList.get(0)) {
-            return yList.get(0) - xList.get(xList.size() - 1);
-        }
-        int ix = 0;
-        int iy = 0;
-        int result = Integer.MAX_VALUE;
-        while (ix < xList.size() && iy < yList.size()) {
-            result = Math.min(result, Math.abs(xList.get(ix) - yList.get(iy)));
-            if (xList.get(ix) == yList.get(iy)) {
-                return 0;
-            } else if (xList.get(ix) < yList.get(iy)) {
-                ix++;
-            } else {
-                iy++;
-            }
-        }
-        return result;
-    }
-
 }
