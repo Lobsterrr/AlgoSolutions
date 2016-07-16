22e43c44ab34d2fa084f4e2105a4d2989fadc3e2
mitcc <mitcc@qq.com>
2016-07-16 12:43:11 +0800

    :basketball:

--- lintcode/SortIntegersII.java
+++ lintcode/SortIntegersII.java
@@ -1,8 +1,15 @@
 /*
- *
+ * Given an integer array, sort it in ascending order. Use quick sort, merge sort, heap sort or any O(nlogn) algorithm.
+
+Have you met this question in a real interview? Yes
+Example
+Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
  */
 public class SortIntegersII {
-
+    /**
+     * @param A an integer array
+     * @return void
+     */
     public void sortIntegers2(int[] A) {
         
     }
