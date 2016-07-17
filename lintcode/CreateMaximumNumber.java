07ce4eeddca878459312e3aa477553c62d6772e1
mitcc <mitcc@qq.com>
2016-07-17 23:55:15 +0800

    :basketball:

--- lintcode/CreateMaximumNumber.java
+++ lintcode/CreateMaximumNumber.java
@@ -3,6 +3,12 @@
  */
 public class CreateMaximumNumber {
 
+    /**
+     * @param nums1 an integer array of length m with digits 0-9
+     * @param nums2 an integer array of length n with digits 0-9
+     * @param k an integer and k <= m + n
+     * @return an integer array
+     */
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
     
     }
