2367fabef15cb9249027be6655e61bbaa427345b
mitcc <mitcc@qq.com>
2016-07-17 23:23:08 +0800

    :basketball:

--- leetcode/algorithms/GuessNumberHigherOrLowerII.java
+++ leetcode/algorithms/GuessNumberHigherOrLowerII.java
@@ -35,6 +35,22 @@ public class GuessNumberHigherOrLowerII {
             return 0;
         }
         int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j <= n; ++j) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; ++k) {
+                    dp[i][j] = Math.min(dp[i][j], k + Math.max(dp[i][k - 1], dp[k + 1][j]));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+
+    public int getMoneyAmount(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        int[][] dp = new int[n + 1][n + 1];
         return getMoneyAmount(dp, 1, n);
     }
 
