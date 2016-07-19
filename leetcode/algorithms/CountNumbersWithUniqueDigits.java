95536e73426cf7083b79fabb4642b6e33355d03b
mitcc <mitcc@qq.com>
2016-07-19 11:07:51 +0800

    :basketball:

--- leetcode/algorithms/CountNumbersWithUniqueDigits.java
+++ leetcode/algorithms/CountNumbersWithUniqueDigits.java
@@ -27,7 +27,8 @@ public class CountNumbersWithUniqueDigits {
             return 0;
         }
         int[] result = {
-            1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691
+            1, 10, 91, 739, 5275, 32491, 168571, 
+            712891, 2345851, 5611771, 8877691
         };
         return result[n];
     }
