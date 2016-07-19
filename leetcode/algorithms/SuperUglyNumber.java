6013c1071401cb38cca38bfc63e1803d3219b030
mitcc <mitcc@qq.com>
2016-07-20 01:34:16 +0800

    :basketball:

--- leetcode/algorithms/SuperUglyNumber.java
+++ leetcode/algorithms/SuperUglyNumber.java
@@ -1,7 +1,10 @@
 /*
  * Write a program to find the nth super ugly number.
 
-Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
+Super ugly numbers are positive numbers whose all prime factors are in the 
+given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 
+26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = 
+[2, 7, 13, 19] of size 4.
 
 Note:
 (1) 1 is a super ugly number for any given primes.
@@ -15,8 +18,18 @@ public class SuperUglyNumber {
         result[0] = 1;
         int[] index = new int[primes.length];
         for (int i = 1; i < n; ++i) {
-
+            result[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < index.length; ++j) {
+                result[i] = Math.min(result, primes[j] * result[index[j]]);
+            }
+            for (int j = 0; j < index.length; ++j) {
+                if (result[i] == primes[j] * result[inde[j]]) {
+                    index[j]++;
+                    break;
+                }
+            }
         }
+        return result[n - 1];
     }
 
 }
