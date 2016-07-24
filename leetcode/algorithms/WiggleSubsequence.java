0f707f733e5ba84776351736165f88fad6e8e5c7
mitcc <mitcc@qq.com>
2016-07-25 00:30:55 +0800

    :basketball:

--- leetcode/algorithms/WiggleSubsequence.java
+++ leetcode/algorithms/WiggleSubsequence.java
@@ -38,17 +38,17 @@ public class WiggleSubsequence {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int count = 1;
+        int result = 1;
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] > nums[i - 1]) {
-                count++;
+                result++;
                 for (; i + 1 < nums.length && nums[i + 1] >= nums[i]; ++i);
             } else if (nums[i] < nums[i - 1]) {
-                count++;
+                result++;
                 for (; i + 1 < nums.length && nums[i + 1] <= nums[i]; ++i);
             }
         }
-        return count;
+        return result;
     }
 
     // O(n ^ 2) time, O(n) space.
