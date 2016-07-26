c6dcc637a61aaa6208c53f4ccaa8797696a9c997
mitcc <mitcc@qq.com>
2016-07-26 23:51:27 +0800

    :basketball:

--- hackerrank/Dynamic Programming/Candies.java
+++ hackerrank/Dynamic Programming/Candies.java
@@ -45,7 +45,30 @@ import java.util.regex.*;
 public class Candies {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[] ratings = new int[N];
 
+    }
+
+    public static long minCandies(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for (int i = 1; i < ratings.length; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        long result = 0;
+        for (int candy : candies) {
+            result += candy;
+        }
+        return result;
     }
 
 }
