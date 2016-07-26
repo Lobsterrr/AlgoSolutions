82cb87b80a8e5bebb3918744e5838632a2ff5de0
mitcc <mitcc@qq.com>
2016-07-26 23:28:47 +0800

    :basketball:

--- hackerrank/Warmup Challenges/CompareTheTriplets.java
+++ hackerrank/Warmup Challenges/CompareTheTriplets.java
@@ -1,25 +1,33 @@
 /*
- * Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from  to  for three categories: problem clarity, originality, and difficulty.
+ * Alice and Bob each created one problem for HackerRank. A reviewer rates the 
+ * two challenges, awarding points on a scale from  to  for three categories: 
+ * problem clarity, originality, and difficulty.
 
-We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+We define the rating for Alice's challenge to be the triplet , and the rating 
+for Bob's challenge to be the triplet .
 
-Your task is to find their comparison scores by comparing  with ,  with , and  with .
+Your task is to find their comparison scores by comparing  with ,  with , and  
+with .
 
 If , then Alice is awarded  point.
 If , then Bob is awarded  point.
 If , then neither person receives a point.
-Given  and , can you compare the two challenges and print their respective comparison points?
+Given  and , can you compare the two challenges and print their respective 
+comparison points?
 
 Input Format
 
-The first line contains  space-separated integers, , , and , describing the respective values in triplet . 
-The second line contains  space-separated integers, , , and , describing the respective values in triplet .
+The first line contains  space-separated integers, , , and , describing the 
+respective values in triplet . 
+The second line contains  space-separated integers, , , and , describing the 
+respective values in triplet .
 
 Constraints
 
 Output Format
 
-Print two space-separated integers denoting the respective comparison scores earned by Alice and Bob.
+Print two space-separated integers denoting the respective comparison scores 
+earned by Alice and Bob.
 
 Sample Input
 
@@ -37,7 +45,9 @@ Now, let's compare each individual score:
 , so Alice receives  point.
 , so nobody receives a point.
 , so Bob receives  point.
-Alice's comparison score is , and Bob's comparison score is . Thus, we print 1 1 (Alice's comparison score followed by Bob's comparison score) on a single line.
+Alice's comparison score is , and Bob's comparison score is . Thus, we print 
+1 1 (Alice's comparison score followed by Bob's comparison score) on a single 
+line.
  */
 import java.io.*;
 import java.util.*;
