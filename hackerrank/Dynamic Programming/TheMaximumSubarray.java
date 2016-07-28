2ac48731d9a65df94bbb4b65db51431282fcc129
mitcc <mitcc@qq.com>
2016-07-28 21:39:27 +0800

    :basketball:

--- hackerrank/Dynamic Programming/TheMaximumSubarray.java
+++ hackerrank/Dynamic Programming/TheMaximumSubarray.java
@@ -8,7 +8,8 @@ Empty subarrays/subsequences should not be considered.
 Input Format
 
 First line of the input has an integer .  cases follow. 
-Each test case begins with an integer . In the next line,  integers follow representing the elements of array .
+Each test case begins with an integer . In the next line,  integers follow 
+representing the elements of array .
 
 Constraints:
 
@@ -16,7 +17,9 @@ The subarray and subsequences you consider should have at least one element.
 
 Output Format
 
-Two, space separated, integers denoting the maximum contiguous and non-contiguous subarray. At least one integer should be selected and put into the subarrays (this may be required in cases where all elements are negative).
+Two, space separated, integers denoting the maximum contiguous and 
+non-contiguous subarray. At least one integer should be selected and put into 
+the subarrays (this may be required in cases where all elements are negative).
 
 Sample Input
 
@@ -32,11 +35,13 @@ Sample Output
 Explanation
 
 In the first case: 
-The max sum for both contiguous and non-contiguous elements is the sum of ALL the elements (as they are all positive).
+The max sum for both contiguous and non-contiguous elements is the sum of ALL 
+the elements (as they are all positive).
 
 In the second case: 
 [2 -1 2 3 4] --> This forms the contiguous sub-array with the maximum sum. 
-For the max sum of a not-necessarily-contiguous group of elements, simply add all the positive elements.
+For the max sum of a not-necessarily-contiguous group of elements, simply add 
+all the positive elements.
  */
 import java.io.*;
 import java.util.*;
