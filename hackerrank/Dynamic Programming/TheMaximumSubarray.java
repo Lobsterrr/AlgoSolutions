6786da2a7f78e1c5b36d77831c41994cea40496c
mitcc <mitcc@qq.com>
2016-07-28 21:32:43 +0800

    :basketball:

--- hackerrank/Dynamic Programming/TheMaximumSubarray.java
+++ hackerrank/Dynamic Programming/TheMaximumSubarray.java
@@ -57,21 +57,20 @@ public class TheMaximumSubarray {
             }
             int sum1 = Integer.MIN_VALUE;
             int sum2 = Integer.MIN_VALUE;
-            for (int j = 0, curSum1 = 0, curSum2 = 0; j < N; ++j) {
+            for (int j = 0, curSum1 = 0; j < N; ++j) {
                 curSum1 += array[j];
-                curSum2 += array[j];
                 sum1 = Math.max(sum1, curSum1);
+                if (curSum1 < 0) {
+                    curSum1 = 0;
+                }
                 if (sum2 == Integer.MIN_VALUE) {
                     sum2 = array[j];
                 }
-                if (array[j] > 0) {
-                    if (sum2 < 0) {
-                        sum2 = 0;
-                    }
+                
+                if (sum2 == Integer.MIN_VALUE) {
                     sum2 += array[j];
-                }
-                if (curSum1 < 0) {
-                    curSum1 = 0;
+                } else {
+                    sum2 = Math.max(sum2 + array[j], Math.max(array[j], sum2));
                 }
             }
             System.out.println(sum1 + " " + sum2);
