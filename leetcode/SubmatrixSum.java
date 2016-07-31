063eebb4dcb6c405c4a329634f17fbee657fd9ce
mitcc <mitcc@qq.com>
2016-07-31 18:48:39 +0800

    :basketball:

--- leetcode/SubmatrixSum.java
+++ leetcode/SubmatrixSum.java
@@ -16,6 +16,10 @@ O(n ^ 3) time.
  */
 public class SubmatrixSum {
 
+    /**
+     * @param matrix an integer matrix
+     * @return the coordinate of the left-up and right-down number
+     */
     public int[][] submatrixSum(int[][] matrix) {
 
     }
