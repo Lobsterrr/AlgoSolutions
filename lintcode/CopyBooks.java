c99f79e82c3a463c8511a500e58298f078d125af
mitcc <mitcc@qq.com>
2016-07-31 14:38:42 +0800

    :basketball:

--- lintcode/CopyBooks.java
+++ lintcode/CopyBooks.java
@@ -1,11 +1,18 @@
 /*
- * Given an array A of integer with size of n( means n books and number of pages of each book) and k people to copy the book. You must distribute the continuous id books to one people to copy. (You can give book A[1],A[2] to one people, but you cannot give book A[1], A[3] to one people, because book A[1] and A[3] is not continuous.) Each person have can copy one page per minute. Return the number of smallest minutes need to copy all the books.
+ * Given an array A of integer with size of n( means n books and 
+ * number of pages of each book) and k people to copy the book. You 
+ * must distribute the continuous id books to one people to copy. 
+ * (You can give book A[1],A[2] to one people, but you cannot give 
+ * book A[1], A[3] to one people, because book A[1] and A[3] is not 
+ * continuous.) Each person have can copy one page per minute. Return 
+ * the number of smallest minutes need to copy all the books.
 
 Have you met this question in a real interview? Yes
 Example
 Given array A = [3,2,4], k = 2.
 
-Return 5( First person spends 5 minutes to copy book 1 and book 2 and second person spends 4 minutes to copy book 3. )
+Return 5( First person spends 5 minutes to copy book 1 and book 2 and 
+second person spends 4 minutes to copy book 3. )
 
 Challenge 
 Could you do this in O(n*k) time ?
