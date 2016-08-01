f06e7b1e2de18e4a9e24e1acecb03ccf80a02a19
mitcc <mitcc@qq.com>
2016-08-01 09:18:53 +0800

    :basketball:

--- leetcode/SubmatrixSum.java
+++ leetcode/SubmatrixSum.java
@@ -29,6 +29,19 @@ public class SubmatrixSum {
         int yLen = matrix[0].length;
         int[][] sum = new int[xLen + 1][yLen + 1];
         for (int i = 0; i < xLen; ++i) {
+            for (int j = 0; j < yLen; ++j) {
+                if (i == 0 && j == 0) {
+
+                } else if (i == 0 && j > 0) {
+
+                } else if (i > 0 && j == 0) {
+
+                } else {
+
+                }
+            }
+        }
+        for (int i = 0; i < xLen; ++i) {
             for (int j = i; j < xLen; ++j) {
                 Map<Integer, Integer> map = new HashMap<Integer, Integer>();
                 for (int k = 0; k < yLen; ++k) {
