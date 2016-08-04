46ab7c152694e9bf4433bb898fc8009c7adf9362
mitcc <mitcc@qq.com>
2016-08-04 23:53:32 +0800

    :basketball:

--- lintcode/BackpackVI.java
+++ lintcode/BackpackVI.java
@@ -25,7 +25,16 @@ public class BackpackVI {
      * @return an integer
      */
     public int backPackVI(int[] nums, int target) {
-
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (i >= num) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
     }
 
 }
