601ee9e9cbd826ecdce909aed12a2a3a72477bad
mitcc <mitcc@qq.com>
2016-08-04 23:51:04 +0800

    :basketball:

---
+++ lintcode/BackpackVI.java
@@ -0,0 +1,15 @@
+/*
+ *
+ */
+public class BackpackVI {
+
+    /**
+     * @param nums an integer array and all positive numbers, no duplicates
+     * @param target an integer
+     * @return an integer
+     */
+    public int backPackVI(int[] nums, int target) {
+
+    }
+
+}
