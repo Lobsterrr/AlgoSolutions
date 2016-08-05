7ccd6cff0ba8ad9d5c4c3639d8448b91a84ecefd
mitcc <mitcc@qq.com>
2016-08-05 13:51:19 +0800

    :basketball:

--- ctci/Chapter7.java
+++ ctci/Chapter7.java
@@ -71,11 +71,13 @@ public class Chapter7 {
 
     // 7.5
     public double[] getBipartition(Point[] A, Point[] B) {
-        double cx = (A[0].x + A[1].x + A[2].x + A[3].x) / 4.0;
-        double cy = (A[0].y + A[1].y + A[2].y + A[3].y) / 4.0;
-        double dx = (B[0].x + B[1].x + B[2].x + B[3].x) / 4.0;
-        double dy = (B[0].y + B[1].y + B[2].y + B[3].y) / 4.0;
-        double k = (dy - cy) / (dx - cx);
+        double xc = (A[0].x + A[1].x + A[2].x + A[3].x) / 4.0;
+        double yc = (A[0].y + A[1].y + A[2].y + A[3].y) / 4.0;
+        double xd = (B[0].x + B[1].x + B[2].x + B[3].x) / 4.0;
+        double yd = (B[0].y + B[1].y + B[2].y + B[3].y) / 4.0;
+        double k = (yd - yc) / (xd - xc);
+        double y0 = yd - xd * (yd - yc) / (xd - xc);
+        return new double[] {k, y0};
     }
 
     // 7.7
