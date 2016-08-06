d510ddd648d2e94f5e0d7c1eceafa0a929dac026
mitcc <mitcc@qq.com>
2016-08-06 22:32:34 +0800

    :basketball:

--- lintcode/CountOfSmallerNumber.java
+++ lintcode/CountOfSmallerNumber.java
@@ -1,8 +1,28 @@
 /*
- *
+ * Give you an integer array (index from 0 to n-1, where n is the size of this array, value from 0 to 10000) and an query list. For each query, give you an integer, return the number of element in the array that are smaller than the given integer.
+
+ Notice
+
+We suggest you finish problem Segment Tree Build and Segment Tree Query II first.
+
+Have you met this question in a real interview? Yes
+Example
+For array [1,2,7,8,5], and queries [1,8,5], return [0,4,2]
+
+Challenge 
+Could you use three ways to do it.
+
+Just loop
+Sort and binary search
+Build Segment Tree and Search.
  */
 public class CountOfSmallerNumber {
 
+    /**
+     * @param A: An integer array
+     * @return: The number of element in the array that 
+     *          are smaller that the given integer
+     */
     public ArrayList<Integer> countOfSmallerNumber(int[] A, int[] queries) {
 
     }
