e266fde0c29a275567288dd3f71dbc4bf9d13186
mitcc <mitcc@qq.com>
2016-08-09 09:44:20 +0800

    :basketball:

---
+++ lintcode/MinimumAdjustCost.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class MinimumAdjustmentCost {
+
+    /**
+     * @param A: An integer array.
+     * @param target: An integer.
+     */
+    public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+
+    }
+
+}
