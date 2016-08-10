93a8a0b1db26befee953f49db81b0d74be59ce22
mitcc <mitcc@qq.com>
2016-08-10 09:18:23 +0800

    :basketball:

---
+++ leetcode/algorithms/LinkedListRandomNode.java
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+public class LinkedListRandomNode {
+
+    /** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+    public LinkedListRandomNode(ListNode head) {
+        
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        
+    }
+
+}
