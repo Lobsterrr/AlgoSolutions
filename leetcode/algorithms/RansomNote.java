737acd2bc6d84a41ac72658990242548a6ac7af3
mitcc <mitcc@qq.com>
2016-08-15 01:45:54 +0800

    :basketball:

--- leetcode/algorithms/RansomNote.java
+++ leetcode/algorithms/RansomNote.java
@@ -13,7 +13,17 @@ canConstruct("aa", "aab") -> true
 public class RansomNote {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        
+        int[] count = new int[256];
+        for (int i = 0; i < magazine.length(); ++i) {
+            count[magazine.charAt(i)]++;
+        }
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            count[ransomNote.charAt(i)]--;
+            if (count[ransomNote.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
