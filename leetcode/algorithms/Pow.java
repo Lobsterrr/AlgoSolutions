84002dff218b39f1604939d64be3f05c056f29ed
mitcc <mitcc@qq.com>
2016-08-15 13:32:53 +0800

    :basketball:

--- leetcode/algorithms/Pow.java
+++ leetcode/algorithms/Pow.java
@@ -4,6 +4,18 @@
 
 public class Pow {
 
+    public double myPow(double x, int n) {
+        double result = 1.0;
+        double base = n >= 0 ? x : 1 / x;
+        n = Math.abs(n);
+        while (n > 0) {
+            result *= base;
+            base *= base;
+            n /= 2;
+        }
+        return result;
+    }
+
     public double pow(double x, int n) {
         if (n == 0)
             return 1;
