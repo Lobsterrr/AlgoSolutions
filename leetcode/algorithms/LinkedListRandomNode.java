9bfca0f61fc5705988aa97934e7e775f04e02c04
mitcc <mitcc@qq.com>
2016-09-09 01:12:43 +0800

    :basketball:

--- leetcode/algorithms/LinkedListRandomNode.java
+++ leetcode/algorithms/LinkedListRandomNode.java
@@ -55,32 +55,6 @@ class Solution {
         return result.val;
     }
 }
-
-class Solution {
-
-    ListNode head;
-    int len;
-
-    /** @param head The linked list's head.
-        Note that the head is guaranteed to be not null, so it contains at least 
-        one node. */
-    public Solution(ListNode head) {
-        this.head = head;
-        for (ListNode cur = head; cur != null; cur = cur.next, len++);
-    }
-    
-    /** Returns a random node's value. */
-    public int getRandom() {
-        Random rnd = new Random();
-        int step = rnd.nextInt(len);
-        ListNode cur = head;
-        for (int i = 0; i < step; ++i) {
-            cur = cur.next;
-        }
-        return cur.val;
-    }
-}
-
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(head);
