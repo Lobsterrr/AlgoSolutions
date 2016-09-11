0c96607e93436106dba9ed433f492391c10cd8f7
mitcc <mitcc@qq.com>
2016-09-11 09:03:04 +0800

    :basketball:

--- lintcode/AddAndSearchWord.java
+++ lintcode/AddAndSearchWord.java
@@ -46,3 +46,7 @@ class TrieNode {
     }
 
 }
+
+class Trie {
+
+}
---
+++ lintcode/WordSearchII.java
@@ -0,0 +1,15 @@
+/*
+ *
+ */
+public class WordSearchII {
+
+    /**
+     * @param board: A list of lists of character
+     * @param words: A list of string
+     * @return: A list of string
+     */
+    public ArrayList<String> wordSearchII(char[][] board, ArrayList<String>) {
+
+    }
+
+}
