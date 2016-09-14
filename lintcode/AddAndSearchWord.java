e960ee9457bc0ad6792703ff26f3b29f7edc8744
mitcc <mitcc@qq.com>
2016-09-14 14:23:39 +0800

    :basketball:

--- lintcode/AddAndSearchWord.java
+++ lintcode/AddAndSearchWord.java
@@ -20,47 +20,19 @@ search("b..")  // return true
  */
 public class AddAndSearchWord {
 
-    Trie trie = new Trie();
 
     // Adds a word into the data structure.
     public void addWord(String word) {
         // Write your code here
-        trie.insert(word);
     }
 
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
         // Write your code here
-        return dfs(trie, word);
     }
 
     public boolean dfs(Trie trie, String s) {
-        if (s == null || s.length() == 0) {
-            return false;
-        }
-        if (s.length() == 1) {
-            if (s.charAt(0) == '.') {
-                for (int i = 0; i < 26; ++i) {
-                    if (trie.children[i] != null && trie.isLeaf) {
-                        return true;
-                    }
-                }
-                return false;
-            } else {
-                return trie.children[s.charAt(0) - 'a'] != null && trie.isLeaf;
-            }
-        } else {
-            if (s.charAt(0) == '.') {
-                boolean flag = false;
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    flag = flag || dfs(trie.children[c - 'a'], s.substring(1));
-                }
-                return flag;
-            } else {
-                return dfs(trie.children[s.charAt(0) - 'a'], s.substring(1));
-            }
-        }
     }
 
 }
