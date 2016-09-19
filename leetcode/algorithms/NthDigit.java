29992d1933a58a0864a633cc752aca0ef14adee6
mitcc <mitcc@qq.com>
2016-09-19 23:57:48 +0800

    :basketball:

--- leetcode/algorithms/NthDigit.java
+++ leetcode/algorithms/NthDigit.java
@@ -1,8 +1,10 @@
 /*
- * Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+ * Find the nth digit of the infinite integer sequence 1, 2,
+ * 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
 
  * Note:
- * n is positive and will fit within the range of a 32-bit signed integer (n < 2^31).
+ * n is positive and will fit within the range of a 32-bit 
+ * signed integer (n < 2^31).
  * 
  * Example 1:
  * 
@@ -20,7 +22,8 @@
  * 0
  * 
  * Explanation:
- * The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+ * The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9,
+ * 10, 11, ... is a 0, which is part of the number 10.
  */
 public class NthDigit {
 
