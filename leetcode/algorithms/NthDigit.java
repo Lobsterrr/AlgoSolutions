28769707a27f291b64103f49d6e68bfc80d2e5f4
mitcc <mitcc@qq.com>
2016-09-22 00:40:01 +0800

    :basketball:

--- leetcode/algorithms/NthDigit.java
+++ leetcode/algorithms/NthDigit.java
@@ -36,8 +36,7 @@ public class NthDigit {
         for (; index < digitSum.length && digitSum[index] < n; ++index);
         int remainder = n - digitSum[index - 1] - 1;
         int num = remainder / index + (int) Math.pow(10, index - 1);
-        int tmpRemainder = digitSum[index - 1] + index * (num - (int) Math.pow(10, index - 1) + 1);
-        int diff = tmpRemainder - n;
+        int diff = index - (remainder % index + 1);
         for (; diff > 0; diff--) {
             num /= 10;
         }
