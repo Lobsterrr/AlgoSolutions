577de4ddb341f8a241807e2bdea8f7525ffc30c2
mitcc <mitcc@qq.com>
2016-09-27 02:46:36 +0800

    :basketball:

--- lintcode/CloneBinaryTree.java
+++ lintcode/CloneBinaryTree.java
@@ -1,8 +1,23 @@
 /*
  *
  */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class CloneBinaryTree {
 
+    /**
+     * @param root: The root of binary tree
+     * @return root of new tree
+     */
     public TreeNode cloneTree(TreeNode root) {
 
     }
