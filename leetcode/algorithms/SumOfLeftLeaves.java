874b5cfd40c4fee8276a982eb82484b16ee6ccde
mitcc <mitcc@qq.com>
2016-09-27 02:09:18 +0800

    :basketball:

--- leetcode/algorithms/SumOfLeftLeaves.java
+++ leetcode/algorithms/SumOfLeftLeaves.java
@@ -1,6 +1,15 @@
 /*
  *
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class SumOfLeftLeaves {
 
     public int sumOfLeftLeaves(TreeNode root) {
