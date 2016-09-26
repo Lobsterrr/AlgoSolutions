ea1c3cda724a033560faa1c34546ccd0219c284f
mitcc <mitcc@qq.com>
2016-09-27 02:46:54 +0800

    :basketball:

--- lintcode/CloneBinaryTree.java
+++ lintcode/CloneBinaryTree.java
@@ -1,5 +1,22 @@
 /*
- *
+ * For the given binary tree, return a deep copy of it.
+
+Have you met this question in a real interview? Yes
+Example
+Given a binary tree:
+
+     1
+   /  \
+  2    3
+ / \
+4   5
+return the new binary tree with same structure and same value:
+
+     1
+   /  \
+  2    3
+ / \
+4   5
  */
 /**
  * Definition of TreeNode:
