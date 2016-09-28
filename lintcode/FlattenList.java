3025422b20d48603d5ed7a7d78b9ba9fca7fb0da
mitcc <mitcc@qq.com>
2016-09-28 23:35:57 +0800

    :basketball:

---
+++ lintcode/FlattenList.java
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+public class FlattenList {
+
+    // @param nestedList a list of NestedInteger
+    // @return a list of integer
+    public List<Integer> flatten(List<NestedInteger> nestedList) {
+
+    }
+
+}
