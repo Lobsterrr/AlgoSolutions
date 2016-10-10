dc75f7258924c270bbec958b5158282871bbf579
mitcc <mitcc@qq.com>
2016-10-10 11:03:38 +0800

    :basketball:

--- leetcode/algorithms/AddString.java
+++ leetcode/algorithms/AddString.java
@@ -1,16 +1,44 @@
 /*
- *
+ * Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+
+ * Note:
+ * 
+ * The length of both num1 and num2 is < 5100.
+ * Both num1 and num2 contains only digits 0-9.
+ * Both num1 and num2 does not contain any leading zero.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
  */
 public class AddString {
+
+    public String addString(String num1, String num2) {
+        String result = "";
+        for (int i = num1.length() - 1, j = num2.length() - 1, carrier = 0; i >= 0 || j >= 0 || carrier > 0; --i, --j) {
+            carrier += i >= 0 ? num1.charAt(i) - '0' : 0;
+            carrier += j >= 0 ? num2.charAt(j) - '0' : 0;
+            result = (carrier % 10) + result;
+            carrier = carrier / 10;
+        }
+        return result;
+    }
 
     public String addString(String num1, String num2) {
         String result = "";
         int carrier = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-        while (i >= 0 || j >= 0) {
-
+        while (i >= 0 || j >= 0 || carrier > 0) {
+            if (i >= 0) {
+                carrier += num1.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                carrier += num2.charAt(j) - '0';
+            }
+            result = (carrier % 10) + result;
+            carrier = carrier / 10;
+            i--;
+            j--;
         }
+        return result;
     }
 
 }
