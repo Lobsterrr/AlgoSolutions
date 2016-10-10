39fb4113e4f63b2fa4f7d4b58120447aad4df1cb
mitcc <mitcc@qq.com>
2016-10-10 08:35:51 +0800

    :basketball:

---
+++ leetcode/algorithms/AddString.java
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+public class AddString {
+
+    public String addString(String num1, String num2) {
+        String result = "";
+        int carrier = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        while (i >= 0 || j >= 0) {
+
+        }
+    }
+
+}
