d5997371e420eeba85efda3d7d04677dc807c19c
mitcc <mitcc@qq.com>
2016-10-12 01:32:15 +0800

    :basketball:

--- leetcode/algorithms/PartitionEqualSubsetSum.java
+++ leetcode/algorithms/PartitionEqualSubsetSum.java
@@ -53,4 +53,46 @@ public class PartitionEqualSubsetSum {
         return false;
     }
 
+/*****************************************************************************/
+
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        Arrays.sort(nums);
+        return dfs(nums, nums.length, sum / 2);
+    }
+
+    public boolean dfs(int[] nums, int position, int sum) {
+        if (sum == 0) {
+            return true;
+        }
+        if (position == 0 && sum != 0) {
+            return false;
+        }
+        if (nums[position - 1] > 0) {
+            return dfs(nums, position - 1, sum);
+        }
+        return dfs(nums, position - 1, sum) || dfs(nums, position - 1, sum - nums[position - 1]);
+
+
+
+        for (int i = position; i < nums.length && sum > 0; ++i) {
+            if (i > position && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (dfs(nums, i + 1, sum - nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
