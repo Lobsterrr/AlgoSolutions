5a7647f73a51ebc43552ee0adc6571fd0b91c97e
mitcc <mitcc@qq.com>
2016-10-18 09:25:21 +0800

    :basketball:

--- leetcode/algorithms/ArithmeticSlices.java
+++ leetcode/algorithms/ArithmeticSlices.java
@@ -2,12 +2,13 @@ public class ArithmeticSlices {
 
     public int numberOfArithmeticSlices(int[] A) {
         int result = 0;
-        for (int i = 2, len = 0; i < A.length; ++i) {
+        for (int i = 2, len = 2; i < A.length; ++i) {
             if (A[i] == A[i - 1] && A[i - 1] == A[i - 2]) {
                 len++;
             }
             if (len > 2 && (A[i] != A[i - 1] || A[i] != A[i - 2] || i == A.length - 1)) {
                 result += (len - 1) * (len - 2) / 2;
+                len = 2;
             }
         }
         return result;
