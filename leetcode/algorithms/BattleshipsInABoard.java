939cfd70c8fa9379fd1a47b65b23d15d9b97dc52
mitcc <mitcc@qq.com>
2016-10-19 00:06:01 +0800

    :basketball:

--- leetcode/algorithms/BattleshipsInABoard.java
+++ leetcode/algorithms/BattleshipsInABoard.java
@@ -32,7 +32,8 @@ public class BattleshipsInABoard {
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
                 if (board[i][j] == 'X') {
-                    if ((i == board.length - 1 || board[i + 1][j] == '.') && (j == board[0].length - 1 || board[i][j + 1] == '.')) {
+                    if ((i == board.length - 1 || board[i + 1][j] == '.') 
+                            && (j == board[0].length - 1 || board[i][j + 1] == '.')) {
                         result++;
                     }
                 }
