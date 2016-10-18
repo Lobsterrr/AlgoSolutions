e9c054b8df582f7d497934fc7d303da08fa74362
mitcc <mitcc@qq.com>
2016-10-18 09:31:42 +0800

    :basketball:

--- leetcode/algorithms/ArithmeticSlices.java
+++ leetcode/algorithms/ArithmeticSlices.java
@@ -10,10 +10,12 @@
  * 
  * 1, 1, 2, 5, 7
  * 
- * A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+ * A zero-indexed array A consisting of N numbers is given. A slice of that 
+ * array is any pair of integers (P, Q) such that 0 <= P < Q < N.
  * 
  * A slice (P, Q) of array A is called arithmetic if the sequence:
- * A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+ * A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means
+ * that P + 1 < Q.
  * 
  * The function should return the number of arithmetic slices in the array A.
  * 
@@ -22,7 +24,8 @@
  * 
  * A = [1, 2, 3, 4]
  * 
- * return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+ * return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and 
+ * [1, 2, 3, 4] itself.
  */
 public class ArithmeticSlices {
 
