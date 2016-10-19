f9f52ff667e361a84e7bcba913d1dbeb01c539d8
mitcc <mitcc@qq.com>
2016-10-19 09:39:46 +0800

    :basketball:

--- leetcode/algorithms/TwoSumII.java
+++ leetcode/algorithms/TwoSumII.java
@@ -13,22 +13,8 @@ public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
         int start = 0;
         int end = numbers.length - 1;
-        while (start < numbers.length && end >= 0) {
-            if (numbers[start] + numbers[end] == target) {
-                return new int[] {start + 1, end + 1};
-            } else if (numbers[start] + numbers[end] < target) {
-                start++;
-            } else {
-                end--;
-            }
-        } 
-        return new int[2];
-    }
-
-    public int[] twoSum(int[] numbers, int target) {
-        int start = 0;
-        int end = numbers.length - 1;
-        while (start < numbers.length && end >= 0 && numbers[start] + numbers[end] != target) {
+        while (start < numbers.length && end >= 0 
+                && numbers[start] + numbers[end] != target) {
             if (numbers[start] + numbers[end] < target) {
                 start++;
             } else {
