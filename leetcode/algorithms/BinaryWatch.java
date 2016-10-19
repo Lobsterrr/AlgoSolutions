1b25a9024cc6f80a211f6e599a7f468a6e2b90e8
mitcc <mitcc@qq.com>
2016-10-19 22:32:31 +0800

    :basketball:

--- leetcode/algorithms/BinaryWatch.java
+++ leetcode/algorithms/BinaryWatch.java
@@ -26,8 +26,9 @@ public class BinaryWatch {
             return result;
         }
         int min = Math.max(0, num - 5);
-        for (int i = min; i <= 3; ++i) {
-            for (int j = 0; j <= 5; ++j) {
+        for (int h = min; h <= 3; ++h) {
+            int max = Math.max(0, num - h);
+            for (int m = 0; m <= max; ++m) {
 
             }
         }
