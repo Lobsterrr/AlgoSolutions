d009553d5385bf4ce5471c7fb133b02e102725cb
mitcc <mitcc@qq.com>
2016-10-21 11:17:18 +0800

    :basketball:

--- leetcode/algorithms/RemoveKDigits.java
+++ leetcode/algorithms/RemoveKDigits.java
@@ -48,4 +48,24 @@ public class RemoveKDigits {
         return result.length() == 0 ? "0" : result;
     }
 
+/*****************************************************************************/
+
+    public String removeKdigits(String num, int k) {
+        StringBuffer sb = new StringBuffer(num);
+        int i;
+        int j;
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < sb.length() - 1 
+                    && sb.charAt(j) <= sb.charAt(j + 1); j++);
+            sb.delete(j, j + 1);
+        }
+        while (sb.length() > 1 && sb.charAt(0)=='0') {
+            sb.delete(0,1);
+        }
+        if (sb.length() == 0) {
+            sb.append("0");
+        }
+        return sb.toString();
+    }
+
 }
