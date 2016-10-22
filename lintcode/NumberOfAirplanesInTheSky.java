54cf6189a2855d656a1ec685b0e044e65fb8957b
mitcc <mitcc@qq.com>
2016-10-22 22:34:07 +0800

    :basketball:

---
+++ lintcode/NumberOfAirplanesInTheSky.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+public class NumberOfAirplanesInTheSky {
+
+    /**
+     * @param intervals: An interval array
+     * @return: Count of airplanes are in the sky.
+     */
+    public int countOfAirplanes(List<Interval> airplanes) {
+
+    }
+
+}
