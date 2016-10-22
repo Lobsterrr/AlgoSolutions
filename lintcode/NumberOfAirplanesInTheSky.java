2cf372738ac89a435c2676860a99e4613f55702b
mitcc <mitcc@qq.com>
2016-10-22 22:34:29 +0800

    :basketball:

--- lintcode/NumberOfAirplanesInTheSky.java
+++ lintcode/NumberOfAirplanesInTheSky.java
@@ -1,5 +1,21 @@
 /*
- *
+ * Given an interval list which are flying and landing time of the flight. How many airplanes are on the sky at most?
+
+ Notice
+
+If landing and flying happens at the same time, we consider landing should happen at first.
+
+Have you met this question in a real interview? Yes
+Example
+For interval list
+
+[
+  [1,10],
+  [2,3],
+  [5,8],
+  [4,7]
+]
+Return 3
  */
 /**
  * Definition of Interval:
