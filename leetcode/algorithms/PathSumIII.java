1b04bac8693fb1c2378ec05540b32b28e0f9da71
mitcc <mitcc@qq.com>
2016-10-24 08:38:27 +0800

    :basketball:

--- leetcode/algorithms/PathSumIII.java
+++ leetcode/algorithms/PathSumIII.java
@@ -1,6 +1,15 @@
 /*
  *
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class PathSumIII {
 
     public int pathSum(TreeNode root, int sum) {
