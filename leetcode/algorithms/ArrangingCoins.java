b3de833122c4291dc196650e492ba2cf08ea662d
mitcc <mitcc@qq.com>
2016-11-05 15:35:56 +0800

    :basketball:

--- leetcode/algorithms/ArrangingCoins.java
+++ leetcode/algorithms/ArrangingCoins.java
@@ -1,9 +1,11 @@
 /*
- * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+ * You have a total of n coins that you want to form in a staircase shape, where
+ * every k-th row must have exactly k coins.
 
  * Given n, find the total number of full staircase rows that can be formed.
  * 
- * n is a non-negative integer and fits within the range of a 32-bit signed integer.
+ * n is a non-negative integer and fits within the range of a 32-bit signed 
+ * integer.
  * 
  * Example 1:
  * 
@@ -32,6 +34,8 @@ public class ArrangingCoins {
     public int arrangeCoins(int n) {
         return (int) ((Math.sqrt(1 + 8.0 * n) - 1) / 2);
     }
+
+/*****************************************************************************/
 
     public int arrangeCoins(int n) {
         int result = 0;
