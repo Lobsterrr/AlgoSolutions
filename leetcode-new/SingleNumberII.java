c0eca2f5994c681c958c8fffe283e88e601cdfcf
mitcc <mitcc@qq.com>
2016-11-10 02:15:06 +0800

    :basketball:

--- leetcode-new/SingleNumberII.java
+++ leetcode-new/SingleNumberII.java
@@ -20,6 +20,20 @@ public class SingleNumberII {
         return result;
     }
 
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+        int threes = 0;
+        for (int num : nums) {
+            twos |= ones & num;
+            ones ^= num;
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+
 /*****************************************************************************/
 
     public int singleNumber(int[] nums) {
