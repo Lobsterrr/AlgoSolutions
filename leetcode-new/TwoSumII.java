ea76e9cf123a5cd5b0f57cba64ceaa933927fcef
mitcc <mitcc@qq.com>
2016-11-09 21:15:18 +0800

    :basketball:

--- leetcode-new/TwoSumII.java
+++ leetcode-new/TwoSumII.java
@@ -1,7 +1,10 @@
 /*
- * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+ * Given an array of integers that is already sorted in ascending order, find 
+ * two numbers such that they add up to a specific target number.
 
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+The function twoSum should return indices of the two numbers such that they add
+up to the target, where index1 must be less than index2. Please note that your 
+returned answers (both index1 and index2) are not zero-based.
 
 You may assume that each input would have exactly one solution.
 
@@ -16,7 +19,7 @@ public class TwoSumII {
         while (i < j && numbers[i] + numbers[j] != target) {
             if (numbers[i] + numbers[j] < target) {
                 i++;
-            } else {
+            } else if (numbers[i] + numbers[j] > target) {
                 j--;
             }
         }
