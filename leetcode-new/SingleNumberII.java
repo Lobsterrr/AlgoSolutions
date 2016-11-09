b95a2ed286109d0eb4a1df1daf34efece7fa8b9e
mitcc <mitcc@qq.com>
2016-11-10 01:27:39 +0800

    :basketball:

--- leetcode-new/SingleNumberII.java
+++ leetcode-new/SingleNumberII.java
@@ -1,13 +1,24 @@
 /*
- * Given an array of integers, every element appears three times except for one. Find that single one.
+ * Given an array of integers, every element appears three times except for 
+ * one. Find that single one.
 
 Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Your algorithm should have a linear runtime complexity. Could you implement it 
+without using extra memory?
  */
 public class SingleNumberII {
 
     public int singleNumber(int[] nums) {
-
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            map.put(num, 1 + (map.containsKey(num) ? 1 : 0));
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 
 }
