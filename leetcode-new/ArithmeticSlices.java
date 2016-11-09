0c23e9b64efbf29bac861216923685f6f3d754e1
mitcc <mitcc@qq.com>
2016-11-09 15:45:16 +0800

    :basketball:

--- leetcode-new/ArithmeticSlices.java
+++ leetcode-new/ArithmeticSlices.java
@@ -7,6 +7,7 @@ public class ArithMeticSlices {
         if (A == null || A.length < 3) {
             return 0;
         }
+        int result = 0;
         for (int i = A.length - 3; i < A.length; ++i) {
             if (A[i] + A[i - 2] == 2 * A[i - 1]) {
                 result++;
