adecc0ebee44be5d3a02647b2419ec89e62c7236
mitcc <mitcc@qq.com>
2016-11-09 23:41:24 +0800

    :basketball:

--- leetcode-new/ReverseLinkedList.java
+++ leetcode-new/ReverseLinkedList.java
@@ -15,7 +15,18 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 public class ReverseLinkedList {
 
     public ListNode reverseList(ListNode head) {
-
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            prev.next = cur.next;
+            cur.next = head;
+            head = cur;
+            cur = prev.next;
+        }
+        return head;
     }
 
 }
