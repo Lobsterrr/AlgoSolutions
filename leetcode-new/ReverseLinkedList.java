5065d6fdc671157292abe7183628664613fbb259
mitcc <mitcc@qq.com>
2016-11-09 23:37:09 +0800

    :basketball:

---
+++ leetcode-new/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+/*
+ * Reverse a singly linked list.
+
+Hint:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+
+    }
+
+}
