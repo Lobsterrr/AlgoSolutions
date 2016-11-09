b2b6da6fcfb00d2b80e63012841a361883b8043a
mitcc <mitcc@qq.com>
2016-11-09 22:22:58 +0800

    :basketball:

--- leetcode-new/SameTree.java
+++ leetcode-new/SameTree.java
@@ -18,7 +18,8 @@ public class SameTree {
         if (p == null || q == null) {
             return p == null && q == null;
         }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return p.val == q.val && isSameTree(p.left, q.left) 
+            && isSameTree(p.right, q.right);
     }
 
 }
