fdfb0bcd0ba6b9d5cc941106380e0750c8e3e86d
mitcc <mitcc@qq.com>
2016-11-09 15:11:57 +0800

    :basketball:

--- leetcode/algorithms/SumOfTwoIntegers.java
+++ leetcode/algorithms/SumOfTwoIntegers.java
@@ -17,10 +17,7 @@ public class SumOfTwoIntegers {
     }
 
     public int getSum(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return getSum(a ^ b, (a & b) << 1);
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
     }
 
 }
