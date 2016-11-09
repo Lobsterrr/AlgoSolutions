096564d3b2707a6909d4d3426111483f1f834e39
mitcc <mitcc@qq.com>
2016-11-09 13:02:10 +0800

    :basketball:

--- leetcode-new/CountingBits.java
+++ leetcode-new/CountingBits.java
@@ -5,7 +5,7 @@ public class CountingBits {
 
     public int[] countBits(int num) {
         int[] result = new int[num + 1];
-        for (int i = 1; i <= num; ++i) {
+        for (int i = 0; i <= num; ++i) {
             result[i] = result[i / 2] + (i % 2 != 0 ? 1 : 0);
         }
         return result;
