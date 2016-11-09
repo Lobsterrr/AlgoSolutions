9bd91a7b0c9f2bb98d090742fddb0cbbb195ee67
mitcc <mitcc@qq.com>
2016-11-09 20:43:21 +0800

    :basketball:

--- leetcode-new/SingleNumberIII.java
+++ leetcode-new/SingleNumberIII.java
@@ -16,7 +16,20 @@ public class SingleNumberIII {
         for (int num : nums) {
             xor ^= num;
         }
-
+        int mask = 1;
+        while (mask & xor == 0) {
+            mask <<= 1;
+        }
+        int a = 0;
+        int b = 0;
+        for (int num : nums) {
+            if (num & mask == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return new int[2] {a, b};
     }
 
 }
