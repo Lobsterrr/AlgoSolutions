b3dc9f497855138518d7defb1d727ece8be956bf
mitcc <mitcc@qq.com>
2016-11-10 16:45:23 +0800

    :basketball:

--- leetcode-new/PascalTriangleII.java
+++ leetcode-new/PascalTriangleII.java
@@ -11,9 +11,6 @@ public class PascalTriangleII {
 
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<Integer>();
-        if (rowIndex < 0) {
-            return result;
-        }
         for (int k = 0, nCk = 1; k <= rowIndex; ++k) {
             result.add(nCk);
             nCk = (int) ((long) nCk * (rowIndex - k) / (k + 1)); 
