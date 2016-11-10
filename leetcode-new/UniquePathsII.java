90bca14e34992057b5b3438183fd76d95928028f
mitcc <mitcc@qq.com>
2016-11-10 13:20:25 +0800

    :basketball:

--- leetcode-new/UniquePathsII.java
+++ leetcode-new/UniquePathsII.java
@@ -34,12 +34,12 @@ public class UniquePathsII {
                 } else {
                     if (i == 0 && j == 0) {
                         dp[i][j] = 1;
-                    } else if (i == 0 && j > 0) {
-                        dp[i][j] += dp[i][j - 1];
-                    } else if (i > 0 && j == 0) {
+                    }
+                    if (i > 0) {
                         dp[i][j] += dp[i - 1][j];
-                    } else {
-                        dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
                     }
                 }
             }
