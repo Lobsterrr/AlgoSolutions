f1487f51e1af8ec330f2a967ab785cc1fcbd9ff9
mitcc <mitcc@qq.com>
2016-11-10 23:34:08 +0800

    :basketball:

--- leetcode-new/PlusOne.java
+++ leetcode-new/PlusOne.java
@@ -8,6 +8,18 @@ the list.
 public class PlusOne {
 
     public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        for (; i >= 0 && digits[i] == 9; --i) {
+            digits[i] = 0;
+        }
+        if (i < 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+        return digits;
+    }
+
+    public int[] plusOne(int[] digits) {
         int i = 0;
         for (; i < digits.length; ++i) {
             if (digits[i] != 9) {
