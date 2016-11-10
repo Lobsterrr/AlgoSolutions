059cf77602d2416ae03825dd6b3f16ddea0b262e
mitcc <mitcc@qq.com>
2016-11-10 23:46:25 +0800

    :basketball:

--- leetcode-new/BalancedBinaryTree.java
+++ leetcode-new/BalancedBinaryTree.java
@@ -16,7 +16,18 @@ which the depth of the two subtrees of every node never differ by more than 1.
 public class BalancedBinaryTree {
 
     public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 
+            && isBalanced(root.left) && isBalanced(root.right);
+    }
 
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(depth(root.left), depth(root.right));
     }
 
 }
