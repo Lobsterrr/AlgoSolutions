f2727263c7d965bcfbf9b38daede9118ea3bc471
mitcc <mitcc@qq.com>
2016-11-10 13:29:09 +0800

    :basketball:

--- leetcode-new/UniquePaths.java
+++ leetcode-new/UniquePaths.java
@@ -22,10 +22,8 @@ public class UniquePaths {
         int[] dp = new int[n];
         dp[0] = 1;
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (j > 0) {
-                    dp[j] += dp[j - 1];
-                }
+            for (int j = 1; j < n; ++j) {
+                dp[j] += dp[j - 1];
             }
         }
         return dp[n - 1];
