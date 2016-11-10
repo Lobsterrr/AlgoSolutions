14a2e73d84a2ef21bb499ecfbda332ab5db33e91
mitcc <mitcc@qq.com>
2016-11-10 11:59:13 +0800

    :basketball:

--- leetcode-new/FirstUniqueCharacterInAString.java
+++ leetcode-new/FirstUniqueCharacterInAString.java
@@ -1,5 +1,6 @@
 /*
-Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+Given a string, find the first non-repeating character in it and return it's 
+index. If it doesn't exist, return -1.
 
 Examples:
 
@@ -13,6 +14,9 @@ Note: You may assume the string contain only lowercase letters.
 public class FirstUniqueCharacterInAString {
 
     public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
         int[] hash = new int[256];
         for (int i = 0; i < s.length(); ++i) {
             hash[s.charAt(i)]++;
