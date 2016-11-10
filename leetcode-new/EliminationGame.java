84c3ff1a3da16c17b1f6d004fdfb19c24bde065f
mitcc <mitcc@qq.com>
2016-11-10 10:55:05 +0800

    :basketball:

--- leetcode-new/EliminationGame.java
+++ leetcode-new/EliminationGame.java
@@ -41,4 +41,11 @@ public class EliminationGame {
         return head;
     }
 
+    public int lastRemaining(int n) {
+        if (n == 1) {
+            return n;
+        }
+        return 2 * (n / 2 + 1 - lastRemaining(n / 2));
+    }
+
 }
