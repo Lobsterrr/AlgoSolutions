79f96cdf4694cd18f6986c62e18c524898301bd4
mitcc <mitcc@qq.com>
2016-11-12 01:41:28 +0800

    :basketball:

--- leetcode-new/IntersectionOfTwoArraysII.java
+++ leetcode-new/IntersectionOfTwoArraysII.java
@@ -22,14 +22,15 @@ public class IntersectionOfTwoArraysII {
         if (nums1 == null || nums2 == null) {
             return null;
         }
-        Set<Integer> set = new HashSet<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums1) {
-            set.add(num);
+            map.put(num, 1 + (map.containsKey(num) ? map.get(num) : 0));
         }
         List<Integer> list = new ArrayList<Integer>();
         for (int num : nums2) {
-            if (set.contains(num)) {
+            if (map.get(num) != null && map.get(num) > 0) {
                 list.add(num);
+                map.put(num, map.get(num) - 1);
             }
         }
         int[] result = new int[list.size()];
