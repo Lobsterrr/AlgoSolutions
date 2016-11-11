8873501aecb0b6aa458d1686832a259745e66112
mitcc <mitcc@qq.com>
2016-11-12 00:17:47 +0800

    :basketball:

--- leetcode-new/LinkedListRandomNode.java
+++ leetcode-new/LinkedListRandomNode.java
@@ -1,8 +1,10 @@
 /*
-Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+Given a singly linked list, return a random node's value from the linked list. 
+Each node must have the same probability of being chosen.
 
 Follow up:
-What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+What if the linked list is extremely large and its length is unknown to you? 
+Could you solve this efficiently without using extra space?
 
 Example:
 
@@ -12,7 +14,8 @@ head.next = new ListNode(2);
 head.next.next = new ListNode(3);
 Solution solution = new Solution(head);
 
-// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+// getRandom() should return either 1, 2, or 3 randomly. Each element should 
+// have equal probability of returning.
 solution.getRandom();
  */
 public class LinkedListRandomNode {
