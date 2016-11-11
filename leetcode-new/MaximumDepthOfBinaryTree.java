26946972ee00cedb75522455619a55ee2acc4641
mitcc <mitcc@qq.com>
2016-11-11 23:42:49 +0800

    :basketball:

--- leetcode-new/MaximumDepthOfBinaryTree.java
+++ leetcode-new/MaximumDepthOfBinaryTree.java
@@ -22,4 +22,10 @@ public class MaximumDepthOfBinaryTree {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+    }
+
 }
