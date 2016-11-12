b0de24d08edb191b62a7766f22d0b4e110507dfa
mitcc <mitcc@qq.com>
2016-11-12 23:36:09 +0800

    :basketball:

--- leetcode-new/BestTimeToBuyAndSellStockII.java
+++ leetcode-new/BestTimeToBuyAndSellStockII.java
@@ -6,7 +6,16 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 public class BestTimeToBuyAndSellStockII {
 
     public int maxProfit(int[] prices) {
-
+        if (prices == null) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            if (prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
     }
 
 }
