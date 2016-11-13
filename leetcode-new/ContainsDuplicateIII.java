0fdbc3e7ea7d6dcf6b453ad8d5551412d4e7b7ad
mitcc <mitcc@qq.com>
2016-11-13 22:32:13 +0800

    :basketball:

--- leetcode-new/ContainsDuplicateIII.java
+++ leetcode-new/ContainsDuplicateIII.java
@@ -6,7 +6,21 @@ most t and the difference between i and j is at most k.
 public class ContainsDuplicateIII {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.size() > 0 && nums[i] - set.first() <= t) {
+                return true;
+            } else {
+                set.add(nums[i]);
+                if (set.size() > k) {
+                    set.remove(nums[i - k]);
+                }
+            }
+        }
+        return false;
     }
 
 }
