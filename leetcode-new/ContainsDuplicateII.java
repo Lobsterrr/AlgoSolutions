ad6b65ca474f79436c35c655044dc9af9c7318e9
mitcc <mitcc@qq.com>
2016-11-13 19:29:06 +0800

    :basketball:

--- leetcode-new/ContainsDuplicateII.java
+++ leetcode-new/ContainsDuplicateII.java
@@ -27,13 +27,10 @@ public class ContainsDuplicateII {
         }
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; ++i) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            } else {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
             }
+            map.put(nums[i], i);
         }
         return false;
     }
