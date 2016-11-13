c3b8c70bb53120a109713e9ab69d6058c016a62d
mitcc <mitcc@qq.com>
2016-11-13 19:02:26 +0800

    :basketball:

--- leetcode-new/ContainsDuplicate.java
+++ leetcode-new/ContainsDuplicate.java
@@ -10,10 +10,10 @@ public class ContainsDuplicate {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] == nums[i - 1]) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
 }
