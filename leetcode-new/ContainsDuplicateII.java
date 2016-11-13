d9a42424a5e92eff063faf69675bc757eff68025
mitcc <mitcc@qq.com>
2016-11-13 19:25:52 +0800

    :basketball:

--- leetcode-new/ContainsDuplicateII.java
+++ leetcode-new/ContainsDuplicateII.java
@@ -21,4 +21,21 @@ public class ContainsDuplicateII {
         return false;
     }
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
