adfb626e04b66e36a81004235b5c0e19b3b7c45b
mitcc <mitcc@qq.com>
2016-11-14 22:05:44 +0800

    :basketball:

--- leetcode-new/AddBinary.java
+++ leetcode-new/AddBinary.java
@@ -9,7 +9,20 @@ Return "100".
 public class AddBinary {
 
     public String addBinary(String a, String b) {
-
+        String result = "";
+        for (int i = 0, carrier = 0; i <= Math.max(a.length(), b.length()); ++i) {
+            int va = 0;
+            int vb = 0;
+            if (i < a.length()) {
+                va = a.charAt(a.length() - 1 - i) - '0';
+            }
+            if (i < b.length()) {
+                vb = b.charAt(b.length() - 1 - i) - '0';
+            }
+            result = ((va + vb + carrier) % 2) + result;
+            carrier = (va + vb + carrier) / 2;
+        }
+        return result;
     }
 
 }
