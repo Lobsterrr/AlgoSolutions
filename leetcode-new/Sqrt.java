34f020d8ff3c153bb7038bdc8350a2259ab4612a
mitcc <mitcc@qq.com>
2016-11-14 21:16:48 +0800

    :basketball:

--- leetcode-new/Sqrt.java
+++ leetcode-new/Sqrt.java
@@ -6,6 +6,14 @@ Compute and return the square root of x.
 public class Sqrt {
 
     public int mySqrt(int x) {
+        int sqrt = x;
+        while ((long) sqrt * sqrt > x) {
+            sqrt = (sqrt + x / sqrt) / 2;
+        }
+        return sqrt;
+    }
+
+    public int mySqrt(int x) {
         double sqrt = x;
         while (true) {
             double y = (sqrt + x / sqrt) / 2;
