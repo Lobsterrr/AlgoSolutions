1b22f62523ea12112d0c78d46a583e7dff1a5965
mitcc <mitcc@qq.com>
2016-11-14 22:24:44 +0800

    :basketball:

--- leetcode-new/AddTwoNumbers.java
+++ leetcode-new/AddTwoNumbers.java
@@ -1,5 +1,7 @@
 /*
-You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two linked lists representing two non-negative numbers. The 
+digits are stored in reverse order and each of their nodes contain a single 
+digit. Add the two numbers and return it as a linked list.
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
@@ -23,14 +25,14 @@ public class AddTwoNumbers {
         while (cur1 != null || cur2 != null) {
             int value1 = cur1 != null ? cur1.val : 0;
             int value2 = cur2 != null ? cur2.val : 0;
-            cur.next = new ListNode((value1 + value2 + carrier) % 10);
-            cur = cur.next;
             if (cur1 != null) {
                 cur1 = cur1.next;
             }
             if (cur2 != null) {
                 cur2 = cur2.next;
             }
+            cur.next = new ListNode((value1 + value2 + carrier) % 10);
+            cur = cur.next;
             carrier = (value1 + value2 + carrier) / 10;
         }
         if (carrier != 0) {
