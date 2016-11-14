d51602b0f129f1be32bc8ea2272448e9a61d4870
mitcc <mitcc@qq.com>
2016-11-14 21:35:31 +0800

    :basketball:

--- leetcode-new/ValidPerfectSquare.java
+++ leetcode-new/ValidPerfectSquare.java
@@ -15,7 +15,22 @@ Returns: False
 public class ValidPerfectSquare {
 
     public boolean isPerfectSquare(int num) {
+        int sqrt = mySqrt(num);
+        return sqrt * sqrt == num;
+    }
 
+    public int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid <= x / mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low - 1;
     }
 
 }
