fc85b246a930c4b70c275b22dfaee5682ff26591
mitcc <mitcc@qq.com>
2016-11-14 09:15:12 +0800

    :basketball:

--- leetcode-new/Pow.java
+++ leetcode-new/Pow.java
@@ -4,7 +4,16 @@ Implement pow(x, n).
 public class Pow {
 
     public double myPow(double x, int n) {
-
+        double half = myPow(x, n / 2);
+        result = half * half;
+        if (n % 2 != 0) {
+            if (n > 0) {
+                result *= x;
+            } else {
+                result /= x;
+            }
+        }
+        return result;
     }
 
 }
