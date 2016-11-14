2afc8f8bc6a8b62830de78e8e59dc6188fde8263
mitcc <mitcc@qq.com>
2016-11-14 21:11:36 +0800

    :basketball:

--- leetcode-new/Sqrt.java
+++ leetcode-new/Sqrt.java
@@ -8,10 +8,11 @@ public class Sqrt {
     public int mySqrt(int x) {
         double sqrt = x;
         while (true) {
-            if ((int) sqrt == (int) (1 + sqrt) / 2) {
-                return (int) sqrt;
+            double y = (1.0 + x / sqrt) / 2;
+            if ((int) sqrt == (int) y) {
+                return true;
             }
-            sqrt = (1.0 + sqrt) / 2;
+            sqrt = y;
         }
         return (int) sqrt;
     }
