3d00a940dba07a9b1d71c2ac175f211b37d5d3b2
mitcc <mitcc@qq.com>
2016-11-16 11:03:15 +0800

    :basketball:

--- leetcode-new/BinaryTreeInorderTraversal.java
+++ leetcode-new/BinaryTreeInorderTraversal.java
@@ -40,4 +40,17 @@ public class BinaryTreeInorderTraversal {
         return result;
     }
 
+/*****************************************************************************/
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
+    }
+
 }
