551b9cb51b815bed54d68854233be8de800b8fd6
mitcc <mitcc@qq.com>
2016-11-18 01:34:27 +0800

    :basketball:

--- leetcode-new/UniqueBinarySearchTrees.java
+++ leetcode-new/UniqueBinarySearchTrees.java
@@ -15,8 +15,8 @@ public class UniqueBinarySearchTrees {
 
     public int numTrees(int n) {
         int result = 1;
-        for (int i = 1; i <= n; ++i) {
-            result = result * 2 * (2 * i - 1) / (i + 1);
+        for (int i = 0; i < n; ++i) {
+            result = result * 2 * (2 * i + 1) / (i + 2);
         }
         return result;
     }
