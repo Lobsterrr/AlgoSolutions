1ba0fc654ca3eaf6e1c90cba12f7c3fd91293217
mitcc <mitcc@qq.com>
2016-11-18 22:36:42 +0800

    :basketball:

--- leetcode-new/BinaryTreePostorderTraversal.java
+++ leetcode-new/BinaryTreePostorderTraversal.java
@@ -28,7 +28,19 @@ public class BinaryTreePostorderTraversal {
         if (root == null) {
             return result;
         }
-
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            result.add(cur.val);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        Collections.reverse(result);
         return result;
     }
 
