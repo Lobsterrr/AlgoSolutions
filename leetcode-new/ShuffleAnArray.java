501e769f1f8b0953a9f8c6f8aaa8ce4d540effa8
mitcc <mitcc@qq.com>
2016-11-19 22:19:23 +0800

    :basketball:

--- leetcode-new/ShuffleAnArray.java
+++ leetcode-new/ShuffleAnArray.java
@@ -26,12 +26,21 @@ class Solution {
 
     }
     
+    /** Resets the array to its original configuration and return it. */
     public int[] reset() {
 
     }
 
+    /** Returns a random shuffling of the array. */
     public int[] shuffle() {
 
     }
 
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
