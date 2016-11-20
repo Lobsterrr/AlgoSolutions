62043d9462d8486ea6a37e3e078c30356c5f9dc2
mitcc <mitcc@qq.com>
2016-11-20 23:14:33 +0800

    :basketball:

--- leetcode-new/TopKFrequentElements.java
+++ leetcode-new/TopKFrequentElements.java
@@ -13,7 +13,7 @@ public class TopKFrequentElements {
 
     public List<Integer> topKFrequent(int[] nums, int k) {
         List<Integer> result = new ArrayList<Integer>();
-        Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums) {
             map.put(num, 1 + (map.containsKey(num) ? map.get(num) : 0));
         }
