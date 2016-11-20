e8868ec2698eb65a01c1d42d98415ac7a5ffe456
mitcc <mitcc@qq.com>
2016-11-20 20:15:23 +0800

    :basketball:

--- leetcode-new/GenerateParentheses.java
+++ leetcode-new/GenerateParentheses.java
@@ -41,8 +41,8 @@ public class GenerateParentheses {
             result.add("()");
         } else if (n > 1) {
             for (int i = 0; i < n; ++i) {
-                for (String inner : generateParenthesis(i)) {
-                    for (String outer : generateParenthesis(n - i - 1)) {
+                for (String outer : generateParenthesis(i)) {
+                    for (String inner : generateParenthesis(n - i - 1)) {
                         result.add("(" + inner + ")" + outer);
                     }
                 }
