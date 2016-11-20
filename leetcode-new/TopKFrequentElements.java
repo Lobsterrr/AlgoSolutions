a94fdd4fb521b4a0ef719d7c6e8cd6ee9288473a
mitcc <mitcc@qq.com>
2016-11-20 23:11:48 +0800

    :basketball:

--- leetcode-new/TopKFrequentElements.java
+++ leetcode-new/TopKFrequentElements.java
@@ -20,7 +20,7 @@ public class TopKFrequentElements {
         List<Map.Entry<Integer, Integer>> list = new LinkedList<Map.Entry<Integer, Integer>>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
+                return (o2.getValue()).compareTo(o1.getValue());
             }
         });
         for (int i = 0; i < k; ++i) {
