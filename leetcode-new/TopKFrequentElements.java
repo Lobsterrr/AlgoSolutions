a9dccc41b736c9b179c55702c0ed22ff9179b027
mitcc <mitcc@qq.com>
2016-11-20 23:38:58 +0800

    :basketball:

--- leetcode-new/TopKFrequentElements.java
+++ leetcode-new/TopKFrequentElements.java
@@ -65,7 +65,7 @@ public class TopKFrequentElements {
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<Map.Entry<Integer, Integer>>(k, new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o2.getValue() - o1.getValue();
+                return o1.getValue() - o2.getValue();
             }
         });
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
