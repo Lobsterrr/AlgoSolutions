e0da38e21bf123ea16a428b2897a35334a79d6c5
mitcc <mitcc@qq.com>
2016-11-20 15:20:38 +0800

    :basketball:

---
+++ leetcode-new/IsSubsequence.java
@@ -0,0 +1,19 @@
+/*
+ *
+ */
+public class IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        for (int j = 0; j < t.length(); ++j) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                if (i == s.length()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
