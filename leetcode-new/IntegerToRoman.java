f4eb1be7f359513b748f8a10ad1d6ae7c4a21828
mitcc <mitcc@qq.com>
2016-11-23 01:17:05 +0800

    :basketball:

--- leetcode-new/IntegerToRoman.java
+++ leetcode-new/IntegerToRoman.java
@@ -6,6 +6,14 @@ Input is guaranteed to be within the range from 1 to 3999.
 public class IntegerToRoman {
 
     public String intToRoman(int num) {
+        Map<Integer, Character> map = new HashMap<Integer, Character>();
+        map.put(1, 'I');
+        map.put(5, 'V');
+        map.put(10, 'X');
+        map.put(50, 'L');
+        map.put(100, 'C');
+        map.put(500, 'D');
+        map.put(1000, 'M');
         String reslt = "";
 
         return result;
