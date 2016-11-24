688add0fff0fccb2b79e890058e7b015ee40ffdb
mitcc <mitcc@qq.com>
2016-11-24 10:01:14 +0800

    :basketball:

--- leetcode-new/IntegerToRoman.java
+++ leetcode-new/IntegerToRoman.java
@@ -11,7 +11,7 @@ public class IntegerToRoman {
         String[] tens = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] hundreds = {"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String[] thousands = {"M", "MM", "MMM", "", "", "", "", "", ""};
-
+        result = thousands[num / 1000 % 10] + hundreds[num / 100 % 10] + tens[num / 10 % 10] + ones[num % 10];
         return result;
     }
 
