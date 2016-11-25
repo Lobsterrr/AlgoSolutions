6eaf71defc17a1c76ab9b8f49c0aed0fbc68a50e
mitcc <mitcc@qq.com>
2016-11-25 13:41:34 +0800

    :basketball:

--- leetcode/leetcode/BinaryWatch.java
+++ leetcode/leetcode/BinaryWatch.java
@@ -19,6 +19,8 @@ The minute must be consist of two digits and may contain a leading zero, for exa
  */
 public class BinaryWatch {
 
+    public List<String> readBinaryWatch(int num) {
 
+    }
 
 }
