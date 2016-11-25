0695b8b2c5fde3d56481090d89e28def5e37b71e
mitcc <mitcc@qq.com>
2016-11-25 21:03:59 +0800

    :basketball:

--- leetcode/leetcode/BinaryWatch.java
+++ leetcode/leetcode/BinaryWatch.java
@@ -34,12 +34,12 @@ public class BinaryWatch {
         return result;
     }
 
-    private hammingWeight(int n) {
-        n = (n & 0x55555555) & ((n & 0xAAAAAAAA) >>> 1);
-        n = (n & 0x33333333) & ((n & 0xCCCCCCCC) >>> 2);
-        n = (n & 0x0F0F0F0F) & ((n & 0xF0F0F0F0) >>> 4);
-        n = (n & 0x00FF00FF) & ((n & 0xFF00FF00) >>> 8);
-        n = (n & 0x0000FFFF) & ((n & 0xFFFF0000) >>> 16);
+    private int hammingWeight(int n) {
+        n = (n & 0x55555555) | ((n & 0xAAAAAAAA) >>> 1);
+        n = (n & 0x33333333) | ((n & 0xCCCCCCCC) >>> 2);
+        n = (n & 0x0F0F0F0F) | ((n & 0xF0F0F0F0) >>> 4);
+        n = (n & 0x00FF00FF) | ((n & 0xFF00FF00) >>> 8);
+        n = (n & 0x0000FFFF) | ((n & 0xFFFF0000) >>> 16);
         return n;
     }
 
