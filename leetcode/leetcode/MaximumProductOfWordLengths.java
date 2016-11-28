4a4fe90673325dc8acfeee094ed15272ce7e2c0d
mitcc <mitcc@qq.com>
2016-11-28 22:29:14 +0800

    :basketball:

--- leetcode/leetcode/MaximumProductOfWordLengths.java
+++ leetcode/leetcode/MaximumProductOfWordLengths.java
@@ -21,4 +21,20 @@ No such pair of words.
  */
 public class MaximumProductOfWordLengths {
 
+    public int maxProduct(String[] words) {
+        int result = 0;
+        int[] mask = new int[words.length];
+        for (int i = 0; i < words.length; ++i) {
+            for (int j = 0; j < words[i].length(); ++j) {
+                mask[i] |= 1 << (words[i].charAt(j) - 'a');
+            }
+            for (int j = 0; j < i; ++j) {
+                if (mask[i] & mask[j] != 0) {
+                    result = Math.max(result, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return result;
+    }
+
 }
