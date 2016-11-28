7be34df721488633c18991c6d23796d4b2211565
mitcc <mitcc@qq.com>
2016-11-28 21:33:21 +0800

    :basketball:

--- leetcode/leetcode/MinimumMovesToEqualArrayElements.java
+++ leetcode/leetcode/MinimumMovesToEqualArrayElements.java
@@ -1,5 +1,7 @@
 /*
-Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+Given a non-empty integer array of size n, find the minimum number of moves 
+required to make all array elements equal, where a move is incrementing n - 1 
+elements by 1.
 
 Example:
 
@@ -17,8 +19,17 @@ Only three moves are needed (remember each move increments two elements):
 
 public class MinimumMovesToEqualArrayElements {
 
-    public int minMoves(int[] nums) {
 
+    public int minMoves(int[] nums) {
+        int result = 0;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        for (int num : nums) {
+            result += max - num;
+        }
+        return result;
     }
 
 }
