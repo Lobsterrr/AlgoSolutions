5088af0c9afcafca633e98481f3838a7acea2610
mitcc <mitcc@qq.com>
2016-11-28 22:14:21 +0800

    :basketball:

--- leetcode/leetcode/MissingNumber.java
+++ leetcode/leetcode/MissingNumber.java
@@ -13,7 +13,11 @@ using only constant extra space complexity?
 public class MissingNumber {
 
     public int missingNumber(int[] nums) {
-
+        int xor = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            xor ^= (i + 1) ^ nums[i];
+        }
+        return xor;
     }
 
 /*****************************************************************************/
