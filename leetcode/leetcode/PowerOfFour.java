81352e8618be37a3fc50558a805d968e9cbbe114
mitcc <mitcc@qq.com>
2016-11-29 00:31:58 +0800

    :basketball:

--- leetcode/leetcode/PowerOfFour.java
+++ leetcode/leetcode/PowerOfFour.java
@@ -10,6 +10,12 @@ Follow up: Could you solve it without loops/recursion?
 public class PowerOfFour {
 
     public boolean isPowerOfFour(int num) {
+        return (num & 0x55555555) != 0 && (num & (num - 1)) == 0;
+    }
+
+/*****************************************************************************/
+
+    public boolean isPowerOfFour(int num) {
         while (num > 1) {
             if (num % 4 != 0) {
                 return false;
