9eb3f5f30949a098bfc13635d4577e81109d80b1
mitcc <mitcc@qq.com>
2016-11-30 00:01:22 +0800

    :basketball:

--- leetcode/leetcode/ReverseInteger.java
+++ leetcode/leetcode/ReverseInteger.java
@@ -27,12 +27,13 @@ public class ReverseInteger {
 
     public int reverse(int x) {
         long reverse = 0;
-        while (x != 0) {
+        for (;; x /= 10) {
             reverse = 10 * reverse + x % 10;
-            x /= 10;
+            if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+                return 0;
+            }
         }
-        return reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE 
-            ? 0 : (int) reverse;
+        return (int) reverse;
     }
 
 }
