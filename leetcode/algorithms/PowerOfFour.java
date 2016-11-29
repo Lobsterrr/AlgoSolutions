2741d38c2d01502d7e487ac4b153ff9027ae8955
mitcc <mitcc@qq.com>
2016-11-29 08:52:59 +0800

    :basketball:

--- leetcode/algorithms/PowerOfFour.java
+++ leetcode/algorithms/PowerOfFour.java
@@ -16,7 +16,7 @@ public class PowerOfFour {
 /*****************************************************************************/
 
     public boolean isPowerOfFour(int num) {
-        return (num & 0x55555555) != 0 && (num & (num - 1)) == 0;
+        return Integer.toString(num, 4).matches("10*");
     }
 
 }
