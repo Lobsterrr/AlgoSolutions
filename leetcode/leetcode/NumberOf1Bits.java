caeaa19e12e3e46abace4b6b74c94169316fe4b6
mitcc <mitcc@qq.com>
2016-11-29 09:33:13 +0800

    :basketball:

--- leetcode/leetcode/NumberOf1Bits.java
+++ leetcode/leetcode/NumberOf1Bits.java
@@ -8,7 +8,12 @@ For example, the 32-bit integer ’11' has binary representation
 public class NumberOf1Bits {
 
     public int hammingWeight(int n) {
-
+        n = (n & 0x55555555) + ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >>> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >>> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >>> 16) & 0x0000ffff);
+        return n;
     }
 
 }
