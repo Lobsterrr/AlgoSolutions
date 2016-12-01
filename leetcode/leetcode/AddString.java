8e24cd0750f743851a78caf0191df53021ef5f2d
mitcc <mitcc@qq.com>
2016-12-01 11:49:16 +0800

    :basketball:

--- leetcode/leetcode/AddString.java
+++ leetcode/leetcode/AddString.java
@@ -13,7 +13,17 @@ integer directly.
 public class AddString {
 
     public String addStrings(String num1, String num2) {
-
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carrier = 0;
+        String result = "";
+        while (i >= 0 || j >= 0 || carrier > 0) {
+            int value1 = i >= 0 ? (num1.charAt(i--) - '0') : 0;
+            int value2 = j >= 0 ? (num2.charAt(j--) - '0') : 0;
+            result = (value1 + value2 + carrier) % 10 + result;
+            carrier = (value1 + value2 + carrier) / 10;
+        }
+        return result;
     }
 
 }
