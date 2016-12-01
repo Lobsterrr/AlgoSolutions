ced1b747d1b692142cd5af9bd176f43199dce81f
mitcc <mitcc@qq.com>
2016-12-01 12:50:01 +0800

    :basketball:

--- leetcode/leetcode/AddTwoNumbers.java
+++ leetcode/leetcode/AddTwoNumbers.java
@@ -22,6 +22,29 @@ public class AddTwoNumbers {
         ListNode cur1 = l1;
         ListNode cur2 = l2;
         int carrier = 0;
+        while (cur1 != null || cur2 != null || carrier > 0) {
+            carrier += cur1 != null ? cur1.val : 0;
+            carrier += cur2 != null ? cur2.val : 0;
+            if (cur1 != null) {
+                cur1 = cur1.next;
+            }
+            if (cur2 != null) {
+                cur2 = cur2.next;
+            }
+            cur.next = new ListNode(carrier % 10);
+            cur = cur.next;
+            carrier /= 10;
+        }
+        return dummy.next;
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        int carrier = 0;
         while (cur1 != null || cur2 != null) {
             int value1 = cur1 != null ? cur1.val : 0;
             int value2 = cur2 != null ? cur2.val : 0;
