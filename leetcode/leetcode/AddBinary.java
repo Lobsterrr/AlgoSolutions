f56b20579ecd9c7aeeeda7fa91ad3e81c647a8c0
mitcc <mitcc@qq.com>
2016-12-01 12:43:44 +0800

    :basketball:

--- leetcode/leetcode/AddBinary.java
+++ leetcode/leetcode/AddBinary.java
@@ -10,7 +10,7 @@ public class AddBinary {
 
     public String addBinary(String a, String b) {
         String result = "";
-        for (int i = a.length() - 1, j = b.length() - 1, carrier = 0; a >= 0 || b >= 0 || carrier > 0; --i, --j, carrier /= 2) {
+        for (int i = a.length() - 1, j = b.length() - 1, carrier = 0; i >= 0 || j >= 0 || carrier > 0; --i, --j, carrier /= 2) {
             carrier += i >= 0 ? (a.charAt(i) - '0') : 0;
             carrier += j >= 0 ? (b.charAt(j) - '0') : 0;
             result = carrier % 2 + result;
