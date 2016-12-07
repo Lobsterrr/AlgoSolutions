afaa40f49d86d2a3ef7db4a7043e7b2888220bc4
mitcc <mitcc@qq.com>
2016-12-08 00:25:57 +0800

    :basketball:

--- leetcode/leetcode/MergeSortedArray.java
+++ leetcode/leetcode/MergeSortedArray.java
@@ -11,7 +11,15 @@ initialized in nums1 and nums2 are m and n respectively.
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        for (int i = nums1.length + nums2.length - 1; i >= 0; --i) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[i] = nums1[m - 1];
+                m--;
+            } else {
+                nums1[i] = nums2[n - 1];
+                n--;
+            }
+        }
     }
 
 }
