0b37906d34f28dd8aff428b4a16c6f58dc539d86
mitcc <mitcc@qq.com>
2016-12-10 19:27:46 +0800

    :basketball:

--- leetcode/leetcode/ReverseVowelsOfAString.java
+++ leetcode/leetcode/ReverseVowelsOfAString.java
@@ -17,4 +17,9 @@ public class ReverseVowelsOfAString {
 
     }
 
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' 
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
 }
