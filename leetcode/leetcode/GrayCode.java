d7d749cd63c463bfa9ab3ccb610d9464ccbf3f63
mitcc <mitcc@qq.com>
2016-12-12 10:49:27 +0800

    :basketball:

--- leetcode/leetcode/GrayCode.java
+++ leetcode/leetcode/GrayCode.java
@@ -24,9 +24,9 @@ public class GrayCode {
 
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<Integer>();
-
-
-
+        for (int i = 0; i < (1 << n); ++i) {
+            result.add(i ^ (i >>> 1));
+        }
         return result;
     }
 
