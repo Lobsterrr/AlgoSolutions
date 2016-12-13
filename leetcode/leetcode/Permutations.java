8589b3319241416e47dbc716085985a01c88a729
mitcc <mitcc@qq.com>
2016-12-13 15:32:02 +0800

    :basketball:

--- leetcode/leetcode/Permutations.java
+++ leetcode/leetcode/Permutations.java
@@ -16,8 +16,17 @@ public class Permutations {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-
+        for (int i = 0; i < nums.length; ++i) {
+            int size = result.size();
+            for (int j = 0; j < size; ++j) {
+                List<Integer> list = result.remove(0);
+                for (int k = 0; k <= list.size(); ++k) {
+                    List<Integer> item = new ArrayList<Integer>(list);
+                    item.add(k, nums[i]);
+                    result.add(item);
+                }
+            }
+        }
         return result;
     }
 
