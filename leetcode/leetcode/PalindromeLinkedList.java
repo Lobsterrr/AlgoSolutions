9c0c823c5636f17b96e8dcf1ff983de5e3d3868c
mitcc <mitcc@qq.com>
2016-12-18 01:23:01 +0800

    :basketball:

--- leetcode/leetcode/PalindromeLinkedList.java
+++ leetcode/leetcode/PalindromeLinkedList.java
@@ -18,7 +18,8 @@ public class PalindromeLinkedList {
         if (head == null) {
             return true;
         }
-        ListNode second = reverse(getSecondHalf(head));
+        ListNode second = getSecondHalf(head);
+        second = reverse(second);
         ListNode cur1 = head;
         ListNode cur2 = second;
         while (cur2 != null) {
