754c27b277428cb7c16d9fbc59914c2724850386
mitcc <mitcc@qq.com>
2016-12-21 12:39:45 +0800

    :basketball:

--- leetcode/leetcode/ShortestPalindrome.java
+++ leetcode/leetcode/ShortestPalindrome.java
@@ -10,11 +10,23 @@ Given "abcd", return "dcbabcd".
 public class ShortestPalindrome {
 
     public String shortestPalindrome(String s) {
-
+        int i = s.length() - 1;
+        for (; i >= 0 && !isPalindrome(s, 0, i); --i);
+        StringBuilder sb = new StringBuilder(s.substring(i + 1));
+        sb = sb.reverse().append(s);
+        return sb.toString();
     }
 
     private boolean isPalindrome(String s, int start, int end) {
-
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        for (; start < end; start++, end--) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
