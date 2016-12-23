5bacbacc4ffea42c6387056f642b0c8dea652f42
mitcc <mitcc@qq.com>
2016-12-23 17:21:19 +0800

    :basketball:

--- leetcode/leetcode/Combinations.java
+++ leetcode/leetcode/Combinations.java
@@ -18,13 +18,18 @@ public class Combinations {
 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        dfs(result, new ArrayList<Integer>(), 0, k);
+        dfs(result, new ArrayList<Integer>(), n, k, 0);
         return result;
     }
 
-    public void dfs(List<List<Integer>> result, List<Integer> list, int start, k) {
-        for (int i = start; i < list.size() - k; ++i) {
-
+    public void dfs(List<List<Integer>> result, List<Integer> list, int n, int k, int start) {
+        if (list.size() == k) {
+            result.add(list);
+            return;
+        }
+        for (int i = start; i <= n; ++i) {
+            List<Integer> item = new ArrayList<Integer>(list);
+            item.add(i);
         }
     }
 
