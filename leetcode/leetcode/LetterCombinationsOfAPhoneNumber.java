55f9bdab09f6efde28871b08db241c5991dbb728
mitcc <mitcc@qq.com>
2017-01-14 00:30:05 +0800

    :basketball:

--- leetcode/leetcode/LetterCombinationsOfAPhoneNumber.java
+++ leetcode/leetcode/LetterCombinationsOfAPhoneNumber.java
@@ -18,6 +18,16 @@ public class LetterCombinationsOfAPhoneNumber {
 
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<String>();
+        Map<Character, String> map = new HashMap<Character, String>();
+        map.put('1', "");
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
         for (int i = 0; i < digits.length(); ++i) {
 
         }
