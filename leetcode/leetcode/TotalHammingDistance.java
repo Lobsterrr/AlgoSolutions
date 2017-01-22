735f87073e720d858202705806554c4a26215889
mitcc <mitcc@qq.com>
2017-01-22 11:00:15 +0800

    :basketball:

--- leetcode/leetcode/TotalHammingDistance.java
+++ leetcode/leetcode/TotalHammingDistance.java
@@ -11,8 +11,7 @@ Input: 4, 14, 2
 Output: 6
 
 Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010
-(just
-showing the four bits relevant in this case). So the answer will be:
+(just showing the four bits relevant in this case). So the answer will be:
 HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) 
 = 2 + 2 + 2 = 6.
 Note:
