1fa5719de48c498a4dbb6a29bbaf303d0bf720bc
mitcc <mitcc@qq.com>
2017-01-30 22:41:20 +0800

    :basketball:

--- leetcode/leetcode/TotalHammingDistance.java
+++ leetcode/leetcode/TotalHammingDistance.java
@@ -33,26 +33,4 @@ public class TotalHammingDistance {
         return result;
     }
 
-/*****************************************************************************/
-
-    // O(n ^ 2) time, O(1) space.
-    public int totalHammingDistance(int[] nums) {
-        int result = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = i + 1; j < nums.length; ++j) {
-                result += hammingWeight(nums[i] ^ nums[j]);
-            }
-        }
-        return result;
-    }
-
-    public int hammingWeight(int n) {
-        n = (n & 0x55555555) + ((n >>> 1) & 0x55555555);
-        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
-        n = (n & 0x0f0f0f0f) + ((n >>> 4) & 0x0f0f0f0f);
-        n = (n & 0x00ff00ff) + ((n >>> 8) & 0x00ff00ff);
-        n = (n & 0x0000ffff) + ((n >>> 16) & 0x0000ffff);
-        return n;
-    }
-
 }
