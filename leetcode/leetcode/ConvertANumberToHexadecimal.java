6767b059156bbe02817b6d24e5385a4e48d660ad
mitcc <mitcc@qq.com>
2017-02-03 10:51:58 +0800

    :basketball:

--- leetcode/leetcode/ConvertANumberToHexadecimal.java
+++ leetcode/leetcode/ConvertANumberToHexadecimal.java
@@ -32,9 +32,13 @@ public class ConvertANumberToHexadecimal {
     public String toHex(int num) {
         String result = "";
         while (num > 0) {
-
+            int n = num % 16;
+            if (n < 10) {
+                result = n + result;
+            } else {
+                result = ('a' + (n - 10)) + result;
+            }
         }
-        
         return result;
     }
 
