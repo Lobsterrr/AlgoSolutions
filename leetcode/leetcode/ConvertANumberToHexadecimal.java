4479a826d415acdf4557f891fa144c480add7da2
mitcc <mitcc@qq.com>
2017-02-07 11:23:19 +0800

    :basketball:

--- leetcode/leetcode/ConvertANumberToHexadecimal.java
+++ leetcode/leetcode/ConvertANumberToHexadecimal.java
@@ -33,6 +33,19 @@ public class ConvertANumberToHexadecimal {
         if (num == 0) {
             return "0";
         }
+        char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        String result = "";
+        while (num != 0) {
+            result = map[num & 15] + result;
+            num = num >>> 4;
+        }
+        return result;
+    }
+
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
         String result = "";
         while (num != 0) {
             int n = num & 15;
