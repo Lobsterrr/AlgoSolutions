71306b375701326fb7989ecd02b002ebce40c85b
mitcc <mitcc@qq.com>
2017-02-09 16:50:09 +0800

    :basketball:

--- leetcode/leetcode/Subsets.java
+++ leetcode/leetcode/Subsets.java
@@ -37,7 +37,7 @@ public class Subsets {
 
 /*****************************************************************************/
     
-    public List<List<Integer>>subsets(int[] nums) {
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         result.add(new ArrayList<Integer>());
         for (int i = 0; i < nums.length; ++i) {
