3f788a68d3994fb0873e74892e450ece47e35b0f
mitcc <mitcc@qq.com>
2017-02-26 11:51:06 +0800

    :basketball:

--- leetcode/leetcode/SearchA2DMatrix.java
+++ leetcode/leetcode/SearchA2DMatrix.java
@@ -19,6 +19,9 @@ Given target = 3, return true.
 public class SearchA2DMatrix {
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
         int i = 0;
         int j = matrix[0].length - 1;
         while (i < matrix.length && j >= 0) {
