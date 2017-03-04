6caae8f1c5682b47383ef58ba4d5ff883bbc7843
mitcc <mitcc@qq.com>
2017-03-04 16:11:41 +0800

    :basketball:

--- leetcode/leetcode/SearchA2DMatrix.java
+++ leetcode/leetcode/SearchA2DMatrix.java
@@ -22,26 +22,6 @@ public class SearchA2DMatrix {
         if (matrix == null || matrix.length == 0) {
             return false;
         }
-        int i = 0;
-        int j = matrix[0].length - 1;
-        while (i < matrix.length && j >= 0) {
-            if (matrix[i][j] == target) {
-                return true;
-            } else if (matrix[i][j] < target) {
-                i++;
-            } else {
-                j--;
-            }
-        }
-        return false;
-    }
-
-/*****************************************************************************/
-
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) {
-            return false;
-        }
         int xLen = matrix.length;
         int yLen = matrix[0].length;
         int i = 0;
@@ -58,6 +38,26 @@ public class SearchA2DMatrix {
         }
         return false;
     }
+    
+/*****************************************************************************/
 
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+    
 }
 
