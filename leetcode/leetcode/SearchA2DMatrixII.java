a650ee7f9c71b7772014dde0d92381fbdd5c4307
mitcc <mitcc@qq.com>
2017-03-06 00:37:35 +0800

    :basketball:

--- leetcode/leetcode/SearchA2DMatrixII.java
+++ leetcode/leetcode/SearchA2DMatrixII.java
@@ -25,6 +25,9 @@ public class SearchA2DMatrixII {
         if (matrix == null || matrix.length == 0) {
             return false;
         }
+        int xLen = matrix.length;
+        int yLen = matrix[0].length;
+
     }
 
 }
