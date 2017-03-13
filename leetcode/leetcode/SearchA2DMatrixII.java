297158f318ce6462ebe00af0ab3d46b4de402139
mitcc <mitcc@qq.com>
2017-03-13 10:56:12 +0800

    :basketball:

--- leetcode/leetcode/SearchA2DMatrixII.java
+++ leetcode/leetcode/SearchA2DMatrixII.java
@@ -48,7 +48,8 @@ public class SearchA2DMatrixII {
 
     public boolean binarySearch(int[][] matrix, int target, int rowLow, int rowHigh, int colLow, int colHigh) {
         int row = rowLow + (rowHigh - rowLow) / 2;
-
+        int col = colLow + (colHigh - colLow) / 2;
+        while (row < rowHigh && col )
     }
 
 }
