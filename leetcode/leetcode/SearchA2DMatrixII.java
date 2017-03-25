ecb913af91ccd8e5764db756430aef0e06c08964
mitcc <mitcc@qq.com>
2017-03-25 21:07:24 +0800

    :basketball:

--- leetcode/leetcode/SearchA2DMatrixII.java
+++ leetcode/leetcode/SearchA2DMatrixII.java
@@ -52,6 +52,16 @@ public class SearchA2DMatrixII {
 
     public boolean binarySearch(int[][] matrix, int target, int rowLow, 
             int rowHigh, int colLow, int colHigh) {
+public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        return binarySearch(matrix, target, 0, matrix.length - 1, 0, 
+                matrix[0].length - 1);
+    }
+
+    public boolean binarySearch(int[][] matrix, int target, int rowLow, 
+            int rowHigh, int colLow, int colHigh) {
         if (rowLow > rowHigh || colLow > colHigh) {
             return false;
         }
@@ -63,7 +73,7 @@ public class SearchA2DMatrixII {
             if (matrix[row][mid] == target) {
                 return true;
             }
-            if (matrix[row][mid] < target && target < matrix[row][mid + 1]) {
+            if (mid < colHigh && matrix[row][mid] < target && target < matrix[row][mid + 1]) {
                 break;
             } else {
                 if (matrix[row][mid] < target) {
@@ -81,6 +91,11 @@ public class SearchA2DMatrixII {
                 }
             }
             mid = start + (end - start) / 2;
+        }
+        return binarySearch(matrix, target, row + 1, rowHigh, colLow, mid) 
+            || binarySearch(matrix, target, rowLow, row - 1, mid + 1, colHigh);
+    }
+
         }
         return binarySearch(matrix, target, row + 1, rowHigh, colLow, mid) 
             || binarySearch(matrix, target, rowLow, row - 1, mid + 1, colHigh);
