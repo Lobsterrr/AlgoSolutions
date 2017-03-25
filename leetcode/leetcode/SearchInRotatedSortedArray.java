23033eacbab5511bef52fcdd98b58003046cc0b2
mitcc <mitcc@qq.com>
2017-03-25 23:37:58 +0800

    :basketball:

--- leetcode/leetcode/SearchInRotatedSortedArray.java
+++ leetcode/leetcode/SearchInRotatedSortedArray.java
@@ -26,7 +26,7 @@ public class SearchInRotatedSortedArray {
         }
         // 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 1, 2, 3
         // 6, 7, 0, 1, 2, 3, 4, 5
-        if (nums[low] <= nums[mid] && nums[mid] > target) {
+        if (nums[low] <= nums[mid] && nums[low] < target && target < nums[mid]) {
             return binarySearch(nums, target, low, mid - 1);
         }
         return binarySearch(nums, target, mid + 1, high);
