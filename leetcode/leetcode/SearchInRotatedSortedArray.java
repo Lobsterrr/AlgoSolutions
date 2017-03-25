909c18ad780dd337959362f9bdb874c1f8524ec7
mitcc <mitcc@qq.com>
2017-03-26 00:46:24 +0800

    :basketball:

--- leetcode/leetcode/SearchInRotatedSortedArray.java
+++ leetcode/leetcode/SearchInRotatedSortedArray.java
@@ -22,7 +22,7 @@ public class SearchInRotatedSortedArray {
         }
         int mid = low + (high - low) / 2;
         if (nums[mid] == target) {
-            return mid + 1;
+            return mid;
         }
         // 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 1, 2, 3
         // 6, 7, 0, 1, 2, 3, 4, 5
