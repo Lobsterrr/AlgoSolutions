9cadaec5f386facc858d9929aae5b1280a98023d
mitcc <mitcc@qq.com>
2017-03-26 03:47:52 +0800

    :basketball:

--- leetcode/leetcode/SearchInRotatedSortedArray.java
+++ leetcode/leetcode/SearchInRotatedSortedArray.java
@@ -1,9 +1,11 @@
 /*
-Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+Suppose an array sorted in ascending order is rotated at some pivot unknown to
+you beforehand.
 
 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 
-You are given a target value to search. If found in the array return its index, otherwise return -1.
+You are given a target value to search. If found in the array return its index,
+otherwise return -1.
 
 You may assume no duplicate exists in the array.
  */
@@ -30,6 +32,32 @@ public class SearchInRotatedSortedArray {
             return binarySearch(nums, target, low, mid - 1);
         }
         return binarySearch(nums, target, mid + 1, high);
+
+        if (target < nums[low]) {
+            if (nums[mid] > nums[low]) {
+                if (target < nums[mid]) {
+                    // no posible.
+                } else {
+                    return binarySearch(nums, target, mid + 1, high);
+                }
+            } else {
+                if (nums[mid] < target) {
+                    return binarySearch(nums, target, mid + 1, high);
+                } else {
+                    return binarySearch(nums, target, low, mid - 1);
+                }
+            }
+        } else { // target > nums[low]
+            if (nums[mid] > nums[low]) {
+                if (nums[mid] > target) {
+
+                } else {
+
+                }
+            } else {
+
+            }
+        }
     }
 
 }
