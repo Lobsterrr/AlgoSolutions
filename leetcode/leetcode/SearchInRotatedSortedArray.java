d379dd7e915bdbf7def3518fdc969c8e6b9fd819
mitcc <mitcc@qq.com>
2017-03-26 11:28:50 +0800

    :basketball:

--- leetcode/leetcode/SearchInRotatedSortedArray.java
+++ leetcode/leetcode/SearchInRotatedSortedArray.java
@@ -18,7 +18,23 @@ public class SearchInRotatedSortedArray {
         int low = 0;
         int high = nums.length - 1;
         while (low <= high) {
-
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (target < nums[low]) {
+                if (nums[mid] < nums[low] && nums[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // target > nums[low]
+                if (nums[mid] >= nums[low] && nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         return -1;
     }
