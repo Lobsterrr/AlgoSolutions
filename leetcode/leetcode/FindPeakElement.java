b8ef6ca69fac46f7bf085c7bfec279bfc5d86ead
mitcc <mitcc@qq.com>
2017-03-27 13:31:43 +0800

    :basketball:

--- leetcode/leetcode/FindPeakElement.java
+++ leetcode/leetcode/FindPeakElement.java
@@ -20,7 +20,17 @@ Your solution should be in logarithmic complexity.
 public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
-
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return low;
     }
 
 }
