61fe4a61b769145e60b3058105176cb96275c101
mitcc <mitcc@qq.com>
2017-04-01 04:03:04 +0800

    :basketball:

--- leetcode/leetcode/TwoSum.java
+++ leetcode/leetcode/TwoSum.java
@@ -18,7 +18,7 @@ public class TwoSum {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; ++i) {
             if (map.containsKey(nums[i])) {
-                return int[2] {map.get(nums[i]), i};
+                return new int[]{map.get(nums[i]), i};
             }
             map.put(target - nums[i], i);
         }
