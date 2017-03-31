0d147be7e09e61004e172f9b4148687e5f570561
mitcc <mitcc@qq.com>
2017-03-31 13:23:49 +0800

    :basketball:

--- leetcode/leetcode/ThreeSum.java
+++ leetcode/leetcode/ThreeSum.java
@@ -16,7 +16,17 @@ A solution set is:
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; ++i) {
+            int low = i + 1;
+            int high = nums.length - 1;
+            while (low < high) {
+                int threeSum = nums[i] + nums[low] + nums[high];
+                if (threeSum == target) {
 
+                }
+            }
+        }
     }
 
 }
