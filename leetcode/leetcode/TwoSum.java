eae3271b51ab4233e15b9f3c604edc8227af3a16
mitcc <mitcc@qq.com>
2017-04-01 04:05:42 +0800

    :basketball:

--- leetcode/leetcode/TwoSum.java
+++ leetcode/leetcode/TwoSum.java
@@ -13,6 +13,20 @@ return [0, 1].
  */
 public class TwoSum {
 
+    // O(n) time, O(n) space.
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return new int[2];
+    }
+
+/*****************************************************************************/
+
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
