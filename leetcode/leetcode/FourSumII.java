1e917287daf191e2264afbefcb89078c1a53bb14
mitcc <mitcc@qq.com>
2017-04-01 16:22:03 +0800

    :basketball:

--- leetcode/leetcode/FourSumII.java
+++ leetcode/leetcode/FourSumII.java
@@ -23,8 +23,17 @@ public class FourSumII {
 
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         int result = 0;
-
-
+        for (int a = 0; a < A.length; ++a) {
+            for (int b = 0; b < B.length; ++b) {
+                for (int c = 0; c < C.length; ++c) {
+                    for (int d = 0; d < D.length; ++d) {
+                        if (A[a] + B[b] + C[c] + D[d] == 0) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
         return result;
     }
 
