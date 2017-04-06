503b4cb536fcaf5d6119309db9a4d7ca132b2f3f
mitcc <mitcc@qq.com>
2017-04-06 11:44:10 +0800

    :basketball:

--- leetcode/leetcode/FindMinimumInRotatedSortedArrayII.java
+++ leetcode/leetcode/FindMinimumInRotatedSortedArrayII.java
@@ -21,7 +21,7 @@ public class FindMinimumInRotatedSortedArrayII {
         // 7, 8, 1, 2, 3, 4, 5, 6
         while (low < high) {
             for (; low < high - 1 && nums[low] == nums[low + 1]; ++low);
-            for (; hihg > low + 1 && nums[high] == nums[high - 1]; --high);
+            for (; high > low + 1 && nums[high] == nums[high - 1]; --high);
             int mid = low + (high - low >> 1);
             // TODO index check
             if (low < high && nums[mid] < nums[high]) {
