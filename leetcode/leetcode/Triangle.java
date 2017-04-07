ecaa294a302060e1209b1747d8e4df18fc3991c7
mitcc <mitcc@qq.com>
2017-04-07 14:34:01 +0800

    :basketball:

--- leetcode/leetcode/Triangle.java
+++ leetcode/leetcode/Triangle.java
@@ -16,9 +16,13 @@ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 public class Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        for (int i = 0; i < triangle.size(); ++i) {
-
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                int value = triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+                triangle.get(i).set(j, value);
+            }
         }
+        return triangle.get(0).get(0);
     }
 
 }
