599244b9d8e0ac59ea16ca2a69cad26cee33ef1c
mitcc <mitcc@qq.com>
2017-04-07 15:21:14 +0800

    :basketball:

--- leetcode/leetcode/Triangle.java
+++ leetcode/leetcode/Triangle.java
@@ -18,6 +18,16 @@ the total number of rows in the triangle.
 public class Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
         for (int i = triangle.size() - 2; i >= 0; --i) {
             for (int j = 0; j < triangle.get(i).size(); ++j) {
                 triangle.get(i).set(j, triangle.get(i).get(j) 
