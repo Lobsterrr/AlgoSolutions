ebb55b1cb882ac98e5f7c2c824988fb898adbdb5
mitcc <mitcc@qq.com>
2017-04-08 13:39:23 +0800

    :basketball:

--- leetcode/leetcode/SpiralMatrix.java
+++ leetcode/leetcode/SpiralMatrix.java
@@ -15,7 +15,14 @@ You should return [1,2,3,6,9,8,7,4,5].
 public class SpiralMatrix {
 
     public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<Integer>();
+        int xLen = matrix.length;
+        int yLen = matrix[0].length;
+        for (int i = 0; i < (xLen + 1) / 2; ++i) {
 
+        }
+
+        return result;
     }
 
 }
