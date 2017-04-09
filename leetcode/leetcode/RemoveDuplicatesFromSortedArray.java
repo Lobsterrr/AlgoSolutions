e62578013fc294d2e3673cb4540fa759073bee63
mitcc <mitcc@qq.com>
2017-04-09 11:14:02 +0800

    :basketball:

--- leetcode/leetcode/RemoveDuplicatesFromSortedArray.java
+++ leetcode/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -16,7 +16,16 @@ length.
 public class RemoveDuplicatesFromSortedArray {
 
     public int removeDuplicates(int[] nums) {
-
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int index = 0;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[index] != nums[i]) {
+                nums[++index] = nums[i];
+            }
+        }
+        return index + 1;
     }
 
 }
