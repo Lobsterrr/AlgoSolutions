0eeb96cd5049347298fb183232030f7355cab13f
mitcc <mitcc@qq.com>
2017-04-10 11:17:14 +0800

    :basketball:

--- leetcode/leetcode/RemoveDuplicatesFromSortedArrayII.java
+++ leetcode/leetcode/RemoveDuplicatesFromSortedArrayII.java
@@ -11,16 +11,16 @@ being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 public class RemoveDuplicatesFromSortedArrayII {
 
     public int removeDuplicates(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        if (nums.length <= 2) {
-            return nums.length;
+        if (nums == null || nums.length <= 2) {
+            return nums == null ? 0 : nums.length;
         }
         int index = 1;
         for (int i = 2; i < nums.length; ++i) {
-
+            if (nums[i] != nums[index] || nums[index] != nums[index - 1]) {
+                nums[++index] = nums[i];
+            }
         }
+        return index + 1;
     }
 
 }
