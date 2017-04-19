e2105ee43c38cd098b4422dacbfcec0d01399609
mitcc <mitcc@qq.com>
2017-04-19 14:47:08 +0800

    :basketball:

--- leetcode/leetcode/ClimbingStairs.java
+++ leetcode/leetcode/ClimbingStairs.java
@@ -9,15 +9,6 @@ Note: Given n will be a positive integer.
 public class ClimbingStairs {
 
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
-
-    public int climbStairs(int n) {
         int result = 1;
         for (int i = 1, j = 0, k = 1; i <= n; ++i) {
             result = j + k;
@@ -27,4 +18,16 @@ public class ClimbingStairs {
         return result;
     }
 
+/*****************************************************************************/
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    
 }
