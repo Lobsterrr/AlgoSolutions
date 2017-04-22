4342ae138a8223db4beb73e7cad3d6d8f8edc1f8
mitcc <mitcc@qq.com>
2017-04-23 00:31:51 +0800

    :basketball:

--- leetcode/leetcode/UglyNumber.java
+++ leetcode/leetcode/UglyNumber.java
@@ -10,7 +10,7 @@ Note that 1 is typically treated as an ugly number.
 public class UglyNumber {
 
     public boolean isUgly(int num) {
-        while (num > 0 && num % 2 == 0 || num % 3 == 0 || num % 5 == 0) {
+        while (num > 0 && (num % 2 == 0 || num % 3 == 0 || num % 5 == 0)) {
             if (num % 2 == 0) {
                 num /= 2;
             }
