581466ce737328c6ad9c40713798574e246226b5
mitcc <mitcc@qq.com>
2017-04-23 00:31:01 +0800

    :basketball:

--- leetcode/leetcode/UglyNumber.java
+++ leetcode/leetcode/UglyNumber.java
@@ -12,7 +12,7 @@ public class UglyNumber {
     public boolean isUgly(int num) {
         while (num > 0 && num % 2 == 0 || num % 3 == 0 || num % 5 == 0) {
             if (num % 2 == 0) {
-                numt /= 2;
+                num /= 2;
             }
             if (num % 3 == 0) {
                 num /= 3;
