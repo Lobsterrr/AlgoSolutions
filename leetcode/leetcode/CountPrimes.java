80398a6422c08bc8dfabbf4d94843165a9c122d8
mitcc <mitcc@qq.com>
2017-04-23 13:16:24 +0800

    :basketball:

--- leetcode/leetcode/CountPrimes.java
+++ leetcode/leetcode/CountPrimes.java
@@ -6,9 +6,6 @@ Count the number of prime numbers less than a non-negative number, n.
 public class CountPrimes {
 
     public int countPrimes(int n) {
-        if (n <= 1) {
-            return 0;
-        }
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
         isPrime[0] = isPrime[1] = false;
@@ -19,11 +16,11 @@ public class CountPrimes {
                 }
             }
         }
-        int result = 0;
-        for (boolean num : isPrime) {
-            result += num ? 1 : 0;
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            count += isPrime[i] ? 1 : 0;
         }
-        return result;
+        return count;
     }
 
 }
