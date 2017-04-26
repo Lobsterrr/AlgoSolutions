0fe6c28c5a308333898544fd41092c729ec836c1
mitcc <mitcc@qq.com>
2017-04-26 14:05:54 +0800

    :basketball:

--- leetcode/leetcode/UglyNumberII.java
+++ leetcode/leetcode/UglyNumberII.java
@@ -10,30 +10,6 @@ Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
 public class UglyNumberII {
 
     public int nthUglyNumber(int n) {
-        int i2 = 0;
-        int i3 = 0;
-        int i5 = 0;
-        int next2 = 2;
-        int next3 = 3;
-        int next5 = 5;
-        int[] result = new int[n];
-        result[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            result[i] = Math.min(next2, Math.min(next3, next5));
-            if (result[i] == next2) {
-                next2 = 2 * result[++i2];
-            }
-            if (result[i] == next3) {
-                next3 = 3 * result[++i3];
-            }
-            if (result[i] == next5) {
-                next5 = 5 * result[++i5];
-            }
-        }
-        return result[n - 1];
-    }
-
-    public int nthUglyNumber(int n) {
         int[] result = new int[n];
         result[0] = 1;
         int i2 = 0;
