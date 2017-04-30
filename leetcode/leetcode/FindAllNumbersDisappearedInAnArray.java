f29847d0308367e52cf438ec8eafe9f38eb5b865
mitcc <mitcc@qq.com>
2017-05-01 00:38:53 +0800

    :basketball:

--- leetcode/leetcode/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/leetcode/FindAllNumbersDisappearedInAnArray.java
@@ -19,6 +19,11 @@ public class FindAllNumbersDisappearedInAnArray {
 
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > 0 && nums[nums[i] - 1] > 0) {
+                nums[nums[i] - 1] *= -1;
+            }
+        }
 
         return result;
     }
