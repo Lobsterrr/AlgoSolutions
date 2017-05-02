27e1979cae5e40fab1c06b3b25a64a959e39fd28
mitcc <mitcc@qq.com>
2017-05-02 10:49:36 +0800

    :basketball:

--- leetcode/leetcode/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/leetcode/FindAllNumbersDisappearedInAnArray.java
@@ -24,10 +24,17 @@ public class FindAllNumbersDisappearedInAnArray {
                 if (nums[nums[i] - 1] > 0) {
                     nums[nums[i] - 1] *= -1;
                 }
+            } else {
+                int index = -nums[i];
+                if (nums[index - 1] > 0) {
+                    nums[index - 1] *= -1;
+                }
             }
         }
         for (int i = 0; i < nums.length; ++i) {
-
+            if (nums[i] > 0) {
+                result.add(nums[i]);
+            }
         }
         return result;
     }
