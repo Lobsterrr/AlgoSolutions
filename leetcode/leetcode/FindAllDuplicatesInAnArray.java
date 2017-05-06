93f6cd87a7bacd144f3fe9461eb3f712e19d4da1
mitcc <mitcc@qq.com>
2017-05-06 15:45:14 +0800

    :basketball:

--- leetcode/leetcode/FindAllDuplicatesInAnArray.java
+++ leetcode/leetcode/FindAllDuplicatesInAnArray.java
@@ -20,7 +20,7 @@ public class FindAllDuplicatesInAnArray {
         for (int i = 0; i < nums.length; ++i) {
             int index = Math.abs(nums[i]) - 1;
             if (nums[index] < 0) {
-                result.add(-nums[index]);
+                result.add(index + 1);
             }
             if (nums[index] > 0) {
                 nums[index] *= -1;
