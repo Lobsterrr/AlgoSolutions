3c04c10248ac92b2054780833195c6fa8e1645e3
mitcc <mitcc@qq.com>
2017-05-08 13:38:50 +0800

    :basketball:

--- leetcode/leetcode/SearchInsertPosition.java
+++ leetcode/leetcode/SearchInsertPosition.java
@@ -16,9 +16,16 @@ public class SearchInsertPosition {
         int low = 0;
         int high = nums.length - 1;
         while (low < high) {
-
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-
+        return low;
     }
 
 }
