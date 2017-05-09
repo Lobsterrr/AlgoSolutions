aeb0e9a9f37381944e4e1532a848ed304c7cf97f
mitcc <mitcc@qq.com>
2017-05-09 15:35:27 +0800

    :basketball:

--- leetcode/leetcode/SearchInsertPosition.java
+++ leetcode/leetcode/SearchInsertPosition.java
@@ -15,12 +15,14 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        while (low < high) {
+        while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if (nums[mid] < target) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
-                high = mid;
+                high = mid - 1;
             }
         }
         return low;
