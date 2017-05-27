3726fb99d65a4effdbfe09deda378cbd2f905e5d
mitcc <mitcc@qq.com>
2017-05-27 12:49:01 +0800

    :basketball:

--- leetcode/leetcode/RemoveElement.java
+++ leetcode/leetcode/RemoveElement.java
@@ -20,6 +20,18 @@ public class RemoveElement {
         int index = 0;
         for (int i = 0; i < nums.length; ++i) {
             if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+
+/*****************************************************************************/
+
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != val) {
                 swap(nums, index++, i);
             }
         }
