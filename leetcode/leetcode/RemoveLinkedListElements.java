021bc705945a077f616e60c5f44b4a66cb250389
mitcc <mitcc@qq.com>
2017-05-28 13:53:58 +0800

    :basketball:

--- leetcode/leetcode/RemoveLinkedListElements.java
+++ leetcode/leetcode/RemoveLinkedListElements.java
@@ -16,7 +16,17 @@ Return: 1 --> 2 --> 3 --> 4 --> 5
 public class RemoveLinkedListElements {
 
     public ListNode removeElements(ListNode head, int val) {
-
+        LisNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
     }
 
 }
