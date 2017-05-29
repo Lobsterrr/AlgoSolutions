606984c751470ffc6e81754424b7a7c4a757b166
mitcc <mitcc@qq.com>
2017-05-29 14:20:30 +0800

    :basketball:

---
+++ leetcode/leetcode/InsertionSortList.java
@@ -0,0 +1,18 @@
+/*
+Sort a linked list using insertion sort.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class InsertionSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+
+    }
+
+}
