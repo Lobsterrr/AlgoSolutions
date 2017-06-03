b05e2cfe68404c90ea3c78eacc29b682fa2b1ccd
mitcc <mitcc@qq.com>
2017-06-04 01:59:16 +0800

    :basketball:

--- leetcode/leetcode/JumpGameII.java
+++ leetcode/leetcode/JumpGameII.java
@@ -19,26 +19,14 @@ public class JumpGameII {
 
     public int jump(int[] nums) {
         int result = 0;
-        for (int i = 0, end = 0, maxIndex = 0; i < nums.length - 1; ++i) {
+        for (int i = 0, end = 0, maxIndex = 0; end < nums.length - 1; ++i) {
             maxIndex = Math.max(maxIndex, i + nums[i]);
             if (i >= end) {
-                result++;
                 end = maxIndex;
+                result++;
             }
         }
         return result;
-    }
-
-    public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = i + 1; j <= i + nums[i] && j < nums.length; ++j) {
-                dp[j] = Math.min(dp[j], dp[i] + 1);
-            }
-        }
-        return dp[nums.length - 1];
     }
 
 }
