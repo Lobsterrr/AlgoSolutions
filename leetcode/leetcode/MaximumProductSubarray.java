5cae06ddc781e737a1f0e5ee5cf80b8fbb64baa6
mitcc <mitcc@qq.com>
2017-06-05 11:45:31 +0800

    :basketball:

--- leetcode/leetcode/MaximumProductSubarray.java
+++ leetcode/leetcode/MaximumProductSubarray.java
@@ -8,7 +8,13 @@ the contiguous subarray [2,3] has the largest product = 6.
 public class MaximumProductSubarray {
 
     public int maxProduct(int[] nums) {
-
+        int result = Integer.MIN_VALUE;
+        for (int i = 0, maxProduct = Integer.MIN_VALUE, minProduct = Integer.MAX_VALUE; i < nums.length; ++i) {
+            maxProduct = Math.max(nums[i], Math.max(nums[i] * maxProduct, nums[i] * minProduct));
+            minProduct = Math.min(nums[i], Math.min(nums[i] * maxProduct, nums[i] * minProduct));
+            result = Math.max(result, maxProduct);
+        }
+        return result;
     }
 
 }
