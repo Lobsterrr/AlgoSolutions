2515430a09cbcf003663da4470e9751b787d48b6
mitcc <mitcc@qq.com>
2017-06-06 11:57:40 +0800

    :basketball:

--- leetcode/leetcode/HouseRobber.java
+++ leetcode/leetcode/HouseRobber.java
@@ -21,21 +21,4 @@ public class HouseRobber {
         return result;
     }
 
-    public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int[] dp = new int[nums.length];
-        for (int i = 0; i < nums.length; ++i) {
-            if (i == 0) {
-                dp[i] = nums[i];
-            } else if (i == 1) {
-                dp[i] = Math.max(dp[i - 1], nums[i]);
-            } else {
-                dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-            }
-        }
-        return dp[nums.length - 1];
-    }
-
 }
