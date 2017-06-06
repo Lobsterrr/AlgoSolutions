b9b87c46851534cff17e3468d272e85e1448b32e
mitcc <mitcc@qq.com>
2017-06-06 11:53:28 +0800

    :basketball:

--- leetcode/leetcode/HouseRobber.java
+++ leetcode/leetcode/HouseRobber.java
@@ -29,7 +29,7 @@ public class HouseRobber {
             if (i == 0) {
                 dp[i] = nums[i];
             } else if (i == 1) {
-                dp[i] == Math.max(dp[i - 1], nums[i]);
+                dp[i] = Math.max(dp[i - 1], nums[i]);
             } else {
                 dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
             }
