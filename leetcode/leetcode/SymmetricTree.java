2180b63347fa49dd0d57aa0a87d6d85705dcdd6e
mitcc <mitcc@qq.com>
2017-06-15 12:55:46 +0800

    :basketball:

--- leetcode/leetcode/SymmetricTree.java
+++ leetcode/leetcode/SymmetricTree.java
@@ -1,5 +1,20 @@
 /*
- *
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric 
+around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
  */
 /**
  * Definition for a binary tree node.
@@ -13,7 +28,9 @@
 public class SymmetricTree {
 
     public boolean isSymmetric(TreeNode root) {
-        
+        if (root == null || root.left == null || root.right == null) {
+            return true;
+        }
     }
 
 }
