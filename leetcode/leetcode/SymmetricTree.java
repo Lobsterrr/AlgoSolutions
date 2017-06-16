6e47ffed8ccb1eff2cac1c9dc614376ff1591e4b
mitcc <mitcc@qq.com>
2017-06-16 13:07:10 +0800

    :basketball:

--- leetcode/leetcode/SymmetricTree.java
+++ leetcode/leetcode/SymmetricTree.java
@@ -34,6 +34,15 @@ public class SymmetricTree {
         return isSymmetric(root.left, root.right);
     }
 
+/*****************************************************************************/
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
     public boolean isSymmetric(TreeNode left, TreeNode right) {
         if (left == null && right == null) {
             return true;
