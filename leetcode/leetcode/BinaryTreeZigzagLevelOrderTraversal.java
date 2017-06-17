72a37d1f6aed528a532713309c9e8dfe4a0c81d9
mitcc <mitcc@qq.com>
2017-06-18 00:24:13 +0800

    :basketball:

--- leetcode/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ leetcode/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,5 +1,7 @@
 /*
-Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+Given a binary tree, return the zigzag level order traversal of its nodes' 
+values. (ie, from left to right, then right to left for the next level and 
+alternate between).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
@@ -14,6 +16,15 @@ return its zigzag level order traversal as:
   [20,9],
   [15,7]
 ]
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class BinaryTreeZigzagLevelOrderTraversal {
 
