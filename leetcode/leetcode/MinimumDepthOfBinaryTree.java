0ce0d6c29d1c0a6cd13bb88df1157433a638797d
mitcc <mitcc@qq.com>
2017-06-17 23:40:08 +0800

    :basketball:

--- leetcode/leetcode/MinimumDepthOfBinaryTree.java
+++ leetcode/leetcode/MinimumDepthOfBinaryTree.java
@@ -16,7 +16,10 @@ node down to the nearest leaf node.
 public class MinimumDepthOfBinaryTree {
 
     public int minDepth(TreeNode root) {
-        
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 
 }
