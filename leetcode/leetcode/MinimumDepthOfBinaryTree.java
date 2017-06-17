bc4a075af377be34d5401f648eafabfec85c90ac
mitcc <mitcc@qq.com>
2017-06-17 23:48:56 +0800

    :basketball:

--- leetcode/leetcode/MinimumDepthOfBinaryTree.java
+++ leetcode/leetcode/MinimumDepthOfBinaryTree.java
@@ -19,6 +19,12 @@ public class MinimumDepthOfBinaryTree {
         if (root == null) {
             return 0;
         }
+        if (root.left != null && root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        if (root.left == null && root.right != null) {
+            return 1 + minDepth(root.right);
+        }
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 
