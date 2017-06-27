6eb3d680eb362a283c4a8fc3bf649686195f5bc5
mitcc <mitcc@qq.com>
2017-06-27 14:16:20 +0800

    :basketball:

--- leetcode/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -29,20 +29,4 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return result;
     }
 
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int result = 0;
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        for (int i = 0, j = 0; i < s.length(); ++i) {
-            if (map.get(s.charAt(i)) != null) {
-                j = Math.max(j, map.get(s.charAt(i)) + 1);
-            }
-            map.put(s.charAt(i), i);
-            result = Math.max(result, i - j + 1);
-        }
-        return result;
-    }
-
 }
