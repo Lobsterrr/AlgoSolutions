44fe6281ff87c23f4212b623f25b76162417af4f
mitcc <mitcc@qq.com>
2017-06-30 03:21:25 +0800

    :basketball:

--- leetcode/leetcode/SwapNodesInPairs.java
+++ leetcode/leetcode/SwapNodesInPairs.java
@@ -7,6 +7,14 @@ Given 1->2->3->4, you should return the list as 2->1->4->3.
 Your algorithm should use only constant space. You may not modify the values in
 the list, only nodes itself can be changed.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class SwapNodesInPairs {
 
 }
