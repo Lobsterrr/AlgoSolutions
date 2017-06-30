6f9f03f06d89883e349e5cecdce3816c422ce2ce
mitcc <mitcc@qq.com>
2017-07-01 00:59:24 +0800

    :basketball:

--- leetcode/leetcode/TrappingRainWater.java
+++ leetcode/leetcode/TrappingRainWater.java
@@ -17,18 +17,38 @@ public class TrappingRainWater {
             return 0;
         }
         int len = height.length;
-        int[] leftMost = new int[len];
-        int[] rightMost = new int[len];
-        leftMost[0] = height[0];
-        rightMost[len - 1] = height[len - 1];
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+        leftMax[0] = height[0];
+        rightMax[len - 1] = height[len - 1];
         for (int i = 1; i < len; ++i) {
-            leftMost[i] = Math.max(leftMost[i - 1], height[i - 1]);
-            rightMost[len - 1 - i] = Math.max(rightMost[len - i], height[len - i]);
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            rightMax[len - 1 - i] = Math.max(rightMax[len - i], height[len - 1 - i]);
         }
         int result = 0;
         for (int i = 1; i < len - 1; ++i) {
-            if (height[i] < leftMost[i] && height[i] < rightMost[i]) {
-                result += Math.min(leftMost[i], rightMost[i]) - height[i];
+            result += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return result;
+    }
+
+    public int trap(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0;
+        }
+        int len = height.length;
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+        leftMax[0] = height[0];
+        rightMax[len - 1] = height[len - 1];
+        for (int i = 1; i < len; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
+            rightMax[len - 1 - i] = Math.max(rightMax[len - i], height[len - i]);
+        }
+        int result = 0;
+        for (int i = 1; i < len - 1; ++i) {
+            if (height[i] < leftMax[i] && height[i] < rightMax[i]) {
+                result += Math.min(leftMax[i], rightMax[i]) - height[i];
             }
         }
         return result;
