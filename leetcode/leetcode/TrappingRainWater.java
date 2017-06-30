559ce8dc611e313dc4fb022016335fee95e3203c
mitcc <mitcc@qq.com>
2017-07-01 01:00:11 +0800

    :basketball:

--- leetcode/leetcode/TrappingRainWater.java
+++ leetcode/leetcode/TrappingRainWater.java
@@ -32,26 +32,4 @@ public class TrappingRainWater {
         return result;
     }
 
-    public int trap(int[] height) {
-        if (height == null || height.length < 3) {
-            return 0;
-        }
-        int len = height.length;
-        int[] leftMax = new int[len];
-        int[] rightMax = new int[len];
-        leftMax[0] = height[0];
-        rightMax[len - 1] = height[len - 1];
-        for (int i = 1; i < len; ++i) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
-            rightMax[len - 1 - i] = Math.max(rightMax[len - i], height[len - i]);
-        }
-        int result = 0;
-        for (int i = 1; i < len - 1; ++i) {
-            if (height[i] < leftMax[i] && height[i] < rightMax[i]) {
-                result += Math.min(leftMax[i], rightMax[i]) - height[i];
-            }
-        }
-        return result;
-    }
-
 }
