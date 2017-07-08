eb1790196f24be9040f52c10c4ba3fc3c8bbb4ea
mitcc <mitcc@qq.com>
2017-07-08 22:23:08 +0800

    :basketball:

--- lintcode/ClassicalBinarySearch.java
+++ lintcode/ClassicalBinarySearch.java
@@ -16,4 +16,22 @@ O(logn) time
  */
 public class ClassicalBinarySearch {
 
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int findPosition(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+
+            }
+        }
+    }
+
 }
