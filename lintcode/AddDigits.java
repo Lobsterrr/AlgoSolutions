3855ba1e26145ba3f1a933bfac510ac095b12dff
mitcc <mitcc@qq.com>
2017-07-09 00:43:43 +0800

    :basketball:

--- lintcode/AddDigits.java
+++ lintcode/AddDigits.java
@@ -17,7 +17,7 @@ public class AddDigits {
      * @return one digit
      */
     public int addDigits(int num) {
-
+        return (num - 1) % 9 + 1;
     }
 
 /*****************************************************************************/
