94b07f705f6a5014413368c2a36ca5058307bef2
mitcc <mitcc@qq.com>
2017-07-08 23:42:03 +0800

    :basketball:

--- lintcode/ClassicalBinarySearch.java
+++ lintcode/ClassicalBinarySearch.java
@@ -29,9 +29,12 @@ public class ClassicalBinarySearch {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+        return -1;
     }
 
 }
