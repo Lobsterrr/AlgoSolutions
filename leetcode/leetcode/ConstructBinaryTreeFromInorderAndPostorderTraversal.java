913fd54554b1d5fcde325a43e70f3b677408e4f8
mitcc <mitcc@qq.com>
2017-07-15 05:00:57 +0800

    :basketball:

--- leetcode/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ leetcode/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -5,6 +5,19 @@ Note:
 You may assume that duplicates do not exist in the tree.
 
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        
+    }
 
 }
