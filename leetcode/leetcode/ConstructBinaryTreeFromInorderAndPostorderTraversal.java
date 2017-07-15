449a1ead5c4c20555088a1389689c9cbe03fd650
mitcc <mitcc@qq.com>
2017-07-15 19:46:57 +0800

    :basketball:

--- leetcode/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ leetcode/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -17,7 +17,17 @@ You may assume that duplicates do not exist in the tree.
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
+        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
 
+    public TreeNode buildTree(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd) {
+        if (inorder == null || iStart < 0 || iEnd >= inorder.length || iStart > iEnd || postorder == null || pStart < 0 || pEnd >= postorder.length || pStart > pEnd || iEnd - iStart != pEnd - pStart) {
+            return null;
+        }
+        TreeNode root = new TreeNode(inorder[iStart]);
+        int i = pStart;
+        for (; i <= pEnd && postorder[i] != inorder[iStart]; ++i);
+        root.left = buildTree(inorder, iStart + 1, iStart + i - pStart, pStart, i - 1);
+        root.right = buildTree(inorder, iStart + i - pStart + 1, iEnd, postorder, i + 1, pEnd);
+        return root;
 }
