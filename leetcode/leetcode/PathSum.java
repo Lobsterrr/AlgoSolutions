a2cdb96a9ca7bebd083f4a240e2ebbddd610c83f
mitcc <mitcc@qq.com>
2017-07-22 05:38:29 +0800

    :basketball:

--- leetcode/leetcode/PathSum.java
+++ leetcode/leetcode/PathSum.java
@@ -28,8 +28,8 @@ public class PathSum {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null && root.val == sum) {
-            return true;
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
         }
         return hasPathSum(root.left, sum - root.val) 
             || hasPathSum(root.right, sum - root.val);
