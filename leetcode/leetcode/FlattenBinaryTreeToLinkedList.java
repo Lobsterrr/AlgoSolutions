ef656f803ff585a3b87b02c5295fd82e6ebffd97
mitcc <mitcc@qq.com>
2017-07-22 04:29:51 +0800

    :basketball:

--- leetcode/leetcode/FlattenBinaryTreeToLinkedList.java
+++ leetcode/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -49,6 +49,23 @@ public class FlattenBinaryTreeToLinkedList {
         }
     }
 
+/*****************************************************************************/
+
+    public void flatten(TreeNode root) {
+        flatten(root, null);
+    }
+
+    public TreeNode flatten(TreeNode root, TreeNode tail) {
+        if (root == null) {
+            return tail;
+        }
+        root.right = flatten(root.left, flatten(root.right, tail));
+        root.left = null;
+        return root;
+    }
+
+/*****************************************************************************/
+
     public void flatten(TreeNode root) {
         if (root == null) {
             return;
