6c5b288554a542a296649cb755aa084ad4baf259
mitcc <mitcc@qq.com>
2017-07-23 00:01:22 +0800

    :basketball:

--- leetcode/leetcode/SumRootToLeafNumbers.java
+++ leetcode/leetcode/SumRootToLeafNumbers.java
@@ -15,6 +15,42 @@ The root-to-leaf path 1->3 represents the number 13.
 
 Return the sum = 12 + 13 = 25.
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class SumRootToLeafNumbers {
+
+    public int sumNumbers(TreeNode root) {
+        int result = 0;
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        Stack<Integer> valueStack = new Stack<Integer>();
+        nodeStack.push(root);
+        valueStack.push(root.val);
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int value = valueStack.pop();
+            if (node.left == null && node.right == null) {
+                result += value;
+            }
+            if (node.left != null) {
+                nodeStack.push(node.left);
+                valueStack.push(10 * value + node.left.val);
+            }
+            if (node.right != null) {
+                nodeStack.push(node.right);
+                valueStack.push(10 * value + node.right.val);
+            }
+        }
+        return result;
+    }
 
 }
