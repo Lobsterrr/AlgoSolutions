fcecb7ea3767cd5c92073eebb5e79435f6ed98bc
mitcc <mitcc@qq.com>
2017-07-22 19:51:45 +0800

    :basketball:

--- leetcode/leetcode/PathSum.java
+++ leetcode/leetcode/PathSum.java
@@ -25,6 +25,31 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 public class PathSum {
 
     public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<Integer> sumStack = new Stack<Integer>();
+        stack.push(root);
+        sumStack.push(root.val);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int nodeSum = sumStack.pop();
+            if (node.left == null && node.right == null && nodeSum == sum) {
+                return true;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+                sumStack.push(nodeSum + node.left.val);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+                sumStack.push(nodeSum + node.right.val);
+            }
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
         Stack<TreeNode> stack = new Stack<TreeNode>();
         TreeNode cur = root;
         while (!stack.isEmpty() || cur != null) {
@@ -37,10 +62,10 @@ public class PathSum {
                 cur = cur.left;
             } else {
                 cur = stack.pop();
-                if (cur.right != null) {
+                if (cur.right == null) {
                     sum += cur.val;
-                    cur = cur.right;
                 }
+                cur = cur.right;
             }
         }
         return false;
