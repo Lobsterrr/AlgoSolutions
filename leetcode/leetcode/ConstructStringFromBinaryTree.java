dcb50f06d4ab9e80b885a8379887580ff55deec9
mitcc <mitcc@qq.com>
2017-07-29 22:14:18 +0800

    :basketball:

--- leetcode/leetcode/ConstructStringFromBinaryTree.java
+++ leetcode/leetcode/ConstructStringFromBinaryTree.java
@@ -1,7 +1,10 @@
 /*
-You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+You need to construct a string consists of parenthesis and integers from a 
+binary tree with the preorder traversing way.
 
-The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+The null node needs to be represented by empty parenthesis pair "()". And you 
+need to omit all the empty parenthesis pairs that don't affect the one-to-one 
+mapping relationship between the string and the original binary tree.
 
 Input: Binary tree: [1,2,3,4]
        1
@@ -26,8 +29,22 @@ Input: Binary tree: [1,2,3,null,4]
 Output: "1(2()(4))(3)"
 
 Explanation: Almost the same as the first example, 
-except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+except we can't omit the first parenthesis pair to break the one-to-one mapping
+relationship between the input and the output.
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class ConstructStringFromBinaryTree {
+
+    public String tree2str(TreeNode t) {
+        
+    }
 
 }
