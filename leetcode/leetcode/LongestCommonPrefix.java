89cf1e5a0c290245e2475196b1d412ff1d81b736
mitcc <mitcc@qq.com>
2017-07-29 22:08:00 +0800

    :basketball:

--- leetcode/leetcode/LongestCommonPrefix.java
+++ leetcode/leetcode/LongestCommonPrefix.java
@@ -10,7 +10,8 @@ public class LongestCommonPrefix {
         }
         for (int i = 0; i < strs[0].length(); ++i) {
             for (int j = 1; j < strs.length; ++j) {
-                if (i >= strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)) {
+                if (i >= strs[j].length() 
+                        || strs[0].charAt(i) != strs[j].charAt(i)) {
                     return strs[0].substring(0, i);
                 }
             }
