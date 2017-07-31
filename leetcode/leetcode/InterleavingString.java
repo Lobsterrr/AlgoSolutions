3ac3f627ad729fe4ea419d1b9e6092a27d5ccb7e
mitcc <mitcc@qq.com>
2017-08-01 00:23:05 +0800

    :basketball:

--- leetcode/leetcode/InterleavingString.java
+++ leetcode/leetcode/InterleavingString.java
@@ -12,13 +12,17 @@ When s3 = "aadbbbaccc", return false.
 public class InterleavingString {
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1 == null || s2 == null || s3 == null || s1.length() + s2.length() != s3.length()) {
+        if (s1 == null || s2 == null || s3 == null || 
+                s1.length() + s2.length() != s3.length()) {
             return false;
         }
         int len1 = s1.length();
         int len2 = s2.length();
         int len3 = s3.length();
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        for (int i = 0; i < len1; ++i) {
+
+        }
     }
 
 }
