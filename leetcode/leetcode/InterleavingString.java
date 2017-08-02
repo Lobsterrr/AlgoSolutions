a5147dc9ccb7ffe521b9a9ca6bfe4890e0a85dcd
mitcc <mitcc@qq.com>
2017-08-02 12:51:20 +0800

    :basketball:

--- leetcode/leetcode/InterleavingString.java
+++ leetcode/leetcode/InterleavingString.java
@@ -20,8 +20,17 @@ public class InterleavingString {
         int len2 = s2.length();
         int len3 = s3.length();
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
         for (int i = 0; i < len1; ++i) {
+            dp[i + 1][0] = dp[i][0] && s1.charAt(i) == s3.charAt(i);
+        }
+        for (int i = 0; i < len2; ++i) {
+            dp[0][i + 1] = dp[0][i] && s2.charAt(i) == s3.charAt(i);
+        }
+        for (int i = 1; i < s1.length; ++i) {
+            for (int j = 1; j < s2.length; ++j) {
 
+            }
         }
     }
 
