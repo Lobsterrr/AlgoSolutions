62d0dc5bc7a233293f1c1841cb5bfddfa2931874
mitcc <mitcc@qq.com>
2017-08-05 23:57:39 +0800

    :basketball:

--- leetcode/leetcode/BinaryTreeRightSideView.java
+++ leetcode/leetcode/BinaryTreeRightSideView.java
@@ -47,4 +47,31 @@ public class BinaryTreeRightSideView {
         return result;
     }
 
+/*****************************************************************************/
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> curLayer = new LinkedList<TreeNode>();
+        Queue<TreeNode> nextLayer = new LinkedList<TreeNode>();
+        curLayer.offer(root);
+        while (!curLayer.isEmpty()) {
+            TreeNode node = curLayer.poll();
+            if (node.left != null) {
+                nextLayer.offer(node.left);
+            }
+            if (node.right != null) {
+                nextLayer.offer(node.right);
+            }
+            if (curLayer.isEmpty()) {
+                result.add(node.val);
+                curLayer = nextLayer;
+                nextLayer = new LinkedList<TreeNode>();
+            }
+        }
+        return result;
+    }
+
 }
