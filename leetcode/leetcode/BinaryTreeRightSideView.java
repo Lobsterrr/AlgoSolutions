9ddf2470941ff9e3529210706d0356bf5a3fdd25
mitcc <mitcc@qq.com>
2017-08-06 00:11:41 +0800

    :basketball:

--- leetcode/leetcode/BinaryTreeRightSideView.java
+++ leetcode/leetcode/BinaryTreeRightSideView.java
@@ -1,5 +1,5 @@
 /*
-Given a binary tree, imagine yourself standing on the right side of it, return 
+Given a binary tree, imagine yourself standing on the right side of it, return
 the values of the nodes you can see ordered from top to bottom.
 
 For example:
