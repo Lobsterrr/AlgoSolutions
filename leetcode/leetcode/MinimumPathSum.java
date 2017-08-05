eca608204d5464084954c52f600e78e10a68c52f
mitcc <mitcc@qq.com>
2017-08-05 14:27:56 +0800

    :basketball:

--- leetcode/leetcode/MinimumPathSum.java
+++ leetcode/leetcode/MinimumPathSum.java
@@ -9,13 +9,11 @@ public class MinimumPathSum {
     public int minPathSum(int[][] grid) {
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[0].length; ++j) {
-                if (i == 0 && j == 0) {
-                    continue;
-                } else if (i == 0 && j > 0) {
+                if (i == 0 && j > 0) {
                     grid[i][j] += grid[i][j - 1];
                 } else if (i > 0 && j == 0) {
                     grid[i][j] += grid[i - 1][j];
-                } else {
+                } else if (i > 0 && j > 0) {
                     grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
                 }
             }
