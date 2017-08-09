5aaa2bacd5889067a741f02d6845d4a41dfe0948
mitcc <mitcc@qq.com>
2017-08-10 02:50:47 +0800

    :basketball:

--- lintcode/PalindromeNumber.java
+++ lintcode/PalindromeNumber.java
@@ -1,11 +1,13 @@
 /*
 Check a positive number is a palindrome or not.
 
-A palindrome number is that if you reverse the whole number you will get exactly the same number.
+A palindrome number is that if you reverse the whole number you will get
+exactly the same number.
 
  Notice
 
-It's guaranteed the input number is a 32-bit integer, but after reversion, the number may exceed the 32-bit integer.
+It's guaranteed the input number is a 32-bit integer, but after reversion, the
+number may exceed the 32-bit integer.
 
 Example
 11, 121, 1, 12321 are palindrome numbers.
