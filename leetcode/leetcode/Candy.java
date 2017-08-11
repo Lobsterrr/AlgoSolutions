21c3c14543f8b79a61ac1daa17eda21a24db66d4
mitcc <mitcc@qq.com>
2017-08-11 22:49:17 +0800

    :basketball:

--- leetcode/leetcode/Candy.java
+++ leetcode/leetcode/Candy.java
@@ -10,7 +10,25 @@ What is the minimum candies you must give?
 public class Candy {
 
     public int candy(int[] ratings) {
-
+        int[] candies = new int[ratings.length];
+        candies[0] = candies[candies.length - 1] = 1;
+        for (int i = 1; i < candies.length; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            } else {
+                candies[i] = 1;
+            }
+        }
+        for (int i = candies.length - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        int result = 0;
+        for (int candy : candies) {
+            result += candy;
+        }
+        return result;
     }
 
 }
