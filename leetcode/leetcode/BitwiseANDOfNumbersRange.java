28d3327a53d14e3198ed310a21853606907a084a
mitcc <mitcc@qq.com>
2017-08-12 18:28:40 +0800

    :basketball:

--- leetcode/leetcode/BitwiseANDOfNumbersRange.java
+++ leetcode/leetcode/BitwiseANDOfNumbersRange.java
@@ -12,11 +12,15 @@ public class BitwiseANDOfNumbersRange {
         return m & mask;
     }
 
+/*****************************************************************************/
+
     public int rangeBitwiseAnd(int m, int n) {
         int count = 0;
         for (; m != n; m >>= 1, n >>= 1, ++count);
         return m << count;
     }
+
+/*****************************************************************************/
 
     public int rangeBitwiseAnd(int m, int n) {
         int delta = n - m;
