e0483512daa2afde9cc28bffd0e2e25a9811fade
mitcc <mitcc@qq.com>
2017-08-12 22:57:28 +0800

    :basketball:

--- leetcode/leetcode/BinarySearchTreeIterator.java
+++ leetcode/leetcode/BinarySearchTreeIterator.java
@@ -4,8 +4,24 @@ initialized with the root node of a BST.
 
 Calling next() will return the next smallest number in the BST.
 
-Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+Note: next() and hasNext() should run in average O(1) time and uses O(h)
+memory, where h is the height of the tree.
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class BinarySearchTreeIterator {
 
 }
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
