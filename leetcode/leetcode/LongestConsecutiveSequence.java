f5fc054268a1a5f8e64b328301f73ba79b00b4e9
mitcc <mitcc@qq.com>
2017-08-16 02:33:08 +0800

    :basketball:

--- leetcode/leetcode/LongestConsecutiveSequence.java
+++ leetcode/leetcode/LongestConsecutiveSequence.java
@@ -20,14 +20,11 @@ public class LongestConsecutiveSequence {
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] == nums[i - 1] + 1) {
                 count++;
-            } else if (nums[i] == nums[i - 1]) {
-                continue;
-            } else {
-                result = Math.max(result, count);
+            } else if (nums[i] != nums[i - 1]) {
                 count = 1;
             }
+            result = Math.max(result, count);
         }
-        result = Math.max(result, count);
         return result;
     }
 
