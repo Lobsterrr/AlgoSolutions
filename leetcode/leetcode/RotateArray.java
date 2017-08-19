63115d165565637cd08b4abd42507d31233c6ea6
mitcc <mitcc@qq.com>
2017-08-20 01:16:47 +0800

    :basketball:

--- leetcode/leetcode/RotateArray.java
+++ leetcode/leetcode/RotateArray.java
@@ -19,6 +19,7 @@ public class RotateArray {
     // O(n) time, O(1) space.
     public void rotate(int[] nums, int k) {
         int len = nums.length;
+        k = (k % len + len) % len;
         reverse(nums, 0, len - 1 - k);
         reverse(nums, len - k, len - 1);
         reverse(nums, 0, len - 1);
