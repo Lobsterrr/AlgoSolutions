738d186f610f6d747a6c5c9fdfa32128a43e969e
mitcc <mitcc@qq.com>
2017-08-27 00:15:16 +0800

    :basketball:

--- leetcode/leetcode/MergeKSortedLists.java
+++ leetcode/leetcode/MergeKSortedLists.java
@@ -13,13 +13,34 @@ describe its complexity.
 public class MergeKSortedLists {
 
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
+        ListNode result = lists[0];
+        for (int i = 1; i < lists.length; ++i) {
+            result = merge2Lists(result, lists[i]);
         }
-        ListNode[] index = new ListNode[lists.length];
-        for (int i = 0; i < index.length; ++i) {
+        return result;
+    }
 
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        while (l1 != null && l2 != null) {
+            if (cur1.val < cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
         }
+        if (cur1 != null) {
+            cur.next = cur1;
+        } else {
+            cur.next = cur2;
+        }
+        return dummy.next;
     }
 
 }
