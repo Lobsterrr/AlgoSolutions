f7aba422d043f140b4b68f200d3dad68a30ac8e8
mitcc <mitcc@qq.com>
2017-08-26 15:24:40 +0800

    :basketball:

--- leetcode/leetcode/MergeKSortedLists.java
+++ leetcode/leetcode/MergeKSortedLists.java
@@ -2,6 +2,18 @@
 Merge k sorted linked lists and return it as one sorted list. Analyze and
 describe its complexity.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
 
 }
