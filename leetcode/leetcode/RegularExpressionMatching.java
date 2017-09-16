a58e7e4804d542151a87966e5dbaa73d3d5ae9c1
mitcc <mitcc@qq.com>
2017-09-16 18:01:39 +0800

    :basketball:

--- leetcode/leetcode/RegularExpressionMatching.java
+++ leetcode/leetcode/RegularExpressionMatching.java
@@ -21,6 +21,37 @@ isMatch("aab", "c*a*b") → true
 public class RegularExpressionMatching {
 
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return s == p;
+        }
+        int len1 = s.length();
+        int len2 = p.length();
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < len2; ++i) {
+            if (p.charAt(i) == '*' && dp[0][i]) {
+                dp[0][i + 1] = true;
+            }
+        }
+        for (int i = 0; i < len1; ++i) {
+            for (int j = 0; j < len2; ++j) {
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*' && j > 0) {
+                    if (p.charAt(j - 1) != s.charAt(i)) {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    }
+                    if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i)) {
+                        dp[i + 1][j + 1] = dp[i][j + 1] || dp[i + 1][j] || dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public boolean isMatch(String s, String p) {
         if (p == null) {
             return s == null;
         }
