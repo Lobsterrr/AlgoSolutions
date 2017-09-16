b9e4b45d77098560fad4460f1ba805155ed77826
mitcc <mitcc@qq.com>
2017-09-17 02:44:59 +0800

    :basketball:

--- leetcode/leetcode/RegularExpressionMatching.java
+++ leetcode/leetcode/RegularExpressionMatching.java
@@ -29,7 +29,7 @@ public class RegularExpressionMatching {
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
         dp[0][0] = true;
         for (int i = 0; i < len2; ++i) {
-            if (p.charAt(i) == '*'{
+            if (p.charAt(i) == '*') {
                 if (dp[0][i] || i >= 1 && dp[0][i - 1]) {
                     dp[0][i + 1] = true;
                 }
