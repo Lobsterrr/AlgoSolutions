d029ca53b73c3abc750bbed69979fa1506bbee0b
mitcc <mitcc@qq.com>
2017-10-02 20:20:23 +0800

    :basketball:

--- lintcode/FirstPositionUniqueCharacter.java
+++ lintcode/FirstPositionUniqueCharacter.java
@@ -1,6 +1,19 @@
 /*
- *
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Example
+Given s = "lintcode", return 0.
+
+Given s = "lovelintcode", return 2.
  */
 public class FirstPositionUniqueCharacter {
+
+    /*
+     * @param s: a string
+     * @return: it's index
+     */
+    public int firstUniqChar(String s) {
+        // write your code here
+    }
 
 }
