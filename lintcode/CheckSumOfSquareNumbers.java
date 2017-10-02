7b7211e106ca70e2821c2f2ada257859d39e3fe8
mitcc <mitcc@qq.com>
2017-10-03 00:51:15 +0800

    :basketball:

---
+++ lintcode/CheckSumOfSquareNumbers.java
@@ -0,0 +1,22 @@
+/*
+Given a integer c, your task is to decide whether there're two integers a and b
+such that a^2 + b^2 = c.
+
+Example
+Given n = 5
+Return true // 1 * 1 + 2 * 2 = 5
+
+Given n = -5
+Return false
+ */
+public class CheckSumOfSquareNumbers {
+
+    /*
+     * @param : the given number
+     * @return: whether whether there're two integers
+     */
+    public boolean checkSumOfSquareNumbers(int num) {
+        // write your code here
+    }
+
+}
