b67a813674ff1945eb34f953564eb9363b107942
mitcc <mitcc@qq.com>
2017-10-02 20:24:35 +0800

    :basketball:

--- lintcode/FirstPositionUniqueCharacter.java
+++ lintcode/FirstPositionUniqueCharacter.java
@@ -13,7 +13,19 @@ public class FirstPositionUniqueCharacter {
      * @return: it's index
      */
     public int firstUniqChar(String s) {
-        // write your code here
+        if (s == null) {
+            return -1;
+        }
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length; ++i) {
+            map.put(s.charAt(i), 1 + (map.get(s.charAt(i)) == null ? 0 : map.get(s.charAt(i))));
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
