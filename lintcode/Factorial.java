64873bf4cc725da4819a90bdb1fa2336f528d1da
mitcc <mitcc@qq.com>
2017-10-03 00:44:36 +0800

    :basketball:

--- lintcode/Factorial.java
+++ lintcode/Factorial.java
@@ -7,4 +7,12 @@ return 2432902008176640000
  */
 public class Factorial {
 
+    /*
+     * @param : an integer
+     * @return:  the factorial of n
+     */
+    public String factorial(int n) {
+        // write your code here
+    }
+
 }
