a6ec01df52b04a8809177773bf5b6f6d881db4ef
mitcc <mitcc@qq.com>
2017-10-04 19:57:05 +0800

    :basketball:

--- lintcode/CheckSumOfKPrimes.java
+++ lintcode/CheckSumOfKPrimes.java
@@ -1,5 +1,18 @@
 /*
- *
+Given two numbers n and k. We need to find out if n can be written as sum of k prime numbers.
+
+ Notice
+
+n <= 10 ^ 9
+
+Goldbach's conjecture
+
+Example
+Given n = 10, k = 2
+Return true   // 10 = 5 + 5
+
+Given n = 2, k = 2
+Return false
  */
 public class CheckSumOfKPrimes {
 
