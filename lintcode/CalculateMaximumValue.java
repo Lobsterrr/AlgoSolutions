02a6642a310dcc50d9efe66b906fd61c19519e59
mitcc <mitcc@qq.com>
2017-10-21 18:09:00 +0800

    :basketball:

---
+++ lintcode/CalculateMaximumValue.java
@@ -0,0 +1,22 @@
+/*
+Given a string of numbers, write a function to find the maximum value from the
+string, you can add a + or * sign between any two numbers.
+
+Example
+Given str = 01231, return 10
+((((0 + 1) + 2) * 3) + 1) = 10 we get the maximum value 10
+
+Given str = 891, return 73
+As 8 * 9 * 1 = 72 and 8 * 9 + 1 = 73 so 73 is maximum.
+ */
+public class CalculateMaximumValue {
+
+    /*
+     * @param : the given string
+     * @return: the maximum value
+     */
+    public int calcMaxValue(String str) {
+        // write your code here
+    }
+
+}
