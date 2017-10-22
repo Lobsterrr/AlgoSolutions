c8244598fd3c05a687fd4265a337c65b5d94f13b
mitcc <mitcc@qq.com>
2017-10-22 23:15:25 +0800

    :basketball:

--- lintcode/TwoSumBSTEdition.java
+++ lintcode/TwoSumBSTEdition.java
@@ -1,6 +1,42 @@
 /*
- *
+Given a binary search tree and a number n, find two numbers in the tree that
+sums up to n.
+
+ Notice
+
+Without any extra space.
+
+Example
+Given a binary search tree:
+
+    4
+   / \
+  2   5
+ / \
+1   3
+and a number n = 3
+return [1, 2] or [2, 1]
+ */
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
 public class TwoSumBSTEdition {
+
+    /*
+     * @param : the root of tree
+     * @param : the target sum
+     * @return: two numbers from tree which sum is n
+     */
+    public int[] twoSum(TreeNode root, int n) {
+        // write your code here
+    }
 
 }
