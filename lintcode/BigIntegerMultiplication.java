25ce18878666c14749aa985edac5e6b4fee1d4e6
mitcc <mitcc@qq.com>
2017-10-28 16:52:28 +0800

    :basketball:

--- lintcode/BigIntegerMultiplication.java
+++ lintcode/BigIntegerMultiplication.java
@@ -10,7 +10,30 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 public class BigIntegerMultiplication {
 
     public String multiply(String num1, String num2) {
-        // write your code here
+        int[] arr1 = new int[num1.length()];
+        int[] arr2 = new int[num2.length()];
+        for (int i = 0; i < num1.length(); ++i) {
+            arr1[i] = num1.charAt(i) - '0';
+        }
+        for (int i = 0; i < num2.length(); ++i) {
+            arr2[i] = num2.charAt(i) - '0';
+        }
+        int[] arr3 = new int[arr1.length + arr2.length];
+        for (int i = arr1.length - 1; i >= 0; --i) {
+            int carrier = 0;
+            for (int j = arr2.length - 1; j >= 0; --j) {
+                int product = arr1[i] * arr2[j];
+                int sum = product + carrier + arr3[i + j];
+                carrier = sum / 10;
+                arr3 = sum % 10;
+            }
+            arr3[arr1.length - 1 - i + arr2.length] = carrier;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr3.length; ++i) {
+            sb.append(arr3[i]);
+        }
+        return sb.toString();
     }
 
 }
