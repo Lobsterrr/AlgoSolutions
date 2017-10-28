52af666e4963833055c1acd04598d6966379e6a9
mitcc <mitcc@qq.com>
2017-10-29 03:21:02 +0800

    :basketball:

--- lintcode/LongestPalindrome.java
+++ lintcode/LongestPalindrome.java
@@ -37,7 +37,7 @@ public class LongestPalindrome {
             return false;
         }
         for (int i = 0; i < s.length() / 2; ++i) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }
         }
