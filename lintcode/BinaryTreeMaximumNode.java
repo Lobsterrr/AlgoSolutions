f606b7cb8449e7eee4442af08e3bcd4c38cb2b10
mitcc <mitcc@qq.com>
2017-10-28 20:36:59 +0800

    :basketball:

---
+++ lintcode/BinaryTreeMaximumNode.java
@@ -0,0 +1,24 @@
+/*
+Find the maximum node in a binary tree, return the node.
+
+Example
+Given a binary tree:
+
+     1
+   /   \
+ -5     2
+ / \   /  \
+0   3 -4  -5
+return the node with value 3.
+ */
+public class BinaryTreeMaximumNode {
+
+    /*
+     * @param root: the root of tree
+     * @return: the max node
+     */
+    public TreeNode maxNode(TreeNode root) {
+        // write your code here
+    }
+
+}
