8c45a481cd5759db1ba2dfce7feb9482afe39e6f
mitcc <mitcc@qq.com>
2017-10-30 11:16:59 +0800

    :basketball:

--- leetcode/algorithms/LRUCache.java
+++ leetcode/algorithms/LRUCache.java
@@ -28,15 +28,15 @@ cache.get(4);       // returns 4
 public class LRUCache {
 
     public LRUCache(int capacity) {
-        
+
     }
-    
+
     public int get(int key) {
-        
+
     }
-    
+
     public void put(int key, int value) {
-        
+
     }
 
 }
