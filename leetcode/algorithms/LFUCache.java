e9f6900f4bd56ef949c4f91e461f05b845f037ea
mitcc <mitcc@qq.com>
2017-11-13 00:02:28 +0800

    :basketball:

--- leetcode/algorithms/LFUCache.java
+++ leetcode/algorithms/LFUCache.java
@@ -30,4 +30,23 @@ cache.get(4);       // returns 4
  */
 public class LFUCache {
 
+    public LFUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+
 }
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
